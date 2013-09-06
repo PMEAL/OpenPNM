2bc1068292ca1e84652132617ebe73f0dff0acf5
jgostick <jeff@gostick.ca>
2013-09-05 21:58:15 -0400

    Improvement to the PhysicsModule basedon telecon discussion of Sept 5th.

---
+++ OpenPNM/PHYS/CapillaryPressure.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: CEF PNM Team
+# License: TBD
+# Copyright (c) 2012
+
+#from __future__ import print_function
+
+"""
+module __GenericPhysics__: Base class to define pore scale physics
+==================================================================
+
+.. warning:: The classes of this module should be loaded through the 'PHYS.__init__.py' file.
+
+"""
+
+import OpenPNM
+import scipy as sp
+import numpy as np
+
+
+
+def Washburn(network,sigma,theta):
+    return -4*sigma*np.cos(np.radians(theta))/network.throat_properties['diameter']
+    
+
