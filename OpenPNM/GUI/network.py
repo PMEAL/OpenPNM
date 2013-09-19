d3427122590d6eaa1cf971f9924aa8f2577af96e
Roderic Day <roderic.day@gmail.com>
2013-09-19 16:06:10 -0400

    Better samples
    Former-commit-id: 6d888325b1d6d90d21d789e76865731000673003
    Former-commit-id: aea86375cf4ecb5a279348c92fc421619e9791a6

--- OpenPNM/GUI/network.py
+++ OpenPNM/GUI/network.py
@@ -1,6 +1,13 @@
 from __future__ import absolute_import
+import numpy as np
 from .. import GEN
 
-def Cubic(divisions=[10,10,10],lattice_spacing=.0005,loglevel=20,btype = [0,0,0]):
+def Cubic(divisions=[10,10,10], lattice_spacing=.0005, loglevel=20, btype=[0,0,0], C=0.9, D=2.09e-9):
   gn = GEN.Cubic(divisions=divisions, lattice_spacing=lattice_spacing, loglevel=loglevel, btype=btype)
-  return {'network':gn.generate()}
+  pn = gn.generate()
+  setattr(pn,"Total_Conc",C)
+  setattr(pn,"Diff_Coefficient",D)
+  setattr(pn,"divisions",divisions)
+  setattr(pn,"lattice_spacing",lattice_spacing)
+  pn.throat_properties['Pc_entry'] = -4*0.072*np.cos(np.radians(105))/pn.throat_properties['diameter']
+  return {'network': pn}
