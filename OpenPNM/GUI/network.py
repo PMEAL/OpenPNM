f4e0fe7a19d53291be8ec09c10d7521e1019cd98
jgostick <jeff@gostick.ca>
2013-09-26 09:00:38 -0400

    I have attempted to apply the folder/file name changes from develop onto the GUI branch. I have messed something up.  The gui runs but it ony allows for a generic network, so something is not import correctly.

--- OpenPNM/GUI/network.py
+++ OpenPNM/GUI/network.py
@@ -1,9 +1,9 @@
 from __future__ import absolute_import
 import numpy as np
-from .. import GEN
+from .. import Generators
 
 def Cubic(divisions=[10,10,10], lattice_spacing=.0005, loglevel=20, btype=[0,0,0], C=0.9, D=2.09e-9):
-  gn = GEN.Cubic(divisions=divisions, lattice_spacing=lattice_spacing, loglevel=loglevel, btype=btype)
+  gn = Generators.Cubic(divisions=divisions, lattice_spacing=lattice_spacing, loglevel=loglevel, btype=btype)
   pn = gn.generate()
   setattr(pn,"Total_Conc",C)
   setattr(pn,"Diff_Coefficient",D)
--- run_gui.py
+++ run_gui.py
@@ -1,6 +1,6 @@
 import flow_gui
 from OpenPNM import GUI
 
-flow_gui.add_source(GUI.network)
-flow_gui.add_source(GUI.algorithm)
+flow_gui.add_source(GUI.Network)
+flow_gui.add_source(GUI.Algorithms)
 flow_gui.run()
