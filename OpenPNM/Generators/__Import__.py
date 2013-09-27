7ed7e4ad1b7ebce74632d73c6b09d1a24ca95f13
jhinebau <jhinebau@mie.utoronto.ca>
2013-09-26 21:53:45 -0400

    Code written, just debugging now.
    Former-commit-id: df6db188b3fd90bfb80dca55b27035b0bf8e327a
    Former-commit-id: 3661154bde952a67a47b78e2a8adabd91348e29e

--- OpenPNM/Generators/__Import__.py
+++ OpenPNM/Generators/__Import__.py
@@ -1,2 +1,121 @@
 # -*- coding: utf-8 -*-
+# Author: CEF PNM Team
+# License: TBD
+# Copyright (c) 2012
 
+#from __future__ import print_function
+
+"""
+module __GenericGenerator__: Base class to construct pore networks
+==================================================================
+
+.. warning:: The classes of this module should be loaded through the 'Generators.__init__.py' file.
+
+"""
+
+import OpenPNM
+import scipy as sp
+import numpy as np
+import scipy.sparse as sprs
+import scipy.stats as spst
+from __GenericGenerator__ import GenericGenerator
+
+class MatFile(GenericGenerator):
+    r"""
+    MatFile - constructs a pore network from a perfectly formatted .mat file (MATLAB)
+    
+    This class contains the interface definition for the construction
+    of networks
+    
+    Parameters
+    ----------
+    filename: str
+        name of input file
+    path: str
+        path of the input file
+    loglevel : int
+        Level of the logger (10=Debug, 20=INFO, 30=Warning, 40=Error, 50=Critical)
+    
+    
+        
+    Attributes
+    ----------
+        net :   OpenPNM.Network.GenericNetwork
+            Initialized network
+        
+            
+    Examples
+    --------
+    
+    To import the example_network.mat file in your LocalFiles folder
+    
+    >>> import OpenPNM as PNM
+    >>> net=PNM.Generators.MatFile(filename='example_network', path='D:\\AFCC code\\GitHub projects\\OpenPNM\\LocalFiles').generate()
+    
+    """
+    def __init__(self, filename='example_network', path='D:\\AFCC code\\GitHub projects\\OpenPNM\\LocalFiles', **kwargs):
+        
+        r"""
+        Initialize
+        """
+        super(MatFile,self).__init__(**kwargs)
+        self._logger.debug("Method: Constructor")
+        self._mat=OpenPNM.IO.ImportMat(filename=filename,path=path)
+        self._Np=np.size(self._mat.getvar('pnumbering'))
+        self._Nt=np.size(self._mat.getvar('tnumbering'))
+        self._net=OpenPNM.Network.GenericNetwork(num_pores=self._Np, num_throats=self._Nt)
+        
+    def generate(self):
+        r"""
+        Generate the network
+        """
+        self._logger.info('Writing pore properties')
+        self._logger.debug('Writing pore volumes')
+        self._net.pore_properties['volume']=np.reshape(self._mat.getvar('pvolume'),(self._Np))
+        self._logger.debug('Writing pore seeds')
+        self._net.throat_properties['seed']=np.zeros((self._Np),dtype=np.float)
+        self._logger.debug('Writing pore diameters')
+        self._net.pore_properties['diameter']=np.reshape(self._mat.getvar('pdiameter'),(self._Np))
+        self._logger.debug('Writing pore numbering')
+        self._net.pore_properties['numbering']=np.reshape(self._mat.getvar('pnumbering'),(self._Np))
+        self._logger.debug('Writing pore type')
+        self._net.pore_properties['types']=np.reshape(self._mat.getvar('ptype'),(self._Np))
+        self._logger.debug('Writing pore coordinates')
+        self._net.pore_properties['coords']=self._mat.getvar('pcoords')
+        
+        self._logger.info('Writing throat properties')
+        self._logger.debug('Writing throat lengths')
+        self._net.throat_properties['length']=np.zeros((self._Nt),dtype=np.float)
+        self._logger.debug('Writing throat seeds')
+        self._net.throat_properties['seed']=np.zeros((self._Nt),dtype=np.float)
+        self._logger.debug('Writing throat diameters')
+        self._net.throat_properties['diameter']=np.reshape(self._mat.getvar('tdiameter'),(self._Nt))
+        self._logger.debug('Writing throat numbering')
+        self._net.throat_properties['numbering']=np.reshape(self._mat.getvar('tnumbering'),(self._Nt))
+        self._logger.debug('Writing throat type')
+        self._net.throat_properties['type']=np.reshape(self._mat.getvar('ttype'),(self._Nt))
+        self._logger.debug('Writing throat volumes')
+        self._net.pore_properties['volume']=np.zeros((self._Nt),dtype=np.float)
+        self._logger.debug('Writing throat connections')
+        self._net.throat_properties['connections']=self._mat.getvar('tconnections')
+        
+        
+        #self._logger.debug("self.generate()")
+        #self.generate_pores()
+        #self.generate_throats()
+        #self._net.update()
+        #self.add_boundaries()
+        #self.generate_pore_seeds()
+        #self.generate_throat_seeds()
+        #self.generate_pore_diameters()
+        #self.generate_throat_diameters()
+        #self.calc_pore_volumes()
+        #self.calc_throat_lengths()
+        #self.calc_throat_volumes()
+        #self._net.update()
+        #self._logger.debug("\t end of self.generate()")
+        return self._net  
+
+if __name__ == '__main__':
+    self=MatFile(filename='example_network', path='D:\\AFCC code\\GitHub projects\\OpenPNM\\LocalFiles')
+    pn = self.generate()
