a1f1d89bf6ce01b2cb5ba724f23b98f38eec4713
jgostick <jeff@gostick.ca>
2013-09-26 20:40:34 -0400

    Tiny update to call of OrdinaryPercolation
    Former-commit-id: 2eda9f9fd93dff4449498a41d62b12d9c5d3f32e
    Former-commit-id: 1724f84728881ea222d23fc29dce9270bc833c8d

--- OpenPNM/GUI/algorithm.py
+++ OpenPNM/GUI/algorithm.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 import OpenPNM
 import numpy as np
 
-def OrdinaryPercolation(network, npts=100, inv_faces=[1], P=0.04):
+def OrdinaryPercolation(network, npts=100, inv_sites=[1]):
   OP = OpenPNM.Algorithms.OrdinaryPercolation(network, npts=npts, inv_faces=inv_faces)
   OP.run()
   network.set_pore_property('INVADED PORES',np.multiply(np.ones(network.get_num_pores()),network.pore_properties['Pc_invaded']<P))
