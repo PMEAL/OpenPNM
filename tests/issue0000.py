063f025e101d49f50109dd9268bf0e93373373a2
jgostick <jeff@gostick.ca>
2013-10-07 22:45:55 -0400

    Removed issue36.py script.  Added a general template called issue0000.py that should be used to create new tests.

---
+++ tests/issue0000.py
@@ -0,0 +1,7 @@
+import sys
+projpath =  __file__.rsplit('\\',2)[0]
+sys.path.append(projpath)
+import OpenPNM
+
+#Place error causing lines below this line
+#-----------------------------------------------------------------------------
