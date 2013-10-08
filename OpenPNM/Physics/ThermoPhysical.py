c0d2f652820d9a33d029800fd3e1bfd2354e9c8e
jgostick <jeff@gostick.ca>
2013-10-07 23:18:31 -0400

    Changed all Physics classes to modules, with no inits and no inheritance.  This is better for the GUI apparently, but also makes more sense to the program.  The concept of generic physics didn't really make sense. - Note: Physics was like this previously, but I changed them all to classes and created the GenericPhysics class just to be consistent with the rest of the code.

---
+++ OpenPNM/Physics/ThermoPhysical.py
@@ -0,0 +1,23 @@
+
+"""
+module ThermoPhysics
+===============================================================================
+
+"""
+
+import OpenPNM
+import scipy as sp
+
+def Antoine(network):
+    r"""
+    ----
+    """
+    
+    return 'nothing yet'
+    
+def RaoultsLaw(network):
+    r"""
+    ----
+    """
+    
+    return 'nothing yet'
