d2f52d1cec36c0bd5f685501b6b02fa95f1c1650
jgostick <jeff@gostick.ca>
2013-10-07 23:18:31 -0400

    Changed all Physics classes to modules, with no inits and no inheritance.  This is better for the GUI apparently, but also makes more sense to the program.  The concept of generic physics didn't really make sense. - Note: Physics was like this previously, but I changed them all to classes and created the GenericPhysics class just to be consistent with the rest of the code.
    Former-commit-id: c0d2f652820d9a33d029800fd3e1bfd2354e9c8e
    Former-commit-id: ea6634554cd5b8ea831aeace1ff40cec634818dc

---
+++ OpenPNM/Physics/FluidFlow.py
@@ -0,0 +1,27 @@
+
+"""
+module FluidFlow
+===============================================================================
+
+
+"""
+
+import OpenPNM
+import scipy as sp
+
+
+def HagenPoiseuille(net,viscosity):
+    r"""
+    Calculates the hydraulic conductvity of throat assuming cylindrical geometry
+    
+    Parameters
+    ----------
+    network : OpenPNM Object
+        The network for which the calculations are desired
+        
+    viscosity : float
+        The viscosity of the fluid
+    """
+    
+    return 'nothing yet'
+
