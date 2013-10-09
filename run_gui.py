b0a87b7501c117fe08218397505cf824a1e00aaa
Roderic Day <roderic.day@gmail.com>
2013-10-09 10:08:17 -0400

    Latest changes with Jeff
    Former-commit-id: f6d1de328301426ad09708307685072fa25efe2d
    Former-commit-id: e79e7f028c1b70e94af498114c50479f35c3e113

--- run_gui.py
+++ run_gui.py
@@ -3,6 +3,7 @@ import OpenPNM
 
 
 flow_gui.add_source(OpenPNM.GUI.network)
-flow_gui.add_source(OpenPNM.GUI.poreproperty)
+flow_gui.add_source(OpenPNM.GUI.physics)
 flow_gui.add_source(OpenPNM.GUI.algorithm)
+flow_gui.add_source(OpenPNM.GUI.output)
 flow_gui.run()
