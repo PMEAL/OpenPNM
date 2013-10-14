1180a77cffb583635985277c666b9310c5710bec
jgostick <jeff@gostick.ca>
2013-10-14 15:08:35 -0400

    Bug Fix + Enhancement of Ordinary Percolation - OP had a major bug, almost as if a few key lines were missing (perhaps a git merge error, or just accidentally deleted during sloppy late night coding?) - It will now be slower that it was recently, but faster than it was originally - OP can now simulate NON-access limited percolation by specifying ALL sites as inlets.
    Former-commit-id: b860176232db0d334ef488c5cf392663ee8047fa
    Former-commit-id: de59ff563ed2fa292af6a654df62cb292b912ad3

--- OpenPNM/Algorithms/__OrdinaryPercolation__.py
+++ OpenPNM/Algorithms/__OrdinaryPercolation__.py
@@ -23,55 +23,53 @@ from __GenericAlgorithm__ import GenericAlgorithm
 
 class OrdinaryPercolation(GenericAlgorithm):
     r"""
-    Calculates a capillary pressure curve by looping through a list
-    of capillary pressures
-    
-    This function produces a pore_properties array called 'Pc_invaded' which contains the
-    pressure at which a given pore was invaded. This list can be useful for
-    reproducing the simulation for plotting or determining late pore filling.
+    Simulates a capillary drainage experiment by looping through a list of capillary pressures
+
+    This function produces a pore_properties array called 'Pc_invaded' which contains the pressure at which a given pore was invaded. This list can be useful for reproducing the simulation for plotting or determining late pore filling.
 
     Parameters
     ----------
-    
+
     npts: int
         number of simulation steps (pressures); default 25
 
     inv_sites: array_like
         invasion pores i.e. [1,2,3,4,10]; default = [0]
-    
 
+    Note
+    ----
+    Some info about OP?
     """
-    
+
     def __init__(self, net ,npts=25,inv_sites=[0],**kwargs):
         r"""
-        
+
         """
         super(OrdinaryPercolation,self).__init__(net = net,**kwargs)
         self._logger.debug("Create Drainage Percolation Algorithm Object")
         self._npts = npts
-        self._inv_src = inv_sites
+        self._inv_sites = inv_sites
         self._setup()
-        
+
     def _setup(self):
         #Create a pore and throat list to store inv_val at which each is invaded
-        self._net.pore_properties['Pc_invaded'] = np.zeros(self._net.get_num_pores(),np.float)
-        self._net.throat_properties['Pc_invaded'] = np.zeros(self._net.get_num_throats(),np.float)
+        self._net.pore_conditions['Pc_invaded'] = np.zeros(self._net.get_num_pores(),np.float)
+        self._net.throat_conditions['Pc_invaded'] = np.zeros(self._net.get_num_throats(),np.float)
         #Create a throat list to temporarily store the invasion state of throats
         self._net.throat_properties['invaded'] = np.zeros(self._net.get_num_throats())
         #Determine the invasion pressures to apply
-        min_p = np.min(self._net.throat_properties['Pc_entry'])
-        max_p = np.max(self._net.throat_properties['Pc_entry'])
+        min_p = np.min(self._net.throat_conditions['Pc_entry'])
+        max_p = np.max(self._net.throat_conditions['Pc_entry'])
         self._inv_points = np.logspace(np.log10(min_p),np.log10(max_p),self._npts)
-        print self._inv_points
-        
+
     def _do_outer_iteration_stage(self):
         #Generate curve from points
         for inv_val in self._inv_points:
             self._logger.info("Applying Pc = "+str(int(inv_val)))
             #Apply one applied pressure and determine invaded pores
-            inv_clusters = self._do_one_inner_iteration(inv_val)
+            mask = self._do_one_inner_iteration(inv_val)
             #Store result of invasion step
-            self._net.pore_properties['Pc_invaded'][(self._net.pore_properties['Pc_invaded']==0)*(inv_clusters>0)] = inv_val
+            self._net.pore_conditions['Pc_invaded'][(self._net.pore_conditions['Pc_invaded']==0)*(mask)] = inv_val
             r"""
             TODO:
             Tracking the pressure at which each throat is invaded has not been
@@ -82,49 +80,37 @@ class OrdinaryPercolation(GenericAlgorithm):
 #            connected_throats = self._net.get_neighbor_throats(invaded_pores)
 #            self._net.throat_properties['Pc_invaded'][self._net.throat_properties['Pc_invaded']==0] = inv_val
         del self._net.throat_properties['invaded']
-            
+
     def _do_one_inner_iteration(self,inv_val):
         r"""
         Determines which throats are invaded at a given applied capillary pressure
-        
+
         This function uses the scipy.csgraph module for the graph theory cluster
         labeling algorithm (connected_components)
-        
+
         """
         #Generate a tlist containing boolean values for throat state
-        self._net.throat_properties['invaded'] = self._net.throat_properties['Pc_entry']<inv_val
+        self._net.throat_conditions['invaded'] = self._net.throat_conditions['Pc_entry']<inv_val
+        sum(self._net.throat_conditions['invaded'])
         #Fill adjacency matrix with invasion state info
-        self._net.create_adjacency_matrix('invaded',sprsfmt='csr',dropzeros=True)
-        #This step seems to be very slow!
+        I = {'invaded': self._net.throat_conditions['Pc_entry']<inv_val}
+        self._net.create_adjacency_matrix(I,sprsfmt='csr',dropzeros=True)
         clusters = sprs.csgraph.connected_components(self._net.adjacency_matrix['csr']['invaded'])[1]
-        #Clean up (not invaded = -2, invaded >0)
-        clusters = (clusters[0:]>=0)*(clusters[0:]+1)
+        #Find all pores with at least 1 invaded throat
+        Pinvaded = sp.zeros_like(clusters,dtype=bool)
+        temp = self._net.get_connected_pores(np.r_[0:self._net.get_num_throats()])
+        temp = temp[self._net.throat_conditions['invaded']]
+        temp = sp.hstack((temp[:,0],temp[:,1]))
+        Pinvaded[temp] = True
+        #Add injection sites to Pinvaded for ALOP
+        if sp.shape(self._inv_sites)<sp.shape(clusters):
+            Pinvaded[self._inv_sites] = True
+        #Clean up clusters (not invaded = -1, invaded >=0)
+        clusters = clusters*(Pinvaded) - (~Pinvaded)
         #Identify clusters connected to invasion sites
-        inj_clusters = np.in1d(self._inv_src,self._net.pore_properties['numbering'][clusters>0])
-        #Trim non-connected clusters
-        inv_clusters2 = sp.zeros([np.size(clusters,0)],np.int32)
-        inv_clusters2[np.in1d(clusters,inj_clusters)] = 1
-        temp = sp.unique(inj_clusters[sp.nonzero(inj_clusters)])
-        inv_clusters = sp.zeros([np.size(clusters,0)],np.int32)
-        for i in range(0,np.shape(temp)[0]):
-            pores=sp.where(clusters==temp[i])[0]
-            inv_clusters[pores] = temp[i]
-        return(inv_clusters)
-            
-    def _plot_results(self):
-        r"""
-        Plot the numerical output of the OP algorithm
-        """
-        PcPoints = np.unique(self._net.pore_properties['Pc_invaded'])
-        Snwp = np.zeros_like(PcPoints)
-        Ps = np.where(self._net.pore_properties['type']==0)
-        for i in range(1,np.size(PcPoints)):
-            Pc = PcPoints[i]
-            Snwp[i] = sum((self._net.pore_properties['Pc_invaded'][Ps]<Pc)*(self._net.pore_properties['volume'][Ps]))/sum(self._net.pore_properties['volume'][Ps])
-        plt.plot(PcPoints,Snwp,'r.-')
-        plt.show(block = False)
-        self._results = np.vstack((PcPoints,Snwp)).T
-        
-        
+        inv_clusters = clusters[self._inv_sites]
+        return np.in1d(clusters,inv_clusters)*(clusters>=0)
+
+
 if __name__ == '__main__':
     print "Create a test"
