1221bcc789fc74a10a393c39840a1591658e836e
jhinebau <jhinebau@mie.utoronto.ca>
2013-10-14 23:52:35 -0400

    Quick fix on print_overview, to handle pore/throat_conditons when they are scalars.
    Edits for readability and consistancy in data_storage.rst
    
    
    Former-commit-id: 6c4a35c59b748539f639920440006a427e5c4fe3
    Former-commit-id: 21f1f08185073061a05a97e402a5cea9da0d4d17

--- examples_from_doc.py
+++ examples_from_doc.py
@@ -5,13 +5,13 @@
 import OpenPNM
 gn1 = OpenPNM.Geometry.Cubic()
 pn1 = gn1.generate()
-print pn1.pore_properties['diameter'][0]
 print pn1
 pn2 = OpenPNM.Geometry.Cubic().generate()
-print pn1.pore_properties['diameter'][0]
-print pn2.pore_properties['diameter'][0]
-
 
 #Network Architecture
 pn = OpenPNM.Geometry.Cubic().generate(divisions=[3,3,3],lattice_spacing=[1])
+print pn
+pn.print_overview()
+print pn.pore_properties.keys()
+pn.pore_conditions['temperature'] = 80.0
 pn.print_overview()
