f1dcbc8b9e5e561bf3d060259b7941f1c869c878
jhinebau <jhinebau@mie.utoronto.ca>
2013-10-14 22:10:54 -0400

    Some edits to getting_started.rst and data_storage.rst
    Moved Adjaceny matrix discussion to sidebar.

--- examples_from_doc.py
+++ examples_from_doc.py
@@ -7,8 +7,11 @@ gn1 = OpenPNM.Geometry.Cubic()
 pn1 = gn1.generate()
 print pn1.pore_properties['diameter'][0]
 print pn1
-pn2 = gn1.generate()
+pn2 = OpenPNM.Geometry.Cubic().generate()
 print pn1.pore_properties['diameter'][0]
 print pn2.pore_properties['diameter'][0]
 
 
+#Network Architecture
+pn = OpenPNM.Geometry.Cubic().generate(divisions=[3,3,3],lattice_spacing=[1])
+pn.print_overview()
