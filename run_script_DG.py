d7298e60d08b54ff81b06cc01e0b863e228a75bd
Debarun Gupta <dgupta599@gmail.com>
2013-10-16 14:02:52 -0400

    Modified to new code structure

--- run_script_DG.py
+++ run_script_DG.py
@@ -8,27 +8,27 @@ Created on Thu Sep 26 15:35:56 2013
 import OpenPNM
 import scipy as sp
 from time import clock
+import scipy.ndimage as spim
+
 
 params = {
-#'domain_size': [0.001,0.001,0.0004],  #physical network size [meters]
-'divisions': [20,15,10], #Number of pores in each direction
-'lattice_spacing': 1.0,  #spacing between pores [meters]
-#'num_pores': 1000, #This is used for random networks where spacing is irrelevant
-'btype': [0,0,0],  #boundary type to apply to opposing faces [x,y,z] (1=periodic)
-#The following parameters are for the statistical functions used to generate
-#pore and throat size distributions.  See scipy.stats for options and parameters.
-'psd_dist': 'weibull_min',  #distribution to be used
-'psd_shape': 1.5,  #spread or skew of distribution
-'psd_loc': 6e-6,  #minimum pore diameter [meters]
-'psd_scale': 2e-5,  #controls maximum pore size
-'tsd_dist': 'weibull_min',  #distribution to be used
-'tsd_shape': 1.5,  #spread or skew of distribution
-'tsd_loc': 6e-6,  #minimum throat diameter [meters]
-'tsd_scale': 2e-5  #controls maximum throat size
+'domain_size'           : [0.001,0.001,0.0004],  #physical network size [meters]
+'divisions'             : [], #Number of pores in each direction
+'lattice_spacing'       : [.0001],  #spacing between pores [meters]
+'num_pores'             : 1000, #This is used for random networks where spacing is irrelevant
+'template'              : template, #This is used for the Template based network generation
+'psd_info'   : {'name'  : 'weibull_min', #Each statistical package takes different params, so send as dict
+                'shape' : 1.5,
+                'loc'   : 6e-6,
+                'scale' : 2e-5},
+'tsd_info'   : {'name'  : 'weibull_min',
+                'shape' : 1.5,
+                'loc'   : 6e-6,
+                'scale' : 2e-5},
+'btype'                 : [0,0,0],  #boundary type to apply to opposing faces [x,y,z] (1=periodic)
 }
 
-start=clock()
-pn = OpenPNM.Generators.Cubic(loglevel=10,**params).generate()
+pn = OpenPNM.Geometry.Cubic().generate(**params)
 
 '''
 boundaries = sp.array(sp.repeat(params,6)) # Creates a list of 
