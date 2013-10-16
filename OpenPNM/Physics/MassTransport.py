4c7b7bc22f1a62d9e14cd65821b81ab4d80dbc87
jgostick <jeff@gostick.ca>
2013-10-16 12:03:25 -0400

    Changes to Physics.MassTransfer module * Module uses network conditions exclusively

--- OpenPNM/Physics/MassTransport.py
+++ OpenPNM/Physics/MassTransport.py
@@ -13,29 +13,45 @@ def Chapman_Enskog_DiffusionCoefficient(T,P):
     
     return
     
-def Fuller_DiffusionCoefficient(T,P,MA=31.99,MB=28.01,VA=16.6,VB=17.9): 
+def Fuller(T,P,MA=31.99,MB=28.01,VA=16.6,VB=17.9): 
     
     MAB = 2*(1/MA+1/MB)**(-1)    
     D = 0.00143*T**1.75/(P*MAB**0.5*(VA**(1/3)+VB**(1/3))**2)*1e-4    
 
     return D
-
-def Conduits_DiffusionConductivity(network,T,P,DAB_method):
+    
+def FullerScale(network,DABo,To=298,Po=101325): 
     r"""
-    This does not account for temperature gradients
-    Binary_Diff_In_a_Half_Pore_Throat_Half_Pore_Conduit
+    
     """
-    R = 8.314462    
-    C = P/(R*T)
-    if DAB_method=='Fuller':
-        DAB = OpenPNM.Physics.MassTransport.Fuller_DiffusionCoefficient(T,P)
-    elif DAB_method=='Chapman_Enskog':
-        DAB = OpenPNM.Physics.MassTransport.Chapman_Enskog_DiffusionCoefficient(T,P)
+    T1 = network.pore_conditions['temperature']
+    P1 = network.pore_conditions['pressure']
+    D1 = DABo*(Po/To**1.75)*(T1**1.75/P1)
+
+    return D1
+
+def Conduits_DiffusionConductivity(network):
+    r"""
+    
+    """
+    R = 8.314462
+    P = network.pore_conditions['pressure']
+    T = network.pore_conditions['temperature']
+    network.pore_conditions['molar_density'] = P/(R*T)
+    network.interpolate_throat_conditions('molar_density')
+    ct = network.throat_conditions['molar_density']
+    cp = network.pore_conditions['molar_density']
+    network.interpolate_throat_conditions('DAB')
+    DABt = network.throat_conditions['DAB']
+    DABp = network.pore_conditions['DAB']
         
-    gt = C*DAB*network.throat_properties['diameter']**2/network.throat_properties['length']
+    gt = ct*DABt*network.throat_properties['diameter']**2/network.throat_properties['length']
     pores = network.get_connected_pores(network.throat_properties['numbering'],flatten=0)
-    gp1 = C*DAB*network.pore_properties['diameter'][pores[:,0]]**2/(network.pore_properties['diameter'][pores[:,0]]/2)
-    gp2 = C*DAB*network.pore_properties['diameter'][pores[:,1]]**2/(network.pore_properties['diameter'][pores[:,1]]/2) 
+    #Find g for half of pore 1
+    gp1 = cp*DABp*network.pore_properties['diameter'][pores[:,0]]**2/(network.pore_properties['diameter'][pores[:,0]]/2)
+    #Find g for half of pore 2
+    gp2 = cp*DABp*network.pore_properties['diameter'][pores[:,1]]**2/(network.pore_properties['diameter'][pores[:,1]]/2) 
     g = (1/gt + 1/gp1 + 1/gp2)**(-1)
+    
     return g
     
