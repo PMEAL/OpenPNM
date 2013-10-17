5701a129f38b41e96ac650e5b6ab6505d6080fdc
Debarun Gupta <dgupta599@gmail.com>
2013-10-17 01:38:52 -0400

    Major changes to script. Could be precursor to new objects and classes to stitch boundaries
    Former-commit-id: ced246514fc841b0fa2745a8af8c5ef3c72d465c
    Former-commit-id: 2d095e8a9727e84565aca8d2ec575075fab07288

--- run_script_DG.py
+++ run_script_DG.py
@@ -21,26 +21,47 @@ params = {
                 'loc'   : 6e-6,
                 'scale' : 2e-5},
 'btype'                 : [0,0,0],  #boundary type to apply to opposing faces [x,y,z] (1=periodic)
-}
-
-specific_params_1 = {
-'divisions'             : [10,10,10], #Number of pores in each direction
 'lattice_spacing'       : [.01],  #spacing between pores [meters]
 }
 
-specific_params_2 = {
-'divisions'             : [10,10,1], #Number of pores in each direction
-'lattice_spacing'       : [.01],  #spacing between pores [meters]
-}
+main_params = {'divisions' : [4,4,4]}
+side_1 = {'divisions' : [2,4,4]}
+side_2 = {'divisions' : [4,1,4]}
+side_3 = {'divisions' : [4,4,1]}
+side_4 = {'divisions' : [1,4,4]}
+side_5 = {'divisions' : [4,1,4]}
+side_6 = {'divisions' : [4,4,1]}
 
-network1_props = dict(params.items() + specific_params_1.items())
-network2_props = dict(params.items() + specific_params_2.items())
+network_main = dict(params.items() + main_params.items())
+pn = OpenPNM.Geometry.Cubic().generate(**network_main)
 
-pn1 = OpenPNM.Geometry.Cubic().generate(**network1_props)
-pn2 = OpenPNM.Geometry.Cubic().generate(**network2_props)
+net_side1 = dict(params.items() + side_1.items())
+net_side2 = dict(params.items() + side_2.items())
+net_side3 = dict(params.items() + side_3.items())
+net_side4 = dict(params.items() + side_4.items())
+net_side5 = dict(params.items() + side_5.items())
+net_side6 = dict(params.items() + side_6.items())
 
-OpenPNM.Geometry.GenericGeometry().translate_coordinates(pn2,displacement = [0,0,pn1.pore_properties['coords'][:,2].max()])
-OpenPNM.Geometry.GenericGeometry().stitch(pn1,pn2)
+pn1 = OpenPNM.Geometry.Cubic().generate(**net_side1)
+pn2 = OpenPNM.Geometry.Cubic().generate(**net_side2)
+pn3 = OpenPNM.Geometry.Cubic().generate(**net_side3)
+pn4 = OpenPNM.Geometry.Cubic().generate(**net_side4)
+pn5 = OpenPNM.Geometry.Cubic().generate(**net_side5)
+pn6 = OpenPNM.Geometry.Cubic().generate(**net_side6)
+
+OpenPNM.Geometry.GenericGeometry().translate_coordinates(pn1,displacement = [pn.pore_properties['coords'][:,0].max(),0,0])
+OpenPNM.Geometry.GenericGeometry().translate_coordinates(pn2,displacement = [0,pn.pore_properties['coords'][:,1].max(),0])
+OpenPNM.Geometry.GenericGeometry().translate_coordinates(pn3,displacement = [0,0,pn.pore_properties['coords'][:,2].max()])
+OpenPNM.Geometry.GenericGeometry().translate_coordinates(pn4,displacement = [-1*pn.pore_properties['coords'][:,0].min(),0,0])
+OpenPNM.Geometry.GenericGeometry().translate_coordinates(pn5,displacement = [0,-1*pn.pore_properties['coords'][:,1].min(),0])
+OpenPNM.Geometry.GenericGeometry().translate_coordinates(pn6,displacement = [0,0,-1*pn.pore_properties['coords'][:,2].min()])
+
+OpenPNM.Geometry.GenericGeometry().stitch(pn,pn1,edge=1)
+OpenPNM.Geometry.GenericGeometry().stitch(pn,pn2,edge=2)
+OpenPNM.Geometry.GenericGeometry().stitch(pn,pn3,edge=3)
+OpenPNM.Geometry.GenericGeometry().stitch(pn,pn4,edge=4)
+OpenPNM.Geometry.GenericGeometry().stitch(pn,pn5,edge=5)
+OpenPNM.Geometry.GenericGeometry().stitch(pn,pn6,edge=6)
 
 # Can we add extra keys in the dictionary to return the input parmeters so we can access it for stitching?
 
