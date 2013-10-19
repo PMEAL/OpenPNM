7b056ff92a4bb641ddfb49adeb31c9924406f8f3
jhinebau <jhinebau@mie.utoronto.ca>
2013-10-19 18:18:42 -0400

    A small debug in the GUI
    Former-commit-id: 76e916dc203994d5238b8d210b42e32ffcd5b451
    Former-commit-id: 73efc2227d1f0551b993a6d40e1de77174c420fc

--- OpenPNM/Physics/CapillaryPressure.py
+++ OpenPNM/Physics/CapillaryPressure.py
@@ -37,7 +37,6 @@ def Washburn(net,sigma=0.072,theta=120.0):
 
     vals = -4*sigma*_sp.cos(_sp.radians(theta))/net.throat_properties['diameter']
     net.throat_conditions['Pc_entry'] = vals
-    return {'network': net}
 
 def Purcell(net,sigma,theta,r_toroid):
     r"""
