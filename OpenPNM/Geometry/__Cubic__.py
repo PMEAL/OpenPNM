4614020dfb8feaaa1b115167dd45c47db3145b78
Debarun Gupta <dgupta599@gmail.com>
2013-10-20 13:22:59 -0400

    All connections and pores made, pore properties line up. Need to get rid of edge connections on throats

--- OpenPNM/Geometry/__Cubic__.py
+++ OpenPNM/Geometry/__Cubic__.py
@@ -161,7 +161,7 @@ class Cubic(GenericGeometry):
         return net    
         self._logger.debug("generate_boundaries: End of method")
 
-    def stitch_network(self,net1,net2,edge = 0, stitch_nets = 1):
+    def stitch_network(self,net1,net2,edge = 0, stitch_nets = 1, stitch_side = 'top'):
         r"""
         Stitch two networks together
         
@@ -186,6 +186,27 @@ class Cubic(GenericGeometry):
         net2.pore_properties['type']        = sp.repeat(edge,len(net2.pore_properties['type']))
         net1.pore_properties['type']        = sp.concatenate((net1.pore_properties['type'],net2.pore_properties['type']),axis = 0)
         net1.pore_properties['diameter']    = sp.concatenate((net1.pore_properties['diameter'],net2.pore_properties['diameter']),axis = 0)
+        
+        if stitch_nets:
+            if stitch_side == 'top':
+                def_translate = net1.pore_properties['coords'][:,0].max()+net1.pore_properties['coords'][:,0].min()
+                OpenPNM.Geometry.GenericGeometry.translate_coordinates(net2,displacement=[def_translate,0,0])
+            elif stitch_side == 'bottom':
+                def_translate = net1.pore_properties['coords'][:,0].max()+net1.pore_properties['coords'][:,0].min()
+                OpenPNM.Geometry.GenericGeometry.translate_coordinates(net2,displacement=[-1*def_translate,0,0])
+            elif stitch_side == 'left':
+                def_translate = net1.pore_properties['coords'][:,0].max()+net1.pore_properties['coords'][:,0].min()
+                OpenPNM.Geometry.GenericGeometry.translate_coordinates(net2,displacement=[def_translate,0,0])
+            elif stitch_side == 'right':
+                def_translate = net1.pore_properties['coords'][:,0].max()+net1.pore_properties['coords'][:,0].min()
+                OpenPNM.Geometry.GenericGeometry.translate_coordinates(net2,displacement=[def_translate,0,0])
+            elif stitch_side == 'front':
+                def_translate = net1.pore_properties['coords'][:,0].max()+net1.pore_properties['coords'][:,0].min()
+                OpenPNM.Geometry.GenericGeometry.translate_coordinates(net2,displacement=[def_translate,0,0])
+            elif stitch_side == 'back':
+                def_translate = net1.pore_properties['coords'][:,0].max()+net1.pore_properties['coords'][:,0].min()
+                OpenPNM.Geometry.GenericGeometry.translate_coordinates(net2,displacement=[def_translate,0,0])
+        
         net1.pore_properties['coords']      = sp.concatenate((net1.pore_properties['coords'],net2.pore_properties['coords']),axis = 0)
         
         net2.throat_properties['numbering'] = len(net1.throat_properties['numbering']) + net2.throat_properties['numbering']
