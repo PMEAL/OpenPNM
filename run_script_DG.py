67c565cf19a74e418dfaab61c657904db3282c06
Debarun Gupta <dgupta599@gmail.com>
2013-10-21 18:04:02 -0400

    Comment out throat update sizes
    Former-commit-id: 7cf39fa8fad94ac1e3dd6123917a41f304eeaa7c
    Former-commit-id: 95581fee4258715be85b4c093e6721dcbb382e6b

--- run_script_DG.py
+++ run_script_DG.py
@@ -8,9 +8,9 @@ Created on Thu Sep 26 15:35:56 2013
 import OpenPNM
 
 # Parameters unique to all matricies.
-Nx = 5
-Ny = 5
-Nz = 5
+Nx = 3
+Ny = 3
+Nz = 3
 
 network_main = {
 'psd_info'   : {'name'  : 'weibull_min', #Each statistical package takes different params, so send as dict
@@ -30,12 +30,12 @@ network_main = {
 
 #Generate the main pore networks.
 pn1 = OpenPNM.Geometry.Cubic().generate(**network_main)
-#pn2 = OpenPNM.Geometry.Cubic().generate(**network_main)
+pn2 = OpenPNM.Geometry.Cubic().generate(**network_main)
 
 #Add boundaries to the networks
 #OpenPNM.Geometry.Cubic()._generate_boundaries(pn1,**network_main)
 
 #Stitch the networks
-#OpenPNM.Geometry.Cubic().stitch_network(pn1,pn2,stitch_side = 'top') # can be stitched to top, bottom, left right etc.
+OpenPNM.Geometry.Cubic().stitch_network(pn1,pn2,stitch_side = 'top') # can be stitched to top, bottom, left right etc.
 
 OpenPNM.Geometry.GenericGeometry().plot_net(pn1)
