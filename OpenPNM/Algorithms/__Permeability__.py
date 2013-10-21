4f40004bd82a903539f91f2f226c3b713ef66acf
Mahmoudreza Aghighi <mahmoudreza.aghighi@mail.mcgill.ca>
2013-10-21 14:50:02 -0400

    Changing the solver for Permeability
    Former-commit-id: 168e814ae562a6618de5d5a4b3c282c2b98f60d8
    Former-commit-id: 19835233da4b5cc489fc6b97f35187489b0aaa7c

--- OpenPNM/Algorithms/__Permeability__.py
+++ OpenPNM/Algorithms/__Permeability__.py
@@ -13,7 +13,7 @@ module __FickianDiffusion__: Fick's Law Diffusion
 
 import OpenPNM
 import scipy as sp
-from __TransportsLinearSolver__ import TransportsLinearSolver
+from __LinearSolver__ import LinearSolver
 
 class Permeability(LinearSolver):
     r"""   
