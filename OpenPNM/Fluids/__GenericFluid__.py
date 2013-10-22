3d0ee833910d2483195e57a2cd2b6a2d5ca2bdcb
jhinebau <jhinebau@mie.utoronto.ca>
2013-10-21 21:34:30 -0400

    Some changes to the Fluid module. Now you create a permanent fluid object to interact with for the rest of the script.  I think it is a bit more logical this way.
    Jeff has final say on how it works since I probably won't have much more time to commit to it. But please carefully walk through the new JTG3 script before adding or deleting anything.

--- OpenPNM/Fluids/__GenericFluid__.py
+++ OpenPNM/Fluids/__GenericFluid__.py
@@ -1,5 +1,6 @@
 import OpenPNM
 import scipy as sp
+import copy
 
 class GenericFluid(OpenPNM.Utilities.OpenPNMbase):
     r"""
@@ -9,54 +10,95 @@ class GenericFluid(OpenPNM.Utilities.OpenPNMbase):
     ----------
 
     """
-    def __init__(self,**kwargs):
+    def __init__(self,fluid_dict,**kwargs):
         super(GenericFluid,self).__init__(**kwargs)
         self._logger.debug("Construct class")
+        self._fluid_dict = copy.deepcopy(fluid_dict)
+        # the following two lines track the methods that update pore or throat properties
+        self._accepted_pore_methods = ['diffusivity','viscosity','molar_density']
+        self._accepted_throat_methods = []
 
-    def create(self,fluid):
-        self.fluid_dict = fluid
-        return self.fluid_dict
+    def assign(self,network):
+        network.phases.update({self._fluid_dict['name']: self._fluid_dict})
+        self.refresh(network)
 
-    def assign(self,network,fluid):
-        network.phases.update({fluid['name']: fluid})
-        self.refresh(network,fluid['name'])
-
-    def remove(self,network,fluid_name):
+    def remove(self,network):
+        fluid_name = self._fluid_dict['name']
         del network.phases[fluid_name]
+        for i in self._accepted_pore_methods:
+            print "{prop}_{fluid}".format(prop=i, fluid=fluid_name)
+            try:
+                del network.pore_conditions["{prop}_{fluid}".format(prop=i, fluid=fluid_name)]
+            except:
+                self._logger.warn("Attempted to remove a pore condition that doesn't exist")
+                                
+        for i in self._accepted_throat_methods:
+            try:
+                del network.throat_conditions["{prop}_{fluid}".format(prop=i, fluid=fluid_name)]
+            except:
+                self._logger.warn("Attempted to remove a throat condition that doesn't exist")
 
-    def rename(self,fluid,fluid_name):
-        fluid['name'] = fluid_name
+    def rename(self,new_name):
+        self._fluid_dict['name'] = new_name
+        self._logger.warn("Warning: renaming a fluid could complicate fluid updates in existing networks")
 
-    def refresh(self,network,fluid_name):
-        params = network.phases[fluid_name]
-        self.fluid_dict = {}
-        self.fluid_dict.update({'diffusivity': self.diffusivity(network,params['diffusivity'])})
-        self.fluid_dict.update({'viscosity': self.viscosity(network,params['viscosity'])})
-        self.fluid_dict.update({'molar_density': self.molar_density(network,params['molar_density'])})
-        for i in self.fluid_dict.keys():
-            network.pore_conditions.update({ "{prop}_{fluid}".format(prop=i, fluid=fluid_name) : self.fluid_dict[i]})
-        
-    def diffusivity(self,network,params):
+    def refresh(self,network):
+        fluid_name = self._fluid_dict['name']
+        if 'temperature' not in network.pore_conditions.keys():
+            network.pore_conditions['temperature'] = 293.15
+        if 'pressure' not in network.pore_conditions.keys():
+            network.pore_conditions['pressure'] = 101325
+        for i in self._accepted_pore_methods:
+            network.pore_conditions.update({ "{prop}_{fluid}".format(prop=i, fluid=fluid_name) : getattr(self,i)(network)})
+        for i in self._accepted_throat_methods:
+            network.throat_conditions.update({ "{prop}_{fluid}".format(prop=i, fluid=fluid_name) : getattr(self,i)(network)})
+            
+    def diffusivity(self,network):
+        params = self._fluid_dict['diffusivity']
         eqn = getattr(OpenPNM.Fluids.Diffusivity,params['method'])
         vals = eqn(network,**params)
         return sp.array(vals,ndmin=1)
 
-    def viscosity(self,network,params):
+    def viscosity(self,network):
+        params = self._fluid_dict['viscosity']
         eqn = getattr(OpenPNM.Fluids.Viscosity,params['method'])
         vals = eqn(network,**params)
         return sp.array(vals,ndmin=1)
 
-    def molar_density(self,network,params):
+    def molar_density(self,network):        
+        params = self._fluid_dict['molar_density']
         eqn = getattr(OpenPNM.Fluids.MolarDensity,params['method'])
         vals = eqn(network,**params)
         return sp.array(vals,ndmin=1)
 
 if __name__ =="__main__":
-    testfluid_dict ={   'name':  'testfluid',
-                       'phase':  1,
-                 'diffusivity':  2.09e-5,
-                   'viscosity':  1.73e-5,
-                'molardensity':  40.9}
-    pn = OpenPNM.Geometry.Cubic().generate()
-    testfluid = GenericFluid(testfluid_dict,loggername="TestGenericFluid")
-    testfluid.update(pn)
+
+    pn = OpenPNM.Geometry.Cubic(loglevel=40).generate()
+        
+    #Define the fluids and set their properties
+    params_air = {       'name': 'air',
+                           'Pc': 3.771e6, #Pa
+                           'Tc': 132.65,  #K
+                           'MW': 0.0291,  #kg/mol
+                  'diffusivity': {'method': 'Fuller',
+                                      'MA': 31.99,
+                                      'MB': 28.01,
+                                      'vA': 16.6,
+                                      'vB': 17.9},
+                    'viscosity': {'method': 'Reynolds',
+                                      'uo': 0.001,
+                                       'b': 0.1},
+                'molar_density': {'method': 'ideal_gas',
+                                       'R': 8.413},
+                   }
+    #Create fluids
+    air = OpenPNM.Fluids.GenericFluid(params_air)
+    
+    #Assign fluids to network
+    air.assign(pn)
+    print ''
+    print 'current pore conditions:'
+    for i in pn.pore_conditions.keys():
+        print i,'=',pn.pore_conditions[i]
+    print ''
+    
