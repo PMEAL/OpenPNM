2735232059db17307d4ae3629fb5fcf0286be52e
Mahmoudreza Aghighi <mahmoudreza.aghighi@mail.mcgill.ca>
2013-10-22 10:24:01 -0400

    Some changes and updates to Fickian and Fluid
    Former-commit-id: 3f2bd5a0f9ec228b0b9cf9fd085402fd438f971b
    Former-commit-id: 65217651154703361b06c68b5fa505ed6e3ef0e4

--- OpenPNM/Physics/FluidFlow.py
+++ OpenPNM/Physics/FluidFlow.py
@@ -10,7 +10,7 @@ import OpenPNM
 import scipy as sp
 
 
-def HagenPoiseuille(network,viscosity):
+def HagenPoiseuille(network,fluid):
     r"""
     Calculates the hydraulic conductvity of throat assuming cylindrical geometry
     
@@ -22,11 +22,21 @@ def HagenPoiseuille(network,viscosity):
     viscosity : float
         The viscosity of the fluid
     """
-    gt = 2.28*(network.throat_properties['diameter']/2)**4/(2*network.throat_properties['length']*viscosity)
+    
+    vp = fluid['viscosity']
+    vt = network.interpolate_throat_values(vp)
+    #Get Nt-by-2 list of pores connected to each throat
     pores = network.get_connected_pores(network.throat_properties['numbering'],flatten=0)
-    gp1 = 2.28*(network.pore_properties['diameter'][pores[:,0]]/2)**4/(network.pore_properties['diameter'][pores[:,0]]*viscosity)
-    gp2 = 2.28*(network.pore_properties['diameter'][pores[:,1]]/2)**4/(network.pore_properties['diameter'][pores[:,1]]*viscosity) 
+    #Find g for half of pore 1
+    gp1 = 2.28*(network.pore_properties['diameter'][pores[:,0]]/2)**4/(network.pore_properties['diameter'][pores[:,0]]*vp[pores[:,0]])
+    gp1[~(gp1>0)] = sp.inf #Set 0 conductance pores (boundaries) to inf
+    #Find g for half of pore 2
+    gp2 = 2.28*(network.pore_properties['diameter'][pores[:,1]]/2)**4/(network.pore_properties['diameter'][pores[:,1]]*vp[:,1])
+    gp2[~(gp2>0)] = sp.inf #Set 0 conductance pores (boundaries) to inf
+    #Find g for full throat
+    gt = 2.28*(network.throat_properties['diameter']/2)**4/(2*network.throat_properties['length']*vt)
     g = (1/gt + 1/gp1 + 1/gp2)**(-1)
-    return g
+
+    fluid.update({'hydraulic_conductance': g})    
 
 
--- run_script_MRA.py
+++ run_script_MRA.py
@@ -4,35 +4,80 @@ Created on Tue Jul 09 10:54:37 2013
 
 @author: Mahmoudreza Aghighi
 """
-
 import OpenPNM
 import scipy as sp
 from time import clock
 
-r"""
-Except Network generator and Algorithms, all of the following parameters and variables 
-should be in the other dictionaries or classes.
-In here, just for the sake of simplicity, they have been set in the run-script file
-"""
+sphere = sp.ones((51,51,51),dtype=sp.bool8)
+sphere[26,26,26] = 0
+sphere = spim.distance_transform_edt(sphere)
+template  = sphere<20
 
-C = 0.9
-D = 2.09e-9
-params = {
-'domain_size'               : [1,1,1],  #physical network size [meters]
-'divisions'                 : [10,10,10], #Number of pores in each direction
-'lattice_spacing'           : [],  #spacing between pores [meters]
-'stats_pores'   : {  'name' : 'weibull_min', #Each statistical package takes different params, so send as dict
-                    'shape' : 1.5,
-                      'loc' : 6e-6,
-                    'scale' : 2e-5},
-'stats_throats' : {  'name' : 'weibull_min',
-                    'shape' : 1.5,
-                      'loc' : 6e-6,
-                    'scale' : 2e-5},
-'btype'                     : [0,0,0],  #boundary type to apply to opposing faces [x,y,z] (1=periodic)
-}
 start=clock()
-pn = OpenPNM.Geometry.Cubic().generate(**params)
+
+pn = OpenPNM.Network.GenericNetwork()
+
+params_geom1 =     {'domain_size': [],  #physical network size [meters]
+                'divisions': [10,10,10], #Number of pores in each direction
+          'lattice_spacing': [0.1],  #spacing between pores [meters]
+                'num_pores': 1000, #This is used for random networks where spacing is irrelevant
+                 'template': template, #This is used for the Template based network generation
+             'stats_pores' : {'name': 'weibull_min', #Each statistical package takes different params, so send as dict
+                             'shape': 1.5,
+                               'loc': 6e-6,
+                             'scale': 2e-5},
+           'stats_throats' : {'name': 'weibull_min',
+                             'shape': 1.5,
+                               'loc': 6e-6,
+                             'scale': 2e-5},
+                    'btype': [0,0,0]  #boundary type to apply to opposing faces [x,y,z] (1=periodic)
+}
+
+#Generate Network Geometry
+pn = OpenPNM.Geometry.Cubic(loglevel=40).generate(**params_geom1)
+#pn = OpenPNM.Geometry.Delaunay().generate(**params)
+#pn = OpenPNM.Geometry.Template().generate(**params)
+
+#Set Base Conditions in the Network
+pn.pore_conditions['temperature'] = 353
+pn.pore_conditions['pressure'] = 101325
+
+#Define the fluids and set their properties
+params_air = {       'name': 'air',
+                       'Pc': 3.771e6, #Pa
+                       'Tc': 132.65,  #K
+                       'MW': 0.0291,  #kg/mol
+              'diffusivity': {'method': 'Fuller',
+                                  'MA': 31.99,
+                                  'MB': 28.01,
+                                  'vA': 16.6,
+                                  'vB': 17.9},
+                'viscosity': {'method': 'Reynolds',
+                                  'uo': 0.001,
+                                   'b': 0.1},
+             'wettability' : 'wp',
+            'molar_density': {'method': 'ideal_gas',
+                                   'R': 8.413},
+}
+params_water = {     'name': 'water',
+                       'Pc': 2.206e6, #Pa
+                       'Tc': 647,     #K
+                       'MW': 0.0181,  #kg/mol
+              'diffusivity': {'method': 'constant',
+                                 'DAB': 1e-12},
+                'viscosity': {'method': 'constant',
+                                  'mu': 0.001},
+             'wettability' : 'nwp',
+            'molar_density': {'method': 'constant',
+                                   'c': 44445},
+}
+#Create fluids
+air = OpenPNM.Fluids.GenericFluid(params_air)
+water= OpenPNM.Fluids.GenericFluid(params_water,log_level=10)
+
+#Assign fluids to network
+air.assign(pn)
+water.assign(pn)
 #inlets = sp.nonzero(pn.pore_properties['type']==1)[0]
 #pn.throat_properties['Pc_entry'] = OpenPNM.Physics.CapillaryPressure().Washburn(pn,0.072,110)  #This should be set somewhere else
 #OP = OpenPNM.Algorithms.OrdinaryPercolation(pn, npts=50, inv_sites=inlets)
@@ -47,7 +92,7 @@ BCvalues[pn.pore_properties['type']==6]=0.2
 
 Alg1=OpenPNM.Algorithms.FickianDiffusion()
 Alg1.set_boundary_conditions(types=BCtypes,values=BCvalues)
-Alg1.run()
+Alg1.run(pn)
 
 #pn.update()
 #setattr(pn,"Total_Conc",C)
