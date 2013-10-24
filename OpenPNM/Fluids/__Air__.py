f652b4705a01ba5a60c1ccee6775a1d263612a79
jhinebau <jhinebau@mie.utoronto.ca>
2013-10-23 23:24:46 -0400

    Adjusted documentation in IP, and moved parameters to run. I'm anticipating a nice conflict with develop now.

--- OpenPNM/Fluids/__Air__.py
+++ OpenPNM/Fluids/__Air__.py
@@ -6,15 +6,24 @@ from __GenericFluid__ import GenericFluid
 
 class Air(GenericFluid):
     r"""
-    Creates Fluid object named 'air'
-
+    Creates Fluid object with a default name 'air' and preset values
     """
     def __init__(self,**kwargs):
         super(Air,self).__init__(**kwargs)
         self._logger.debug("Construct class")
         
-    def create(self,name='air'):
-        self._fluid_recipe = {   'name': name,
+    def create(self,fluid_name='air'):
+        r"""
+        Creates Fluid object with a default name 'air'
+        
+        Parameters
+        ----------
+
+        fluid_name : string
+            fluid_name = 'air' (default)\n
+            fluid name that gets tagged to fluid-specific pore and throat conditions\n
+        """
+        self._fluid_recipe = {   'name': fluid_name,
                                    'Pc': 3.771e6, #Pa
                                    'Tc': 132.65,  #K
                                    'MW': 0.0291,  #kg/mol
