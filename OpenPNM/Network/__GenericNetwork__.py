cd947e555b428d396fe33486e81b667bd767d2c0
Debarun Gupta <dgupta599@gmail.com>
2013-10-24 17:04:02 -0400

    Turned adj matrix into sparse IJV
    Former-commit-id: c0b2547362d6b4c050d821c143db13e480ffccba
    Former-commit-id: 408603974b670639d389e52b25768d4d9bc27061

--- OpenPNM/Network/__GenericNetwork__.py
+++ OpenPNM/Network/__GenericNetwork__.py
@@ -579,11 +579,6 @@ class GenericNetwork(OpenPNM.Utilities.OpenPNMbase):
 
         return str_overview+str_pore+str_throat+str_pore_cond+str_throat_cond
     
-    def fastest_calc_dist(self,p1,p2):
-        return math.sqrt((p2[0] - p1[0]) ** 2 +
-                     (p2[1] - p1[1]) ** 2 +
-                     (p2[2] - p1[2]) ** 2)    
-                     
     def update(self):
         self.create_adjacency_matrix()
         self.create_incidence_matrix()
