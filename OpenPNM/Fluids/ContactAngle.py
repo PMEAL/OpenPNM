e02d3756703d0ad3a2cf4d325f50ddd7bbeb0249
jgostick <jeff@gostick.ca>
2013-10-25 05:51:37 -0700

    ContactAngle added properly

---
+++ OpenPNM/Fluids/ContactAngle.py
@@ -0,0 +1,19 @@
+
+"""
+module ContactAngle
+===============================================================================
+
+"""
+import OpenPNM
+import scipy as sp
+
+def constant(fluid,value=120,**params):
+    return value
+
+def na(fluid,**params):
+    return []
+
+
+
+
+
