6e718a4e2098dc71e24afa965f2c689be4c5684e
jhinebau <jhinebau@mie.utoronto.ca>
2013-11-02 15:12:49 -0400

    print statements to jth runscript
    Former-commit-id: 2caaef9e7813d41dc0861b401622cdc809c96f3e
    Former-commit-id: 93a0ed8f8f07f316e65ea4be421e567be9d3bf09

--- run_script_JTH.py
+++ run_script_JTH.py
@@ -9,8 +9,8 @@ import OpenPNM
 from time import clock
 import scipy as sp
 clock()
-print 'Creating 1,000 pore cubic network'
-cubic = OpenPNM.Geometry.Cubic().generate(domain_size=[10,10,10],lattice_spacing=[1.0])
+print 'Creating 25,000 pore cubic network'
+cubic = OpenPNM.Geometry.Cubic().generate(domain_size=[50,50,10],lattice_spacing=[1.0])
 print 'Finished at time =',sp.round_(clock(),2),'seconds'
 print ''
 print 'Creating 100 pore Delauny network'
@@ -29,11 +29,13 @@ air_delaunay = OpenPNM.Fluids.Air().create(fluid_name='air_delaunay')
 water_delaunay = OpenPNM.Fluids.Water().create(fluid_name='water_delaunay')
 air_matfile = OpenPNM.Fluids.Air().create(fluid_name='air_matfile')
 water_matfile = OpenPNM.Fluids.Water().create(fluid_name='water_matfile')
+print 'Finished at time =',sp.round_(clock(),2),'seconds'
 print ''
 print "pairing fluids"
 air_cubic.set_pair(water_cubic)
 air_delaunay.set_pair(water_delaunay)
 air_matfile.set_pair(water_matfile)
+print 'Finished at time =',sp.round_(clock(),2),'seconds'
 print ''
 print 'Creating inlets and outlets'
 inlets_cubic = cubic.pore_properties['numbering'][cubic.pore_properties['type']==1]
@@ -42,24 +44,31 @@ inlets_delaunay = delaunay.pore_properties['numbering'][delaunay.pore_properties
 outlets_delaunay = delaunay.pore_properties['numbering'][delaunay.pore_properties['type']==6]
 inlets_matfile = matfile.pore_properties['numbering'][matfile.pore_properties['type']==1]
 outlets_matfile = matfile.pore_properties['numbering'][matfile.pore_properties['type']==6]
+print 'Finished at time =',sp.round_(clock(),2),'seconds'
 print ''
 print 'Running Invasion Percolation algorithm for cubic'
 OpenPNM.Algorithms.InvasionPercolation().run(cubic,invading_fluid=water_cubic,inlets=inlets_cubic,outlets=outlets_cubic)
+print 'Finished at time =',sp.round_(clock(),2),'seconds'
 print ''
 print 'Running Invasion Percolation algorithm for delaunay'
 OpenPNM.Algorithms.InvasionPercolation().run(delaunay,invading_fluid=water_delaunay,inlets=inlets_delaunay,outlets=outlets_delaunay)
+print 'Finished at time =',sp.round_(clock(),2),'seconds'
 print ''
 print 'Running Invasion Percolation algorithm for matfile'
 OpenPNM.Algorithms.InvasionPercolation().run(matfile,invading_fluid=water_matfile,inlets=inlets_matfile,outlets=outlets_matfile)
+print 'Finished at time =',sp.round_(clock(),2),'seconds'
 print ''
 print 'Running Ordinary Percolation algorithm for cubic'
 OpenPNM.Algorithms.OrdinaryPercolation().run(network=cubic,invading_fluid=water_cubic,defending_fluid=air_cubic,inv_sites=inlets_cubic,npts=50,AL=True)
+print 'Finished at time =',sp.round_(clock(),2),'seconds'
 print ''
 print 'Running Ordinary Percolation algorithm for delaunay'
 OpenPNM.Algorithms.OrdinaryPercolation().run(network=cubic,invading_fluid=water_cubic,defending_fluid=air_cubic,inv_sites=inlets_cubic,npts=50,AL=True)
+print 'Finished at time =',sp.round_(clock(),2),'seconds'
 print ''
 print 'Running Ordinary Percolation algorithm for matfile'
 OpenPNM.Algorithms.OrdinaryPercolation().run(network=cubic,invading_fluid=water_cubic,defending_fluid=air_cubic,inv_sites=inlets_cubic,npts=50,AL=True)
+print 'Finished at time =',sp.round_(clock(),2),'seconds'
 print ''
 print 'NEED MORE ALGORITHMS AND PHYSICS IN THIS TESTER. PLEASE ADD'
 print ''
@@ -67,4 +76,5 @@ print 'writing VTK files'
 OpenPNM.Visualization.VTK().write(cubic,water_cubic,'cubic.vtp')
 OpenPNM.Visualization.VTK().write(delaunay,water_delaunay,'delaunay.vtp')
 OpenPNM.Visualization.VTK().write(matfile,water_matfile,'matfile.vtp')
+print 'Finished at time =',sp.round_(clock(),2),'seconds'
 
