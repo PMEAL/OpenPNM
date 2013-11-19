8f0ada08a6f9774a732552f535755c5681168e70
jgostick <jeff@gostick.ca>
2013-11-18 23:19:03 -0500

    Changes to Fluids to accomodate solid phase electron conduction
    Former-commit-id: 4ff8e370fb1d894f2d95ce1969cb69c79d4b897c
    Former-commit-id: 87c0f1c5c5bfec2218e85d972818f1717101ecc5

---
+++ OpenPNM/Fluids/ElectricalConductivity.py
@@ -0,0 +1,15 @@
+
+"""
+module ElectricalConductivity
+===============================================================================
+
+"""
+import OpenPNM
+import scipy as sp
+
+def constant(fluid,value=1,**params):
+    return value
+
+def na(fluid,**params):
+    return -1
+
