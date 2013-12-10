754f6ed3c01ec718df6d759a166e90ef07674431
jgostick <jeff@gostick.ca>
2013-12-10 15:14:45 -0500

    Moved size calcs to Geometry methods
    Former-commit-id: d0c234e7683625c53e03a7c214dd511d7d89a38a
    Former-commit-id: 9899db2edcad0b934ede8a7436c0d3ea20ba82a2

--- OpenPNM/Geometry/ThroatLength.py
+++ OpenPNM/Geometry/ThroatLength.py
@@ -6,16 +6,23 @@ module ThroatLength
 """
 import scipy as sp
 
-def constant(value,**params):
+def constant(network,value,**params):
     r"""
     Assigns specified constant value
     """
-    print('constant')
-    return value
+    network.throat_properties['length'] = value
 
-def straight(**params):
+def straight(network,**params):
     r"""
     Calculate throat length 
     """
-    print('cylinder: nothing yet')
+    #Initialize throat_property['length']
+    network.throat_properties['length'] = sp.zeros_like(network.throat_properties['type'])
+    C1 = network.pore_properties['coords'][network.throat_properties['connections'][:,0]]
+    C2 = network.pore_properties['coords'][network.throat_properties['connections'][:,1]]
+    E = sp.sqrt(sp.sum((C1-C2)**2,axis=1))  #Euclidean distance between pores
+    D1 = network.pore_properties['diameter'][network.throat_properties['connections'][:,0]]
+    D2 = network.pore_properties['diameter'][network.throat_properties['connections'][:,1]]
+    network.throat_properties['length'] = E - (D1 + D2)/2
+        
     
