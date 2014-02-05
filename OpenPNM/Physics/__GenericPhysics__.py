1664fef442ed0c45975863d3c1608d6d8d12e5d2
jgostick <jeff@gostick.ca>
2014-02-05 12:44:04 -0500

    Outline of changes to the way fluid, geometry and physics are created - Only physics is changed right now, and it doesn't quite work

--- OpenPNM/Physics/__GenericPhysics__.py
+++ OpenPNM/Physics/__GenericPhysics__.py
@@ -16,49 +16,31 @@ class GenericPhysics(OpenPNM.Base.Utilities):
     ----------
 
     """
-    def __init__(self,**kwargs):
+    def __init__(self,network,fluid,**kwargs):
         super(GenericPhysics,self).__init__(**kwargs)
         self._logger.debug("Construct class")
-        self._prop_list = {}
+        self._prop_list = []
         self._fluid = []
-        
-    def create(self,network,fluid,**recipe):
-        r"""
-        Create a fluid object using the supplied parameters
-        """
-        try: recipe = self.recipe #check if recipe is pre-existing on self (from init of subclassed methods)
-        except: pass
-        try: self.name = recipe['name']
-        except: self._logger.error('Physics name must be given')
         #bind objects togoether
         self._fluid.append(fluid) #attach fluid to physics
         fluid._physics.append(self)
-        for key, args in recipe.items():
-            try:
-                function = getattr( getattr(OpenPNM.Physics, key), args['method'] ) # this gets the method from the file
-                preloaded_fn = partial(function, physics=self, network=network, fluid=fluid, **args) #
-                setattr(self, key, preloaded_fn)
-                self._logger.info("Successfully loaded {}.".format(key))
-                self._prop_list[key] = True
-            except AttributeError: pass
-        self.regenerate()
-        return self
+        self._net = network
 
     def regenerate(self):
         r'''
         This updates all properties using the methods indicated in the recipe.
         '''
-        for item in self._prop_list.keys():
+        for item in self._prop_list:
             getattr(self,item)()
-#        try: self.capillary_pressure()
-#        except: self._logger.error('Failed to load')
-#        try: self.thermal_conductance()
-#        except: self._logger.error('Failed to load')
-#        try: self.hydraulic_conductance()
-#        except: self._logger.error('Failed to load')
-#        try: self.diffusive_conductance()
-#        except: self._logger.error('Failed to load')
-#        try: self.electronic_conductance()
-#        except: self._logger.error('Failed to load')
+            
+    def add_method(self,prop='',**args):
+        try:
+            function = getattr( getattr(OpenPNM.Physics, prop), args['model'] ) # this gets the method from the file
+            preloaded_fn = partial(function, physics=self, network=self._net, fluid=self._fluid[0], **args) #
+            setattr(self, prop, preloaded_fn)
+            self._logger.info("Successfully loaded {}.".format(prop))
+            self._prop_list.append(prop)
+        except AttributeError: print('could not find',args['model'])
+
 
 
