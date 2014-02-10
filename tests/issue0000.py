3b0043b74fcfd9d4bb95a348c99f64960638b5b5
Jeff Gostick <jeff@gostick.ca>
2014-02-09 23:03:47 -0500

    set_data bug issue #84 on Github

--- tests/issue0000.py
+++ tests/issue0000.py
@@ -1,8 +1,9 @@
 import sys, os
 parent_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-sys.path.insert(1, parent_dir)
+if sys.path[1] != parent_dir:
+    sys.path.insert(1, parent_dir)
 import OpenPNM
 
 #Place error causing lines below this line
 #-----------------------------------------------------------------------------
-pn = OpenPNM.Network.Cubic(division=[10,10,10],lattice_spacing=1,name='cubic1')
+pn = OpenPNM.Network.TestNet()
