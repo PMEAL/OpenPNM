2cccc3d5810732fb6b1250cf6ec8d64b9aa9e7d2
Jeff Gostick <jeff@gostick.ca>
2014-02-13 08:13:18 -0500

    issue0087.py

--- tests/issue0087.py
+++ tests/issue0087.py
@@ -3,13 +3,11 @@ parent_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__f
 if sys.path[1] != parent_dir:
     sys.path.insert(1, parent_dir)
 import OpenPNM
+import sp
 
 #Place error causing lines below this line
 #-----------------------------------------------------------------------------
 pn = OpenPNM.Network.TestNet()
-loc = pn.get_pore_indices()
-geo = OpenPNM.Geometry.GenericGeometry(name='geo_test',network=pn,locations=loc)
-geo.add_method(prop='pore_seed',model='constant',value=0.123)
-seeds = pn.get_pore_data(subdomain='geo_test',prop='pore_seed')
-type(seeds) #this is empty
-pn._pore_data.keys() #pore_seed has not been added
+loc = [0,1]
+pn.set_pore_info(label='test',locations=loc) #this fails
+pn.set_pore_info(label='test',locations=sp.array(loc)) #this works
