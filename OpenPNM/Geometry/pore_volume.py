4ef7ae6913675ec387a5b292888eac0b42015817
Mahmoudreza Aghighi <mahmoudreza.aghighi@mail.mcgill.ca>
2014-02-13 16:53:36 -0500

    Added 'naming' functionality to pore geometries.

--- OpenPNM/Geometry/pore_volume.py
+++ OpenPNM/Geometry/pore_volume.py
@@ -6,22 +6,34 @@ module pore_volume
 """
 import scipy as sp
 
-def constant(geometry,network,propname,value,**params):
+def constant(geometry,
+             network,
+             propname,
+             value,
+             **params):
     r"""
     Assigns specified constant value
     """
     network.set_pore_data(locations=geometry,prop=propname,data=value)
 
-def sphere(geometry,network,propname,**params):
+def sphere(geometry,
+           network,
+           propname,
+           pore_diameter='diameter',
+           **params):
     r"""
     Calculate pore volume from diameter for a spherical pore body
     """
-    value=sp.pi/6*network.get_pore_data(prop='diameter')**3
+    value=sp.pi/6*network.get_pore_data(prop=pore_diameter)**3
     network.set_pore_data(locations=geometry,prop=propname,data=value)
     
-def cube(geometry,network,propname,**params):
+def cube(geometry,
+         network,
+         propname,
+         pore_diameter='diameter',
+         **params):
     r"""
     Calculate pore volume from diameter for a cubic pore body
     """
-    value=network.get_pore_data(prop='diameter')**3
+    value=network.get_pore_data(prop=pore_diameter)**3
     network.set_pore_data(locations=geometry,prop=propname,data=value)
