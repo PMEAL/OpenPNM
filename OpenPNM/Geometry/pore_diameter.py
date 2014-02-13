056fba96810552c087b1b86195a0efb5b457e9fc
Mahmoudreza Aghighi <mahmoudreza.aghighi@mail.mcgill.ca>
2014-02-13 16:53:36 -0500

    Added 'naming' functionality to pore geometries.
    Former-commit-id: 4ef7ae6913675ec387a5b292888eac0b42015817
    Former-commit-id: d68ec58ccda0bed5c1fc563d862c2a26f7269071

--- OpenPNM/Geometry/pore_diameter.py
+++ OpenPNM/Geometry/pore_diameter.py
@@ -8,19 +8,27 @@ import scipy as sp
 import scipy.stats as spst
 
 
-def constant(geometry,network,propname,value,**params):
+def constant(geometry,
+             network,
+             propname,
+             value,
+             **params):
     r"""
     Assign specified constant value
     """
     network.set_pore_data(locations=geometry,prop=propname,data=value)
 
-def sphere(geometry,network,propname,**params):
+def sphere(geometry,
+           network,
+           propname,
+           seed='seed',
+           **params):
     r"""
     Calculate pore diameter from seed values for a spherical pore body
     """
     prob_fn = getattr(spst,params['name'])
     P = prob_fn(params['shape'],loc=params['loc'],scale=params['scale'])
-    value = P.ppf(network.get_pore_data(prop='seed'))
+    value = P.ppf(network.get_pore_data(prop=seed))
     network.set_pore_data(locations=geometry,prop=propname,data=value)
 
     
