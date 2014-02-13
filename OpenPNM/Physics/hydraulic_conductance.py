de085302880cc9dae2ec4bc6e820dbb8bcc7ec6a
Mahmoudreza Aghighi <mahmoudreza.aghighi@mail.mcgill.ca>
2014-02-13 14:44:51 -0500

    Added the functionality for naming properties.
    Same as diffusive and electronic conductance.
    
    
    Former-commit-id: fe86c50b504b8307e84a7327261345db6e21d420
    Former-commit-id: 793132e4ccdbf6dd6900a54e07819c9c98b8ab24

--- OpenPNM/Physics/hydraulic_conductance.py
+++ OpenPNM/Physics/hydraulic_conductance.py
@@ -7,17 +7,34 @@ module hydraulic_conductance
 
 import scipy as sp
 
-def constant(physics,network,fluid,propname,value,**params):
+def constant(physics,
+             network,
+             fluid,
+             propname,
+             value,
+             **params):
     r"""
     Assigns specified constant value
     """
     network.set_throat_data(phase=fluid,prop=propname,data=value)
 
-def na(physics,network,fluid,propname,**params):
+def na(physics,
+       network,
+       fluid,
+       propname,
+       **params):
     value = -1
     network.set_throat_data(phase=fluid,prop=propname,data=value)
 
-def hagen_poiseuille(physics,network,fluid,propname,**params):
+def hagen_poiseuille(physics,
+                     network,
+                     fluid,
+                     propname,
+                     viscosity='viscosity',
+                     throat_diameter = 'diameter',
+                     throat_length = 'length',
+                     pore_diameter = 'diameter',
+                     **params):
     r"""
     Calculates the hydraulic conductvity of throat assuming square geometry using a modified Hagen Poiseuille model
 
@@ -27,20 +44,23 @@ def hagen_poiseuille(physics,network,fluid,propname,**params):
 
     fluid : OpenPNM Fluid Object
     """
-    mup = network.get_pore_data(phase=fluid,prop='viscosity')
+    mup = network.get_pore_data(phase=fluid,prop=viscosity)
     mut = network.interpolate_throat_data(mup)
     #Get Nt-by-2 list of pores connected to each throat
-    pores = network.find_connected_pores(network.get_throat_data(prop='numbering'),flatten=0)
+    tind = network.get_throat_indices()
+    pores = network.find_connected_pores(tind,flatten=0)
     #Find g for half of pore 1
-    gp1 = 2.28*(network.get_pore_data(prop='diameter')[pores[:,0]]/2)**4/(network.get_pore_data(prop='diameter')[pores[:,0]]*mut)
+    pdia = network.get_pore_data(prop=pore_diameter)
+    gp1 = 2.28*(pdia[pores[:,0]]/2)**4/(pdia[pores[:,0]]*mut)
     gp1[~(gp1>0)] = sp.inf #Set 0 conductance pores (boundaries) to inf
     #Find g for half of pore 2
-    gp2 = 2.28*(network.get_pore_data(prop='diameter')[pores[:,1]]/2)**4/(network.get_pore_data(prop='diameter')[pores[:,1]]*mut)
+    gp2 = 2.28*(pdia[pores[:,1]]/2)**4/(pdia[pores[:,1]]*mut)
     gp2[~(gp2>0)] = sp.inf #Set 0 conductance pores (boundaries) to inf
     #Find g for full throat
-    gt = 2.28*(network.get_throat_data(prop='diameter')/2)**4/(2*network.get_throat_data(prop='length')*mut)
+    tdia = network.get_throat_data(prop=throat_diameter)
+    tlen = network.get_throat_data(prop=throat_length)
+    gt = 2.28*(tdia/2)**4/(2*tlen*mut)
     value = (1/gt + 1/gp1 + 1/gp2)**(-1)
     network.set_throat_data(phase=fluid,prop=propname,data=value)
-
 
 
