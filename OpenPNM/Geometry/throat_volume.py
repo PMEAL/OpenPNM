d76b26092119fa5ca79f552da21a7dcdb09c9256
Jeff Gostick <jeff@gostick.ca>
2014-03-04 22:28:18 -0500

    ???

--- OpenPNM/Geometry/throat_volume.py
+++ OpenPNM/Geometry/throat_volume.py
@@ -26,7 +26,7 @@ def cylinder(geometry,
     Calculate throat diameter from seeds for a cylindrical throat
     - note: this will need to account for volume taken up by spherical pore bodies
     """
-    value=sp.pi/4*network.get_throat_data(prop=throat_length)*network.get_throat_data(prop=throat_diameter)**2
+    value=sp.pi/4*network.get_throat_data(prop=throat_length,locations=geometry)*network.get_throat_data(prop=throat_diameter,locations=geometry)**2
     network.set_throat_data(locations=geometry,prop=propname,data=value)
 
 def cuboid(geometry,
@@ -39,5 +39,5 @@ def cuboid(geometry,
     Calculate throat volume of cuboidal throat
     - note: this will need to account for volume taken up by spherical pore bodies
     """
-    value=network.get_throat_data(prop=throat_length)*network.get_throat_data(prop=throat_diameter)**2
+    value=network.get_throat_data(prop=throat_length,locations=geometry)*network.get_throat_data(prop=throat_diameter,locations=geometry)**2
     network.set_throat_data(locations=geometry,prop=propname,data=value)
