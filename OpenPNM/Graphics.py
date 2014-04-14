c419920be209a4046659cd5569c48694d3fd38e9
HaroldDay <harold.day@mail.mcgill.ca>
2014-04-14 14:46:03 -0700

    Lightweight function to allow VTK previews without loading Paraview. Python2 only

---
+++ OpenPNM/Graphics.py
@@ -0,0 +1,48 @@
+def preview(pn, values=[]):
+    import vtk
+
+    coords = pn.get_pore_data(prop='coords')
+    heads, tails = pn.get_throat_data(prop='connections').T
+
+    points = vtk.vtkPoints()
+    for x,y,z in coords:
+        points.InsertNextPoint(x, y, z)
+
+    polys = vtk.vtkCellArray()
+    for hi, ti in zip(heads, tails):
+        vil = vtk.vtkIdList()
+        vil.InsertNextId(hi)
+        vil.InsertNextId(ti)
+        polys.InsertNextCell(vil)
+
+    colors = vtk.vtkUnsignedCharArray()
+    colors.SetNumberOfComponents(3)
+    for v in values:
+        r = 255*(v)
+        g = 0
+        b = 255*(1-v)
+        colors.InsertNextTuple3(r,g,b)
+
+    polydata = vtk.vtkPolyData()
+    polydata.SetPoints(points)
+    polydata.SetLines(polys)
+    polydata.GetPointData().SetScalars(colors)
+
+    mapper = vtk.vtkPolyDataMapper()
+    mapper.SetInput(polydata)
+
+    actor = vtk.vtkActor()
+    actor.SetMapper(mapper)
+
+    ren = vtk.vtkRenderer()
+    ren.AddActor(actor)
+
+    renWin = vtk.vtkRenderWindow()
+    renWin.AddRenderer(ren)
+
+    iren = vtk.vtkRenderWindowInteractor()
+    iren.SetRenderWindow(renWin)
+
+    iren.Initialize()
+    renWin.Render()
+    iren.Start()
