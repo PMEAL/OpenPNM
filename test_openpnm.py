35ff6b14cbfe6fb19e44adf12ed2568efc2f2873
HaroldDay <harold.day@mail.mcgill.ca>
2014-04-14 15:43:07 -0700

    Corresponding tests and lightweight documentation written
    Former-commit-id: 9db77aff509f380ceaca5775ab402d722544f942
    Former-commit-id: a469d50e85b5d816083749a6637d1fe000e85ea9

--- test_openpnm.py
+++ test_openpnm.py
@@ -4,15 +4,17 @@
 """ Tests for OpenPNM """
 
 from __future__ import print_function, division, absolute_import
-import unittest
 import pytest
 
 import OpenPNM
+import numpy as np
 
-class NetworkGeneration(unittest.TestCase):
-
-    def test(self):
-        assert True
+def test_template_generator():
+    R = np.array([[[0,0],[0,1]]])
+    pn = OpenPNM.Network.Template(name='template')
+    pn.generate(R, threshold=0.5)
+    assert len(pn.get_pore_data(prop='coords'))==3
+    assert len(pn.get_throat_data(prop='connections'))==2
 
 if __name__ == '__main__':
-    unittest.main()
+    pytest.main([__file__])
