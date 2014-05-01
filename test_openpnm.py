0de777b8355a0f0d6e76aa20b1e50aa962f3c095
Harold Day <harold.day@mail.mcgill.ca>
2014-05-01 10:05:56 -0700

    Temporarily added `Shortcuts` module, containing a straightforward linear solver. Also added corresponding test, and reshuffled the way the template is able to "prune" unneeded branches.

--- test_openpnm.py
+++ test_openpnm.py
@@ -12,9 +12,23 @@ import numpy as np
 def test_template_generator():
     R = np.array([[[0,0],[0,1]]])
     pn = OpenPNM.Network.Template(name='template')
-    pn.generate(R, threshold=0.5)
+    pn.generate(R)
+    pn.prune(R>0.5)
     assert len(pn.get_pore_data(prop='coords'))==3
     assert len(pn.get_throat_data(prop='connections'))==2
+
+def test_linear_solver():
+    # fix cube dimensions?
+    pn = OpenPNM.Network.Cubic(name='net')
+    pn.generate(add_boundaries=False)
+
+    x,y,z = pn.get_pore_data(prop='coords').T
+    left = x==x.min()
+    right = x==x.max()
+    ics = 2*left + 1*right
+
+    sol = OpenPNM.Shortcuts.solve_linear(pn, ics)
+    assert( np.allclose(sol, np.array([2,1.5,1]*9)) )
 
 if __name__ == '__main__':
     pytest.main([__file__])
