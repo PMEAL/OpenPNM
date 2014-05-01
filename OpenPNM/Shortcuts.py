1d360986abb143b4e6ea67baef1655967c757e1f
Harold Day <harold.day@mail.mcgill.ca>
2014-05-01 16:30:31 -0700

    Circumvented naming bug by generating random serials for linear solver.
    Former-commit-id: d07244101f82dfc72ada29f384e4eb26f2dedf82
    Former-commit-id: 697054da875fc9c89de09ce8cd98e04879f03d5a

--- OpenPNM/Shortcuts.py
+++ OpenPNM/Shortcuts.py
@@ -1,7 +1,10 @@
+import string, random
 import OpenPNM
 
 def solve_linear(pn, ics):
-    pseudo = OpenPNM.Fluids.GenericFluid(network=pn, name='none', loglevel=0)
+    # circumvent bug with naming by creating random names
+    name = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
+    pseudo = OpenPNM.Fluids.GenericFluid(network=pn, name=name, loglevel=0)
     pseudo.set_throat_data(prop='electronic_conductance', data=1)
 
     alg = OpenPNM.Algorithms.OhmicConduction(network=pn, name='alg', loglevel=0)
@@ -10,16 +13,5 @@ def solve_linear(pn, ics):
     alg.run(active_fluid=pseudo)
     alg.update()
 
-    return pseudo.get_pore_data(prop='voltage')
-
-if __name__ == '__main__':
-    pn = OpenPNM.Network.Cubic(name='net')
-    pn.generate(add_boundaries=False)
-
-    x,y,z = pn.get_pore_data(prop='coords').T
-    left = x==x.min()
-    right = x==x.max()
-    ics = 2*left + 1*right
-
-    sol = solve_linear(pn, ics)
-    print sol.shape
+    out = pseudo.get_pore_data(prop='voltage')
+    return out
