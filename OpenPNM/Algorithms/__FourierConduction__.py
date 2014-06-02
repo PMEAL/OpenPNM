03fb7fc66728af7c99fb9af4ea0c344732ddb109
Mahmoudreza Aghighi <mahmoudreza.aghighi@mail.mcgill.ca>
2014-06-02 09:58:17 -0400

    Minor changes in the transport codes
    These modifications do not change the way code was working. They only
    simplify the code behind the scene, So everybody can easily change the
    transport codes.

--- OpenPNM/Algorithms/__FourierConduction__.py
+++ OpenPNM/Algorithms/__FourierConduction__.py
@@ -40,47 +40,37 @@ class FourierConduction(LinearSolver):
         r"""
 
         This function executes the essential mathods for building matrices in Linear solution 
-        """
-        self._setup = 0 
-        try :
-            self.bc_setup
-            if self.bc_setup==1:
-                self._logger.info("Setup for Fourier Algorithm")        
-                self._fluid = params['active_fluid']
-                try: self._fluid = self._net._fluids[self._fluid] 
-                except: pass #Accept object
+        """   
 
-                success_1 = self._fluid.check_throat_health(props=occupancy)
-                success_2 = self._fluid.check_throat_health(props=thermal_conductance)
-                if not success_1:  
-                    self._fluid.set_data(prop=occupancy,throats='all',data=1)
-                    self._fluid.set_data(prop=occupancy,pores='all',data=1)
-                    self._logger.info('By default, it will be assumed that occupancy for '+self._fluid.name+' is equal to 1 in the entire network!')
-                if success_2: 
-                    # Building thermal conductance based on occupancy
-                    g = self._fluid.get_throat_data(prop=thermal_conductance)
-                    s = self._fluid.get_throat_data(prop=occupancy)
-                    self._conductance = g*s+g*(-s)/1e3
-                    self._setup = 1
-                else: 
-                    self._logger.error('In '+self._fluid.name+', there is an error for the property: '+thermal_conductance)
-                    self._setup = 0
-            else: 
-                self._logger.error('There is an error in applying boundary conditions!')
-                self._setup = 0
-        except:
-            self._logger.error('No boundary condition has been implemented for algorithm: '+self.name+'!') 
+        self._logger.info("Setup for Fourier Algorithm")        
+        self._fluid = params['active_fluid']
+        try: self._fluid = self._net._fluids[self._fluid] 
+        except: pass #Accept object
+        success_1 = self._fluid.check_throat_health(props=occupancy)
+        success_2 = self._fluid.check_throat_health(props=thermal_conductance)
+        if not success_1:  
+            self._fluid.set_data(prop=occupancy,throats='all',data=1)
+            self._fluid.set_data(prop=occupancy,pores='all',data=1)
+            self._logger.info('By default, it will be assumed that occupancy for '+self._fluid.name+' is equal to 1 in the entire network!')
+        if success_2: 
+            # Building thermal conductance based on occupancy
+            g = self._fluid.get_throat_data(prop=thermal_conductance)
+            s = self._fluid.get_throat_data(prop=occupancy)
+            self._conductance = g*s+g*(-s)/1e3
+            setup_conductance = True
+        try:    setup_conductance
+        except: raise Exception('There is an error for the throat property: '+thermal_conductance+'!')
+        try:    self.existing_bc
+        except: raise Exception('There is an error in applying boundary conditions!')
 
     
     def _do_inner_iteration_stage(self):
         r"""
          main section of the algorithm              
         """
-        if self._setup==1:
-            T = self._do_one_inner_iteration()       
-            self.set_pore_data(prop='temperature',data= T)
-            self._logger.info('Solving process finished successfully!')
-        else: raise Exception('Error in setup section of the algorithm!'+self.name+' cannot be executed!')
+        T = self._do_one_inner_iteration()       
+        self.set_pore_data(prop='temperature',data= T)
+        self._logger.info('Solving process finished successfully!')
 
 
     def update(self):
--- OpenPNM/Algorithms/__StokesFlow__.py
+++ OpenPNM/Algorithms/__StokesFlow__.py
@@ -36,54 +36,48 @@ class StokesFlow(LinearSolver):
             
     def _setup(self,
                hydraulic_conductance='hydraulic_conductance',
+               x_term = 'pressure',
                occupancy='occupancy',
                **params):
         r"""
         This function executes the essential mathods before building matrices in Linear solution 
         """
-        self._setup = 0 
-        try :
-            self.bc_setup
-            if self.bc_setup==1:
-                self._logger.info("Setup for Stokes Flow Algorithm")
-                self._fluid = params['active_fluid']
-                try: self._fluid = self._net._fluids[self._fluid] 
-                except: pass #Accept object
-                self._X_name = 'pressure'
-                success_1 = self._fluid.check_throat_health(props=occupancy)
-                success_2 = self._fluid.check_throat_health(props=hydraulic_conductance)
-                if not success_1:  
-                    self._fluid.set_data(prop=occupancy,throats='all',data=1)
-                    self._fluid.set_data(prop=occupancy,pores='all',data=1)
-                    self._logger.info('By default, it will be assumed that occupancy for '+self._fluid.name+' is equal to 1 in the entire network!')
-                if success_2: 
-                    # Building hydraulic conductance based on occupancy
-                    g = self._fluid.get_throat_data(prop=hydraulic_conductance)
-                    s = self._fluid.get_throat_data(prop=occupancy)
-                    self._conductance = g*s+g*(-s)/1e3
-                    self._setup = 1
-                else: 
-                    self._logger.error('In '+self._fluid.name+', there is an error for the property: '+hydraulic_conductance)
-                    self._setup = 0
-            else: 
-                self._logger.error('There is an error in applying boundary conditions!')
-                self._setup = 0
-        except:
-            self._logger.error('No boundary condition has been implemented for algorithm: '+self.name+'!') 
+
+        self._logger.info("Setup for Stokes Flow Algorithm")
+        self._fluid = params['active_fluid']
+        try: self._fluid = self._net._fluids[self._fluid] 
+        except: pass #Accept object
+        self._X_name = x_term
+        success_1 = self._fluid.check_throat_health(props=occupancy)
+        success_2 = self._fluid.check_throat_health(props=hydraulic_conductance)
+        if not success_1:  
+            self._fluid.set_data(prop=occupancy,throats='all',data=1)
+            self._fluid.set_data(prop=occupancy,pores='all',data=1)
+            self._logger.info('By default, it will be assumed that occupancy for '+self._fluid.name+' is equal to 1 in the entire network!')
+        if success_2: 
+            # Building hydraulic conductance based on occupancy
+            g = self._fluid.get_throat_data(prop=hydraulic_conductance)
+            s = self._fluid.get_throat_data(prop=occupancy)
+            self._conductance = g*s+g*(-s)/1e3
+            setup_conductance = True
+        try:    setup_conductance
+        except: raise Exception('There is an error for the throat property: '+ hydraulic_conductance+'!')
+        try:    self.existing_bc
+        except: raise Exception('There is an error in applying boundary conditions!')
 
 
     def _do_inner_iteration_stage(self):
-        
-        if self._setup==1:
-            p = self._do_one_inner_iteration()
-            self.set_pore_data(prop='pressure',data = p)
-            self._logger.info('Solving process finished successfully!')
-        else: raise Exception('Error in setup section of the algorithm!'+self.name+' cannot be executed!')
+        r"""
+         main section of the algorithm              
+        """
+        p = self._do_one_inner_iteration()
+        self.set_pore_data(prop=self._X_name,data = p)
+        self._logger.info('Solving process finished successfully!')
 
     def update(self):
         
-        p = self.get_pore_data(prop='pressure')
-        self._fluid.set_pore_data(prop='pressure',data=p)
+        p = self.get_pore_data(prop=self._X_name)
+        self._fluid.set_pore_data(prop=self._X_name,data=p)
         self._logger.info('Results of ('+self.name+') algorithm have been updated successfully.')
 
 
