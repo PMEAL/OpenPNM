f8d70f7d9872e7817e26d06b6f79c767907438c9
jhinebau <jhinebau@mie.utoronto.ca>
2014-06-13 11:50:52 -0400

    Deleted old Vtp and __VTK__ files. Renamed __Vtp__.py as __VTK__.py Renamed Vtp_class as VTK Updated run_script and ip_demo to behave properly
    Former-commit-id: 43b5d65c33fa870dfed53fc5900e2ebc1101f63a
    Former-commit-id: c61f086f07bb39a36a5e6f16014f33d4a705307a

--- OpenPNM/Visualization/__VTK__.py
+++ OpenPNM/Visualization/__VTK__.py
@@ -1,40 +1,22 @@
-"""
-module __GenericVisualization__: Base class to visualize networks
-==================================================================
+from xml.etree import ElementTree as ET
 
-.. warning:: The classes of this module should be loaded through the 'Visualization/__init__.py' file.
-
-"""
-
-import OpenPNM
-import scipy as sp
 import numpy as np
-import os
+from OpenPNM.Visualization import GenericVisualization
 
-from .__GenericVisualization__ import GenericVisualization
 
 class VTK(GenericVisualization):
     r"""
-    writeVTK - Class for writing a VTK file
+    VTK - Class for writing a Vtp file to be read by ParaView
 
-    Parameters
-    ----------
-    pn : OpenPNM Network Object
-        The network which is to be written to the VTK file
-
-    filename : String, optional
-        The desire location and file name of the output file.  If not specified defaults to 'output.vtk'
-
+    
     Examples
     --------
     Create and store a basic network.
 
     >>> import OpenPNM as PNM
     >>> net = PNM.Generators.SimpleCubic(divisions = [40,40,20],shape=[0.,4.,0.,4.,0.,2.]).generate()
-    >>> PNM.Visualization.VTK(net=net,filename = 'testvtp.vtp')
-
-    .. note::
-    n/a
+    >>> vis = PNM.Visualization.VTK()
+    >>> vis.write(net)
 
     """
 
@@ -42,111 +24,134 @@ class VTK(GenericVisualization):
         r"""
         Initialize
         """
+        self._TEMPLATE = '''
+        <?xml version="1.0" ?>
+        <VTKFile byte_order="LittleEndian" type="PolyData" version="0.1">
+            <PolyData>
+                <Piece NumberOfLines="0" NumberOfPoints="0">
+                    <Points>
+                    </Points>
+                    <Lines>
+                    </Lines>
+                    <PointData>
+                    </PointData>
+                </Piece>
+            </PolyData>
+        </VTKFile>
+        '''.strip()
+        
         super(VTK,self).__init__(**kwargs)
 #        self._logger.debug("Execute constructor")
-
-    def write(self, net, fluids='none', filename=None):
+    
+    def _array_to_element(self, name, array, n=1):
+        dtype_map = {
+            'int8'   : 'Int8',
+            'int16'  : 'Int16',
+            'int32'  : 'Int32',
+            'int64'  : 'Int64',
+            'uint8'  : 'UInt8',
+            'uint16' : 'UInt16',
+            'uint32' : 'UInt32',
+            'uint64' : 'UInt64',
+            'float32': 'Float32',
+            'float64': 'Float64',
+            'str'    : 'String',
+        }
+        element = ET.Element('DataArray')
+        element.set("Name", name)
+        element.set("NumberOfComponents", str(n))
+        element.set("type", dtype_map[str(array.dtype)])
+        element.text = '\t'.join(map(str,array.ravel()))
+        return element
+    
+    def _element_to_array(self, element, n=1):
+        string = element.text
+        dtype = element.get("type")
+        array = np.fromstring(string, sep='\t')
+        array = array.astype(dtype)
+        if n is not 1:
+            array = array.reshape(array.size//n, n)
+        return array
+    
+    def write(self, network, filename='output_file.vtp', fluids=[], pretty=True):
         r"""
         Write Network to a VTK file for visualizing in Paraview
-
+    
         Parameters
         ----------
-        net : OpenPNM Network Object
-
+    
+        network : OpenPNM Network Object
+    
         filename : string
             Full path to desired file location
-
+            
+        fluids : Fluids that have properties we want to write to file
+    
+        pretty : Add linebreaks at the end of tag closures
         """
-        if not filename:
-            filename='output_file.vtp'
-            output_path = os.path.join( os.path.expanduser('~'), filename )
-        else:
-            output_path = filename
-        self._file_name = filename
-        self._f = open(output_path,'w')
-        self._net=net
-#        if type(fluids)!= sp.ndarray and fluids!='none': 
-#            fluids = sp.array(fluids,ndmin=1)
-        self._fluids = fluids
-        self._write_vtk_header()
-        self._write_vtk_points()
-        self._write_vtk_connections()
-        self._write_point_data()
-        self._write_footer()
-        self._f.close()
-
-    def _write_vtk_header(self):
-        self._f.write('<?xml version="1.0"?>\n')
-        self._f.write('<VTKFile type="PolyData" version="0.1" byte_order="LittleEndian">\n')
-        self._f.write('<PolyData>\n')
-        self._f.write('<Piece NumberOfPoints="')
-        #text = str(pn.getNumPores())
-        self._f.write(str(self._net.num_pores()))
-        self._f.write('" NumberOfVerts="0" NumberOfLines="')
-        #text = str(pn.getNumThroats())
-        self._f.write(str(self._net.num_throats()))
-        self._f.write('" NumberOfStrips="0" NumberOfPolys="0">\n')
-
-    def _write_vtk_points(self):
-        self._f.write('<Points>\n')
-        self._f.write('<DataArray type="Float32" NumberOfComponents="3" format="ascii">\n')
-        for i in list(range(self._net.num_pores())):
-            self._f.write(str(self._net.get_pore_data(prop='coords')[i,0]))
-            self._f.write(' ')
-            self._f.write(str(self._net.get_pore_data(prop='coords')[i,1]))
-            self._f.write(' ')
-            self._f.write(str(self._net.get_pore_data(prop='coords')[i,2]))
-            self._f.write('\n')
-        self._f.write('\n</DataArray>\n</Points>\n')
-
-    def _write_vtk_connections(self):
-        self._f.write('<Lines>\n<DataArray type="Int32" Name="connectivity" format="ascii">\n')
-        for i in list(range(self._net.num_throats())):
-            self._f.write(str(self._net.get_throat_data(prop='conns')[i,0]))
-            self._f.write(' ')
-            self._f.write(str(self._net.get_throat_data(prop='conns')[i,1]))
-            self._f.write(' ')
-        self._f.write('\n</DataArray>\n<DataArray type="Int32" Name="offsets" format="ascii">\n')
-        for i in list(range(self._net.num_throats())):
-            self._f.write(str((i+1)*2))
-            self._f.write(' ')
-        self._f.write('\n</DataArray>\n</Lines>\n')
-
-    def _write_point_data(self):
-        network = self._net
-        pore_amalgamate = network.amalgamate_data(fluids=self._fluids)
-#        throat_amalgamate = network.amalgamate_throat_data(fluids=self._fluids)
-#        total_amalgamate = dict(pore_amalgamate,**throat_amalgamate)
-        total_amalgamate = pore_amalgamate
-        total_keys = list(total_amalgamate.keys())
-        num_keys = sp.size(total_keys)
-        self._f.write('<PointData Scalars="_data">\n')
-        for j in list(range(num_keys)):
-            if total_keys[j] !='pore_coords' and total_keys[j] !='throat_connections':
-                element = 'pores'
-#                if total_keys[j] in pore_amalgamate: element='pores'
-#               else: element='throats'
-                self._f.write('<DataArray type="Float32" Name="')
-                self._f.write(total_keys[j])
-                self._f.write('" format="ascii">\n')
-                shape =  np.shape(total_amalgamate[total_keys[j]])
-                if np.size(shape) == 1:
-                    total_amalgamate[total_keys[j]] = total_amalgamate[total_keys[j]]*sp.ones(getattr(network,'num_'+element)())
-                    for i in list(range(getattr(network,'num_'+element)())):
-                        self._f.write(str(total_amalgamate[total_keys[j]][i]))
-                        self._f.write(' ')
-                else:
-                    for i in list(range(getattr(network,'num_'+element)())):
-                        self._f.write(str(total_amalgamate[total_keys[j]][i][0]))
-                        self._f.write(' ')
-                self._f.write('\n</DataArray>\n')
-
-        output_path = os.path.join( os.path.expanduser('~'), self._file_name )
-        print('     oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo')
-        print('      Writing VTK file:', output_path)
-        print('     oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo')
-        self._f.write('</PointData>\n')
-
-    def _write_footer(self):
-        self._f.write('</Piece>\n</PolyData>\n</VTKFile>')
-
+        
+        root = ET.fromstring(self._TEMPLATE)
+        objs = []
+        if np.shape(fluids)==():
+            fluids = [fluids]
+        for fluid in fluids:
+            objs.append(fluid)
+        objs.append(network)
+        am = network.amalgamate_data(objs=objs)
+        key_list = list(sorted(am.keys()))
+        points = am[network.name+'.pore.coords']
+        pairs = network.get_throat_data(prop='conns')
+    
+        num_points = len(points)
+        num_throats = len(pairs)
+        
+        piece_node = root.find('PolyData').find('Piece')
+        piece_node.set("NumberOfPoints", str(num_points))
+        piece_node.set("NumberOfLines", str(num_throats))
+    
+        points_node = piece_node.find('Points')
+        coords = self._array_to_element("coords", points.T.ravel('F'), n=3)
+        points_node.append(coords)
+    
+        lines_node = piece_node.find('Lines')
+        connectivity = self._array_to_element("connectivity", pairs)
+        lines_node.append(connectivity)
+        offsets = self._array_to_element("offsets", 2*np.arange(len(pairs))+2)
+        lines_node.append(offsets)
+    
+        point_data_node = piece_node.find('PointData')
+        for key in key_list:
+            array = am[key]
+            if array.dtype == np.bool: array = array.astype(int)
+            if array.size != num_points: continue
+            element = self._array_to_element(key, array)
+            point_data_node.append(element)
+    
+        tree = ET.ElementTree(root)
+        tree.write(filename)
+    
+        if pretty:
+            with open(filename, "r+") as f:
+                string = f.read()
+                string = string.replace("</DataArray>", "</DataArray>\n\t\t\t")
+                f.seek(0)
+                # consider adding header: '<?xml version="1.0"?>\n'+
+                f.write(string)
+    
+    def read(filename):
+        network = {}
+        tree = ET.parse(filename)
+        piece_node = tree.find('PolyData').find('Piece')
+    
+        # extract connectivity
+        conn_element = piece_node.find('Lines').find('DataArray')
+        array = self._element_to_array(conn_element, 2)
+        network['heads'], network['tails'] = array.T
+    
+        for element in piece_node.find('PointData').iter('DataArray'):
+    
+            key = element.get('Name')
+            array = self._element_to_array(element)
+            network[key] = array
+    
+        return network
