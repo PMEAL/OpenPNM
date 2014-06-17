6de57aff2c7c0d28377fa345782737e5352328ed
Jeff Gostick <jeff@gostick.ca>
2014-06-17 17:37:47 -0400

    Refinements to Tortuosity - It returns the ratio of network distance to crow-flies distance between every two pores - It now has a 'estimate_time' method that works quite well
    Former-commit-id: 982865f912491698f66e9088300893d3a86e630e
    Former-commit-id: 092e7dcef33c3f862d02288241969e892e874bfb

--- OpenPNM/Algorithms/__Tortuosity__.py
+++ OpenPNM/Algorithms/__Tortuosity__.py
@@ -14,9 +14,8 @@ module __Tortuosity__: Network Tortuosity Algorithm
 """
 
 import scipy as sp
-import numpy as np
-import scipy.sparse as sprs
-import matplotlib.pyplot as plt
+import scipy.sparse.csgraph as spgr
+import OpenPNM
 
 from .__GenericAlgorithm__ import GenericAlgorithm
 
@@ -45,22 +44,53 @@ class Tortuosity(GenericAlgorithm):
         super(Tortuosity,self).__init__(**kwargs)
         self._logger.debug("Create Tortuosity Object")
         
-    def setup(self,
-              inlets,
-              outlets,
-              **params):
+    def estimate_time(self):
         r'''
         '''
-        # Parse args
-        self._in = inlets
-        self._out = outlets
+        pn_temp = OpenPNM.Network.TestNet()
+        graph = pn_temp.create_adjacency_matrix(sprsfmt='csr')
+        self._net.tic()
+        path = spgr.shortest_path(csgraph = graph, method='D', directed = False)
+        t = self._net.toc(quiet=True)
+        N = 125
+        k = 6
+        O = t/(N*(N*k + N*sp.log(N)))
+        N = self._net.num_pores()
+        k = sp.median(self._net.num_neighbors(pores=self._net.pores()))
+        t_est = O*(N*(N*k + N*sp.log(N)))
+        return t_est
 
-    def run(self):
+    def run(self,fluid):
         r'''
         '''
-        self._net.create_adjacency_matrix()
-        graph = pn._adjacency_matrix['csr']['conns']
-        temp = spgr.shortest_path(csgraph = graph, method='D', directed = False)
+        self._fluid = fluid
+        self._logger.warning("This algorithm can take some time...")
+        if 'throat.occupancy' in self._fluid.props():
+            graph = self._net.create_adjacency_matrix(prop=self._fluid['throat.occupancy'],sprsfmt='csr')
+        elif 'conns' in self._net._adjacency_matrix['csr'].keys():
+            graph = self._net._adjacency_matrix['csr']['conns']
+        else:
+            self._net.create_adjacency_matrix()
+            graph = self._net._adjacency_matrix['csr']['conns']
+        self._net.tic()
+        path = spgr.shortest_path(csgraph = graph, method='D', directed = False)
+        self._net.toc()
+        path = path*self._net._Lc
+        
+        Px = sp.array(self._net['pore.coords'][:,0],ndmin=2)
+        Py = sp.array(self._net['pore.coords'][:,1],ndmin=2)
+        Pz = sp.array(self._net['pore.coords'][:,2],ndmin=2)
+        
+        Cx = sp.square(Px.T - Px)
+        Cy = sp.square(Py.T - Py)
+        Cz = sp.square(Pz.T - Pz)
+        Ds = sp.sqrt(Cx + Cy + Cz)
+        
+        temp = path/Ds
+        
+        temp[sp.isnan(temp)] = 0
+        
+        return temp
         
         
         
