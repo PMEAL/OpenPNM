245b670724e01af2a3cafd09b9f72aefc11a7415
Jeff Gostick <jeff@gostick.ca>
2014-06-18 09:18:49 -0400

    Added final touches to Tortuosity - If no fluid is sent, it get dry tortuosity between every pair of pores - If a fluid IS sent, it will apply the 'throat.occupancy' to the connectivity matrix, to get the tortuosity in that phase. - This method returns as a 2D Nt x Nt matrix that can be nicely visualized with plt.imshow

--- OpenPNM/Algorithms/__Tortuosity__.py
+++ OpenPNM/Algorithms/__Tortuosity__.py
@@ -58,24 +58,25 @@ class Tortuosity(GenericAlgorithm):
         N = self._net.num_pores()
         k = sp.median(self._net.num_neighbors(pores=self._net.pores()))
         t_est = O*(N*(N*k + N*sp.log(N)))
-        return t_est
+        print("Based on the network size and PC performance, this algorithm will require: ",t_est,' seconds')
+        return 
 
-    def run(self,fluid):
+    def run(self,fluid=None):
         r'''
         '''
-        self._fluid = fluid
-        self._logger.warning("This algorithm can take some time...")
-        if 'throat.occupancy' in self._fluid.props():
-            graph = self._net.create_adjacency_matrix(prop=self._fluid['throat.occupancy'],sprsfmt='csr')
-        elif 'conns' in self._net._adjacency_matrix['csr'].keys():
-            graph = self._net._adjacency_matrix['csr']['conns']
-        else:
-            self._net.create_adjacency_matrix()
-            graph = self._net._adjacency_matrix['csr']['conns']
+        self._logger.warning("This algorithm can take some time. \
+        Use the estimate_time method to check time requirements for present network.")
+        graph = self._net.create_adjacency_matrix(data=self._net['throat.length'],sprsfmt='csr')
+        
+        if fluid != None:
+            self._fluid = fluid
+            if 'throat.occupancy' in self._fluid.props():
+                temp = self._net['throat.length']*(self._fluid['throat.occupancy']==1)
+                graph = self._net.create_adjacency_matrix(data=temp,sprsfmt='csr',prop='temp')
+        
         self._net.tic()
         path = spgr.shortest_path(csgraph = graph, method='D', directed = False)
         self._net.toc()
-        path = path*self._net._Lc
         
         Px = sp.array(self._net['pore.coords'][:,0],ndmin=2)
         Py = sp.array(self._net['pore.coords'][:,1],ndmin=2)
@@ -89,6 +90,7 @@ class Tortuosity(GenericAlgorithm):
         temp = path/Ds
         
         temp[sp.isnan(temp)] = 0
+        temp[sp.isinf(temp)] = 0
         
         return temp
         
