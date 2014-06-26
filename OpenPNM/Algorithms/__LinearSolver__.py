f74398003339b9ce80f8897a3f081a3092e71069
Jeff Gostick <jeff@gostick.ca>
2014-06-25 22:51:32 -0400

    Clean up of the Transport Algorithms - most of the code was moved to LinearSolver (which we can rename if we like). - the point of the subclasses is literally to just set some default argument values (eg. quantity = mole_fraction or temperature) - note on Fickian, it no longer uses ln(1-xA).  You have to do this yourself via boundary conditions and altering the final results

--- OpenPNM/Algorithms/__LinearSolver__.py
+++ OpenPNM/Algorithms/__LinearSolver__.py
@@ -1,10 +1,3 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-# Author: CEF PNM Team
-# License: TBD
-# Copyright (c) 2012
-
-#from __future__ import print_function
 """
 module __LinearSolver__: Algorithm for solving transport processes
 ========================================================================
@@ -28,26 +21,38 @@ class LinearSolver(GenericAlgorithm):
     """
 
     def __init__(self,**kwargs):
-        r"""
+        r'''
         Initializing the class
-        """
+        '''
         super(LinearSolver,self).__init__(**kwargs)
-
-    def _do_one_inner_iteration(self):
-
         
-        self._logger.info("Creating Coefficient matrix for the algorithm")
-        A = self._build_coefficient_matrix()
-        self._logger.info("Creating RHS matrix for the algorithm")
-        B = self._build_RHS_matrix()
-        self._logger.info("Solving AX = B for the sparse matrices")
-        X = sprslin.spsolve(A,B)
-        self._Neumann_super_X = X[-sp.in1d(sp.r_[0:len(X)],sp.r_[0:self.num_pores()])]
-        self._result = X[sp.r_[0:self.num_pores()]]        
-        return(self._result)
+    def setup(self,fluid,conductance,quantity):
+        r'''
+        '''
+        self._fluid = fluid
+        self._conductance = 'throat.'+conductance.split('.')[-1]
+        self._quantity = 'pore.'+quantity.split('.')[-1]
+        
+        #Check health of conductance vector
+        if self._fluid.check_throat_health(props=self._conductance):
+            #If no nans, check for 0's
+            temp = sp.where(self._fluid[self._conductance]==0)[0]
+            self._fluid[self._conductance][temp] = 1e-30
+        else:
+            raise Exception('The provided throat conductance has problems')
+            
+    def update(self):
+        r'''
+        Send results of simulation out the the appropriate locations.
+        
+        This is a basic version of the update that simply sends out the main
+        result (quantity). More elaborate updates should be subclassed.
+        '''        
+        self._fluid[self._quantity] = self[self._quantity]
+        self._logger.debug('Results of '+self.name+' algorithm have been added to '+self._fluid.name)
 
     def set_boundary_conditions(self,bctype='',bcvalue=[],pores=[],mode='merge'):
-        r"""
+        r'''
         Apply boundary conditions to specified pores.  This does not support
         throat boundary conditions yet.
         
@@ -78,7 +83,7 @@ class LinearSolver(GenericAlgorithm):
         in the same pore, so when new conditions are applied any existing ones
         are removed from all other boundary types.
         2. It is also not yet possible to apply boundary conditions to throats.
-        """
+        '''
         
         BC_default = ['Dirichlet','Neumann','Neumann_group']
 
@@ -153,34 +158,29 @@ class LinearSolver(GenericAlgorithm):
                 self['pore.'+bctype][pores] = False
 
     def _build_coefficient_matrix(self):
-       
+        r'''
+        '''
         # Filling coefficient matrix
-        tpore1 = self._net.get_throat_data(prop='conns')[:,0]
-        tpore2 = self._net.get_throat_data(prop='conns')[:,1]
-
-        try:            
-            Dir_pores = self.pores('Dirichlet')
-            non_Dir_pores = self.pores('Dirichlet',mode='difference')
-            loc1 = sp.in1d(tpore1,non_Dir_pores)
-            loc2 = sp.in1d(tpore2,non_Dir_pores)
-        except: 
-            loc1 = sp.ones(len(tpore1),dtype=bool)
-            loc2 = sp.ones(len(tpore2),dtype=bool)
+        tpore1 = self._net['throat.conns'][:,0]
+        tpore2 = self._net['throat.conns'][:,1]
         
+        #Identify Dirichlet pores, if any
+        try:
+            temp = self.pores('Dirichlet',mode='difference')
+        except:
+            temp = self.pores()
+        loc1 = sp.in1d(tpore1,temp)
+        loc2 = sp.in1d(tpore2,temp)
         modified_tpore1 = tpore1[loc1]
         modified_tpore2 = tpore2[loc1]
         row = modified_tpore1
         col = modified_tpore2
-        if sp.size(self._conductance)==1:
-            self._conductance = self._conductance*sp.ones(self.num_throats())
-        data_main = self._conductance
-        try:
-            self.pores('Neumann_insulated')
-            insulated_pores = self.pores('Neumann_insulated')
-            insulated_throats = self._net.find_neighbor_throats(insulated_pores,flatten=True,mode='not_intersection')
-            data_main[insulated_throats] = 1e-60
-        except: pass
         
+        #Expand the conductance to a vector if necessary
+        g = self._fluid[self._conductance]
+        if sp.size(g) == 1:
+            g = g*sp.ones(self.num_throats())
+        data_main = g
         data = data_main[loc1]
 
         modified_tpore2 = tpore2[loc2]
@@ -188,9 +188,9 @@ class LinearSolver(GenericAlgorithm):
         row = sp.append(row,modified_tpore2)
         col = sp.append(col,modified_tpore1)
         data = sp.append(data,data_main[loc2])
-
         A_dim = self.num_pores()
-
+        
+        #Check for Neuman_group BCs and add superpores if necessary
         try:
             self.pores('Neumann_group')
             group_values = self.get_data(prop='bcval_Neumann_group',pores=self.pores('Neumann_group'))
@@ -207,27 +207,29 @@ class LinearSolver(GenericAlgorithm):
                 row = sp.append(row,len(neu_tpore2)*[A_dim-item-1])
                 col = sp.append(col,neu_tpore2)
                 data = sp.append(data,len(neu_tpore2)*[self._g_super])
-
-        except: pass
+        except: 
+            pass
+        
         # Adding positions for diagonal
-        dia = sp.r_[0:A_dim]
+        diag = sp.r_[0:A_dim]
         try:
-            Dir_pores
-            row = sp.append(row,dia[Dir_pores])
-            col = sp.append(col,dia[Dir_pores])
-            data = sp.append(data,sp.ones_like(dia[Dir_pores]))
+            pores = self.pores('Dirichlet')
+            row = sp.append(row,diag[pores])
+            col = sp.append(col,diag[pores])
+            data = sp.append(data,sp.ones_like(diag[pores]))
             temp_data = sp.copy(data)
-            temp_data[sp.in1d(row,dia[Dir_pores])] = 0
-            non_Dir_dia = dia[-sp.in1d(dia,dia[Dir_pores])]
+            temp_data[sp.in1d(row,diag[pores])] = 0
+            non_Dir_diag = diag[-sp.in1d(diag,diag[pores])]
         except:
             temp_data = sp.copy(data)
-            non_Dir_dia = dia
+            non_Dir_diag = diag
+            
         S_temp = sp.zeros(A_dim)
         for i in sp.r_[0:len(row)]:
             S_temp[row[i]] = S_temp[row[i]] - temp_data[i]
-        data = sp.append(data,S_temp[non_Dir_dia])
-        row = sp.append(row,non_Dir_dia)
-        col = sp.append(col,non_Dir_dia)
+        data = sp.append(data,S_temp[non_Dir_diag])
+        row = sp.append(row,non_Dir_diag)
+        col = sp.append(col,non_Dir_diag)
         
         self._Coeff_dimension = A_dim
         a = sprs.coo.coo_matrix((data,(row,col)),(A_dim,A_dim))
@@ -236,17 +238,18 @@ class LinearSolver(GenericAlgorithm):
 
 
     def _build_RHS_matrix(self):        
-        
+        r'''
+        '''
         A_dim = self._Coeff_dimension
         B = sp.zeros([A_dim,1])
         try:
             Dir_pores = self.pores('Dirichlet')
-            Dir_pores_vals = self.get_data(prop='bcval_Dirichlet',pores=Dir_pores)
+            Dir_pores_vals = self['pore.bcval_Dirichlet'][Dir_pores]
             B[Dir_pores] = sp.reshape(Dir_pores_vals,[len(Dir_pores),1])
         except: pass
         try:
             individual_Neu_pores = self.pores('Neumann')
-            individual_Neu_pores_vals = self.get_data(prop='bcval_Neumann',pores=individual_Neu_pores)
+            individual_Neu_pores_vals = self['pore.bcval_Neumann'][individual_Neu_pores]
             B[individual_Neu_pores] = sp.reshape(individual_Neu_pores_vals,[len(individual_Neu_pores),1])
         except: pass
         try:
@@ -255,7 +258,6 @@ class LinearSolver(GenericAlgorithm):
             B[sp.r_[pnum:(pnum+len(self._group_Neumann_vals))]] = sp.reshape(self._group_Neumann_vals[sp.r_[0:len(self._group_Neumann_vals)]],[len(self._group_Neumann_vals),1])
         except: pass
         return(B)
-
 
     def rate(self,pores='',throats=''):
         r'''
@@ -280,136 +282,19 @@ class LinearSolver(GenericAlgorithm):
         R = sp.sum(sp.multiply(g,(X1-X2)))
         return(R)
         
-    def _calc_eff_prop(self,                            
-                       fluid,
-                       alg,
-                       d_term,
-                       x_term,
-                       conductance,
-                       occupancy,
-                       direction,
-                       **params):
-                
-        network =self._net
-        ftype1 = []
-        ftype2 = []
-        effective_prop = []  
-        result = {}
-        try: fluid = self.find_object_by_name(fluid) 
-        except: pass #Accept object
-        if type(direction)==str and direction=='': 
-            ftype1 = ['front','right','top']
-            ftype2 = ['back','left','bottom']            
-        elif type(direction)==str: direction = sp.array(direction,ndmin=1)              
-        if type(direction)==sp.ndarray:
-            ftype1 = []
-            ftype2 = []
-            for d in direction:
-                if d=='x' or d=='X' or d=='front' or d=='back': 
-                    ftype1.append('front')
-                    ftype2.append('back')
-                elif d=='y' or d=='Y'or d=='left' or d=='right': 
-                    ftype1.append('left')
-                    ftype2.append('right')
-                elif d=='z' or d=='Z'or d=='top' or d=='bottom': 
-                    ftype1.append('top')
-                    ftype2.append('bottom') 
-                else: self._logger.error('wrong input for direction!')
+    def _do_one_inner_iteration(self):
+        r'''
+        '''
+        self._logger.info("Creating Coefficient matrix for the algorithm")
+        A = self._build_coefficient_matrix()
+        self._logger.info("Creating RHS matrix for the algorithm")
+        B = self._build_RHS_matrix()
+        self._logger.info("Solving AX = B for the sparse matrices")
+        X = sprslin.spsolve(A,B)
+        self._Neumann_super_X = X[-sp.in1d(sp.r_[0:len(X)],sp.r_[0:self.num_pores()])]
+        self._result = X[sp.r_[0:self.num_pores()]]        
+        self._logger.info('Writing result to '+self.__class__.__name__+'[\''+self._conductance+'\']')
+        self[self._quantity] = self._result
         
-        if 'pore.Dirichlet' in self:
-            self._dir = self.get_pore_info(label='Dirichlet')
-            del self['pore.Dirichlet']
-        if 'pore.BCval' in self:
-            self._BCval_temp = self.get_pore_data(prop='BCval')
-            del self['pore.BCval']
-            try:
-                self._BCtypes_temp = sp.copy(self._BCtypes)
-                delattr (self,'_BCtypes')
-                self._BCvalues_temp = sp.copy(self._BCvalues)
-                delattr(self,'_BCvalues')
-            except: pass
-        try: self._X_temp = self.get_pore_data(prop=self._X_name)  
-        except: pass          
-        tensor = sp.zeros([3,3])
-        for i in sp.r_[0:len(ftype1)]:
-            face1 = ftype1[i] 
-            face2 = ftype2[i]
-            if face1=='front' or face1=='back': direct = 'X'
-            elif face1=='left' or face1=='right': direct = 'Y'
-            elif face1=='top' or face1=='bottom': direct = 'Z'
-            if 'boundary' in self._net._pore_info:
-                face1_pores = network.get_pore_indices(labels=[face1,'boundary'],mode='intersection')
-                face2_pores = network.get_pore_indices(labels=[face2,'boundary'],mode='intersection')
-            else:    
-                face1_pores = network.get_pore_indices(face1)
-                face2_pores = network.get_pore_indices(face2)            
-            ## Assign Dirichlet boundary conditions
-            ## BC1
-            BC1_pores = face1_pores  
-            self.set_pore_info(label='Dirichlet',locations=BC1_pores,mode='overwrite')
-            BC1_values = 0.8
-            self.set_pore_data(prop='BCval',data=BC1_values,locations=BC1_pores)
-            ## BC2
-            BC2_pores = face2_pores
-            self.set_pore_info(label='Dirichlet',locations=BC2_pores)
-            BC2_values = 0.4
-            self.set_pore_data(prop='BCval',data=BC2_values,locations=BC2_pores)        
-            self.run(active_fluid=fluid,
-                           x_term=x_term,
-                           conductance=conductance,
-                           occupancy=occupancy) 
-            x = self.get_pore_data(prop=x_term)
-            if alg=='Fickian':
-                X1 = sp.log(1-x[face1_pores])
-                X2 = sp.log(1-x[face2_pores])
-            elif alg=='Stokes':
-                X1 = x[face1_pores]
-                X2 = x[face2_pores]
-            delta_X = sp.absolute(sp.average(X2)-sp.average(X1)) 
-            d_force =sp.average(fluid.get_pore_data(prop=d_term))
-
-            if  face1=='top' or face1=='bottom': 
-                L = self._net.domain_size('height')
-                A = self._net.domain_size('top')
-            elif  face1=='left' or face1=='right':
-                L = self._net.domain_size('depth')
-                A = self._net.domain_size('left')
-            elif  face1=='front' or face1=='back':
-                L = self._net.domain_size('width')
-                A = self._net.domain_size('front')
-            fn = network.find_neighbor_pores(face1_pores,excl_self=True)
-            fn = fn[sp.in1d(fn,network.get_pore_indices('internal'))]
-            ft = network.find_connecting_throat(face1_pores,fn)
-            if alg=='Fickian': X_temp = sp.log(1-x[fn])
-            elif alg=='Stokes':
-                X_temp = x[fn]
-                d_force = 1/d_force
-            cond = self._conductance
-            N = sp.sum(cond[ft]*sp.absolute(X1-X_temp))
-            eff = N*L/(d_force*A*delta_X)
-            effective_prop.append(eff)
-            del self._pore_info['Dirichlet']
-            del self._pore_data['BCval']
-            delattr (self,'_BCtypes')
-            delattr(self,'_BCvalues')            
-            result[ftype1[i]+'/'+ftype2[i]+'('+direct+')'] = sp.array(effective_prop[i],ndmin=1)
-            if ftype1[i]=='top' or ftype1[i]=='bottom': tensor[2,2] = effective_prop[i]
-            elif ftype1[i]=='right' or ftype1[i]=='left': tensor[1,1] = effective_prop[i]
-            elif ftype1[i]=='front' or ftype1[i]=='back': tensor[0,0] = effective_prop[i]
         
-        try:
-            self.set_pore_data(prop=self._X_name,data=self._X_temp)
-            delattr (self,'_X_temp')
-        except : del self._pore_data[self._X_name]
-        try:
-            self.set_pore_info(label='Dirichlet',locations=self._dir,mode='overwrite')
-            delattr (self,'_dir')
-            self.set_pore_data(prop='BCval',data=self._BCval_temp)
-            delattr (self,'_BCval_temp')
-            self._BCtypes = self._BCtypes_temp
-            delattr (self,'_BCtypes_temp')
-            self._BCvalues = self._BCvalues_temp
-            delattr (self,'_BCvalues_temp')
-        except: pass        
-        if len(ftype1)<3: return result
-        elif len(ftype1)==3 : return tensor
+        
--- OpenPNM/Algorithms/__StokesFlow__.py
+++ OpenPNM/Algorithms/__StokesFlow__.py
@@ -1,16 +1,7 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-# Author: CEF PNM Team
-# License: TBD
-# Copyright (c) 2012
-
-#from __future__ import print_function
 """
 
-module __Permeability__: Hagen Poiseuille permeability
-========================================================================
-
-.. warning:: The classes of this module should be loaded through the 'Algorithms.__init__.py' file.
+module __Permeability__: 
+===============================================================================
 
 """
 
@@ -18,86 +9,18 @@ import scipy as sp
 from .__LinearSolver__ import LinearSolver
 
 class StokesFlow(LinearSolver):
-    r"""   
-    
-    Hagen Poiseuille permeability - Class to run an algorithm to find permeability on constructed networks
-    
-                        It returns pressure gradient inside the network.       
+    r'''
         
-    """
+    '''
     
     def __init__(self,**kwargs):
-        r"""
-        Initializing the class
-        """
+        r'''
+        '''
         super(StokesFlow,self).__init__(**kwargs)
-        self._logger.info("Create Hagen Poiseuille permeability Object")
-
-            
-    def _setup(self,
-               conductance='hydraulic_conductance',
-               species = 'pressure',
-               occupancy='occupancy',
-               **params):
-        r"""
-        This function executes the essential mathods before building matrices in Linear solution 
-        """
-
-        self._logger.info("Setup for Stokes Flow Algorithm")
-        self._fluid = params['active_fluid']
-        try: self._fluid = self._net._fluids[self._fluid] 
-        except: pass #Accept object
-        self._X_name = species
-        success_1 = self._fluid.check_throat_health(props=occupancy)
-        success_2 = self._fluid.check_throat_health(props=conductance)
-        if not success_1:  
-            self._fluid.set_data(prop=occupancy,throats='all',data=1)
-            self._fluid.set_data(prop=occupancy,pores='all',data=1)
-            self._logger.info('By default, it will be assumed that occupancy for '+self._fluid.name+' is equal to 1 in the entire network!')
-        if success_2: 
-            # Building hydraulic conductance based on occupancy
-            g = self._fluid.get_throat_data(prop=conductance)
-            s = self._fluid.get_throat_data(prop=occupancy)
-            self._conductance = g*s+g*(s==0)/1e3
-            setup_conductance = True
-        try:    setup_conductance
-        except: raise Exception('There is an error for the throat property: '+ conductance+'!')
-        try:    self.existing_bc
-        except: raise Exception('There is an error in applying boundary conditions!')
-
-
-    def _do_inner_iteration_stage(self):
-        r"""
-         main section of the algorithm              
-        """
-        p = self._do_one_inner_iteration()
-        self.set_pore_data(prop=self._X_name,data = p)
-        self._logger.info('Solving process finished successfully!')
-
-    def update(self):
+        self._logger.info('Create '+self.__class__.__name__+' Object')
         
-        p = self.get_pore_data(prop=self._X_name)
-        self._fluid.set_pore_data(prop=self._X_name,data=p)
-        self._logger.info('Results of ('+self.name+') algorithm have been updated successfully.')
-
-
-    def effective_permeability(self,
-                               fluid,
-                               direction='',
-                               d_term='viscosity',
-                               species='pressure',
-                               conductance='hydraulic_conductance',
-                               occupancy='occupancy',
-                               **params):
-        r"""
-        This function calculates effective diffusivity of a cubic network between face1 and face2.  
-        face1 and face2 represent types of these two faces.
-
-        """ 
-        return self._calc_eff_prop(alg='Stokes',
-                                  fluid=fluid,
-                                  direction=direction,
-                                  d_term=d_term,
-                                  species=species,
-                                  conductance=conductance,
-                                  occupancy=occupancy)
+    def setup(self,fluid,conductance='hydraulic_conductance',quantity='pressure',**params):
+        r'''
+        '''
+        self._logger.info("Setup "+self.__class__.__name__)         
+        super(StokesFlow,self).setup(fluid=fluid,conductance=conductance,quantity=quantity)
