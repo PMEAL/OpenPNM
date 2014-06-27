a2d43256debbbbbd261af4545b7e1d837f6f7624
jhinebau <jhinebau@mie.utoronto.ca>
2014-06-27 19:13:34 -0400

    Revert "No idea what this work was. 7pm on a Friday."
    This reverts commit f9d53df0b59f7bedec8b032555b4d73546f0d538 [formerly e97352d73d866846364a0c07c0d74142ef6f118d] [formerly f4c82401106a6afd4eefd7302d63978236d1b793].
    
    
    Former-commit-id: 15b9cfe10789d5229b8042192110cb1413a94175
    Former-commit-id: 97951704f91fe6cd3327d80d7fc8ef8b0721b089

--- run_script.py
+++ run_script.py
@@ -74,7 +74,6 @@ alg.setup(fluid=air)
 alg.run()
 alg.update()
 alg.calc_eff_diffusivity()
-print(alg._eff_property/air['pore.diffusivity'])
 
 
 #------------------------------------------------------------------------------
