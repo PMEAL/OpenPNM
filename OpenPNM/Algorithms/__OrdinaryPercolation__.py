154204195bbe7ba89b327b04a4aa41e8b71baccd
Jackie Lunger <Jackie@user143-164.wireless.utoronto.ca>
2014-07-21 13:11:39 -0400

    fixing update to work if given sequence instead of pressure
    Former-commit-id: 83459d07cac4031a8d8b88a11c1c69e0bfc55fef
    Former-commit-id: d452a6ac9e78f4b6e2178422c60c8d14c8cbc749

--- OpenPNM/Algorithms/__OrdinaryPercolation__.py
+++ OpenPNM/Algorithms/__OrdinaryPercolation__.py
@@ -201,11 +201,11 @@ class OrdinaryPercolation(GenericAlgorithm):
             temp = sp.array(t_inv,dtype=sp.float64,ndmin=1)
             self._fluid_inv.set_throat_data(prop=occupancy,data=temp)
             #Apply occupancy to defending fluid
-        if self._fluid_def != None:
-            temp = sp.array(~p_inv,dtype=sp.float64,ndmin=1)
-            self._fluid_def.set_pore_data(prop=occupancy,data=temp)
-            temp = sp.array(~t_inv,dtype=sp.float64,ndmin=1)
-            self._fluid_def.set_throat_data(prop=occupancy,data=temp)
+            if self._fluid_def != None:
+                temp = sp.array(~p_inv,dtype=sp.float64,ndmin=1)
+                self._fluid_def.set_pore_data(prop=occupancy,data=temp)
+                temp = sp.array(~t_inv,dtype=sp.float64,ndmin=1)
+                self._fluid_def.set_throat_data(prop=occupancy,data=temp)
         else:
             p_seq = self.get_pore_data(prop='inv_seq')<=seq
             t_seq = self.get_throat_data(prop='inv_seq')<=seq
@@ -215,10 +215,11 @@ class OrdinaryPercolation(GenericAlgorithm):
             temp = sp.array(t_seq,dtype=sp.float64,ndmin=1)
             self._fluid_inv.set_throat_data(prop=occupancy,data=temp)
             #Apply occupancy to defending fluid
-            temp = sp.array(~p_seq,dtype=sp.float64,ndmin=1)
-            self._fluid_def.set_pore_data(prop=occupancy,data=temp)
-            temp = sp.array(~t_seq,dtype=sp.float64,ndmin=1)
-            self._fluid_def.set_throat_data(prop=occupancy,data=temp)
+            if self._fluid_def != None:
+                temp = sp.array(~p_seq,dtype=sp.float64,ndmin=1)
+                self._fluid_def.set_pore_data(prop=occupancy,data=temp)
+                temp = sp.array(~t_seq,dtype=sp.float64,ndmin=1)
+                self._fluid_def.set_throat_data(prop=occupancy,data=temp)
             
  
             
