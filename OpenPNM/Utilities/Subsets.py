294461b032bde147fcbfe9e57dbc0c3d6d5acbdc
Jeff Gostick <jeff@gostick.ca>
2014-08-03 23:24:02 -0400

    A working version of the subnet/subfluid functionality
    Former-commit-id: 3aeb2ddf24b053df6cbdbc182f6f8ca2cd2899a2
    Former-commit-id: 39bfb601f9f4c183de52e8535c1b4a03f76d2cfd

--- OpenPNM/Utilities/Subsets.py
+++ OpenPNM/Utilities/Subsets.py
@@ -20,20 +20,19 @@ def subset_network(network,pores,name=None):
         
     Notes
     -----
-    This method adds a pore and throat label to the master network (pn) 
-    indicating which pores and throats were part of the sub-network (sn).  
-    This means that the results of the sub-network can be applied to the 
-    correct pores and throats in the main network by calling 
-    pn.pores(sn.name), and pn.throats(sn.name).
-    
-    This also creates a 'pore.map' and 'throat.map' property that lists the 
+    This creates a 'pore.map' and 'throat.map' property that lists the 
     correspondence to the subset pores and throats, to the main network.
+    
+    This method pulls in props from Geometry objects and creates a static copy
+    in the new sub-network's dictionary.
     
     Examples
     --------
     na
     '''
-    newpnm = OpenPNM.Network.GenericNetwork(name=name)
+    
+    subnet = OpenPNM.Network.GenericNetwork(name=name)
+    subnet._network = [network]  # Attach parent network
     pores = sp.array(pores,ndmin=1)
     throats = network.find_neighbor_throats(pores=pores,mode='intersection',flatten=True)
     
@@ -42,12 +41,14 @@ def subset_network(network,pores,name=None):
     Pmap[pores] = sp.arange(0,sp.shape(pores)[0])
     tpore1 = sp.array(Pmap[network['throat.conns'][throats,0]],dtype=int)
     tpore2 = sp.array(Pmap[network['throat.conns'][throats,1]],dtype=int)
-    newpnm['throat.conns'] = sp.vstack((tpore1,tpore2)).T
-    newpnm['pore.coords'] = network['pore.coords'][pores]
-    newpnm['pore.all'] = network['pore.all'][pores]
-    newpnm['throat.all'] = network['throat.all'][throats]
-    newpnm['pore.map'] = pores
-    newpnm['throat.map'] = throats
+    subnet['throat.conns'] = sp.vstack((tpore1,tpore2)).T
+    subnet['pore.coords'] = network['pore.coords'][pores]
+    subnet['pore.all'] = network['pore.all'][pores]
+    subnet['throat.all'] = network['throat.all'][throats]
+    
+    #create a map from new pores/throats to parents
+    subnet['pore.map'] = pores
+    subnet['throat.map'] = throats
     
     #Transfer labels
     labels = network.labels()
@@ -55,34 +56,25 @@ def subset_network(network,pores,name=None):
     labels.remove('throat.all')
     for item in labels:
         if item.split('.')[0] == 'pore':
-            newpnm[item] = network[item][pores]
+            subnet[item] = network[item][pores]
         if item.split('.')[0] == 'throat':
-            newpnm[item] = network[item][throats]
-            
-    #Convert all Geometry properties into static Network properties
-    for geom in network._geometries:
-        for prop in geom.props():
-            network[prop] = network[prop]
+            subnet[item] = network[item][throats]
             
     #Transfer props
     props = network.props()
     props.remove('throat.conns')
     props.remove('pore.coords')
+    for geom in network._geometries:
+        props.extend(geom.props())
     for item in props:
         if item.split('.')[0] == 'pore':
-            newpnm[item] = network[item][pores]
+            subnet[item] = network[item][pores]
         if item.split('.')[0] == 'throat':
-            newpnm[item] = network[item][throats]
+            subnet[item] = network[item][throats]
     
-    #Append pore and throat mapping to main network as attributes and data
-    network['pore.'+newpnm.name] = sp.zeros_like(network['pore.all'],dtype=bool)
-    network['pore.'+newpnm.name][pores] = True
-    network['throat.'+newpnm.name] = sp.zeros_like(network['throat.all'],dtype=bool)
-    network['throat.'+newpnm.name][throats] = True
-    
-    return newpnm
+    return subnet
   
-def subset_fluid(fluid,subset_network):
+def subset_fluid(fluid,subnet,name=None):
     r'''
     This method takes a fluid from the main network, and converts it to the 
     size and shape of the sub-network.
@@ -93,32 +85,47 @@ def subset_fluid(fluid,subset_network):
         A fluid object that is associated with the main network from which the
         subnetwork was extracted.
         
-    subset_network : OpenPNM Network Object
+    subnet : OpenPNM Network Object
         A subset of a Network object.  The 'pore.map' and 'throat.map' on the
         subset are used to extract data from the correct locations.
         
     Returns
     -------
-    newfluid : OpenPNM Fluid Object
+    subfluid : OpenPNM Fluid Object
         A fluid object with the same shape as the sub-network.  It contains all
         the data of the main fluid, but not the property calculation methods.
         
     Notes
     -----
     This method pulls the data in from the associated Physics objects and adds
-    a static copy to the Fluid.
+    a static copy to the new sub-fluid's dictionary.
     '''
-    newfluid = OpenPNM.Fluids.GenericFluid(network=subset_network)
-    for item in fluid.props():
-        element = item.split('.')[0]
-        newfluid[item] = fluid[item][subset_network[element+'.map']]
+    
+    subfluid = OpenPNM.Fluids.GenericFluid(network=subnet,name=name)
+    pores = subnet['pore.map']
+    throats = subnet['throat.map']
+    
+    #Transfer labels
+    labels = fluid.labels()
+    labels.remove('pore.all')
+    labels.remove('throat.all')
+    for item in labels:
+        if item.split('.')[0] == 'pore':
+            subfluid[item] = fluid[item][pores]
+        if item.split('.')[0] == 'throat':
+            subfluid[item] = fluid[item][throats]
+            
+    #Transfer props
+    props = fluid.props()
     for phys in fluid._physics:
-        for item in phys.props():
-            element = item.split('.')[0]
-            newfluid[item] = phys[item][subset_network[element+'.map']]
-    return newfluid
+        props.extend(phys.props())
+    for item in props:
+        if item.split('.')[0] == 'pore':
+            subfluid[item] = fluid[item][pores]
+        if item.split('.')[0] == 'throat':
+            subfluid[item] = fluid[item][throats]
 
-
+    return subfluid
     
     
     
