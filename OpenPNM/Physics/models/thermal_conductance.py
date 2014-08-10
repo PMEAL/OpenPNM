712d82fab813175e75c50ab127116b45b9d35bd4
Jeff Gostick <jeff@gostick.ca>
2014-08-10 14:52:12 -0400

    Fixed old style code in thermal_conductance
    Former-commit-id: cb671fb600c93b55e1c72895cd77239bd75db099
    Former-commit-id: b0cf593b015c1df7f5e6d748753e580932156da8

--- OpenPNM/Physics/models/thermal_conductance.py
+++ OpenPNM/Physics/models/thermal_conductance.py
@@ -7,14 +7,15 @@ Submodule -- thermal_conductance
 
 import scipy as _sp
 
-def thermal_fluid(physics,
-                  fluid,
-                  network,
-                  thermal_conductivity = 'pore.thermal_conductivity',
-                  pore_diameter = 'pore.diameter',
-                  throat_diameter = 'throat.diameter',
-                  throat_length = 'throat.length',
-                  **kwargs):
+def series_resistors(physics,
+                     fluid,
+                     network,
+                     thermal_conductivity = 'pore.thermal_conductivity',
+                     pore_diameter = 'pore.diameter',
+                     pore_area = 'pore.area',
+                     throat_area = 'throat.area',
+                     throat_length = 'throat.length',
+                     **kwargs):
     r"""
     Calculate the thermal conductance of void conduits in network ( 1/2 pore - full throat - 1/2 pore ) based on size
 
@@ -27,20 +28,22 @@ def thermal_fluid(physics,
 
     """
     throats = fluid.throats(physics.name)
-    kt = fluid.get_data(prop=thermal_conductivity,pores='all',mode='interpolate')
+    kp = fluid[thermal_conductivity]
+    kt = fluid.interpolate_data(kp)
     #Get Nt-by-2 list of pores connected to each throat
     pores = network.find_connected_pores(network.throats(),flatten=0)
     #Find g for half of pore 1
     pdia = network[pore_diameter]
-    gp1 = kt*pdia[pores[:,0]]**2/(0.5*pdia[pores[:,0]])
+    parea = network[pore_area]
+    gp1 = kt*parea[pores[:,0]]/(0.5*pdia[pores[:,0]])
     gp1[~(gp1>0)] = _sp.inf #Set 0 conductance pores (boundaries) to inf
     #Find g for half of pore 2
     gp2 = kt*pdia[pores[:,1]]**2/(0.5*pdia[pores[:,1]])
     gp2[~(gp2>0)] = _sp.inf #Set 0 conductance pores (boundaries) to inf
     #Find g for full throat
-    tdia = network[throat_diameter]
+    tarea = network[throat_area]
     tlen = network[throat_length]
-    gt = kt*tdia**2/tlen
+    gt = kt*tarea/tlen
     value = (1/gt + 1/gp1 + 1/gp2)**(-1)
     value = value[throats]
     return value
