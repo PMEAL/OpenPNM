202784f30c89e10e3c69c85750c754d4590b6575
Harold Day <harold.day@mail.mcgill.ca>
2014-08-13 11:00:20 -0400

    Rectilinear integrity test
    Former-commit-id: 8e98fa259ff265dca1abdf0210ea66c0712290a4
    Former-commit-id: 68a96c4b9724f047324cf3ae2331828932fafe6e

--- test_script.py
+++ test_script.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 import OpenPNM
 import pytest
 
@@ -5,6 +7,17 @@ test_network = OpenPNM.Network.TestNet()
 
 def test_print():
   print( test_network )
+
+def test_rectilinear_integrity():
+  R = np.random.rand(2,3,4)
+  # prune the mini way
+  network = OpenPNM.Network.Template()
+  network.generate(R)
+  network.trim( ( R.ravel() <= R.mean() ) )
+  O = network.asarray(network['pore.values'])
+  # what it would look like normally
+  M = np.where(R > R.mean(), R, 0)
+  assert np.allclose(M, O)
 
 if __name__ == '__main__':
   pytest.main()
