daa04d3eb812902343814206156ee5dcfb333022
jhinebau <jhinebau@mie.utoronto.ca>
2014-08-22 09:55:40 -0400

    Added a flag for hydraulic and diffusive conductance calculators to do their own pore length calculations.
    I need this because I don't have symmetrical pores in my extracted networks. Pore radii often exceed the pore-to-throat distance, grossly increasing the accumulated path length for diffusion.
    
    Former-commit-id: 57745e29285f4b1ef15a1210eccf17242d67b771
    Former-commit-id: 9c794fdbf866f67619c93b15aea35df3b1a1a138

--- OpenPNM/Physics/models/diffusive_conductance.py
+++ OpenPNM/Physics/models/diffusive_conductance.py
@@ -16,6 +16,7 @@ def bulk_diffusion(physics,
                    pore_diameter='pore.diameter',
                    throat_area='throat.area',
                    throat_length='throat.length',
+                   calc_pore_len=False,
                    **kwargs):
     r"""
     Calculate the diffusive conductance of conduits in network, where a 
@@ -42,13 +43,28 @@ def bulk_diffusion(physics,
     DABt = phase.interpolate_data(data=DABp)
     #Get Nt-by-2 list of pores connected to each throat
     Ps = network.find_connected_pores(throats=throats)
-    #Find g for half of pore 1
     parea = network[pore_area]
     pdia = network[pore_diameter]
-    gp1 = ct*DABt*parea[Ps[:,0]]/(0.5*pdia[Ps[:,0]])
+    pcoords = network['pore.coords']
+    if calc_pore_len:
+        #Find half-lengths of each pore
+        #   Find the pore-to-pore distance, minus the throat length
+        lengths = _sp.sqrt(_sp.sum(_sp.square(pcoords[Ps[:,0]]-pcoords[Ps[:,1]]),1))-network[throat_length][throats]
+        #   Calculate the fraction of that distance from the first pore    
+        fractions = pdia[Ps[:,0]]/(pdia[Ps[:,0]]+pdia[Ps[:,1]])
+        plen1 = lengths*fractions
+        plen2 = lengths*(1-fractions)
+        #remove any non-positive lengths
+        plen1[plen1<=0]=1e-12
+        plen2[plen2<=0]=1e-12
+    else:        
+        plen1 = (0.5*pdia[Ps[:,0]])
+        plen2 = (0.5*pdia[Ps[:,1]])      
+    #Find g for half of pore 1
+    gp1 = ct*DABt*parea[Ps[:,0]]/plen1
     gp1[~(gp1>0)] = _sp.inf  # Set 0 conductance pores (boundaries) to inf
     #Find g for half of pore 2
-    gp2 = ct*DABt*parea[Ps[:,1]]/(0.5*pdia[Ps[:,1]])
+    gp2 = ct*DABt*parea[Ps[:,1]]/plen2
     gp2[~(gp2>0)] = _sp.inf  # Set 0 conductance pores (boundaries) to inf
     #Find g for full throat
     tarea = network[throat_area]
@@ -57,4 +73,5 @@ def bulk_diffusion(physics,
     value = (1/gt + 1/gp1 + 1/gp2)**(-1)
     value = value[throats]
     return value
+
 
--- OpenPNM/Physics/models/hydraulic_conductance.py
+++ OpenPNM/Physics/models/hydraulic_conductance.py
@@ -14,6 +14,7 @@ def hagen_poiseuille(physics,
                      pore_viscosity='pore.viscosity',
                      throat_length='throat.length',
                      throat_diameter='throat.diameter',
+                     calc_pore_len=False,
                      **kwargs):
     r"""
     Calculates the hydraulic conductivity of throat assuming square geometry 
@@ -30,13 +31,25 @@ def hagen_poiseuille(physics,
     mut = phase.interpolate_data(mup)
     #Get Nt-by-2 list of pores connected to each throat
     Ps = network.find_connected_pores(throats=network.throats(),flatten=0)
-    #Find g for half of pore 1
     pdia = network[pore_diameter]
-    gp1 = 2.28*(pdia[Ps[:,0]])**3/(16*mut)
+    pcoords = network['pore.coords']
+    if calc_pore_len:
+        #Find half-lengths of each pore
+        #   Find the pore-to-pore distance, minus the throat length
+        lengths = _sp.sqrt(_sp.sum(_sp.square(pcoords[Ps[:,0]]-pcoords[Ps[:,1]]),1))-network[throat_length][throats]
+        #   Calculate the fraction of that distance from the first pore    
+        fractions = pdia[Ps[:,0]]/(pdia[Ps[:,0]]+pdia[Ps[:,1]])
+        plen1 = lengths*fractions
+        plen2 = lengths*(1-fractions)
+    else:        
+        plen1 = (0.5*pdia[Ps[:,0]])
+        plen2 = (0.5*pdia[Ps[:,1]])   
+    #Find g for half of pore 1
+    gp1 = 2.28*plen1*(pdia[Ps[:,0]])**2/(8*mut)
     gp1[~(gp1>0)] = _sp.inf #Set 0 conductance pores (boundaries) to inf
     #Find g for half of pore 2
     #gp2 = 2.28*(pdia[pores[:,1]]/2)**4/(pdia[pores[:,1]]*mut)
-    gp2 = 2.28*(pdia[Ps[:,1]])**3/(16*mut)
+    gp2 = 2.28*plen2*(pdia[Ps[:,1]])**2/(8*mut)
     gp2[~(gp2>0)] = _sp.inf #Set 0 conductance pores (boundaries) to inf
     #Find g for full throat
     tdia = network[throat_diameter]
