f97d1bb09cfa7a0e7f3441e11fa218788430d4de
Jeff Gostick <jeff@gostick.ca>
2014-08-24 11:13:21 -0400

    Save and Load now include the phase and subphase hierarchy....only _models is left to tackle
    Former-commit-id: a0f03489d8e47297bdc771c9992199d1112c05cd
    Former-commit-id: 21581480fef9c51815625ddbbb499152c3c07776

--- OpenPNM/Utilities/IO.py
+++ OpenPNM/Utilities/IO.py
@@ -25,9 +25,14 @@ class Save():
         for geom in net._geometries:
             sim['Geometry'+'.'+geom.name] = geom.copy()
         for phase in net._phases:
-            sim['Phase'+'.'+phase.name] = phase.copy()
-            for phys in phase._physics:
-                sim['Physics'+'.'+phys.name+'.'+'Phase'+'.'+phase.name] = phys.copy()
+            phase_name = 'Phase.'+phase.name
+            if phase.phases() != []:  #If phase is a mixture of components
+                for item in phase._phases:
+                    phase_name = phase_name + '.Phase.'+item.name
+            sim[phase_name] = phase.copy()
+        for phys in net._physics:
+            phase = phys._phases[0]
+            sim['Physics'+'.'+phys.name+'.'+'Phase'+'.'+phase.name] = phys.copy()
         sim['save_info'] = 'just a test'
         _sp.savez_compressed(net.name,**sim)
         _os.rename(net.name+'.npz',net.name+'.pnm')
@@ -50,16 +55,29 @@ class Load():
         #Initializing an empty Network
         net = OpenPNM.Network.GenericNetwork(name=name)
         net.update(sim['Network.'+name].item())
-        #Add Phase, Geometry and Physics objects
-        for obj in sim.keys():
+        #Add Phase, Geometry and Physics objects in specified order
+        for obj in sim.keys():  # Geometry objects
             if obj.split('.')[0] == 'Geometry':
                 geom = OpenPNM.Geometry.GenericGeometry(network=net,name=obj.split('.')[1])
                 geom.update(sim[obj].item())
-        for obj in sim.keys():
-            if obj.split('.')[0] == 'Phase':
+        for obj in sim.keys():  # Do Pure phases or independent mixtures first
+            if (obj.split('.')[0] == 'Phase') and (len(obj.split('.')) == 2):
                 phase = OpenPNM.Phases.GenericPhase(network=net,name=obj.split('.')[1])
                 phase.update(sim[obj].item())
-        for obj in sim.keys():
+        for obj in sim.keys():  # Then do proper mixtures which have subphases
+            if (obj.split('.')[0] == 'Phase') and (len(obj.split('.')) > 2):
+                phase_name = obj.split('.')[1]
+                # Begin cleaning up components list
+                components = obj.split('.')
+                components = list(filter(('Phase').__ne__, components))  # Remove 'Phases'
+                components.remove(phase_name)  # Remove current object from list too
+                comps =[]
+                for comp_name in components:
+                    comps.append(net.phases(comp_name))
+                #Instantiate mixutre phase with list of components
+                phase = OpenPNM.Phases.GenericPhase(network=net,name=phase_name,components=comps)
+                phase.update(sim[obj].item())
+        for obj in sim.keys():  # Physics objects associated with mixures
             if obj.split('.')[0] == 'Physics':
                 phase = net._find_object(obj_name=obj.split('.')[3])
                 phys = OpenPNM.Physics.GenericPhysics(network=net,phase=phase,name=obj.split('.')[1])
