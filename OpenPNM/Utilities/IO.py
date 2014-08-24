27a4ef8e7983e54785ab623c9805738b4dc5f813
Jeff Gostick <jeff@gostick.ca>
2014-08-24 01:44:17 -0400

    My kid was sick tonight, so i sat beside him coding.   The result is a new save format that write directly to a numpy zip file, converts the extension to .pnm, and can read the whole shebang back in as well, with the exception of models, and I'm also struggling with sub-phases.
    Former-commit-id: 13f3ccbf2d8f6f095e28312a60e145b31daccd86
    Former-commit-id: 7d929952e27f8d7c1286facd5f8a978060b67e52

--- OpenPNM/Utilities/IO.py
+++ OpenPNM/Utilities/IO.py
@@ -17,85 +17,52 @@ class Save():
             associated with the Network, but will not save any Algorithms.
         
         '''
-        print('Make temporary directory to store numpy zip files')
-        dirname = net.name
-        try:
-            _os.mkdir(dirname)
-        except:
-            raise Exception('A model with that name already exists')
-            
+        #save simulation as a nested dictionary
+        sim = {}
         #Save network
-        print('Save numpy zip files for each object')
-        _sp.savez_compressed(dirname+'/'+'Network'+'.'+net.name+'.npz',**net)
+        sim['Network'+'.'+net.name] = net.copy()
         #Save other objects
         for geom in net._geometries:
-            filename = dirname+'/'+'Geometry'+'.'+geom.name+'.npz'
-            _sp.savez_compressed(filename,**geom)
+            sim['Geometry'+'.'+geom.name] = geom.copy()
         for phase in net._phases:
-            filename = dirname+'/'+'Phase'+'.'+phase.name+'.npz'
-            _sp.savez_compressed(filename,**phase)
+            sim['Phase'+'.'+phase.name] = phase.copy()
             for phys in phase._physics:
-                filename = dirname+'/'+'Physics'+'.'+phys.name+'.'+'Phase'+'.'+phase.name+'.npz'
-                _sp.savez_compressed(filename,**phys)
-        
-#        #Convert directory into a 'pnm' file and remove directory
-#        print('Generate a zip archive from the temporary directory')
-#        shutil.make_archive(base_name=net.name, format="zip", root_dir='temp')
-#        print('Remove temporary directory')
-#        shutil.rmtree('temp')
-#        print('Rename zip file to a pnm file')
-#        os.rename(net.name+'.zip',net.name+'.pnm')
+                sim['Physics'+'.'+phys.name+'.'+'Phase'+'.'+phase.name] = phys.copy()
+        sim['save_info'] = 'just a test'
+        _sp.savez_compressed(net.name,**sim)
+        _os.rename(net.name+'.npz',net.name+'.pnm')
 
 class Load():
     
     @staticmethod
-    def PNM(simname):
+    def PNM(name):
         r'''
         Load a saved simulation
         
         Parameters
         ----------
-        sim_name : string
+        name : string
             The name of the simulation to be read in.
         '''
-#        basename = filename.split('.')[0]
-#        print('Renaming pnm file')
-#        os.rename(basename+'.pnm',basename+'.zip')
-#        print('Reading zip archive file')
-#        zf = zipfile.ZipFile(basename+'.zip')
-#        #Extract to a temporary directory
-#        print('Extracting numpy zip files to a temporary directory')
-#        dirname = 'temp'
-#        zf.extractall(path=dirname)
-        dirname = simname
-        basename = simname
-        object_list = _os.listdir(dirname)
-        #Start by initializing the Network
-        print('Loading the numpy zip files')
-        net = OpenPNM.Network.GenericNetwork(name=dirname)
-        obj = _sp.load(dirname+'/'+'Network'+'.'+basename+'.npz')
-        for item in obj:
-            net.update({item:obj[item]})
+        #Read in file
+        name = name.split('.')[0]
+        sim = _sp.load(name+'.pnm')
+        #Initializing an empty Network
+        net = OpenPNM.Network.GenericNetwork(name=name)
+        net.update(sim['Network.'+name].item())
         #Add Phase, Geometry and Physics objects
-        for filename in object_list:
-            if filename.split('.')[0] == 'Geometry':
-                geom = OpenPNM.Geometry.GenericGeometry(network=net,name=filename.split('.')[1])
-                obj = _sp.load(dirname+'/'+filename)
-                for item in obj:
-                    geom.update({item:obj[item]})
-            if filename.split('.')[0] == 'Phase':
-                phase = OpenPNM.Phases.GenericPhase(network=net,name=filename.split('.')[1])
-                obj = _sp.load(dirname+'/'+filename)
-                for item in obj:
-                    phase.update({item:obj[item]})
-            if filename.split('.')[0] == 'Physics':
-                phase = net._find_object(obj_name=filename.split('.')[3])
-                phys = OpenPNM.Physics.GenericPhysics(network=net,phase=phase,name=filename.split('.')[1])
-                obj = _sp.load(dirname+'/'+filename)
-                for item in obj:
-                    phys.update({item:obj[item]})
-#        #Remove the 'temp' directory before exiting
-#        print('Removing the temporary directory')
-#        shutil.rmtree(dirname)
+        for obj in sim.keys():
+            if obj.split('.')[0] == 'Geometry':
+                geom = OpenPNM.Geometry.GenericGeometry(network=net,name=obj.split('.')[1])
+                geom.update(sim[obj].item())
+        for obj in sim.keys():
+            if obj.split('.')[0] == 'Phase':
+                phase = OpenPNM.Phases.GenericPhase(network=net,name=obj.split('.')[1])
+                phase.update(sim[obj].item())
+        for obj in sim.keys():
+            if obj.split('.')[0] == 'Physics':
+                phase = net._find_object(obj_name=obj.split('.')[3])
+                phys = OpenPNM.Physics.GenericPhysics(network=net,phase=phase,name=obj.split('.')[1])
+                phys.update(sim[obj].item())
         return net
     
