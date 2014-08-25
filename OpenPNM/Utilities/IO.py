8a1a8660612233fcde6685b6cfa048c2f0916860
Jeff Gostick <jeff@gostick.ca>
2014-08-25 00:06:14 -0400

    Load/Save now work COMPLETELY!  They save the object heirarchy, the models, the data, EVERYTHING. Whew...
    Former-commit-id: 292d4bade4138714ee089e8a7259649ee2f41534
    Former-commit-id: 4b0ea84428b8d22973e1c57dcdb6afc385a4ce8d

--- OpenPNM/Utilities/IO.py
+++ OpenPNM/Utilities/IO.py
@@ -19,26 +19,29 @@ class Save():
         '''
         #save simulation as a nested dictionary
         sim = {}
-        #Save network
-        sim['Network'+'.'+net.name] = net.copy()
-        #Save other objects
-        for geom in net._geometries:
-            sim['Geometry'+'.'+geom.name] = geom.copy()
-            sim['Models'+'.'+geom.name] = {}
-            for prop in list(geom._models.keys()):
-                model = Save._save_model(geom,prop)
-                sim['Models'+'.'+geom.name][prop] = model
-        for phase in net._phases:
-            phase_name = 'Phase.'+phase.name
-            if phase.phases() != []:  #If phase is a mixture of components
-                for item in phase._phases:
-                    phase_name = phase_name + '.Phase.'+item.name
-            sim[phase_name] = phase.copy()
-        for phys in net._physics:
-            phase = phys._phases[0]
-            sim['Physics'+'.'+phys.name+'.'+'Phase'+'.'+phase.name] = phys.copy()
-        sim['save_info'] = 'just a test'
+        sim['data'] = {}
+        sim['tree'] = {}
+        sim['mods'] = {}
+        
+        #Collect all objects into a single list
+        all_objs = [net]
+        all_objs.extend(net._geometries)
+        all_objs.extend(net._phases)
+        all_objs.extend(net._physics)
+        
+        #Enter each object's data, object tree and models into dictionary
+        for obj in all_objs:
+            module = obj.__module__.split('.')[1]
+            sim['data'][module+'.'+obj.name] = obj.copy()
+            sim['tree'][module+'.'+obj.name] = {'Geometries' : obj.geometries(),
+                                                'Phases'     : obj.phases(),
+                                                'Physics'    : obj.physics()}
+            sim['mods'][module+'.'+obj.name] = {}
+            for prop in list(obj._models.keys()):
+                sim['mods'][module+'.'+obj.name][prop] = Save._save_model(obj,prop)
+        #Save nested dictionary as a Numpy zip file
         _sp.savez_compressed(net.name,**sim)
+        #Rename the zip extension to pnm for kicks
         _os.rename(net.name+'.npz',net.name+'.pnm')
         
     def _save_model(obj,item):
@@ -54,6 +57,7 @@ class Save():
         model['name'] = f.__name__
         model['args'] = {}
         for item in a:
+            #remove default arguments used in all add_model calls
             if item not in ['physics','network','phase','geometry']:
                 model['args'][item] = a[item]
         return model
@@ -72,44 +76,52 @@ class Load():
         '''
         #Read in file
         name = name.split('.')[0]
-        sim = _sp.load(name+'.pnm')
-        #Initializing an empty Network
-        net = OpenPNM.Network.GenericNetwork(name=name)
-        net.update(sim['Network.'+name].item())
-        #Add Phase, Geometry and Physics objects in specified order
-        for obj in sim.keys():  # Geometry objects
+        temp = _sp.load(name+'.pnm')
+        sim = {}
+        sim['data'] = temp['data'].item()
+        sim['tree'] = temp['tree'].item()
+        sim['mods'] = temp['mods'].item()
+
+        for obj in sim['data'].keys():  # Network object
+            if obj.split('.')[0] == 'Network':
+                net = OpenPNM.Network.GenericNetwork(name=obj.split('.')[1])
+                net.update(sim['data'][obj])
+        
+        for obj in sim['data'].keys():  # Geometry objects
             if obj.split('.')[0] == 'Geometry':
-                geom = OpenPNM.Geometry.GenericGeometry(network=net,name=obj.split('.')[1])
-                geom.update(sim[obj].item())
-        for obj in sim.keys():  # Do Pure phases or independent mixtures first
-            if (obj.split('.')[0] == 'Phase') and (len(obj.split('.')) == 2):
+                Ps = net.pores(obj.split('.')[1])
+                Ts = net.throats(obj.split('.')[1])
+                geom = OpenPNM.Geometry.GenericGeometry(network=net,pores=Ps,throats=Ts,name=obj.split('.')[1])
+                geom.update(sim['data'][obj])
+                for model in sim['mods'][obj].keys():
+                    Load._load_model(geom,sim['mods'][obj][model])
+        
+        for obj in sim['data'].keys():  # Do Pure phases or independent mixtures first
+            if (obj.split('.')[0] == 'Phases') and (sim['tree'][obj]['Phases'] == []):
                 phase = OpenPNM.Phases.GenericPhase(network=net,name=obj.split('.')[1])
-                phase.update(sim[obj].item())
-        for obj in sim.keys():  # Then do proper mixtures which have subphases
-            if (obj.split('.')[0] == 'Phase') and (len(obj.split('.')) > 2):
-                phase_name = obj.split('.')[1]
-                # Begin cleaning up components list
-                components = obj.split('.')
-                components = list(filter(('Phase').__ne__, components))  # Remove 'Phases'
-                components.remove(phase_name)  # Remove current object from list too
-                comps =[]
-                for comp_name in components:
-                    comps.append(net.phases(comp_name))
-                #Instantiate mixutre phase with list of components
-                phase = OpenPNM.Phases.GenericPhase(network=net,name=phase_name,components=comps)
-                phase.update(sim[obj].item())
-        for obj in sim.keys():  # Physics objects associated with mixures
+                phase.update(sim['data'][obj])
+                for model in sim['mods'][obj].keys():
+                    Load._load_model(phase,sim['mods'][obj][model])
+        
+        for obj in sim['data'].keys():  # Then do proper mixtures which have subphases
+            if (obj.split('.')[0] == 'Phases') and (sim['tree'][obj]['Phases'] != []):
+                comps = net.phases(sim['tree'][obj]['Phases'])
+                #Instantiate mixture phase with list of components
+                phase = OpenPNM.Phases.GenericPhase(network=net,name=obj.split('.')[1],components=comps)
+                phase.update(sim['data'][obj])
+                for model in sim['mods'][obj].keys():
+                    Load._load_model(phase,sim['mods'][obj][model])
+        
+        for obj in sim['data'].keys():  # Physics objects associated with mixures
             if obj.split('.')[0] == 'Physics':
-                phase = net._find_object(obj_name=obj.split('.')[3])
-                phys = OpenPNM.Physics.GenericPhysics(network=net,phase=phase,name=obj.split('.')[1])
-                phys.update(sim[obj].item())
-        #Finally scan through and pick out 'Models' dictionaries
-        for obj in sim.keys():
-            if obj.split('.')[0] == 'Models':
-                temp = net._find_object(obj_name=obj.split('.')[1])
-                model_dict = sim[obj].item()
-                for model in model_dict.keys():
-                    Load._load_model(temp,model_dict[model])
+                phase = net.phases(sim['tree'][obj]['Phases'])[0]  # This will always be only 1 phase
+                Ps = phase.pores(obj.split('.')[1])
+                Ts = phase.throats(obj.split('.')[1])
+                phys = OpenPNM.Physics.GenericPhysics(network=net,phase=phase,pores=Ps,throats=Ts,name=obj.split('.')[1])
+                phys.update(sim['data'][obj])
+                for model in sim['mods'][obj].keys():
+                    Load._load_model(phys,sim['mods'][obj][model])
+        
         return net
         
     def _load_model(obj,model):
@@ -117,7 +129,6 @@ class Load():
         '''
         #Import model using stored path and name
         mod = eval(model['path']+'.'+model['name'])
-        print(model)
         #Apply model to object using info in dict
         obj.add_model(model=mod,propname=model['propname'],**model['args'])
 
