6e7f25071859fd249ac2ea7db0ebc63213da14f3
Jeff Gostick <jeff@gostick.ca>
2014-08-25 22:47:54 -0400

    Changes to the init of GenericGeoemtry to allow instantiation of empty Geometry objects.  The intention is then call the .load() method to read in a save object's dict.
    Former-commit-id: 0bac692ee189123a79761694a5740addf491738f
    Former-commit-id: 462106d56d278d0a4db87b7b43b41639cc8aa02d

--- OpenPNM/Geometry/__GenericGeometry__.py
+++ OpenPNM/Geometry/__GenericGeometry__.py
@@ -39,13 +39,16 @@ class GenericGeometry(Core):
         Initialize
         """
         super(GenericGeometry,self).__init__(**kwargs)
-        self._logger.debug("Method: Constructor")
+        self._logger.debug("Class Constructor")
 
         #Initialize geometry locations
         self['pore.all'] = sp.ones((sp.shape(pores)[0],),dtype=bool)
         self['throat.all'] = sp.ones((sp.shape(throats)[0],),dtype=bool)
         
-        if network != None:
+        if network == None:
+            self._net = OpenPNM.Network.GenericNetwork()
+            self.name = name
+        else:
             self._net = network  # Attach network to self
             self._net._geometries.append(self)  # Register self with network.geometries
             self.name = name
