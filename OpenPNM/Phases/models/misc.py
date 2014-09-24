b2458d2159500d3203bb981802d982d63e4da3d2
Jeff Gostick <jeff@gostick.ca>
2014-09-24 09:07:19 -0400

    Changed the 'ideal_mixture' model to accept 'compositions' rather than 'mole_fractions' to be more general

--- OpenPNM/Phases/models/misc.py
+++ OpenPNM/Phases/models/misc.py
@@ -26,14 +26,14 @@ def random(phase,seed=None,**kwargs):
 def linear(phase,m,b,poreprop='pore.temperature',**kwargs):
     r"""
     Calculates a property as a linear function of a given property
-    
+
     Parameters
     ----------
     m, b: floats
         Slope and intercept of the linear corelation
-    
+
     poreprop : string
-        The property name of the independent variable or phase property.  The 
+        The property name of the independent variable or phase property.  The
         default is 'pore.temperature'.
 
     """
@@ -44,16 +44,16 @@ def linear(phase,m,b,poreprop='pore.temperature',**kwargs):
 def polynomial(phase,a,poreprop='pore.temperature',**kwargs):
     r"""
     Calculates a property as a polynomial function of a given property
-    
+
     Parameters
     ----------
     a: list of floats
-        A list containing the polynomial coefficients, where element 0 in the 
+        A list containing the polynomial coefficients, where element 0 in the
         list corresponds to a0 and so on.  Note that no entries can be skipped
         so 0 coefficients must be sent as 0.
-    
+
     poreprop : string
-        The property name of the independent variable or phase property.  The 
+        The property name of the independent variable or phase property.  The
         default is 'pore.temperature'.
 
     """
@@ -62,49 +62,48 @@ def polynomial(phase,a,poreprop='pore.temperature',**kwargs):
     for i in range(0,len(a)):
         value += a[i]*x**i
     return value
-    
-def ideal_mixture(phase,pore_prop,mole_frac='pore.mole_fraction',**kwargs):
+
+def ideal_mixture(phase,propname,composition='pore.mole_fraction',**kwargs):
     r'''
-    Calcualtes a given mixture property as the mole fraction weighted average
+    Calcualtes a given mixture property as the composition weighted average
     of the pure compononent properties
-    
+
     Parameters
     ----------
-    pore_prop : string
+    propname : string
         The name of the property on the pure components
-    mole_frac : string, optional (default is 'pore.mole_fraction')
-        The name of the pore property where the mole fraction information
+    composition : string, optional (default is 'pore.mole_fraction')
+        The name of the pore property where the composition information
         is stored on each pure component
-        
+
     Returns
     -------
-    The mole fraction weighted average of the given property
-    
+    The composition weighted average of the given property
+
     Notes
     -----
-    The mole fraction weighted average is calculated as follows:
-    
+    The average is calculated as follows:
+
     .. math::
-    
+
         P_{mixture}=\Sigma(x_{i}P_{i})
-        
+
     where
-    
+
         :math:`P_{mix}` is the average mixture property
 
-        :math:`x_{i}` is the mole fraction of species *i*
+        :math:`x_{i}` is the fractional composition of species *i*
 
         :math:`P_{i}` is the property of interest for pure species *i*
-        
-        
+
+
     '''
     value = _sp.zeros((phase.Np,))
     for comp in phase._phases:
-        value= value + comp[pore_prop]*comp[mole_frac]
+        value= value + comp[propname]*comp[composition]
     return value
-    
-    
-    
-    
-    
-    
+
+
+
+
+
