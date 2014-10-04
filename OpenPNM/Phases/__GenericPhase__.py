7f9f0ac47c0f5ecab696ff92ff7baf6758d71606
Jeff Gostick <jeff@gostick.ca>
2014-10-04 11:17:11 -0400

    GenericFluid and GenericNetwork now create a 'pore.map' and 'throat.map' property.  I *think* this was causes problems with map_pores and map_throats in cases where physics or geometries had not been defined everywhere.

--- OpenPNM/Phases/__GenericPhase__.py
+++ OpenPNM/Phases/__GenericPhase__.py
@@ -19,17 +19,17 @@ class GenericPhase(Core):
     ----------
     network : OpenPNM Network object
         The network to which this Phase should be attached
-        
+
     components : list of OpenPNM Phase objects
         These Phase objects are ficticious or virtual phases that are the pure
         components from which the mixture is made.  They are used to calculate
-        and store any pure component data.  If none are supplied then this 
+        and store any pure component data.  If none are supplied then this
         object will act like either a pure component, a mixture whose properties
         are well known (like air) and need not to be found from consideration of
         the pure component properties.
-        
+
     name : str, optional
-        A unique string name to identify the Phase object, typically same as 
+        A unique string name to identify the Phase object, typically same as
         instance name but can be anything.
 
     """
@@ -41,12 +41,14 @@ class GenericPhase(Core):
             self._net = OpenPNM.Network.GenericNetwork()
         else:
             self._net = network
-        
+
         self.name = name  # Assign name to object
 
         # Initialize label 'all' in the object's own info dictionaries
         self['pore.all'] = self._net['pore.all']
         self['throat.all'] = self._net['throat.all']
+        self['pore.map'] = self._net.Ps
+        self['throat.map'] = self._net.Ts
 
         #Set standard conditions on the fluid to get started
         self['pore.temperature'] = 298.0
@@ -61,31 +63,31 @@ class GenericPhase(Core):
                 #Move T and P models to beginning of regeneration order
                 comp.reorder_models({'pore.temperature':0,'pore.pressure':1})
         self._net._phases.append(self)  # Append this Phase to the Network
-        
+
     def __setitem__(self,prop,value):
         for phys in self._physics:
             if prop in phys.keys():
                 self._logger.error(prop+' is already defined in at least one associated Geometry object')
                 return
         super(GenericPhase,self).__setitem__(prop,value)
-        
+
     def __getitem__(self,key):
         if key not in self.keys():
             self._logger.debug(key+' not on Phase, constructing data from Physics')
             return self._interleave_data(key,sources=self._physics)
         else:
             return super(GenericPhase,self).__getitem__(key)
-            
+
     def set_component(self,phase,mode='add'):
         r'''
-        This method is used to add or remove a ficticious phase object to this 
-        object.  
-        
+        This method is used to add or remove a ficticious phase object to this
+        object.
+
         Parameters
         ----------
         phase : OpenPNM Phase object
-            This is the ficticious phase object defining a pure component. 
-            
+            This is the ficticious phase object defining a pure component.
+
         mode : string
             Indicates whether to 'add' or 'remove' the supplied object
         '''
@@ -104,15 +106,15 @@ class GenericPhase(Core):
                 self._phases.remove(phase)
             else:
                 self._logger.error('Phase not found')
-                
+
     def regenerate(self,**kwargs):
         for item in self._phases:
             item.regenerate(**kwargs)
         super(GenericPhase,self).regenerate(**kwargs)
-    
+
     #Pull in doc string for the Core regenerate method
     regenerate.__doc__ = Core.regenerate.__doc__
-            
+
     def check_mixture_health(self):
         r'''
         Query the properties of the 'virtual phases' that make up a mixture
@@ -125,7 +127,7 @@ class GenericPhase(Core):
             except:
                 pass
         return mole_sum
-    
+
     def check_physics_health(self):
         r'''
         Perform a check to find pores which have overlapping or undefined Physics
