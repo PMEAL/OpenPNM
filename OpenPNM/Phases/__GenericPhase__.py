9211ab694130c93941dc62f08579a91f7081a3d4
Jeff Gostick <jeff@gostick.ca>
2014-10-15 17:31:40 -0400

    update to generic phase that shouldve been in last commit

--- OpenPNM/Phases/__GenericPhase__.py
+++ OpenPNM/Phases/__GenericPhase__.py
@@ -67,12 +67,16 @@ class GenericPhase(Core):
     def __setitem__(self,prop,value):
         for phys in self._physics:
             if prop in phys.keys():
-                self._logger.error(prop+' is already defined in at least one associated Geometry object')
+                self._logger.error(prop+' is already defined in at least one associated Physics object')
                 return
         super(GenericPhase,self).__setitem__(prop,value)
 
     def __getitem__(self,key):
-        if key not in self.keys():
+        if key.split('.')[-1] == 'map':
+            element = key.split('.')[0]
+            self._logger.debug('Generating '+element+'.map from indices')
+            return self._get_indices(element=element)
+        elif key not in self.keys():
             self._logger.debug(key+' not on Phase, constructing data from Physics')
             return self._interleave_data(key,sources=self._physics)
         else:
