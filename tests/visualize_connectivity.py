753e236f3367b7aae5a6f1e553578de869a4ff63
Harold Day <harold.day@mail.mcgill.ca>
2014-12-12 15:06:47 -0800

    Better connectivity test script

--- tests/visualize_connectivity.py
+++ tests/visualize_connectivity.py
@@ -10,6 +10,8 @@ if __name__ == '__main__':
     for row, l in enumerate([[6, 8, 12], [14, 18, 20], [26]]):
         for col, cval in enumerate(l):
             pn = OpenPNM.Network.Cubic(shape=[3,3,3], connectivity=cval)
+            # delete all pairs that do not involve pore #13
+            pn['throat.conns'] = [conn for conn in pn['throat.conns'] if 13 in conn]
             offset = [3*col, -3*row, 0]
             wires = OpenPNM.Postprocessing.Graphics.Wires(pn['pore.coords']+offset, pn['throat.conns'])
             scene.add_actors([wires])
