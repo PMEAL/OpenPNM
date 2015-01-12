83734ba4843d38f16b96af3055611690fc2b514b
Jeff Gostick <jeff@gostick.ca>
2015-01-12 00:31:19 -0500

    More changes to improve logger output

--- OpenPNM/Base/__Core__.py
+++ OpenPNM/Base/__Core__.py
@@ -20,7 +20,6 @@ class Core(Base):
         Initialize
         '''
         super(Core,self).__init__(**kwargs)
-        logger.name = 'Core'
         logger.debug('Initializing Core class')
 
     def __setitem__(self,key,value):
--- OpenPNM/Base/__init__.py
+++ OpenPNM/Base/__init__.py
@@ -23,7 +23,7 @@ r"""
 import logging as logging
 # set up logging to file - see previous section for more details
 logging.basicConfig(level=logging.WARNING,
-                    format='%(asctime)s | %(levelname)-8s | %(name)s.%(module)s.%(funcName)s | %(message)s',
+                    format='%(asctime)s | %(levelname)-8s | %(name)s.%(funcName)s | %(message)s',
                     )
 
 from .__Controller__ import Controller
