c2ee2ef07d34727c11281b8733477987f3a2efd4
Jeff Gostick <jeff@gostick.ca>
2015-03-05 10:23:55 -0500

    Renamed the shortcuts Pnet and Tnet.  This makes it more consistent with Ps and Ts, IF we begin to think of the 's' as standing for self.   So Ps == Pself, and Ts = Tself.  The 's' was originally a plural.

--- OpenPNM/Base/__Core__.py
+++ OpenPNM/Base/__Core__.py
@@ -1280,8 +1280,8 @@ class Core(dict):
         Ts = self._map(element='throat',locations=throats,target=target,return_mapping=return_mapping)
         return Ts
         
-    netTs = property(fget=map_throats)
-    netPs = property(fget=map_pores)
+    Tnet = property(fget=map_throats)
+    Pnet = property(fget=map_pores)
         
     def _isa(self,keyword=None,obj=None):
         r'''
