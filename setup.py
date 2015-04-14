c9018ca35ae2de83814327e9fbfc80f1f1334f4e
bspellacy <brennanspellacy@gmail.com>
2015-04-14 16:24:49 -0400

    fixed tabbing

--- setup.py
+++ setup.py
@@ -14,34 +14,34 @@ setup(
     description = 'A framework for conducting pore network modeling simulations of multiphase transport in porous materials.',
     version = '1.1',
     classifiers = [
-		'Development Status :: 5 - Production/Stable',
-		'License :: OSI Approved :: MIT License',
-		'Programming Language :: Python',
-		'Topic :: Scientific/Engineering',
-		'Topic :: Scientific/Engineering :: Physics'
-	],
-	packages = [
-		'OpenPNM',
-		'OpenPNM.Base',
-		'OpenPNM.Network',
-		'OpenPNM.Network.models',
-		'OpenPNM.Geometry',
-		'OpenPNM.Geometry.models',
-		'OpenPNM.Phases',
-		'OpenPNM.Phases.models',
-		'OpenPNM.Physics',
-		'OpenPNM.Physics.models',
-		'OpenPNM.Utilities',
-		'OpenPNM.Algorithms',
-		'OpenPNM.Postprocessing'
-	],
-	install_requires = [
-		'scipy>=0.14.0',
-		'matplotlib'
-	],
-	tests_requires = [
-		'pytest'
-	],
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Physics'
+    ],
+    packages = [
+        'OpenPNM',
+        'OpenPNM.Base',
+        'OpenPNM.Network',
+        'OpenPNM.Network.models',
+        'OpenPNM.Geometry',
+        'OpenPNM.Geometry.models',
+        'OpenPNM.Phases',
+        'OpenPNM.Phases.models',
+        'OpenPNM.Physics',
+        'OpenPNM.Physics.models',
+        'OpenPNM.Utilities',
+        'OpenPNM.Algorithms',
+        'OpenPNM.Postprocessing'
+    ],
+    install_requires = [
+        'scipy>=0.14.0',
+        'matplotlib'
+    ],
+    tests_requires = [
+        'pytest'
+    ],
     author = 'OpenPNM Team',
     author_email = 'jeff.gostick@mcgill.ca',
     download_url = 'https://github.com/pmeal/OpenPNM/',
