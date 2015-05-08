297e181c6d3f932ad7fd3f47972c50d97eacbe41
bspellacy <brennanspellacy@gmail.com>
2015-05-08 15:50:46 -0400

    Pep8ified Toray090

--- OpenPNM/Geometry/__Toray090__.py
+++ OpenPNM/Geometry/__Toray090__.py
@@ -3,12 +3,14 @@ module __Toray090__: Subclass of GenericGeometry for a standard Toray TGPH090
 gas diffusion layer.s
 ===============================================================================
 
-.. warning:: The classes of this module should be loaded through the 'Geometry.__init__.py' file.
+.. warning:: The classes of this module should be loaded through the
+             'Geometry.__init__.py' file.
 
 """
 
 from OpenPNM.Geometry import models as gm
 from OpenPNM.Geometry import GenericGeometry
+
 
 class Toray090(GenericGeometry):
     r"""
@@ -17,18 +19,13 @@ class Toray090(GenericGeometry):
     """
 
     def __init__(self, **kwargs):
-        r"""
-        Initialize
-        """
-        super(Toray090,self).__init__(**kwargs)
+        super().__init__(**kwargs)
         self._generate()
 
     def _generate(self):
-        r"""
-        """
         self.models.add(propname='pore.seed',
                         model=gm.pore_misc.random,
-                        num_range=[0,0.95],
+                        num_range=[0, 0.95],
                         seed=self._seed,
                         regen_mode='constant')
         self.models.add(propname='throat.seed',
