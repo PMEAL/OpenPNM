9a55f982a3d227a1d461416172378752fe58bdc8
bspellacy <brennanspellacy@gmail.com>
2015-05-11 14:51:38 -0400

    Pep8ified Physics models

--- OpenPNM/Physics/models/capillary_pressure.py
+++ OpenPNM/Physics/models/capillary_pressure.py
@@ -7,12 +7,9 @@ Submodule -- capillary_pressure
 
 import scipy as _sp
 
-def washburn(physics,
-             phase,
-             network,
-             surface_tension='pore.surface_tension',
-             contact_angle='pore.contact_angle',
-             throat_diameter='throat.diameter',
+
+def washburn(physics, phase, network, surface_tension='pore.surface_tension',
+             contact_angle='pore.contact_angle', throat_diameter='throat.diameter',
              **kwargs):
     r"""
     Computes the capillary entry pressure assuming the throat is a cylindrical tube.
@@ -20,7 +17,7 @@ def washburn(physics,
     Parameters
     ----------
     network : OpenPNM Network Object
-        The Network object is 
+        The Network object is
     phase : OpenPNM Phase Object
         Phase object for the invading phases containing the surface tension and
         contact angle values.
@@ -40,7 +37,7 @@ def washburn(physics,
     .. math::
         P_c = -\frac{2\sigma(cos(\theta))}{r}
 
-    This is the most basic approach to calculating entry pressure and is 
+    This is the most basic approach to calculating entry pressure and is
     suitable for highly non-wetting invading phases in most materials.
 
     """
@@ -59,14 +56,11 @@ def washburn(physics,
     value = value[phase.throats(physics.name)]
     return value
 
-def purcell(physics,
-            phase,
-            network,
-            r_toroid,
+
+def purcell(physics, phase, network, r_toroid,
             surface_tension='pore.surface_tension',
             contact_angle='pore.contact_angle',
-            throat_diameter='throat.diameter',
-            **kwargs):
+            throat_diameter='throat.diameter', **kwargs):
     r"""
     Computes the throat capillary entry pressure assuming the throat is a toroid.
 
@@ -87,18 +81,22 @@ def purcell(physics,
 
     Notes
     -----
-    This approach accounts for the converging-diverging nature of many throat 
-    types.  Advancing the meniscus beyond the apex of the toroid requires an 
-    increase in capillary pressure beyond that for a cylindical tube of the 
-    same radius. The details of this equation are described by Mason and 
-    Morrow [1]_, and explored by Gostick [2]_ in the context of a pore network 
+    This approach accounts for the converging-diverging nature of many throat
+    types.  Advancing the meniscus beyond the apex of the toroid requires an
+    increase in capillary pressure beyond that for a cylindical tube of the
+    same radius. The details of this equation are described by Mason and
+    Morrow [1]_, and explored by Gostick [2]_ in the context of a pore network
     model.
 
     References
     ----------
 
-    .. [1] G. Mason, N. R. Morrow, Effect of contact angle on capillary displacement curvatures in pore throats formed by spheres. J. Colloid Interface Sci. 168, 130 (1994).
-    .. [2] J. Gostick, Random pore network modeling of fibrous PEMFC gas diffusion media using Voronoi and Delaunay tessellations. J. Electrochem. Soc. 160, F731 (2013).
+    .. [1] G. Mason, N. R. Morrow, Effect of contact angle on capillary displacement
+           curvatures in pore throats formed by spheres. J. Colloid Interface
+           Sci. 168, 130 (1994).
+    .. [2] J. Gostick, Random pore network modeling of fibrous PEMFC gas diffusion
+           media using Voronoi and Delaunay tessellations. J. Electrochem.
+           Soc. 160, F731 (2013).
 
     TODO: Triple check the accuracy of this equation
     """
@@ -116,7 +114,8 @@ def purcell(physics,
     r = network[throat_diameter]/2
     R = r_toroid
     alpha = theta - 180 + _sp.arcsin(_sp.sin(_sp.radians(theta)/(1+r/R)))
-    value = (-2*sigma/r)*(_sp.cos(_sp.radians(theta - alpha))/(1 + R/r*(1-_sp.cos(_sp.radians(alpha)))))
+    value = (-2*sigma/r) * \
+        (_sp.cos(_sp.radians(theta - alpha)) /
+            (1 + R/r*(1-_sp.cos(_sp.radians(alpha)))))
     value = value[phase.throats(physics.name)]
     return value
-
