ecad8cb50090323ce6afa9228a3d65e9bdc9d094
Jeff Gostick <jeff@gostick.ca>
2015-06-04 16:41:23 -0400

    Added unit tests for the Controller

--- test/unit/Base/ControllerTest.py
+++ test/unit/Base/ControllerTest.py
@@ -1,3 +1,4 @@
+import OpenPNM
 from OpenPNM.Base import Controller
 
 
@@ -5,6 +6,10 @@ class ControllerTest:
 
     def setup_class(self):
         self.controller = Controller()
+        self.net = OpenPNM.Network.Cubic(shape=[10,10,10])
+        self.geo = OpenPNM.Geometry.TestGeometry(network=self.net,
+                                                 pores=self.net.Ps,
+                                                 throats=self.net.Ts)
 
     def test_str(self):
         pass
@@ -15,3 +20,14 @@ class ControllerTest:
 
     def teardown_class(self):
         del(self.controller)
+
+    def test_save_and_load(self):
+        self.controller.save(self.net.name)
+        self.controller.clear()
+        assert self.controller == {}
+        self.controller.load(self.net.name)
+        assert self.net.name in self.controller.keys()
+        self.controller.save_simulation(self.net)
+        self.controller.purge_object(self.net, mode='complete')
+        self.controller.load_simulation(self.net.name)
+        self.controller.purge_object(self.net, mode='single')
