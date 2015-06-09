0fc2178ed3fb04bb9aa24629d9e7e5d570e65594
Jeff Gostick <jeff@gostick.ca>
2015-06-08 23:16:11 -0400

    Removed string test from Controller, fixed save/load test

--- test/unit/Base/ControllerTest.py
+++ test/unit/Base/ControllerTest.py
@@ -1,35 +1,19 @@
 import OpenPNM
 from OpenPNM.Base import Controller
-from OpenPNM.Network import GenericNetwork
 
 
 class ControllerTest:
 
     def setup_class(self):
         self.controller = Controller()
-        self.net = OpenPNM.Network.Cubic(shape=[10, 10, 10], name='test_net')
+        self.net = OpenPNM.Network.Cubic(shape=[10, 10, 10])
         self.geo = OpenPNM.Geometry.TestGeometry(network=self.net,
                                                  pores=self.net.Ps,
                                                  throats=self.net.Ts)
 
-    def test_str(self):
-        actual_string = self.controller.__str__()
-        expected_string = \
-            '------------------------------------------------------------\n' + \
-            'Object:         Name                 (Class)\n' + \
-            '------------------------------------------------------------\n' + \
-            'Network:        test_net             (Cubic)'
-
-        assert actual_string == expected_string
-
     def test_get_log_level(self):
         self.controller.loglevel = 50
         assert self.controller.loglevel == 'Log level is currently set to: 50'
-
-    def teardown_class(self):
-        del(self.controller)
-        del(self.net)
-        del(self.geo)
 
     def test_save_and_load(self):
         self.controller.save(self.net.name)
@@ -41,3 +25,8 @@ class ControllerTest:
         self.controller.purge_object(self.net, mode='complete')
         self.controller.load_simulation(self.net.name)
         self.controller.purge_object(self.net, mode='single')
+
+    def teardown_class(self):
+        del(self.controller)
+        del(self.net)
+        del(self.geo)
