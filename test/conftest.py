dcb7d3325c3679c3769520cf76a41c8af286a14b
bspellacy <brennanspellacy@gmail.com>
2015-06-23 11:55:10 -0400

    Cleaner fixtures

---
+++ test/conftest.py
@@ -0,0 +1,18 @@
+import pytest
+from os.path import join, dirname, realpath
+from os import mkdir
+from shutil import rmtree
+
+@pytest.fixture(autouse=True)
+def fixtures_directory(request):
+    fixture_dir = join(dirname(realpath(__file__)), 'fixtures')
+    request.function.__globals__['FIXTURE_DIR'] = fixture_dir
+
+@pytest.fixture(autouse=True)
+def temp_directory(request):
+    temp_dir = join(dirname(realpath(__file__)), 'temp')
+    mkdir(temp_dir)
+    request.function.__globals__['TEMP_DIR'] = temp_dir
+    def clean():
+            rmtree(temp_dir)
+    request.addfinalizer(clean)
--- test/unit/Base/ControllerTest.py
+++ test/unit/Base/ControllerTest.py
@@ -1,11 +1,9 @@
 import OpenPNM
-from OpenPNM.Base import Controller
-
+from os.path import join
 
 class ControllerTest:
-
     def setup_class(self):
-        self.controller = Controller()
+        self.controller = OpenPNM.Base.Controller()
         self.net = OpenPNM.Network.Cubic(shape=[10, 10, 10])
         self.geo = OpenPNM.Geometry.TestGeometry(network=self.net,
                                                  pores=self.net.Ps,
@@ -16,11 +14,31 @@ class ControllerTest:
         assert self.controller.loglevel == 'Log level is currently set to: 50'
 
     def test_save_and_load(self):
-        self.controller.save(self.net.name)
+        self.controller.save(join(TEMP_DIR, self.net.name))
         self.controller.clear()
         assert self.controller == {}
-        self.controller.load(self.net.name)
+        self.controller.load(join(TEMP_DIR, self.net.name))
         assert self.net.name in self.controller.keys()
+
+    def test_load_v120_pnm(self):
+        temp = self.controller.copy()
+        self.controller.clear()
+        self.controller.load(join(FIXTURE_DIR, 'test_v120.pnm'))
+        a = [
+            'Boundary_hy4Ey',
+            'FickianDiffusion_LjxxQ',
+            'IP_1',
+            'OrdinaryPercolation_BI85q',
+            'Standard_GIaef',
+            'Standard_HmuMH',
+            'Toray090_935N3',
+            'air',
+            'net',
+            'water'
+        ]
+        assert sorted(list(self.controller.keys())) == a
+        self.controller.clear()
+        self.controller.update(temp)
 
     def teardown_class(self):
         del(self.controller)
