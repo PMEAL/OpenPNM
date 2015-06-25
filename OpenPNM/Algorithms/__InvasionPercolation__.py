af003ba665b64bbf4271ad3be1f752b15c48c6eb
Jeff Gostick <jeff@gostick.ca>
2015-06-24 23:48:39 -0400

    Modernized the api, while trying to keep the old approach working.  Almost got it, but seem to have broken some logic somewhere.

--- OpenPNM/Algorithms/__InvasionPercolation__.py
+++ OpenPNM/Algorithms/__InvasionPercolation__.py
@@ -5,10 +5,8 @@ InvasionPercolationBasic: Simple IP
 ===============================================================================
 
 """
-
+import heapq as hq
 import scipy as sp
-import bisect
-from collections import deque
 from OpenPNM.Algorithms import GenericAlgorithm
 from OpenPNM.Base import logging
 logger = logging.getLogger(__name__)
@@ -32,9 +30,9 @@ class InvasionPercolation(GenericAlgorithm):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-    def run(self, phase, inlets, throat_prop='throat.capillary_pressure'):
+    def setup(self, phase, throat_prop='throat.capillary_pressure', **kwargs):
         r"""
-        Perform the algorithm
+        Set up the required parameters for the algorithm
 
         Parameters
         ----------
@@ -42,52 +40,85 @@ class InvasionPercolation(GenericAlgorithm):
             The phase to be injected into the Network.  The Phase must have the
             capillary entry pressure values for the system.
 
-        inlets : array_like
-            The list of inlet pores from which the Phase can enter the Network
-
         throat_prop : string
             The name of the throat property containing the capillary entry
             pressure.  The default is 'throat.capillary_pressure'.
 
         """
-        import heapq as hq
-        queue = []
-        hq.heapify(queue)
         self._phase = phase
-        net = self._net
         # Setup arrays and info
-        t_entry = phase[throat_prop]
+        self['throat.entry_pressure'] = phase[throat_prop]
         # Indices into t_entry giving a sorted list
-        t_sorted = sp.argsort(t_entry, axis=0)
-        t_order = sp.zeros_like(t_sorted)
-        t_order[t_sorted] = sp.arange(0, net.Nt)
-        t_inv = -sp.ones_like(net.Ts)
-        p_inv = -sp.ones_like(net.Ps)
-        p_inv[inlets] = 0
+        self['throat.sorted'] = sp.argsort(self['throat.entry_pressure'], axis=0)
+        self['throat.order'] = sp.zeros_like(self['throat.sorted'])
+        self['throat.order'][self['throat.sorted']] = sp.arange(0, self._net.Nt)
+        self['throat.invaded'] = -sp.ones((self._net.Nt,))
+        self['pore.invaded'] = -sp.ones((self._net.Np,))
+        self._tcount = 0
+
+    def set_inlets(self, pores=None, **kwargs):
+        r"""
+
+        Parameters
+        ----------
+        pores : array_like
+            The list of inlet pores from which the Phase can enter the Network
+        """
+        if 'inlets' in kwargs.keys():
+            pores = kwargs['inlets']
+        self['pore.invaded'][pores] = 0
+
         # Perform initial analysis on input pores
-        Ts = net.find_neighbor_throats(pores=inlets)
-        [hq.heappush(queue, T) for T in t_order[Ts]]
-        tcount = 1
-        while len(queue) > 0:
+        Ts = self._net.find_neighbor_throats(pores=pores)
+        self.queue = []
+        [hq.heappush(self.queue, T) for T in self['throat.order'][Ts]]
+
+    def run(self, n_steps=None, **kwargs):
+        r"""
+        Perform the algorithm
+
+        Parameters
+        ----------
+        n_steps : int
+            The number of throats to invaded during this step
+
+        """
+        if 'throat.entry_pressure' not in self.keys():
+            self.setup(**kwargs)
+        if sp.all(self['pore.invaded'] == -1):
+            self.set_inlets(**kwargs)
+
+        if n_steps is None:
+            n_steps = sp.inf
+
+        queue = self.queue
+        t_sorted = self['throat.sorted']
+        t_order = self['throat.order']
+        t_inv = self['throat.invaded']
+        p_inv = self['pore.invaded']
+
+        count = 0
+        while (len(queue) > 0) and (count < n_steps):
             # Find throat at the top of the queue
             t = hq.heappop(queue)
             # Extract actual throat number
             t_next = t_sorted[t]
-            t_inv[t_next] = tcount
+            t_inv[t_next] = self._tcount
             # If throat is duplicated
             while len(queue) > 0 and queue[0] == t:
                 # Note: Preventing duplicate entries below might save some time here
                 t = hq.heappop(queue)
             # Find pores connected to newly invaded throat
-            Ps = net['throat.conns'][t_next]
+            Ps = self._net['throat.conns'][t_next]
             # Remove already invaded pores from Ps
             Ps = Ps[p_inv[Ps] < 0]
             if len(Ps) > 0:
-                p_inv[Ps] = tcount
-                Ts = net.find_neighbor_throats(pores=Ps)  # Find connected throats
+                p_inv[Ps] = self._tcount
+                Ts = self._net.find_neighbor_throats(pores=Ps)  # Find connected throats
                 Ts = Ts[t_inv[Ts] < 0]  # Remove already invaded throats from Ts
                 [hq.heappush(queue, T) for T in t_order[Ts]]
-            tcount += 1
+            count += 1
+            self._tcount += 1
         self['throat.invasion_sequence'] = t_inv
         self['pore.invasion_sequence'] = p_inv
 
