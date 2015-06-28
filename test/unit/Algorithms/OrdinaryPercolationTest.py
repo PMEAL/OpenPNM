176a7f3f672c7a9cecb70e96cdf114a75b96dcba
Jeff Gostick <jeff@gostick.ca>
2015-06-28 00:08:13 -0400

    Test plots on the OP algorithms.  These are huge chunks of code, that now return handles to the plots, so we can test the return turn

--- test/unit/Algorithms/OrdinaryPercolationTest.py
+++ test/unit/Algorithms/OrdinaryPercolationTest.py
@@ -1,3 +1,15 @@
+import OpenPNM
+ctrl = OpenPNM.Base.Controller()
+ctrl.loglevel = 60
+
+
 class OrdinaryPercolationTest:
-    def test_do_outer_iteration_stage(self):
-        pass
+    def setup_test(self):
+        self.net = OpenPNM.Network.Cubic(shape=[5, 5, 5])
+        self.geo = OpenPNM.Geometry.Toray090(network=self.net,
+                                             pores=self.net.Ps,
+                                             throats=self.net.Ts)
+        self.phase = OpenPNM.Phases.Water(network=self.net)
+        self.phys = OpenPNM.Physics.Standard(network=self.net,
+                                             pores=self.net.Ps,
+                                             throats=self.net.Ts)
