885ee7bcbf6641432d39811218a33ffaec5ff309
Jeff Gostick <jeff@gostick.ca>
2015-07-01 15:17:45 -0400

    Wow, the only thing left is to calculate each clusters invasion volume specifically from mass transfer.  This even has movies working.  Still trying to save them though.

--- run_script.py
+++ run_script.py
@@ -1,6 +1,7 @@
 import OpenPNM as op
+import scipy as sp
 import matplotlib.pyplot as plt
-plt.ion()
+import matplotlib.animation as animation
 
 print('-----> Using OpenPNM version: '+op.__version__)
 ctrl = op.Base.Controller()
@@ -49,11 +50,43 @@ IP = op.Algorithms.InvasionPercolationDrying(network=pn)
 IP.setup(invading_phase=air, defending_phase=water)
 IP.set_inlets(pores=pn.pores('front'))
 
-for i in range(0, 402):
+
+ims = []
+fig = plt.figure()
+i = 0
+while sp.sum(IP['pore.invaded'] == -1) > 29:
     qs = IP.queues
     for i in range(len(qs)):
         q = qs[i]
         IP.run2(queue=q, volume=0.5)
     IP.qregen()
+    # Recalculate the diffusion of water vapor
+    D = op.Algorithms.FickianDiffusion(network=pn, phase=air)
+    Ps_out = pn.toindices(IP['pore.invaded'] == -1)
+    Ps_in = pn.pores('front')
+    D.set_boundary_conditions(pores=Ps_out,
+                              bcvalue=0.5,
+                              bctype='Dirichlet',
+                              mode='overwrite')
+    D.set_boundary_conditions(pores=Ps_in,
+                              bcvalue=0.1,
+                              bctype='Dirichlet',
+                              mode='merge')
+    D.run()
+    # Create image and store for making an animation
+    im = sp.copy(D['pore.air_mole_fraction'])
+    im[Ps_out] = sp.nan
+    im = pn.asarray(im)[:, :, 0]
+    ims.append((plt.imshow(im, interpolation='none'),))
+    ctrl.purge_object(D)
+    i += 1
 
-plt.imshow(pn.asarray(IP['pore.queue_number'])[:, :, 0], interpolation='none')
+im_ani = animation.ArtistAnimation(fig,
+                                   ims,
+                                   interval=50,
+                                   repeat_delay=3000,
+                                   blit=True)
+
+Writer = animation.writers['imagemagick']
+writer = Writer(fps=15, metadata=dict(artist='Me'), bitrate=1800)
+im_ani.save('test.mp4', writer=writer)
