6f37c62b4693405e73c46ef23f32e54240b51731
Jeff Gostick <jeff@gostick.ca>
2015-11-03 13:24:21 -0500

    Got basic functionality in Import.  It currently accepts a csv file with columns of data.  The header of each column is used as the dictionary key in the network, so 'length' become 'throat.length'.

--- OpenPNM/Network/__Import__.py
+++ OpenPNM/Network/__Import__.py
@@ -19,3 +19,16 @@ class Import(GenericNetwork):
     """
     def __init__(self, file, **kwargs):
         super().__init__(**kwargs)
+        if type(file) is str:
+            pass
+        rec_arr = sp.recfromcsv(file)
+        Np = sp.sum(~sp.isnan(rec_arr['coords1']))
+        Nt = sp.sum(~sp.isnan(rec_arr['conns1']))
+        self.update({'pore.all': sp.ones((Np,), dtype=bool)})
+        self.update({'throat.all': sp.ones((Nt,), dtype=bool)})
+        for item in rec_arr.dtype.names:
+            data = rec_arr[item]
+            if sp.sum(~sp.isnan(data)) == Np:
+                self.update({'pore.'+item: data})
+            if sp.sum(~sp.isnan(data)) == Nt:
+                self.update({'throat.'+item: data})
