cce464fa8d2a961d8547d32e370fd51876925ef4
Jeff Gostick <jeff@gostick.ca>
2015-12-17 20:40:59 -0500

    Added some more integration tests for most cases, but not permutations.

--- test/integration/test_drainage_algorithm.py
+++ test/integration/test_drainage_algorithm.py
@@ -1,10 +1,8 @@
 import scipy as sp
-import matplotlib
 import OpenPNM
 import pytest
 
-#def test_drainage_basic():
-pn = OpenPNM.Network.Cubic(shape=[25, 25, 25], spacing=0.0001)
+pn = OpenPNM.Network.Cubic(shape=[25, 25, 25], spacing=0.00005)
 pn['pore.internal'] = True
 pn['throat.internal'] = True
 pn.add_boundary_pores(pores=pn.pores('top'),
@@ -20,12 +18,59 @@ boun = OpenPNM.Geometry.Boundary(network=pn,
                                  pores=pn.pores('internal', mode='not'),
                                  throats=pn.throats('internal', mode='not'))
 water = OpenPNM.Phases.Water(network=pn)
-phys = OpenPNM.Physics.Standard(network=pn, phase=water, geometry=geom)
-
+phys = OpenPNM.Physics.Standard(network=pn,
+                                phase=water,
+                                pores=pn.Ps,
+                                throats=pn.Ts)
 drainage = OpenPNM.Algorithms.Drainage(network=pn)
-drainage.setup(invading_phase=water)
-drainage.set_inlets(pores=pn.pores('boundary_top'))
-drainage.run()
-data = drainage.get_drainage_data()
-assert sp.amin(data['nonwetting_phase_saturation']) == 0.0
-assert sp.amax(data['nonwetting_phase_saturation']) == 1.0
+
+
+def test_basic():
+    drainage.setup(invading_phase=water)
+    drainage.set_inlets(pores=pn.pores('boundary_top'))
+    drainage.run()
+    data = drainage.get_drainage_data()
+
+    assert sp.amin(data['nonwetting_phase_saturation']) == 0.0
+    assert sp.amax(data['nonwetting_phase_saturation']) == 1.0
+
+
+def test_residual():
+    drainage.setup(invading_phase=water)
+    drainage.set_inlets(pores=pn.pores('boundary_top'))
+    Ps = sp.random.randint(0, pn.Np, 1000)
+    Ts = sp.random.randint(0, pn.Nt, 1000)
+    drainage.set_residual(pores=Ps, throats=Ts)
+    drainage.run()
+    data = drainage.get_drainage_data()
+
+    assert sp.amin(data['nonwetting_phase_saturation']) > 0
+    assert sp.amax(data['nonwetting_phase_saturation']) == 1.0
+
+
+def test_trapping():
+    drainage.setup(invading_phase=water, trapping=True)
+    drainage.set_inlets(pores=pn.pores('boundary_top'))
+    drainage.set_outlets(pores=pn.pores('boundary_bottom')[0:300])
+    drainage.run()
+    data = drainage.get_drainage_data()
+
+    assert sp.amin(data['nonwetting_phase_saturation']) == 0.0
+    assert sp.amax(data['nonwetting_phase_saturation']) < 1.0
+
+
+def test_late_pore_filling():
+    phys.models.add(propname='pore.fractional_filling',
+                    model=OpenPNM.Physics.models.multiphase.late_pore_filling,
+                    Pc=0,
+                    Swp_star=0.2,
+                    eta=1)
+    phys.regenerate()
+    drainage.setup(invading_phase=water,
+                   pore_filling='pore.fractional_filling')
+    drainage.set_inlets(pores=pn.pores('boundary_top'))
+    drainage.run()
+    data = drainage.get_drainage_data()
+
+    assert sp.amin(data['nonwetting_phase_saturation']) == 0.0
+    assert sp.amax(data['nonwetting_phase_saturation']) < 1.0
