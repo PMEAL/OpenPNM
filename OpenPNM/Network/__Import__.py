0af385e985e4688e5b17195022f0d59575afe7db
Jeff Gostick <jeff@gostick.ca>
2016-01-06 09:43:34 -0500

    changed try accept to if else so that method wont crash when called with pores only init can call from_csv or be left blank changed _ trimming to keep all but first

--- OpenPNM/Network/__Import__.py
+++ OpenPNM/Network/__Import__.py
@@ -14,63 +14,66 @@ topo = topology()
 
 
 class Import(GenericNetwork):
+    r"""
+    This method is used to import data resulting from some external network
+    extraction tools {refs here?}.  The aim of this class is to define a
+    standard way to represent network data and transfer into OpenPNM.   The
+    main principle is to keep is as simple and general as possible, so the
+    CSV data format was used.
 
-    def __init__(self, **kwargs):
+    There are a few rules governing how the data should be stored:
+
+    1.  The first row of the file (column headers) should contain the
+    property names
+
+    2.  The property names should be in the format of *pore_volume* or
+    *throat_length*.  In OpenPNM this will become *pore.volume* or
+    *throat.length* (i.e. the underscore is replaced by a dot).
+
+    3.  Each column represents a specific property.  For Np x 1 or Nt x 1
+    data such as *pore_volume* this is straightforward.  For Np x m or
+    Nt x m data, it must be entered in as a set of values NOT separated by
+    commas.  For instance, the *pore_coords* values should be X Y Z with
+    spaces, not commas between them.
+
+    4.  Labels can also be imported by placing the characters T and F in a
+    column corresponding to the label name (i.e. *pore_front*).  T
+    indicates where the label applies and F otherwise.
+    """
+
+    def __init__(self, filename=None, **kwargs):
         super().__init__(**kwargs)
+        if filename:
+            self.from_csv(filename=filename)
 
-    def from_csv(self, file):
-        r"""
-        This method is used to import data resulting from some external network
-        extraction tools {refs here?}.  The aim of this class is to define a
-        standard way to represent network data and transfer into OpenPNM.   The
-        main principle is to keep is as simple and general as possible, so the
-        CSV data format was used.
+    def from_csv(self, filename):
 
-        There are a few rules governing how the data should be stored:
-
-        1.  The first row of the file (column headers) should contain the
-        property names
-
-        2.  The property names should be in the format of *pore_volume* or
-        *throat_length*.  In OpenPNM this will become *pore.volume* or
-        *throat.length* (i.e. the underscore is replaced by a dot).
-
-        3.  Each column represents a specific property.  For Np x 1 or Nt x 1
-        data such as *pore_volume* this is straightforward.  For Np x m or
-        Nt x m data, it must be entered in as a set of values NOT separated by
-        commas.  For instance, the *pore_coords* values should be X Y Z with
-        spaces, not commas between them.
-
-        4.  Labels can also be imported by placing the characters T and F in a
-        column corresponding to the label name (i.e. *pore_front*).  T
-        indicates where the label applies and F otherwise.
-        """
-        rarr = sp.recfromcsv(file)
+        rarr = sp.recfromcsv(filename)
         items = list(rarr.dtype.names)
-        try:
+        if 'throat_conns' in items:
             Nt = len(rarr['throat_conns'])
             self.update({'throat.all': sp.ones((Nt,), dtype=bool)})
             data = [sp.fromstring(rarr['throat_conns'][i], sep=' ')
                     for i in range(Nt)]
             self.update({'throat.conns': sp.vstack(data)})
             items.remove('throat_conns')
-        except:
-            raise Exception('throat_conns was not found, cannot proceed')
-        try:
-            Np = sp.sum([len(rarr['pore_coords'][i]) > 0 for i in range(Nt)])
+        else:
+            logger.warning('\'throat_conns\' not found')
+        if 'pore_coords' in items:
+            Np = len(rarr['pore_coords'])
             self.update({'pore.all': sp.ones((Np,), dtype=bool)})
             data = [sp.fromstring(rarr['pore_coords'][i], sep=' ')
                     for i in range(Np)]
             self.update({'pore.coords': sp.vstack(data)})
             items.remove('pore_coords')
-        except:
-            raise Exception('pore.coords was not found, cannot proceed')
+        else:
+            logger.warning('\'pore_coords\' not found')
 
         # Now parse through all the other items
         for item in items:
             element = item.split('_')[0]
             N = self._count(element)
-            prop = item.split('_')[1]
+            prop = item.split('_', maxsplit=1)[1]
             data = rarr[item]
             if data.dtype.char == 'S':
                 if data[0].decode().upper()[0] in ['T', 'F']:  # If boolean
