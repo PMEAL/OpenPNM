49cef14637795ba8827044ada37f17ce562020f9
Jeff Gostick <jeff@gostick.ca>
2016-01-19 23:42:57 -0500

    Added unit tests for saving a net, reloading the saved file onto a new network, then checking the props of the newly loaded network match the saved one.

--- test/unit/Utilities/IOTest.py
+++ test/unit/Utilities/IOTest.py
@@ -16,26 +16,68 @@ class IOTest:
                                            pores=self.net.Ps,
                                            throats=self.net.Ts)
 
-    def test_save_vtk_no_phases(self):
+    def test_save_load_vtk_no_phases(self):
         fname = os.path.join(TEMP_DIR, 'test_save_vtk_1')
         io.VTK.save(network=self.net, filename=fname)
+        assert os.path.isfile(fname+'.vtp')
+        net = io.VTK.load(fname+'.vtp')
+        assert net.Np == 27
+        assert net.Nt == 54
+        assert sp.shape(net['pore.coords']) == (27, 3)
+        assert sp.shape(net['throat.conns']) == (54, 2)
 
-    def test_save_vtk_w_phases(self):
+    def test_save_and_load_vtk_w_phases(self):
         fname = os.path.join(TEMP_DIR, 'test_save_vtk_2')
         io.VTK.save(network=self.net, filename=fname, phases=self.phase)
+        assert os.path.isfile(fname+'.vtp')
+        net = io.VTK.load(fname+'.vtp')
+        assert net.Np == 27
+        assert net.Nt == 54
+        assert sp.shape(net['pore.coords']) == (27, 3)
+        assert sp.shape(net['throat.conns']) == (54, 2)
+        assert [True for item in net.keys() if 'temperature' in item]
+        assert [True for item in net.keys() if 'diffusive_conductance' in item]
 
-    def test_save_csv_no_phases(self):
+    def test_save_and_load_csv_no_phases(self):
         fname = os.path.join(TEMP_DIR, 'test_save_csv_1')
         io.CSV.save(network=self.net, filename=fname)
+        assert os.path.isfile(fname+'.csv')
+        net = io.CSV.load(fname+'.csv')
+        assert net.Np == 27
+        assert net.Nt == 54
+        assert sp.shape(net['pore.coords']) == (27, 3)
+        assert sp.shape(net['throat.conns']) == (54, 2)
 
-    def test_save_csv_w_phases(self):
+    def test_save_and_load_csv_w_phases(self):
         fname = os.path.join(TEMP_DIR, 'test_save_csv_2')
         io.CSV.save(network=self.net, filename=fname, phases=self.phase)
+        assert os.path.isfile(fname+'.csv')
+        net = io.CSV.load(fname+'.csv')
+        assert net.Np == 27
+        assert net.Nt == 54
+        assert sp.shape(net['pore.coords']) == (27, 3)
+        assert sp.shape(net['throat.conns']) == (54, 2)
+        assert [True for item in net.keys() if 'temperature' in item]
+        assert [True for item in net.keys() if 'diffusive_conductance' in item]
 
-    def test_save_mat_no_phases(self):
+    def test_save_and_load_mat_no_phases(self):
         fname = os.path.join(TEMP_DIR, 'test_save_mat_1')
         io.MAT.save(network=self.net, filename=fname)
+        assert os.path.isfile(fname+'.mat')
+        net = io.MAT.load(fname+'.mat')
+        assert net.Np == 27
+        assert net.Nt == 54
+        assert sp.shape(net['pore.coords']) == (27, 3)
+        assert sp.shape(net['throat.conns']) == (54, 2)
 
-    def test_save_mat_w_phases(self):
+    def test_save_and_load_mat_w_phases(self):
         fname = os.path.join(TEMP_DIR, 'test_save_mat_2')
         io.MAT.save(network=self.net, filename=fname, phases=self.phase)
+        assert os.path.isfile(fname+'.mat')
+        net = io.MAT.load(fname+'.mat')
+        assert net.Np == 27
+        assert net.Nt == 54
+        assert sp.shape(net['pore.coords']) == (27, 3)
+        assert sp.shape(net['throat.conns']) == (54, 2)
+        assert [True for item in net.keys() if 'temperature' in item]
+        assert [True for item in net.keys() if 'diffusive_conductance' in item]
