165ba9b53abf6a9b385397b7185537cd3df3cf33
Jeff Gostick <jeff@gostick.ca>
2016-02-09 00:12:36 -0500

    moving to coveralls

--- test/unit/Network/MatFileTest.py
+++ test/unit/Network/MatFileTest.py
@@ -5,8 +5,4 @@ from os.path import join
 
 class MatFileTest:
     def setup_class(self):
-        fname = join(FIXTURE_DIR, 'example_network.mat')
-        self.net = OpenPNM.Network.MatFile(filename=fname)
-
-    def test_fail(self):
-        assert self.net.Np == 0
+        pass
