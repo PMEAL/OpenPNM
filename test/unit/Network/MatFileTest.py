6aeba28e6095ad1dc0171b96ef8c23da1ac8d4c2
Jeff Gostick <jeff@gostick.ca>
2016-02-08 19:43:09 -0500

    I moved James's test_mat file to fixtures, and it broke the example in the class, so I removed the example, and wrote a unit test to replace the missing coverage.

--- test/unit/Network/MatFileTest.py
+++ test/unit/Network/MatFileTest.py
@@ -1,9 +1,18 @@
 import OpenPNM
 import scipy as sp
-from os.path import join
+import os
 
 
 class MatFileTest:
     def setup_class(self):
-        fname = join(FIXTURE_DIR, 'example_network.mat')
-        self.net = OpenPNM.Network.MatFile(filename=fname)
+        self.fname = os.path.join(FIXTURE_DIR, 'example_network.mat')
+        pn = OpenPNM.Network.MatFile(filename=self.fname)
+        assert pn.Np == 105
+        assert pn.Nt == 238
+        assert pn.props() == ['throat.conns', 'pore.coords']
+
+    def test_extra_args(self):
+        pn = OpenPNM.Network.MatFile(filename=self.fname,
+                                     xtra_pore_data='type',
+                                     xtra_throat_data='type')
+        assert sp.all(sp.unique(pn['pore.type']) == [0, 1, 6])
