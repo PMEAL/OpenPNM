edbd45bdb8233c55ef46434fe686a0c54af7b8e1
Jeff Gostick <jeff@gostick.ca>
2016-02-08 22:30:00 -0500

    WTF Travis!

--- test/unit/Network/MatFileTest.py
+++ test/unit/Network/MatFileTest.py
@@ -5,16 +5,18 @@ import os
 
 class MatFileTest:
     def setup_class(self):
-        self.fname = os.path.join(FIXTURE_DIR, 'example_network.mat')
+        pass
 
     def test_loading(self):
-        pn = OpenPNM.Network.MatFile(filename=self.fname)
+        fname = os.path.join(FIXTURE_DIR, 'example_network.mat')
+        pn = OpenPNM.Network.MatFile(filename=fname)
         assert pn.Np == 105
         assert pn.Nt == 238
         assert pn.props() == ['throat.conns', 'pore.coords']
 
     def test_extra_args(self):
-        pn = OpenPNM.Network.MatFile(filename=self.fname,
+        fname = os.path.join(FIXTURE_DIR, 'example_network.mat')
+        pn = OpenPNM.Network.MatFile(filename=fname,
                                      xtra_pore_data='type',
                                      xtra_throat_data='type')
         assert sp.all(sp.unique(pn['pore.type']) == [0, 1, 6])
