060c3683b1a26a667357d2e9d0a95dbba00cd125
Jeff Gostick <jeff@gostick.ca>
2016-02-23 00:41:12 -0500

    Forgot version number in init [ci skip]

--- OpenPNM/__init__.py
+++ OpenPNM/__init__.py
@@ -45,7 +45,7 @@ if sp.__version__ < '0.14.0':
     raise Exception('OpenPNM requires SciPy version 0.14.0 or greater')
 
 __requires__ = ['scipy']
-__version__ = '1.3.0'
+__version__ = '1.4.0'
 
 from . import Base
 from . import Utilities
