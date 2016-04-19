c1d70dec26e4b8a4b2cb4106478852f5e1ab3abc
Jeff Gostick <jeff@gostick.ca>
2016-04-18 23:21:24 -0400

    Massive changes to GenericGeometry and Core concerning the set_locations, also added better unit tests

--- OpenPNM/Geometry/__GenericGeometry__.py
+++ OpenPNM/Geometry/__GenericGeometry__.py
@@ -83,24 +83,122 @@ class GenericGeometry(Core):
         else:  # ...Then check Network
             return self._net[key][self._net[element+'.'+self.name]]
 
-    def set_locations(self, pores=[], throats=[], mode='add'):
+    def set_locations(self, pores=None, throats=None, mode='add'):
         r"""
-        Set the pore and throat locations of the Geometry object
+        Used for assigning Geometry objects to specified locations
 
         Parameters
         ----------
-        pores and throats : array_like
-            The list of pores and/or throats in the Network where the object
-            should be applied
-        mode : string
-            Indicates whether list of pores or throats is to be added or
-            removed from the object.  Options are 'add' (default) or 'remove'.
+        pores : array_like
+            The pore locations in the Network where this object is to apply
 
+        throats : array_like
+            The throat locations in the Network where this object is to apply
+
+        mode : string
+            Either 'add' (default) or 'remove' the object from the specified
+            locations
+
+        Examples
+        --------
+        >>> import OpenPNM
+        >>> pn = OpenPNM.Network.TestNet()
+        >>> pn.Np
+        125
+        >>> geom = OpenPNM.Geometry.GenericGeometry(network=pn,
+        ...                                         pores=sp.arange(5, 125),
+        ...                                         throats=pn.Ts)
+        >>> [geom.Np, geom.Nt]
+        [120, 300]
+        >>> geom['pore.dummy'] = True
+        >>> health = pn.check_geometry_health()
+        >>> pores = health['undefined_pores']
+        >>> geom.set_locations(pores=pores)
+        >>> [geom.Np, geom.Nt]
+        [125, 300]
+
+        The label 'pore.dummy' was assigned 'before' these pores were added
+        >>> geom.pores(labels='dummy', mode='not')
+        array([0, 1, 2, 3, 4])
+        >>> geom.set_locations(pores=pores, mode='remove')
+        >>> [geom.Np, geom.Nt]
+        [120, 300]
+
+        # All pores without 'pore.dummy' label are gone
+        >>> geom.num_pores(labels='dummy', mode='not')
+        0
         """
-        if sp.size(pores) > 0:
-            self._set_locations(element='pore', locations=pores, mode=mode)
-        if sp.size(throats) > 0:
-            self._set_locations(element='throat', locations=throats, mode=mode)
+        if mode == 'add':
+            # Check if any constant values exist on the object
+            for item in self.props():
+                if (item not in self.models.keys()) or \
+                   (self.models[item]['regen_mode'] == 'constant'):
+                    raise Exception('Constant properties found on object, ' +
+                                    'cannot increase size')
+            if pores is not None:
+                self._add_locations(element='pores', locations=pores)
+            if throats is not None:
+                self._add_locations(element='throats', locations=throats)
+        if mode == 'remove':
+            if pores is not None:
+                self._drop_locations(element='pores', locations=pores)
+            if throats is not None:
+                self._drop_locations(element='throats', locations=throats)
+        # Finally, regenerate models to correct the length of all arrays
+        self.models.regenerate()
+
+    def _drop_locations(self, element, locations):
+        net = self._net
+        element = self._parse_element(element, single=True)
+        locations = self._parse_locations(locations)
+
+        self_inds = net._map(element=element,
+                             locations=locations,
+                             target=self)
+        keep = ~self._tomask(locations=self_inds, element=element)
+        for item in list(self.keys()):
+            if item.split('.')[0] == element:
+                temp = self[item][keep]
+                self.update({item: temp})
+        # Set locations in Network dictionary
+        net[element+'.'+self.name][locations] = False
+
+    def _add_locations(self, element, locations):
+        net = self._net
+        element = self._parse_element(element, single=True)
+        locations = self._parse_locations(locations)
+
+        # Ensure locations are not already assigned to another Geometry
+        temp = sp.zeros(net._count(element=element), dtype=int)
+        geoms = net._find_object(obj_type='geometry')
+        for item in geoms:
+            inds = net._get_indices(element=element, labels=item)
+            temp[inds] += 1
+        temp[locations] += 1  # Increment proposed locations
+        if sp.any(temp[locations] > 1):
+            raise Exception('Some of the given '+element+' are already ' +
+                            'assigned to an existing object')
+
+        # Create new 'all' label for new size
+        new_len = self._count(element=element) + sp.size(locations)
+        self.update({element+'.all': sp.ones((new_len, ), dtype=bool)})
+
+        # Set locations in Network dictionary
+        inds_orig = net._get_indices(element=element, labels=self.name)
+        if element+'.'+self.name not in net.keys():
+            net[element+'.'+self.name] = False
+        net[element+'.'+self.name][locations] = True
+        inds_new = net._get_indices(element=element, labels=self.name)
+
+        # Increase size of labels (add False at new locations)
+        labels = self.labels()
+        labels.remove(element+'.all')
+        for item in labels:
+            if item.split('.')[0] == element:
+                net[element+'.'+'blank'] = False
+                net[element+'.'+'blank'][inds_orig] = self[item]
+                self[item] = net[element+'.'+'blank'][inds_new]
+        net.pop(element+'.'+'blank', None)
 
     def plot_histograms(self,
                         throat_diameter='throat.diameter',
