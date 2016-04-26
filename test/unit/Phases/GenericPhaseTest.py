50ac7a8ad94804c9ad8691f6d4069e5b591a8584
Jeff Gostick <jeff@gostick.ca>
2016-04-26 09:22:07 -0400

    pep8 strikes again...it's always line spacing...bah!

--- test/unit/Phases/GenericPhaseTest.py
+++ test/unit/Phases/GenericPhaseTest.py
@@ -1,18 +1,19 @@
 import pytest
 import OpenPNM
 
+
 class GenericPhaseTest:
     def setup_class(self):
         self.net = OpenPNM.Network.Cubic(shape=[5, 5, 5])
 
     def test_init_w_no_network(self):
-        phase = OpenPNM.Phases.GenericPhase()
+        OpenPNM.Phases.GenericPhase()
 
     def test_init_w_components(self):
         comp1 = OpenPNM.Phases.GenericPhase(network=self.net)
         comp2 = OpenPNM.Phases.GenericPhase(network=self.net)
-        phase = OpenPNM.Phases.GenericPhase(network=self.net,
-                                            components=[comp1, comp2])
+        OpenPNM.Phases.GenericPhase(network=self.net,
+                                    components=[comp1, comp2])
 
     def test_set_component_add(self):
         comp1 = OpenPNM.Phases.GenericPhase(network=self.net)
