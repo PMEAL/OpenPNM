19f69d4e623730748dc47253fb08c68f61fa8709
jgostick <jeff@gostick.ca>
2016-04-27 15:21:13 -0400

    Update __init__.py
    updating __version__ in the __init__ file

--- OpenPNM/__init__.py
+++ OpenPNM/__init__.py
@@ -44,7 +44,7 @@ if _sys.version_info < (3, 3):
 if _sp.__version__ < '0.14.0':
     raise Exception('OpenPNM requires SciPy version 0.14.0 or greater')
 
-__version__ = '1.4.5'
+__version__ = '1.4.6'
 
 from . import Base
 from . import Network
