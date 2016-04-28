b168cab5e30d697067b54f020f00538efd0f2841
Jeff Gostick <jeff@gostick.ca>
2016-04-27 22:11:24 -0400

    Adjusting theme

--- docs/conf.py
+++ docs/conf.py
@@ -79,7 +79,7 @@ exclude_patterns = ['_build']
 # -- Options for HTML output --------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.
-html_theme = 'bootstrap'
+# html_theme = 'rtd'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
