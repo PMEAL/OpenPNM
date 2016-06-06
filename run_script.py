c98c54cced4fb0a7efac9f07a41b42bc08405384
Matthew Stadelman <stadelmanma@gmail.com>
2016-06-06 15:58:58 -0400

    Merge remote-tracking branch 'refs/remotes/origin/fixing-advance-throats' into viscous-drainage
    # Conflicts:
    #	OpenPNM/Algorithms/__ViscousDrainage__.py

--- run_script.py
+++ run_script.py
@@ -5,7 +5,7 @@ from OpenPNM.Geometry import models as gm
 #==============================================================================
 '''Build Topological Network'''
 #==============================================================================
-pn = op.Network.Cubic(shape=[5,5,5],spacing=1,name='net')
+pn = op.Network.Cubic(shape=[5,6,7],spacing=0.0001,name='net')
 pn.add_boundaries()
 
 #==============================================================================
@@ -13,12 +13,8 @@ pn.add_boundaries()
 #==============================================================================
 Ps = pn.pores('boundary',mode='not')
 Ts = pn.find_neighbor_throats(pores=Ps,mode='intersection',flatten=True)
-geom = op.Geometry.GenericGeometry(network=pn,pores=Ps,throats=Ts)
-geom['throat.length'] = 0
-geom['throat.diameter'] = 1
-geom['pore.diameter'] = 1
-geom['pore.area'] = 1
-geom['throat.area'] = 1
+geom = op.Geometry.Toray090(network=pn,pores=Ps,throats=Ts)
+geom.models.add(propname='throat.length',model=gm.throat_length.straight)
 Ps = pn.pores('boundary')
 Ts = pn.find_neighbor_throats(pores=Ps,mode='not_intersection')
 boun = op.Geometry.Boundary(network=pn,pores=Ps,throats=Ts)
@@ -71,18 +67,27 @@ alg.set_boundary_conditions(bctype='Dirichlet', bcvalue=0.6, pores=BC1_pores)
 BC2_pores = pn.pores('left_boundary')
 alg.set_boundary_conditions(bctype='Dirichlet', bcvalue=0.4, pores=BC2_pores)
 #Add new model to air's physics that accounts for water occupancy
-#phys_air.models.add(model=op.Physics.models.multiphase.conduit_conductance,
-#                    propname='throat.conduit_diffusive_conductance',
-#                    throat_conductance='throat.diffusive_conductance',
-#                    throat_occupancy='throat.occupancy',
-#                    pore_occupancy='pore.occupancy',
-#                    mode='strict',
-#                    factor=0)
+phys_air.models.add(model=op.Physics.models.multiphase.conduit_conductance,
+                    propname='throat.conduit_diffusive_conductance',
+                    throat_conductance='throat.diffusive_conductance',
+                    throat_occupancy='throat.occupancy',
+                    pore_occupancy='pore.occupancy',
+                    mode='strict',
+                    factor=0)
 #Use desired diffusive_conductance in the diffusion calculation (conductance for the dry network or water-filled network)
 alg.run(conductance='throat.diffusive_conductance')
 alg.return_results()
 Deff = alg.calc_eff_diffusivity()
-print(Deff/air['pore.diffusivity'][0])
+
+try:
+    # this creates a time step x num_pores, which is what the animated object needs
+    inv_seq = water['pore.IP_inv_seq'].squeeze()
+    history = []
+    for i in sorted(set(inv_seq)):
+      history.append( (inv_seq != 0) & (inv_seq < i) )
+
+except Exception as e:
+    pass
 
 #------------------------------------------------------------------------------
 '''Export to VTK'''
