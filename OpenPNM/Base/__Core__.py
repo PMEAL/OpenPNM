57de130812e3292baff62f601423193b54c1df97
Jeff Gostick <jeff@gostick.ca>
2016-06-22 15:51:09 -0400

    Removed automatic underscore in default object names

--- OpenPNM/Base/__Core__.py
+++ OpenPNM/Base/__Core__.py
@@ -126,7 +126,7 @@ class Core(dict):
             name = ''.join(random.choice(string.ascii_uppercase +
                                          string.ascii_lowercase +
                                          string.digits) for _ in range(5))
-            name = '_' + self.__class__.__name__ + '_' + name
+            name = self.__class__.__name__ + '_' + name
         elif self._name is not None:
             logger.info('Changing the name of '+self.name+' to '+name)
             # Check if name collides with any arrays in the simulation
