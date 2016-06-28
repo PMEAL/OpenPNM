301654aae3bcaa139322455309b7651bb6231b8b
Jeff Gostick <jeff@gostick.ca>
2016-06-28 00:11:20 -0400

    Adding unit tests mostly to satisfy Codecov

--- test/unit/Network/NetworkToolsTest.py
+++ test/unit/Network/NetworkToolsTest.py
@@ -8,6 +8,20 @@ class NetworkToolsTest:
         self.net = op.Network.Cubic(shape=[5, 5, 5], spacing=1)
         self.net['pore.diameter'] = sp.rand(self.net.Np)
 
-    def test_plot_network(self):
+    def test_plot_topology(self):
         a = op.Network.tools.plot_topology(self.net)
         assert isinstance(a, mpl.figure.Figure)
+        b = op.Network.tools.plot_topology(network=self.net, fig=a, c='r')
+        assert b is a
+        c = op.Network.tools.plot_topology(network=self.net, fig=b,
+                                           throats=[1, 2, 3], c='b')
+        assert c is b
+
+    def test_plot_coordinates(self):
+        a = op.Network.tools.plot_coordinates(self.net)
+        assert isinstance(a, mpl.figure.Figure)
+        b = op.Network.tools.plot_coordinates(network=self.net, fig=a, c='r')
+        assert b is a
+        c = op.Network.tools.plot_coordinates(network=self.net, fig=b,
+                                              pores=[1, 2, 3], c='b', s=50)
+        assert c is b
