5d7806179d455073e020bdc6e6d0e7492f4e1d9e
Tom Tranter <t.g.tranter@uwaterloo.ca>
2017-02-16 21:30:42 -0500

    Put a test on site percolation - probably needs some better tests on percolation thresholds maybe

--- test/unit/Algorithms/OrdinaryPercolationTest.py
+++ test/unit/Algorithms/OrdinaryPercolationTest.py
@@ -5,19 +5,31 @@ mgr.loglevel = 60
 
 
 class OrdinaryPercolationTest:
-    def setup_test(self):
+
+    def setup_class(self):
         self.net = op.Network.Cubic(shape=[5, 5, 5])
         self.geo = op.Geometry.Toray090(network=self.net,
                                         pores=self.net.Ps,
                                         throats=self.net.Ts)
         self.phase = op.Phases.Water(network=self.net)
         self.phys = op.Physics.Standard(network=self.net,
+                                        phase=self.phase,
                                         pores=self.net.Ps,
                                         throats=self.net.Ts)
-        self.OP = op.Algorithms.OrdinaryPercolation(network=self.net,
-                                                    invading_phase=self.phase)
-        Ps = self.net.pores(labels=['bottom_boundary'])
-        self.OP.run(inlets=Ps)
-        self.OP.return_results(Pc=7000)
-        lpf = self.OP.evaluate_late_pore_filling(Pc=8000)
+        self.OP1 = op.Algorithms.OrdinaryPercolation(network=self.net,
+                                                     invading_phase=self.phase)
+        Ps = self.net.pores(labels=['bottom'])
+        self.OP1.run(inlets=Ps)
+        self.OP1.return_results(Pc=7000)
+        lpf = self.OP1.evaluate_late_pore_filling(Pc=8000)
+        assert sp.size(lpf) == self.net.Np
+
+    def test_site_percolation(self):
+        self.OP2 = op.Algorithms.OrdinaryPercolation(network=self.net,
+                                                     invading_phase=self.phase,
+                                                     percolation_type='site')
+        Ps = self.net.pores(labels=['bottom'])
+        self.OP2.run(inlets=Ps)
+        self.OP2.return_results(Pc=7000)
+        lpf = self.OP2.evaluate_late_pore_filling(Pc=8000)
         assert sp.size(lpf) == self.net.Np
