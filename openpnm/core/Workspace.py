6aad5524b05b4b5ba31eb8fd494a6e889e813717
jgostick <jeff@gostick.ca>
2017-12-24 10:40:38 -0500

    Small tweak to workspace.settings to print nicer

--- openpnm/core/Workspace.py
+++ openpnm/core/Workspace.py
@@ -3,6 +3,7 @@ import openpnm
 import time
 import copy
 from openpnm.core import logging
+from openpnm.utils.misc import PrintableDict
 logger = logging.getLogger()
 
 
@@ -19,10 +20,17 @@ def singleton(cls):
     return getinstance
 
 
-class Settings(dict):
+class Settings():
     def __init__(self):
-        self.__dict__ = self
         self.toms_way = True
+
+    def __str__(self):
+        temp = PrintableDict(self.__dict__)
+        return temp.__str__()
+
+    def __repr__(self):
+        temp = PrintableDict(self.__dict__)
+        return temp.__str__()
 
 
 @singleton
