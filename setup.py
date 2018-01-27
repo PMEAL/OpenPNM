c8b0fac4f36cb3df87ac7d0cff38b501d08a1b3d
jgostick <jeff@gostick.ca>
2018-01-27 17:33:09 -0500

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+from distutils.util import convert_path
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+# Check Python version
+if sys.version_info < (3, 4):
+    raise Exception('openpnm requires Python 3.4 or greater to run')
+
+sys.path.append(os.getcwd())
+
+main_ = {}
+ver_path = convert_path('openpnm/__init__.py')
+with open(ver_path) as f:
+    for line in f:
+        if line.startswith('__version__'):
+            exec(line, main_)
+
+setup(
+    name='OpenPNM',
+    description = 'A framework for conducting pore network modeling simulations of multiphase transport in porous materials.',
+    version=main_['__version__'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Physics'
+    ],
+    packages=[
+        'openpnm',
+        'openpnm.core',
+        'openpnm.network',
+        'openpnm.network.models',
+        'openpnm.geometry',
+        'openpnm.geometry.models',
+        'openpnm.phases',
+        'openpnm.phases.models',
+        'openpnm.physics',
+        'openpnm.physics.models',
+        'openpnm.utils',
+        'openpnm.algorithms',
+        'openpnm.topotools'
+    ],
+    install_requires=[
+        'numpy',
+        'scipy',
+        'matplotlib',
+        'scikit-image',
+        'transforms3d',
+        'dill',
+        'pandas',
+        'pyyaml'
+    ],
+    author='OpenPNM Team',
+    author_email='jgostick@uwaterloo.ca',
+    download_url='https://github.com/pmeal/OpenPNM/',
+    url='https://github.com/pmeal/OpenPNM'
+)
