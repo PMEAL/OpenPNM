5927cbb0fbc614885ca90fd0a67c535b7636dfb2
Amin Sadeghi <amin.sadeghi@live.com>
2018-01-31 16:38:50 -0500

    Added StokesFlow algorithm.

--- openpnm/algorithms/__init__.py
+++ openpnm/algorithms/__init__.py
@@ -1,6 +1,7 @@
 from .GenericAlgorithm import GenericAlgorithm
 from .GenericLinearTransport import GenericLinearTransport
 from .Drainage import Drainage
+from .StokesFlow import StokesFlow
 from .FickianDiffusion import FickianDiffusion
 from .MIP import MIP
 from .Reaction import GenericReaction
