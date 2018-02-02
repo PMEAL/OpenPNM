8d6c49068a20baffa59801ed5e052617ed4949a8
jgostick <jeff@gostick.ca>
2018-02-01 22:07:25 -0500

    Added row and col attributes to grid, then use them in find_physics

--- openpnm/core/Simulation.py
+++ openpnm/core/Simulation.py
@@ -56,9 +56,16 @@ class Simulation(list):
                 if physics.name == self.grid[g.name][p.name]:
                     return g
 
-    def find_physics(self, geometry, phase):
-        name = self.grid[geometry.name][phase.name]
-        return self[name]
+    def find_physics(self, geometry=None, phase=None):
+        if geometry and phase:
+            name = self.grid[geometry.name][phase.name]
+            return self[name]
+        elif geometry:
+            return self.grid.row(geometry)
+        elif phase:
+            return self.grid.col(phase)
+        else:
+            raise Exception('Must specify at least one of geometry or phase')
 
     def _validate_name(self, name):
         names = [i.name for i in self]
@@ -199,6 +206,15 @@ class Grid(dict):
         return sim.network
 
     network = property(fget=_get_net)
+
+    def row(self, geometry):
+        return list(self[geometry.name].values())
+
+    def col(self, phase):
+        col = []
+        for geo in self.geometries:
+            col.append(self[geo][phase.name])
+        return col
 
     def __repr__(self):
         s = []
