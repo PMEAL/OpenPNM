e9f117e9e59afab6515700da3a703d602a34e6e1
jgostick <jeff@gostick.ca>
2018-03-05 23:28:55 -0500

    MARock tests added

---
+++ tests/unit/io/MARockTest.py
@@ -0,0 +1,41 @@
+import openpnm as op
+import pytest
+import py
+from pathlib import Path
+
+
+class MARockTest:
+
+    def setup_class(self):
+        ws = op.Workspace()
+        ws.clear()
+
+    def teardown_class(self):
+        ws = op.core.Workspace()
+        ws.clear()
+
+    def test_load_MARock(self, tmpdir):
+        path = Path(tmpdir.dirname, '../fixtures/3DMA-Castlegate')
+        project = op.io.MARock.load(path=path)
+        assert len(project) == 1
+        net = project.network
+        assert net.Np == 9915
+        assert net.Nt == 21805
+        a = {'pore.ID_number', 'pore.boundary_type', 'pore.coordination',
+             'pore.coords', 'pore.volume', 'throat.area', 'throat.conns',
+             'throat.coords'}
+        assert a.issubset(net.props())
+
+
+if __name__ == '__main__':
+    # All the tests in this file can be run with 'playing' this file
+    t = MARockTest()
+    self = t  # For interacting with the tests at the command line
+    t.setup_class()
+    for item in t.__dir__():
+        if item.startswith('test'):
+            print('running test: '+item)
+            try:
+                t.__getattribute__(item)()
+            except TypeError:
+                t.__getattribute__(item)(tmpdir=py.path.local())
