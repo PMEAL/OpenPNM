9e9fd0c75b1f620370f401634eca1dd29441f1bb
jgostick <jeff@gostick.ca>
2018-03-07 22:08:07 -0500

    adding xdmf tests

---
+++ tests/unit/io/XDMFTest.py
@@ -0,0 +1,81 @@
+import openpnm as op
+import scipy as sp
+import pytest
+from pathlib import Path
+import py
+import os
+
+
+class XDMFTest:
+
+    def setup_class(self):
+        ws = op.Workspace()
+        ws.settings['local_data'] = True
+        self.net = op.network.Cubic(shape=[2, 2, 2])
+        Ps = [0, 1, 2, 3]
+        Ts = self.net.find_neighbor_throats(pores=Ps)
+        self.geo_1 = op.geometry.GenericGeometry(network=self.net,
+                                                 pores=Ps, throats=Ts)
+        self.geo_1['pore.boo'] = 1
+        self.geo_1['throat.boo'] = 1
+        Ps = [4, 5, 6, 7]
+        Ts = self.net.find_neighbor_throats(pores=Ps, mode='intersection')
+        self.geo_2 = op.geometry.GenericGeometry(network=self.net,
+                                                 pores=Ps, throats=Ts)
+        self.geo_2['pore.boo'] = 1
+        self.geo_2['throat.boo'] = 1
+
+        self.phase_1 = op.phases.GenericPhase(network=self.net)
+        self.phase_1['pore.bar'] = 2
+        self.phase_1['throat.bar'] = 2
+        self.phase_2 = op.phases.GenericPhase(network=self.net)
+        self.phase_2['pore.bar'] = 2
+        self.phase_2['throat.bar'] = 2
+
+        self.phys_1 = op.physics.GenericPhysics(network=self.net,
+                                                phase=self.phase_1,
+                                                geometry=self.geo_1)
+        self.phys_1['pore.baz'] = 11
+        self.phys_1['throat.baz'] = 11
+
+        self.phys_2 = op.physics.GenericPhysics(network=self.net,
+                                                phase=self.phase_1,
+                                                geometry=self.geo_2)
+        self.phys_2['pore.baz'] = 12
+        self.phys_2['throat.baz'] = 12
+
+        self.phys_3 = op.physics.GenericPhysics(network=self.net,
+                                                phase=self.phase_2,
+                                                geometry=self.geo_1)
+        self.phys_3['pore.baz'] = 21
+        self.phys_3['throat.baz'] = 21
+
+        self.phys_4 = op.physics.GenericPhysics(network=self.net,
+                                                phase=self.phase_2,
+                                                geometry=self.geo_2)
+        self.phys_4['pore.baz'] = 22
+        self.phys_4['throat.baz'] = 22
+
+    def teardown_class(self):
+        ws = op.core.Workspace()
+        ws.clear()
+
+    def test_save(self, tmpdir):
+        fname = tmpdir.join('test_file')
+        op.io.XDMF.save(network=self.net, phases=self.phase_1, filename=fname)
+        os.remove(tmpdir.join('test_file.hdf'))
+        os.remove(tmpdir.join('test_file.xmf'))
+
+
+if __name__ == '__main__':
+    # All the tests in this file can be run with 'playing' this file
+    t = XDMFTest()
+    self = t  # For interacting with the tests at the command line
+    t.setup_class()
+    for item in t.__dir__():
+        if item.startswith('test'):
+            print('running test: '+item)
+            try:
+                t.__getattribute__(item)()
+            except TypeError:
+                t.__getattribute__(item)(tmpdir=py.path.local())
