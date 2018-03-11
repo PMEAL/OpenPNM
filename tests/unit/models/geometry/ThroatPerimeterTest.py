c5e5e67dd764f1bd6f84f4c6a2bc48ce57843ec9
jgostick <jeff@gostick.ca>
2018-03-10 23:07:56 -0400

    Beefing up geometry tests

--- tests/unit/models/geometry/ThroatPerimeterTest.py
+++ tests/unit/models/geometry/ThroatPerimeterTest.py
@@ -1,3 +1,43 @@
+import openpnm as op
+import openpnm.models.geometry.throat_perimeter as mods
+import numpy as np
+
+
 class ThroatPerimeterTest:
-    def test_voronoi(test):
-        pass
+    def setup_class(self):
+        self.net = op.network.Cubic(shape=[5, 5, 5], spacing=1.0)
+        self.geo = op.geometry.GenericGeometry(network=self.net,
+                                               pores=self.net.Ps,
+                                               throats=self.net.Ts)
+        self.air = op.phases.Air(network=self.net)
+        self.phys = op.physics.GenericPhysics(network=self.net,
+                                              phase=self.air,
+                                              geometry=self.geo)
+        self.geo['throat.diameter'] = 0.1
+
+    def test_cylinder(self):
+        self.geo.add_model(propname='throat.perimeter',
+                           model=mods.cylinder,
+                           regen_mode='normal')
+        a = np.array([0.31415927])
+        b = np.unique(self.geo['throat.perimeter'])
+        assert np.allclose(a, b)
+
+    def test_cube(self):
+        self.geo.add_model(propname='throat.perimeter',
+                           model=mods.cuboid,
+                           regen_mode='normal')
+        a = np.array([0.4])
+        b = np.unique(self.geo['throat.perimeter'])
+        assert np.allclose(a, b)
+
+
+if __name__ == '__main__':
+
+    t = ThroatPerimeterTest()
+    self = t
+    t.setup_class()
+    for item in t.__dir__():
+        if item.startswith('test'):
+            print('running test: '+item)
+            t.__getattribute__(item)()
