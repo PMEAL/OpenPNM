6f4280e46285c8f8d1a8c8e18c8d23e5e72632c9
jgostick <jeff@gostick.ca>
2018-04-07 17:17:56 -0400

    First attempt at vectorizing dispersion...not working

--- openpnm/algorithms/Dispersion.py
+++ openpnm/algorithms/Dispersion.py
@@ -5,9 +5,11 @@ module Dispersion: Class for solving advection diffusion
 """
 
 import scipy as sp
+from scipy.sparse.csgraph import laplacian
 from openpnm.algorithms import GenericTransport
 from openpnm.core import logging
 logger = logging.getLogger(__name__)
+
 
 class Dispersion(GenericTransport):
     r'''
@@ -15,8 +17,8 @@ class Dispersion(GenericTransport):
 
     Examples
     --------
-    >>>     
-    
+    >>>
+
     '''
     def __init__(self, settings={}, **kwargs):
         super().__init__(**kwargs)
@@ -25,7 +27,7 @@ class Dispersion(GenericTransport):
                               'conductance': 'throat.hydraulic_conductance',
                               'pressure': 'pore.pressure'})
         self.settings.update(settings)
-        
+
     def setup(self, **kwargs):
         r"""
 
@@ -37,55 +39,45 @@ class Dispersion(GenericTransport):
         """
         network = self.project.network
         phase = self.project.phases()[self.settings['phase']]
-        
-        # Get adjacancy and incidence matrices
-        adj_mat = network.create_adjacency_matrix(fmt='csr')
-        inc_mat = network.create_incidence_matrix(fmt='csr')
-        
-        K = network['throat.conns']
-        # Phase must have 'pressure' attached to it.
-        P = phase[self.settings['pressure']]
-        dp = network['pore.diameter']
+
+        # Get conns for triu and full matrix
+        pore_ij = network['throat.conns']
+        conns = sp.vstack((pore_ij, sp.flip(pore_ij, axis=1)))
+
+        # Fetch phase properties, including pressure
+        P = phase['pore.pressure']
+        D = sp.mean(phase['pore.diffusivity'])
+
+        # Fetch geometric properties for pores and throats
+        Dp = network['pore.diameter']
         Vp = network['pore.volume']
         Vt = network['throat.volume']
         Lt = network['throat.length']
-        D = sp.mean(phase['pore.diffusivity'])
-        
-        # Calculating effective length and area
-        Le = Lt + (dp[K[:,0]] + dp[K[:,1]])/2
-        Ae = (Vt + Vp[K[:,0]] + Vp[K[:,1]]) / Le
-        
-        I, J, V = [], [], []
-        for i in range(network.Np):   
-            neighbor_pores = adj_mat[i,:].nonzero()[1]
-            neighbor_throats = inc_mat[i,:].nonzero()[1]
-            
-            neighbor_conns = K[neighbor_throats]
-            neighbor_pores_w_throat_ordering = neighbor_conns[neighbor_conns!=i]
-            Pi, Pk = P[i], P[neighbor_pores_w_throat_ordering]
-            Q_ik = (Pi - Pk) * phase[self.settings['conductance']][neighbor_throats]
-            Le_ik = Le[neighbor_throats]
-            Ae_ik = Ae[neighbor_throats]
-            u_ik = Q_ik / Ae_ik
-            Pe_ik = u_ik * Le_ik / D
-     
-            # Get rid of exp overflow when calculating condunctances
-            Pe_ik[(Pe_ik < 1e-10) & (Pe_ik >= 0)] = 1e-10
-            Pe_ik[(Pe_ik > -1e-10) & (Pe_ik <= 0)] = -1e-10
-            Pe_ik[Pe_ik > 100] = 100
-            Pe_ik[Pe_ik < -100] = -100
-                
-            # Coefficients of center pore
-            val = sp.sum(-Q_ik + Q_ik / (1 - sp.exp(Pe_ik)))
-            V.append(val)
-            I.append(i)
-            J.append(i)
-            
-            # Coefficients of neighbour pores
-            val = -Q_ik / (1 - sp.exp(Pe_ik))
-            V.extend(val)
-            I.extend([i]*len(neighbor_pores))
-            J.extend(neighbor_pores_w_throat_ordering)
-        A = sp.sparse.coo_matrix((V,(I,J)),shape=(network.Np, network.Np))
+
+        # Calculating effective length and area specifically for dispersion
+        Le = Lt + sp.mean(Dp[pore_ij], axis=1)
+        Ae = (Vt + sp.sum(Vp[pore_ij], axis=1)) / Le
+
+        # Calculate conditions in each throat, for both directions
+        g = phase['throat.conductance']
+        g = sp.tile(g, 2)
+        Le_ik = sp.tile(Le, 2)
+        Ae_ik = sp.tile(Ae, 2)
+        Q_ik = g*sp.diff(P[conns], axis=1).squeeze()
+        u_ik = Q_ik / Ae_ik
+        Pe_ik = u_ik * Le_ik / D
+
+        # Condition numerical extremes in Pe_ik array
+        negs = Pe_ik < 0  # Note locations of negative numbers
+        temp = sp.absolute(Pe_ik)
+        Pe_ik = sp.clip(temp, 1e-5, 10)  # Clip large and near-zeros
+        Pe_ik[negs] = -1.0*Pe_ik[negs]  # Replace negative numbers
+
+        # Finally use Pe_ik to calculate effective conductance in each throat
+        vals = -Q_ik + Q_ik / (1 - sp.exp(Pe_ik))
+
+        # Build an adjacency matrix and pass to scipy's laplacian function
+        am = network.create_adjacency_matrix(weights=vals, fmt='coo')
+        A = laplacian(am)
         self.A = A
         return A
