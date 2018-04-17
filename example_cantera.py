1d0575b7bbed401e47c65ddf87acda90ba8dfb38
Amin Sadeghi <amin.sadeghi@live.com>
2018-04-17 05:03:22 -0400

    Add example script that uses Cantera as a pore scale model.

---
+++ example_cantera.py
@@ -0,0 +1,29 @@
+import openpnm as op
+import cantera as ct
+from openpnm.utils.misc import tic, toc
+ws = op.core.Workspace()
+
+net  = op.network.Cubic(shape = [10,10,10], spacing = 1e-6, name = 'test_net')
+proj = net.project
+geom = op.geometry.GenericGeometry(network = net, pores = net.Ps, throats = net.Ts)
+
+air = op.phases.Air(network = net)
+phys_air = op.physics.GenericPhysics(network = net, phase = air, geometry = geom)
+
+cantera = op.models.phase.thermal_conductivity.cantera
+
+print('Calculating thermal conductivity with Cantera')
+tic()
+air.add_model(propname='pore.thermal_conductivity', model=cantera,
+              cantera_phase_obj=ct.Solution('air.xml'))
+air.regenerate_models(propnames='pore.thermal_conductivity')
+toc()
+
+print()
+
+print('Calculating thermal conductivity directly')
+tic()
+water_conductivity = op.models.phase.thermal_conductivity.water
+air.add_model(propname='pore.thermal_conductivity', model=water_conductivity)
+air.regenerate_models(propnames='pore.thermal_conductivity')
+toc()
