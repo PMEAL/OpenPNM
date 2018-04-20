fb036c0f778efaa4a9b0752e404438b0ced409a9
Tom Tranter <t.g.tranter@uwaterloo.ca>
2018-04-20 10:32:49 -0400

    Add sinusoidal capillary model

--- openpnm/models/physics/capillary_pressure.py
+++ openpnm/models/physics/capillary_pressure.py
@@ -6,6 +6,36 @@ Submodule -- capillary_pressure
 """
 
 import scipy as _sp
+import sympy as syp
+import numpy as np
+import pandas as pd
+from OpenPNM.Base import logging
+logger = logging.getLogger(__name__)
+
+def _get_key_props(phase=None, diameter='throat.diameter',
+                   surface_tension='pore.surface_tension',
+                   contact_angle='pore.contact_angle'):
+    r"""
+    Many of the methods are generic to pores and throats. Some information may
+    be stored on either the pore or throat and needs to be interpolated.
+    This is a helper method to return the properties in the correct format.
+    To do:
+        Check for method to convert throat to pore data
+    """
+    element = diameter.split('.')[0]
+    if (surface_tension.split('.')[0] == 'pore' and
+       diameter.split('.')[0] == 'throat'):
+        sigma = phase[surface_tension]
+        sigma = phase.interpolate_data(data=sigma)
+    else:
+        sigma = phase[surface_tension]
+    if (contact_angle.split('.')[0] == 'pore' and
+       diameter.split('.')[0] == 'throat'):
+        theta = phase[contact_angle]
+        theta = phase.interpolate_data(data=theta)
+    else:
+        theta = phase[contact_angle]
+    return element, sigma, theta
 
 
 def washburn(target, surface_tension='pore.surface_tension',
@@ -153,3 +183,210 @@ def purcell(target, r_toroid, surface_tension='pore.surface_tension',
     else:
         value = value[phase.pores(target.name)]
     return value
+
+def sinusoidal(physics, phase, network,
+               mode='max',
+               target_Pc=None,
+               surface_tension='pore.surface_tension',
+               contact_angle='pore.contact_angle',
+               pore_diameter='pore.diameter',
+               throat_diameter='throat.diameter',
+               throat_length='throat.length',
+               **kwargs):
+    r"""
+    The profile of a throat is approximated with a sinusoidal function
+    that depends on the average of the connecting pore diameters and throat
+    diamater. It represents a converging-diverging geometry that has a minima
+    at the mid-point of the throat and produces similar behaviour to the
+    Purcell model but allows for a more slowly varying profile at the
+    ends of the throat.
+
+    Parameters
+    ----------
+    network : OpenPNM Network Object
+        The Network object is
+    phase : OpenPNM Phase Object
+        Phase object for the invading phases containing the surface tension and
+        contact angle values.
+    mode : string (Default is 'max')
+        Determines what information to send back. Options are:
+        'max' : the maximum capillary pressure along the throat axis, does not
+        'men' : return the meniscus info for a target pressure
+    target_Pc : float (Default is None)
+        The target capillary pressure for use with mode 'men'
+    surface_tension : dict key (string)
+        The dictionary key containing the surface tension values to be used. If
+        a pore property is given, it is interpolated to a throat list.
+    contact_angle : dict key (string)
+        The dictionary key containing the contact angle values to be used. If
+        a pore property is given, it is interpolated to a throat list.
+    pore_diameter : dict key (string)
+        The dictionary key containing the pore diameter values to be used.
+    throat_diameter : dict key (string)
+        The dictionary key containing the throat diameter values to be used.
+    throat_length : dict key (string)
+        The dictionary key containing the throat length values to be used.
+
+    Notes
+    -----
+    The capillary pressure equation for a sinusoidal throat is extended from
+    the Washburn equation as [1]_:
+
+    .. math::
+        P_c = -\frac{2\sigma(cos(\alpha + \theta))}{r(x)}
+
+    where alpha is:
+    .. math::
+        \alpha = arctan(\frac{dr}{dx})
+
+    References
+    ----------
+
+    .. [1] A. Forner-Cuenca et. al, Advanced Water Management in PEFCs:
+        Diffusion Layers with Patterned Wettability.
+        J. ECS. 163, 9, F1038-F1048 (2016).
+    """
+    element, sigma, theta = _get_key_props(phase=phase,
+                                           diameter=throat_diameter,
+                                           surface_tension=surface_tension,
+                                           contact_angle=contact_angle)
+    # Symbols
+    # sigma
+    s = syp.Symbol('s')
+    # theta
+    t = syp.Symbol('t')
+    # position of mensicus along throat axis, zero at center
+    x = syp.Symbol('x')
+    # pore radius
+    rp = syp.Symbol('rp')
+    # throat radius
+    rt = syp.Symbol('rt')
+    # throat lenggth
+    l = syp.Symbol('l')
+    # Pressure offset for finding minima
+    off = syp.Symbol('off')
+    # Equations
+    # Radius profile along throat length
+    y = (rp-rt)*(1-syp.cos(2*syp.pi*x/l))/2 + rt
+    # dr/dx used for filling angle
+    yprime = y.diff(x)
+    # Filling angle
+    alpha = syp.atan(yprime)
+    # Meniscus Radius of curvature
+    R = y/syp.cos(alpha+t)
+    # distance from center of curvature to meniscus contact point (Pythagoras)
+    a = syp.sqrt(R*R - y*y)
+    # angle between throat axis, meniscus center and meniscus contact point
+    gamma = syp.asin(y/R)
+    # Capillary Pressure function with target capillary pressure adjustment for
+    # root finding
+    f = -2*s*syp.cos(alpha+t)/y - off
+    # df/dx used for Newton-Raphson method for root finding
+    fprime = f.diff(x)
+    # Callable expressions
+    rx = syp.lambdify((x, rp, rt, l), y, 'numpy')
+    Pc = syp.lambdify((x, rp, rt, l, s, t, off), f, 'numpy')
+    Pc_prime = syp.lambdify((x, rp, rt, l, s, t, off), fprime, 'numpy')
+    rad_curve = syp.lambdify((x, rp, rt, l, s, t, off), R, 'numpy')
+    c2x = syp.lambdify((x, rp, rt, l, s, t, off), a, 'numpy')
+    fill_angle = syp.lambdify((x, rp, rt, l), alpha, 'numpy')
+    cap_angle = syp.lambdify((x, rp, rt, l, s, t, off), gamma, 'numpy')
+    # Network properties
+    throatLength = network[throat_length]
+    poreRad = np.mean(network[pore_diameter][network['throat.conns']], axis=1)
+    poreRad /= 2
+    throatRad = network[throat_diameter]/2
+    Nt = network.Nt
+    # Model ouputs
+    offset = np.zeros(Nt)
+    min_Pc = np.zeros(Nt)
+    max_Pc = np.zeros(Nt)
+    min_arg = np.zeros(Nt, dtype=int)
+    max_arg = np.zeros(Nt, dtype=int)
+    min_point = np.zeros(Nt)
+    max_point = np.zeros(Nt)
+
+    # Preprocessing - go along the throat length and work out min and max Pc
+    # and the position where this occurs
+    for i in range(Nt):
+        points = np.arange(-throatLength[i]/2,
+                           throatLength[i]/2,
+                           throatLength[i]/100)
+        all_Pc = Pc(points,
+                    poreRad[i],
+                    throatRad[i],
+                    throatLength[i],
+                    sigma[i],
+                    np.deg2rad(theta[i]),
+                    offset[i])
+        min_Pc[i] = np.min(all_Pc)
+        max_Pc[i] = np.max(all_Pc)
+        min_arg[i] = np.argmin(all_Pc)
+        max_arg[i] = np.argmax(all_Pc)
+        min_point[i] = points[min_arg[i]]
+        max_point[i] = points[max_arg[i]]
+    if mode == 'max':
+        return max_Pc
+    elif target_Pc is None:
+        logger.exception(msg='Please supply a target capillary pressure')
+    else:
+        pass
+    # If we got this far we're looking for the meniscus information for a
+    # target Capillary pressure
+    Pc_range = max_Pc-min_Pc
+    x_range = max_point - min_point
+
+    # Private helper functions
+    def Newton_Raphson(x0, rp, rt, l, s, t, off):
+        tol = np.ones(len(x0), dtype=float)
+        n = 0
+        n_max = 50
+        while np.any(tol[~np.isnan(tol)] > 1e-6) and n < n_max:
+            func = Pc(x0, rp, rt, l, s, t, off)
+            func_prime = Pc_prime(x0, rp, rt, l, s, t, off)
+            xn = x0 - func/func_prime
+            tol = np.abs((xn - x0)/x0)
+            x0 = xn
+            n += 1
+        return x0
+
+    def in_range(target):
+        r'''
+        Check whether the target pressure is in range for each throat
+        '''
+        return np.logical_and((target >= min_Pc), (target <= max_Pc))
+
+    def get_root(target):
+        r'''
+        Get the root between the minima and maxima
+        '''
+        # interpolated initial guess
+        x0 = min_point+(x_range*(target-min_Pc)/(Pc_range))
+        x0[~in_range(target)] = np.nan
+        # find root with function adjusted for target
+        root = Newton_Raphson(x0,
+                              poreRad,
+                              throatRad,
+                              throatLength,
+                              sigma,
+                              np.deg2rad(theta),
+                              target)
+        return root
+
+    # Now find the positions of the menisci along each throat axis
+    men_data = {}
+    pos = get_root(target_Pc)
+    men_data['pos'] = pos
+    men_data['rx'] = rx(pos, poreRad, throatRad, throatLength)
+    men_data['alpha'] = fill_angle(pos, poreRad, throatRad, throatLength)
+    men_data['beta'] = c2x(pos, poreRad, throatRad, throatLength,
+                           sigma, theta, offset)
+    men_data['gamma'] = cap_angle(pos, poreRad, throatRad, throatLength,
+                                  sigma, theta, offset)
+    men_data['rad'] = rad_curve(pos, poreRad, throatRad, throatLength,
+                                sigma, theta, offset)
+    men_data['cen'] = pos - np.sign(target_Pc)*men_data['alpha']
+    df = pd.DataFrame(men_data)
+    rec_arr = df.to_records(index=False)
+    logger.info(mode+' calculated for Pc: '+str(target_Pc))
+    return rec_arr
