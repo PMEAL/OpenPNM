01dc5c633ae5cdd7a214956584a92554691ed553
Amin Sadeghi <amin.sadeghi@live.com>
2018-04-20 06:35:38 -0400

    Fixed the circular assumption in 'diffusive_conductance.bulk_diffusion'.
    Also removed a few redundant calculations which are passed to this method as argument.

--- openpnm/models/physics/diffusive_conductance.py
+++ openpnm/models/physics/diffusive_conductance.py
@@ -39,13 +39,10 @@ def bulk_diffusion(target, molar_density='pore.molar_density',
     parea = network[pore_area]
     pdia = network[pore_diameter]
     # Get the properties of every throat
-    tdia = network[throat_diameter]
-    tarea = _sp.pi*(tdia/2)**2
+    tarea = network[throat_area]
     tlen = network[throat_length]
     # Interpolate pore phase property values to throats
-    cp = phase[molar_density]
     ct = phase.interpolate_data(propname=molar_density)
-    DABp = phase[diffusivity]
     DABt = phase.interpolate_data(propname=diffusivity)
     if calc_pore_len:
         lengths = op.utils.misc.conduit_lengths(network, mode='centroid')
