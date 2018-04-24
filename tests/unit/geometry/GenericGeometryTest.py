8683a5d64ac25009010ee04e4920b929050438f6
jgostick <jeff@gostick.ca>
2018-04-23 23:51:45 -0400

    testing histogram function

--- tests/unit/geometry/GenericGeometryTest.py
+++ tests/unit/geometry/GenericGeometryTest.py
@@ -1,24 +1,34 @@
 import openpnm as op
-import scipy as sp
-import pytest
 
 
-class BaseTest:
+class GenericGeometryTest:
 
     def setup_class(self):
         self.net = op.network.Cubic(shape=[3, 3, 3])
+        self.geo = op.geometry.StickAndBall(network=self.net,
+                                            pores=self.net.Ps,
+                                            throats=self.net.Ts)
+        self.geo.regenerate_models()
 
     def teardown_class(self):
         mgr = op.Base.Workspace()
         mgr.clear()
 
+    def test_plot_histogram(self):
+        self.geo.show_hist()
+        self.geo.show_hist(props=['pore.diameter', 'pore.volume',
+                                  'throat.length'])
+        self.geo.show_hist(props=['pore.diameter', 'pore.volume',
+                                  'throat.length', 'throat.diameter',
+                                  'pore.seed'])
+
 
 if __name__ == '__main__':
 
-    t = BaseTest()
+    t = GenericGeometryTest()
+    self = t
     t.setup_class()
     for item in t.__dir__():
         if item.startswith('test'):
             print('running test: '+item)
             t.__getattribute__(item)()
-    self = t
