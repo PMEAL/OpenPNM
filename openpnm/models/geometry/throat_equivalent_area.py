0ebaf2218b96240e8153b606870b4c8c0bac290a
Zohaib <zohaib_atiq@yahoo.com>
2018-07-03 18:09:06 -0400

    truncated_pyramid_area model

--- openpnm/models/geometry/throat_equivalent_area.py
+++ openpnm/models/geometry/throat_equivalent_area.py
@@ -33,6 +33,36 @@ def spherical_pores(target, throat_area='throat.area', pore_diameter='pore.diame
             'throat': target[throat_area],
             'pore2': d2*L2*_pi / (2*_np.arctanh(2*L2/d2))}
 
+def truncated_pyramid_area(target, throat_area='throat.area',
+                            pore_diameter='pore.diameter'):
+    r"""
+    Calculate equivalent cross-sectional area for a conduit consisting of two
+    truncated pyramid pores and a constant cross-section throat. This area can be later
+    used to calculate hydraulic or diffusive conductance of the conduit.
+
+    Parameters
+    ----------
+    target : OpenPNM Object
+        The object which this model is associated with. This controls the
+        length of the calculated array, and also provides access to other
+        necessary properties.
+
+    throat_area : string
+        Dictionary key of the throat area values
+
+    pore_diameter : string
+        Dictionary key of the pore diameter values
+
+    """
+    network = target.project.network
+    cn = network['throat.conns']
+    d1 = target[pore_diameter][cn[:, 0]]
+    d2 = target[pore_diameter][cn[:, 1]]
+    td = _np.sqrt(target[throat_area])
+    return {'pore1': d1*td,
+            'throat':target[throat_area],
+            'pore2': d2*td }
+
 
 def circular_pores(target, throat_area='throat.area', pore_diameter='pore.diameter',
                    throat_conduit_lengths='throat.conduit_lengths'):
