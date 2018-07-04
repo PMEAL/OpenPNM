69b8ef5d03fdada993f1e75577459ea483694497
Amin Sadeghi <amin.sadeghi@live.com>
2018-07-04 17:33:55 -0400

    thermal_conductance now uses new conduit_lengths and equival_area models

--- openpnm/models/physics/thermal_conductance.py
+++ openpnm/models/physics/thermal_conductance.py
@@ -11,20 +11,31 @@ import scipy as _sp
 def series_resistors(target,
                      pore_thermal_conductivity='pore.thermal_conductivity',
                      throat_thermal_conductivity='throat.thermal_conductivity',
-                     pore_diameter='pore.diameter',
-                     pore_area='pore.area',
-                     throat_area='throat.area',
-                     throat_length='throat.length'):
+                     throat_diffusivity='throat.diffusivity',
+                     throat_equivalent_area='throat.equivalent_area',
+                     throat_conduit_lengths='throat.conduit_lengths'):
     r"""
     Calculate the thermal conductance of void conduits in network ( 1/2 pore - full
     throat - 1/2 pore ) based on size (assuming cylindrical geometry)
 
     Parameters
     ----------
-    network : OpenPNM Network Object
+    target : OpenPNM Object
+        The object which this model is associated with. This controls the
+        length of the calculated array, and also provides access to other
+        necessary properties.
 
-    phase : OpenPNM Phase Object
-            The phase of interest
+    pore_thermal_conductivity : string
+        Dictionary key of the pore thermal conductivity values
+
+    throat_thermal_conductivity : string
+        Dictionary key of the throat thermal conductivity values
+
+    throat_equivalent_area : string
+        Dictionary key of the throat equivalent area values
+
+    throat_conduit_lengths : string
+        Dictionary key of the throat conduit lengths
 
     Notes
     -----
@@ -36,10 +47,19 @@ def series_resistors(target,
 
     """
     network = target.project.network
+    throats = network.map_throats(target['throat._id'])
     phase = target.project.find_phase(target)
-    # Get Nt-by-2 list of pores connected to each throat
-    Ps = network['throat.conns']
-    # Get properties in every pore in the network
+    geom = target.project.find_geometry(target)
+    cn = network['throat.conns'][throats]
+    # Getting equivalent areas
+    A1 = geom[throat_equivalent_area+'.pore1'][throats]     # Equivalent area pore 1
+    At = geom[throat_equivalent_area+'.throat'][throats]    # Equivalent area throat
+    A2 = geom[throat_equivalent_area+'.pore2'][throats]     # Equivalent area pore 2
+    # Getting conduit lengths
+    L1 = geom[throat_conduit_lengths+'.pore1'][throats]     # Equivalent length pore 1
+    Lt = geom[throat_conduit_lengths+'.throat'][throats]    # Equivalent length throat
+    L2 = geom[throat_conduit_lengths+'.pore2'][throats]     # Equivalent length pore 2
+    # Interpolate pore phase property values to throats
     try:
         kt = phase[throat_thermal_conductivity]
     except KeyError:
@@ -48,25 +68,19 @@ def series_resistors(target,
         kp = phase[pore_thermal_conductivity]
     except KeyError:
         kp = phase.interpolate_data(propname=throat_thermal_conductivity)
+    # Remove any non-positive lengths
+    L1[L1 <= 0] = 1e-12
+    L2[L2 <= 0] = 1e-12
+    Lt[Lt <= 0] = 1e-12
     # Find g for half of pore 1
-    pdia = network[pore_diameter]
-    parea = network[pore_area]
-    pdia1 = pdia[Ps[:, 0]]
-    pdia2 = pdia[Ps[:, 1]]
-    # Remove any non-positive lengths
-    pdia1[pdia1 <= 0] = 1e-12
-    pdia2[pdia2 <= 0] = 1e-12
-    gp1 = kp[Ps[:, 0]]*parea[Ps[:, 0]]/(0.5*pdia1)
-    gp1[~(gp1 > 0)] = _sp.inf  # Set 0 conductance pores (boundaries) to inf
+    gp1 = kp[cn[:, 0]] * A1 / L1
+    gp1[_sp.isnan(gp1)] = _sp.inf
+    gp1[gp1<=0] = _sp.inf  # Set 0 conductance pores (boundaries) to inf
     # Find g for half of pore 2
-    gp2 = kp[Ps[:, 1]]*parea[Ps[:, 1]]/(0.5*pdia2)
-    gp2[~(gp2 > 0)] = _sp.inf  # Set 0 conductance pores (boundaries) to inf
+    gp2 = kp[cn[:, 1]] * A2 / L2
+    gp2[_sp.isnan(gp2)] = _sp.inf
+    gp2[gp2<=0] = _sp.inf  # Set 0 conductance pores (boundaries) to inf
     # Find g for full throat
-    tarea = network[throat_area]
-    tlen = network[throat_length]
-    # Remove any non-positive lengths
-    tlen[tlen <= 0] = 1e-12
-    gt = kt*tarea/tlen
-    value = (1/gt + 1/gp1 + 1/gp2)**(-1)
-    value = value[phase.throats(target.name)]
-    return value
+    gt = kt[throats] * At / Lt
+    gt[gt<=0] = _sp.inf
+    return (1/gt + 1/gp1 + 1/gp2)**(-1)
