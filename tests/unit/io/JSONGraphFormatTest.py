e60a76bce107e8e1d320c0ed95bce9a86affc1e0
RODRIGO NEUMANN BARROS FERREIRA <rneumann@br.ibm.com>
2018-07-05 19:26:52 -0300

    Create skeleton fo `test_load()`

--- tests/unit/io/JSONGraphFormatTest.py
+++ tests/unit/io/JSONGraphFormatTest.py
@@ -1,3 +1,6 @@
+import os
+from pathlib import Path
+
 import openpnm as op
 import py
 
@@ -9,8 +12,30 @@ class JSONGraphFormatTest:
         ws.settings['local_data'] = True
 
     def teardown_class(self):
-        ws = op.core.Workspace()
+        ws = op.Workspace()
         ws.clear()
+
+    def test_load(self):
+        # Load JSON file and ensure project integrity
+        path = Path(os.path.realpath(__file__), '../../fixtures/JSONGraphFormat')
+        project = op.io.JSONGraphFormat.load(path + '/2nodes_1link.json')
+        assert len(project) == 1
+
+        # Ensure overal network properties
+        net = project.network
+        assert net.Np == 2
+        assert net.Nt == 1
+
+        # Ensure existence of throat properties
+        throat_props = {'throat.length', 'throat.conns', 'throat.diameter',
+                        'throat.area', 'throat.volume', 'throat.perimeter',
+                        'throat.surface_area'}
+        assert throat_props.issubset(net.props())
+
+        # Ensure existence of pore properties
+        pore_props = {'pore.index', 'pore.coords', 'pore.diameter',
+                      'pore.area', 'pore.volume'}
+        assert pore_props.issubset(net.props())
 
 
 if __name__ == '__main__':
