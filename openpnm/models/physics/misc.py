e41e260fc1709c84b5aa4e89e2ff7ee650a111ea
Amin Sadeghi <amin.sadeghi@live.com>
2018-07-06 08:26:18 -0400

    Removed all checks for negative throat length in conductance models
    This is no longer needed since the checks have been moved to conduit models which makes more sense to me :)

--- openpnm/models/physics/misc.py
+++ openpnm/models/physics/misc.py
@@ -42,35 +42,29 @@ def poisson_conductance(target, pore_diffusivity, throat_diffusivity,
     geom = target.project.find_geometry(target)
     cn = network['throat.conns'][throats]
     # Getting equivalent areas
-    A1 = geom[throat_equivalent_area+'.pore1'][throats]     # Equivalent area pore 1
-    At = geom[throat_equivalent_area+'.throat'][throats]    # Equivalent area throat
-    A2 = geom[throat_equivalent_area+'.pore2'][throats]     # Equivalent area pore 2
+    A1 = geom[throat_equivalent_area+'.pore1'][throats]
+    At = geom[throat_equivalent_area+'.throat'][throats]
+    A2 = geom[throat_equivalent_area+'.pore2'][throats]
     # Getting conduit lengths
-    L1 = geom[throat_conduit_lengths+'.pore1'][throats]     # Equivalent length pore 1
-    Lt = geom[throat_conduit_lengths+'.throat'][throats]    # Equivalent length throat
-    L2 = geom[throat_conduit_lengths+'.pore2'][throats]     # Equivalent length pore 2
+    L1 = geom[throat_conduit_lengths+'.pore1'][throats]
+    Lt = geom[throat_conduit_lengths+'.throat'][throats]
+    L2 = geom[throat_conduit_lengths+'.pore2'][throats]
     # Interpolate pore phase property values to throats
     try:
-        DABt = phase[throat_diffusivity]
+        Dt = phase[throat_diffusivity]
     except KeyError:
-        DABt = phase.interpolate_data(propname=pore_diffusivity)
+        Dt = phase.interpolate_data(propname=pore_diffusivity)
     try:
-        DABp = phase[pore_diffusivity]
+        Dp = phase[pore_diffusivity]
     except KeyError:
-        DABp = phase.interpolate_data(propname=throat_diffusivity)
-    # Remove any non-positive lengths
-    L1[L1 <= 0] = 1e-12
-    L2[L2 <= 0] = 1e-12
-    Lt[Lt <= 0] = 1e-12
+        Dp = phase.interpolate_data(propname=throat_diffusivity)
     # Find g for half of pore 1
-    gp1 = DABp[cn[:, 0]] * A1 / L1
+    gp1 = Dp[cn[:, 0]] * A1 / L1
     gp1[_sp.isnan(gp1)] = _sp.inf
-    gp1[gp1<=0] = _sp.inf  # Set 0 conductance pores (boundaries) to inf
     # Find g for half of pore 2
-    gp2 = DABp[cn[:, 1]] * A2 / L2
+    gp2 = Dp[cn[:, 1]] * A2 / L2
     gp2[_sp.isnan(gp2)] = _sp.inf
-    gp2[gp2<=0] = _sp.inf  # Set 0 conductance pores (boundaries) to inf
     # Find g for full throat
-    gt = DABt[throats] * At / Lt
+    gt = Dt[throats] * At / Lt
     gt[gt<=0] = _sp.inf
     return (1/gt + 1/gp1 + 1/gp2)**(-1)
