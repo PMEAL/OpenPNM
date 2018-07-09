f0d94770d08f9be709aa37d37b5146464d954e8e
jgostick <jeff@gostick.ca>
2018-07-09 15:25:06 +0800

    fixing merge mess all tests pass locally

---
+++ openpnm/__init__.py
@@ -0,0 +1,23 @@
+import logging as _logging
+
+__version__ = '2.0.0-beta'
+
+from . import utils
+from .utils import Workspace, Project
+from . import core
+from . import models
+from . import network
+from . import topotools
+from . import geometry
+from . import phases
+from . import physics
+from . import algorithms
+from . import io
+from . import materials
+
+
+# Set up logging to file - see previous section for more details
+log_format = \
+    '%(asctime)s | %(levelname)-8s | %(name)s.%(funcName)s | %(message)s'
+_logging.basicConfig(level=_logging.WARNING, format=log_format)
+del log_format
---
+++ openpnm/algorithms/__init__.py
@@ -0,0 +1,24 @@
+r"""
+**openpnm.algorithms**
+----
+The ``algorithms`` module contains classes for conducting transport simulations
+on pore networks.
+"""
+
+from .GenericAlgorithm import GenericAlgorithm
+from .GenericTransport import GenericTransport
+from .ReactiveTransport import ReactiveTransport
+from .TransientReactiveTransport import TransientReactiveTransport
+from .StokesFlow import StokesFlow
+from .FickianDiffusion import FickianDiffusion
+from .TransientFickianDiffusion import TransientFickianDiffusion
+from .AdvectionDiffusion import AdvectionDiffusion
+from .TransientAdvectionDiffusion import TransientAdvectionDiffusion
+from .Dispersion import Dispersion
+from .TransientDispersion import TransientDispersion
+from .FourierConduction import FourierConduction
+from .OhmicConduction import OhmicConduction
+from .OrdinaryPercolation import OrdinaryPercolation
+from .InvasionPercolation import InvasionPercolation
+from .MixedInvasionPercolation import MixedInvasionPercolation
+from .Porosimetry import Porosimetry
