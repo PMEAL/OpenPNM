3ef45976feb82f08e8714fddb7640efee905da9e
jgostick <jeff@gostick.ca>
2018-07-10 13:47:34 +0800

    finishing update of mapping in code

--- openpnm/models/geometry/throat_length.py
+++ openpnm/models/geometry/throat_length.py
@@ -48,7 +48,7 @@ def straight(target, pore_diameter='pore.diameter', L_negative=1e-12):
         ``None``.
     """
     network = target.project.network
-    throats = network.map_throats(target['throat._id'])
+    throats = network.map_throats(throats=target.Ts, target=target)
     cn = network['throat.conns'][throats]
     E = ctc(target, pore_diameter=pore_diameter)
     D1 = network[pore_diameter][cn[:, 0]]
@@ -83,7 +83,7 @@ def spherical_pores(target, pore_diameter='pore.diameter',
 
     """
     network = target.project.network
-    throats = network.map_throats(target['throat._id'])
+    throats = network.map_throats(throats=target.Ts, target=target)
     cn = network['throat.conns'][throats]
     d1 = network[pore_diameter][cn[:, 0]]
     d2 = network[pore_diameter][cn[:, 1]]
