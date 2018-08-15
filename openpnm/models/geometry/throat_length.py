3891405ce8cf79b31ae427f77dbeaf3aeb23f0d1
Amin Sadeghi <amin.sadeghi@live.com>
2018-08-15 14:14:41 -0400

    Modified throat_length models, added a universal one that always works

--- openpnm/models/geometry/throat_length.py
+++ openpnm/models/geometry/throat_length.py
@@ -1,4 +1,4 @@
-import scipy as _sp
+from scipy.linalg import norm as _norm
 from openpnm.utils import logging as _logging
 _logger = _logging.getLogger(__name__)
 
@@ -6,7 +6,8 @@ _logger = _logging.getLogger(__name__)
 def ctc(target, pore_diameter='pore.diameter'):
     r"""
     Calculate throat length assuming point-like pores, i.e. center-to-center
-    distance between pores.
+    distance between pores. Also, this models assumes that pores and throat
+    centroids are colinear.
 
     Parameters
     ----------
@@ -24,14 +25,13 @@ def ctc(target, pore_diameter='pore.diameter'):
     cn = network['throat.conns'][throats]
     C1 = network['pore.coords'][cn[:, 0]]
     C2 = network['pore.coords'][cn[:, 1]]
-    E = _sp.sqrt(_sp.sum((C1-C2)**2, axis=1))
-    return E
+    return _norm(C1 - C2, axis=1)
 
 
-def straight(target, pore_diameter='pore.diameter', L_negative=1e-12):
+def straight(target, throat_endpoints='throat.endpoints',
+             throat_centroid=None):
     r"""
-    Calculate throat length assuming no overlap between the throat and adjacent
-    pores.
+    Calculate throat length. See the notes for more info.
 
     Parameters
     ----------
@@ -40,154 +40,45 @@ def straight(target, pore_diameter='pore.diameter', L_negative=1e-12):
         length of the calculated array, and also provides access to other
         necessary properties.
 
-    pore_diameter : string
-        Dictionary key of the pore diameter values
+    throat_endpoints : string
+        Dictionary key of the throat endpoint values.
 
-    L_negative : float
-        The default throat length to use when negative lengths are found.  The
-        default is 1 pm.  To accept negative throat lengths, set this value to
-        ``None``.
+    throat_centroid : string
+        Dictionary key of the throat centroid values, optional. See the notes.
+
+    Returns
+    -------
+    Lt : ndarray
+        Array containing throat lengths for the given geometry.
+
+    Notes
+    -----
+    (1) By default, the model assumes that the centroids of pores and the
+    connecting throat in each conduit are colinear.
+
+    (2) If `throat_centroid` is passed, the model accounts for the extra
+    length. This could be useful for Voronoi or extracted networks.
+
     """
     network = target.project.network
     throats = network.map_throats(throats=target.Ts, origin=target)
-    cn = network['throat.conns'][throats]
-    E = ctc(target, pore_diameter=pore_diameter)
-    D1 = network[pore_diameter][cn[:, 0]]
-    D2 = network[pore_diameter][cn[:, 1]]
-    value = E-(D1+D2)/2.
-    if _sp.any(value < 0) and L_negative is not None:
-        _logger.warn('Negative throat lengths are calculated. Arbitrary ' +
-                     'positive length assigned: ' + str(L_negative))
-        Ts = _sp.where(value < 0)[0]
-        value[Ts] = L_negative
-    return value
-
-
-def spherical_pores(target, pore_diameter='pore.diameter',
-                    throat_diameter='throat.diameter', L_negative=1e-12):
-    r"""
-    Calculate throat length, asusming pores are perfect spheres.
-
-    Parameters
-    ----------
-    target : OpenPNM Object
-        The object which this model is associated with. This controls the
-        length of the calculated array, and also provides access to other
-        necessary properties.
-
-    pore_diameter : string
-        Dictionary key of the pore diameter values
-
-    throat_diameter : string
-        Dictionary key of the throat diameter values
-
-    L_negative : float
-        The default throat length to use when negative lengths are found.  The
-        default is 1 nm.  To accept negative throat lengths, set this value to
-        ``None``.
-    """
-    network = target.project.network
-    throats = network.map_throats(throats=target.Ts, origin=target)
-    cn = network['throat.conns'][throats]
-    D1 = network[pore_diameter][cn[:, 0]]
-    D2 = network[pore_diameter][cn[:, 1]]
-    Dt = network[throat_diameter][throats]
-    if _sp.any([_sp.isnan(D1), _sp.isnan(D2), _sp.isnan(Dt)]):
-        _logger.warn('Found spanner throats (spans between 2 geometries).' +
-                     ' Either the other geometry is not defined yet, or it' +
-                     ' does not have pore diameter values yet. Run' +
-                     ' regenerate_models() on both geometries to fix.')
-    L = ctc(target, pore_diameter=pore_diameter)
-    L1 = _sp.zeros_like(L)
-    L2 = _sp.zeros_like(L)
-    # Handle the case where throat diam > pore diam
-    mask = Dt > D1
-    L1[mask] = D1[mask]/2
-    L1[~mask] = _sp.sqrt(D1[~mask]**2 - Dt[~mask]**2) / 2
-    mask = Dt > D2
-    L2[mask] = D2[mask]/2
-    L2[~mask] = _sp.sqrt(D2[~mask]**2 - Dt[~mask]**2) / 2
-    # Handle throats w/ overlapping pores
-    L1_temp = (4*L**2+D1**2-D2**2) / (8*L)
-    L2_temp = (4*L**2+D2**2-D1**2) / (8*L)
-    h_temp = (2*_sp.sqrt(D1**2/4 - L1_temp**2)).real
-    mask_overlap = ((L - (D1+D2)/2) < 0) & (Dt < h_temp)
-    L1[mask_overlap] = L1_temp[mask_overlap]
-    L2[mask_overlap] = L2_temp[mask_overlap]
+    # Get throat endpoints
+    EP1 = network[throat_endpoints + '.head'][throats]
+    EP2 = network[throat_endpoints + '.tail'][throats]
     # Calculate throat length
-    Lt = L - (L1+L2)
-    if _sp.any([L1 < 0, L2 < 0, Lt < 0]) and L_negative is not None:
-        _logger.warn('Negative throat lengths are calculated. Arbitrary' +
-                     ' positive length assigned: ' + str(L_negative))
-    # Remove negative lengths
-    Lt[Lt <= 0] = L_negative
+    Lt = _norm(EP1 - EP2, axis=1)
+    # Handle the case where pores & throat centroids are not colinear
+    if throat_centroid:
+        Ct = network[throat_centroid][throats]
+        Lt = _norm(Ct - EP1, axis=1) + _norm(Ct - EP2, axis=1)
     return Lt
 
 
-def truncated_pyramid(target, pore_diameter='pore.diameter',
-                      throat_diameter='throat.diameter', L_negative=1e-12):
-    r"""
-    Calculate throat length, assuming pores are spheres that are truncated
-    to pyramids.
-
-    Parameters
-    ----------
-    target : OpenPNM Object
-        The object which this model is associated with. This controls the
-        length of the calculated array, and also provides access to other
-        necessary properties.
-
-    pore_diameter : string
-        Dictionary key of the pore diameter values
-
-    throat_diameter : string
-        Dictionary key of the throat diameter values
-
-    L_negative : float
-        The default throat length to use when negative lengths are found.  The
-        default is 1 nm.  To accept negative throat lengths, set this value to
-        ``None``.
-    """
-    return spherical_pores(target, pore_diameter=pore_diameter,
-                           throat_diameter=throat_diameter,
-                           L_negative=L_negative)
-
-
-def circular_pores(target, pore_diameter='pore.diameter',
-                   throat_diameter='throat.diameter', L_negative=1e-12):
-    r"""
-    Calculate throat length, assuming pores are perfect circles.
-
-
-    Parameters
-    ----------
-    target : OpenPNM Object
-        The object which this model is associated with. This controls the
-        length of the calculated array, and also provides access to other
-        necessary properties.
-
-    pore_diameter : string
-        Dictionary key of the pore diameter values
-
-    throat_diameter : string
-        Dictionary key of the throat diameter values
-
-    L_negative : float
-        The default throat length to use when negative lengths are found.  The
-        default is 1 nm.  To accept negative throat lengths, set this value to
-        ``None``.
-    """
-    return spherical_pores(target, pore_diameter=pore_diameter,
-                           throat_diameter=throat_diameter,
-                           L_negative=L_negative)
-
-
-def conduit_lengths(target, throat_centroid='throat.centroid',
+def conduit_lengths(target, throat_endpoints='throat.endpoints',
                     throat_length='throat.length'):
     r"""
-    Calculate conduit lengths. A conduit is basically a pore-throat-pore
-    assembly. This method returns a dictionary with the keys 'pore1', 'throat',
-    and 'pore2'.
+    Calculate conduit lengths. A conduit is defined as half pore + throat
+    + half pore.
 
     Parameters
     ----------
@@ -196,27 +87,35 @@ def conduit_lengths(target, throat_centroid='throat.centroid',
         length of the calculated array, and also provides access to other
         necessary properties.
 
-    throat_centroid : string
-        Dictionary key of the throat centroid values
+    throat_endpoints : string
+        Dictionary key of the throat endpoint values.
 
     throat_diameter : string
-        Dictionary key of the throat length values
+        Dictionary key of the throat length values.
+
+    throat_centroid : string
+        Dictionary key of the throat centroid values, optional. See the notes.
+
+    Returns
+    -------
+    CL : dictionary
+        Dictionary containing conduit lengths, which can be accessed
+        via the dict keys 'pore1', 'pore2', and 'throat'.
 
     """
     network = target.project.network
     throats = network.map_throats(throats=target.Ts, origin=target)
     cn = network['throat.conns'][throats]
-    XYZt = network[throat_centroid][throats]
-    XYZp = network['pore.coords']
-    L1 = _sp.linalg.norm(XYZp[cn[:, 0]] - XYZt, axis=1)
-    print(L1.mean())
-    L2 = _sp.linalg.norm(XYZp[cn[:, 1]] - XYZt, axis=1)
-    print(L2.mean())
-    print((L1+L2).mean())
+    # Get pore coordinates
+    C1 = network['pore.coords'][cn[:, 0]]
+    C2 = network['pore.coords'][cn[:, 1]]
+    # Get throat endpoints and length
+    EP1 = network[throat_endpoints + '.head'][throats]
+    EP2 = network[throat_endpoints + '.tail'][throats]
     Lt = network[throat_length][throats]
-    print(Lt)
-    L1 = L1 - Lt/2
-    L2 = L2 - Lt/2
+    # Calculate conduit lengths
+    L1 = _norm(C1 - EP1, axis=1)
+    L2 = _norm(C2 - EP2, axis=1)
     return {'pore1': L1, 'throat': Lt, 'pore2': L2}
 
 
