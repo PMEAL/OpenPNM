74e7bf4b023fde4cdfd97b78d95517cc8cdaa975
Amin Sadeghi <amin.sadeghi@live.com>
2018-08-17 11:42:22 -0400

    Modified Conductance models's tests to accommodate the new changes

--- tests/unit/models/physics/DiffusiveConductanceTest.py
+++ tests/unit/models/physics/DiffusiveConductanceTest.py
@@ -4,34 +4,29 @@ from numpy.testing import assert_approx_equal
 
 class DiffusiveConductanceTest:
     def setup_class(self):
-        self.net = op.network.Cubic(shape=[5, 5, 5], spacing=1.0)
+        self.net = op.network.Cubic(shape=[5, 5, 5])
         self.geo = op.geometry.GenericGeometry(network=self.net,
                                                pores=self.net.Ps,
                                                throats=self.net.Ts)
-        self.geo['pore.diameter'] = 1.0
-        self.geo['pore.area'] = 1.0
-        self.geo['throat.diameter'] = 1.0
-        self.geo['throat.length'] = 1e-9
+        self.geo['pore.area'] = 1
         self.geo['throat.area'] = 1
         self.phase = op.phases.GenericPhase(network=self.net)
-        self.phase['pore.diffusivity'] = 2.06754784e-05
+        self.phase['pore.diffusivity'] = 1
         self.phys = op.physics.GenericPhysics(network=self.net,
                                               phase=self.phase,
                                               geometry=self.geo)
 
     def test_ordinary_diffusion(self):
-        self.geo['throat.conduit_lengths.pore1'] = 0.2
+        self.geo['throat.conduit_lengths.pore1'] = 0.15
         self.geo['throat.conduit_lengths.throat'] = 0.6
-        self.geo['throat.conduit_lengths.pore2'] = 0.2
-        self.geo['throat.equivalent_area.pore1'] = 0.2
-        self.geo['throat.equivalent_area.throat'] = 0.2
-        self.geo['throat.equivalent_area.pore2'] = 0.2
+        self.geo['throat.conduit_lengths.pore2'] = 0.25
         mod = op.models.physics.diffusive_conductance.ordinary_diffusion
         self.phys.add_model(propname='throat.diffusive_conductance',
+                            pore_diffusivity='pore.diffusivity',
                             model=mod)
         self.phys.regenerate_models()
-        assert_approx_equal(4.135096e-06,
-                            self.phys['throat.diffusive_conductance'].mean())
+        actual = self.phys['throat.diffusive_conductance'].mean()
+        assert_approx_equal(actual, desired=1.0)
 
 
 if __name__ == '__main__':
--- tests/unit/models/physics/ElectricalConductanceTest.py
+++ tests/unit/models/physics/ElectricalConductanceTest.py
@@ -8,31 +8,25 @@ class ElectricalConductanceTest:
         self.geo = op.geometry.GenericGeometry(network=self.net,
                                                pores=self.net.Ps,
                                                throats=self.net.Ts)
+        self.geo['pore.area'] = 1
+        self.geo['throat.area'] = 1
         self.phase = op.phases.GenericPhase(network=self.net)
         self.phase['pore.conductivity'] = 1
         self.phys = op.physics.GenericPhysics(network=self.net,
                                               phase=self.phase,
                                               geometry=self.geo)
-        self.geo['pore.diameter'] = 1.0
-        self.geo['pore.area'] = 1.0
-        self.geo['throat.diameter'] = 1.0
-        self.geo['throat.length'] = 1e-9
-        self.geo['throat.area'] = 1
 
     def test_electrical_conductance(self):
-        self.geo['throat.conduit_lengths.pore1'] = 0.2
+        self.geo['throat.conduit_lengths.pore1'] = 0.15
         self.geo['throat.conduit_lengths.throat'] = 0.6
-        self.geo['throat.conduit_lengths.pore2'] = 0.2
-        self.geo['throat.equivalent_area.pore1'] = 0.2
-        self.geo['throat.equivalent_area.throat'] = 0.2
-        self.geo['throat.equivalent_area.pore2'] = 0.2
+        self.geo['throat.conduit_lengths.pore2'] = 0.25
         mod = op.models.physics.electrical_conductance.series_resistors
         self.phys.add_model(propname='throat.electrical_conductance',
                             pore_conductivity='pore.conductivity',
                             model=mod)
         self.phys.regenerate_models()
-        a = 0.2
-        assert_approx_equal(self.phys['throat.electrical_conductance'].mean(), a)
+        actual = self.phys['throat.electrical_conductance'].mean()
+        assert_approx_equal(actual, desired=1.0)
 
 
 if __name__ == '__main__':
