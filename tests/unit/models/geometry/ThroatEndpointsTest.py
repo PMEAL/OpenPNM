7c792edc6e130d35453e4f494302e651cca750f9
Amin Sadeghi <amin.sadeghi@live.com>
2018-08-17 15:19:18 -0400

    Added tests for throat_endpoints models

--- tests/unit/models/geometry/ThroatEndpointsTest.py
+++ tests/unit/models/geometry/ThroatEndpointsTest.py
@@ -120,6 +120,44 @@ class ThroatEndpointsTest:
         assert_allclose(EP1, desired=EP1d)
         assert_allclose(EP2, desired=EP2d)
 
+    def test_spherical_pores_with_zero_diameter(self):
+        self.geo['pore.diameter'] = np.array([0.5, 0.0])
+        self.geo['throat.diameter'] = 0.25
+        self.geo.add_model(propname='throat.endpoints',
+                           model=mods.spherical_pores,
+                           regen_mode='normal')
+        EP1 = self.geo['throat.endpoints.head'][0]
+        EP2 = self.geo['throat.endpoints.tail'][0]
+        EP1d = np.array([0, 0.2165064, 0]) + self.base
+        EP2d = np.array([0, 1, 0]) + self.base
+        assert_allclose(EP1, desired=EP1d)
+        assert_allclose(EP2, desired=EP2d)
+
+    def test_circular_pores_with_zero_diameter(self):
+        self.geo['pore.diameter'] = np.array([0.5, 0.0])
+        self.geo['throat.diameter'] = 0.25
+        self.geo.add_model(propname='throat.endpoints',
+                           model=mods.circular_pores,
+                           regen_mode='normal')
+        EP1 = self.geo['throat.endpoints.head'][0]
+        EP2 = self.geo['throat.endpoints.tail'][0]
+        EP1d = np.array([0, 0.2165064, 0]) + self.base
+        EP2d = np.array([0, 1, 0]) + self.base
+        assert_allclose(EP1, desired=EP1d)
+        assert_allclose(EP2, desired=EP2d)
+
+    def test_cubic_pores_with_zero_diameter(self):
+        self.geo['pore.diameter'] = np.array([0.5, 0.0])
+        self.geo['throat.diameter'] = 0.25
+        self.geo.add_model(propname='throat.endpoints',
+                           model=mods.cubic_pores,
+                           regen_mode='normal')
+        EP1 = self.geo['throat.endpoints.head'][0]
+        EP2 = self.geo['throat.endpoints.tail'][0]
+        EP1d = np.array([0, 0.25, 0]) + self.base
+        EP2d = np.array([0, 1, 0]) + self.base
+        assert_allclose(EP1, desired=EP1d)
+        assert_allclose(EP2, desired=EP2d)
 
 if __name__ == '__main__':
 
