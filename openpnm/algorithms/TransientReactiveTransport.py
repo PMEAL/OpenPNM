fixing residual calculation again

openpnm/algorithms/TransientReactiveTransport.py
@@ -377,18 +377,18 @@ class TransientReactiveTransport(ReactiveTransport):
         self[self.settings['quantity']] = x
         relax = self.settings['relaxation_quantity']
         # Get the absolute values of non zero elements of A and b
-        min_A = np.abs(self.A.data)
-        min_b = np.abs(self.b[np.nonzero(self.b)])
+        min_A = np.unique(np.abs(self.A.data))
+        min_b = np.unique(np.abs(self.b[np.nonzero(self.b)]))
         # min of A & b after getting rid of the possible non conducting throats
-        if min_A.size != 0:
+        if min_A.size > 1:
             min_A = min_A[min_A != min_A.min()].min()
         else:
-            min_A = 0
-        if min_b.size != 0:
+            min_A = 1
+        if min_b.size > 1:
             min_b = min_b[min_b != min_b.min()].min()
         else:
-            min_b = 0
-        ref = min(min_A, min_b)  # Reference for the residual's normalization
+            min_b = 1
+        ref = min(min_A, min_b)  # Reference for residual's normalization
         for itr in range(int(self.settings['max_iter'])):
             self[self.settings['quantity']] = x
             self._A = (self._A_t).copy()