a64a4111b3147ca9b5998cd8a411e991b6a90e1a
jgostick <jeff@gostick.ca>
2018-12-18 14:47:16 -0500

    re-adding generic_function to misc

--- openpnm/models/misc/__init__.py
+++ openpnm/models/misc/__init__.py
@@ -11,6 +11,6 @@ generating an array of random numbers.
 """
 
 from .statistical_distributions import generic_distribution, normal, weibull, random
-from .simple_equations import linear, polynomial
+from .simple_equations import linear, polynomial, generic_function
 from .basic_math import constant, product, scaled
 from .neighbor_lookups import from_neighbor_throats, from_neighbor_pores
