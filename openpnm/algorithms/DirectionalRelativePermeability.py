4cb10781a478bf69f27a28f1cccde8ca76e2b395
Nil2M <misaghian.aut@gmail.com>
2019-04-15 14:59:43 -0400

    directional relperm

---
+++ openpnm/algorithms/DirectionalRelativePermeability.py
@@ -0,0 +1,164 @@
+from openpnm.algorithms import GenericAlgorithm, StokesFlow, InvasionPercolation
+from openpnm.utils import logging
+from openpnm import models
+import numpy as np
+import openpnm
+import scipy as sp
+# import matplotlib.pyplot as plt
+logger = logging.getLogger(__name__)
+
+
+default_settings = {
+                    'mode': 'strict',
+                    'sat': [],
+                    'relperm_wp': [],
+                    'relperm_nwp':[],
+                    'perm_wp':[],
+                    'perm_nwp':[],
+                    'wp': [],
+                    'nwp': [],
+                    'invasion_seq': [],
+                    'flow_inlet': [],
+                    'flow_outlet':[]
+                    }
+class RelativePermeability(GenericAlgorithm):
+    r"""
+    A subclass of Generic Algorithm to calculate relative permeabilities of
+    fluids in a drainage process. The main roles of this subclass are to
+    implement Invasion Percolation if no invasion sequence is given as an
+    argument and to implement a method for calculating the relative
+    permeabilities of the fluids.
+    """
+    def __init__(self, settings={}, **kwargs):
+        super().__init__(**kwargs)
+        self.settings.update(default_settings)
+        self.settings.update(settings)
+
+    def setup(self, invading_phase=None, defending_phase=None, pore_invasion_sequence=None,throat_invasion_sequence=None):
+        self.settings['nwp']=invading_phase
+        self.settings['wp']= defending_phase
+        self.settings['pore_invasion_seq']=pore_invasion_sequence
+        self.settings['throat_invasion_seq']=throat_invasion_sequence
+        
+        #self._setup_ip_algs()
+        #self.run()
+               
+#    def run(self):
+#        self._setup_ip_algs()
+#        return {'ky': self.settings['relperm_wp']['y'], 'Saty': self.settings['sat']['y']}
+    #def _setup_ip_algs(self):
+
+    def set_inlets(self, pores):
+        #network=self.project.network
+        self.settings['flow_inlet'] = pores
+
+    def set_outlets(self, pores):
+        self.settings['flow_outlet'] = pores
+#        pores_out=[]
+#        for outlet_num in range(len(pores)):
+#            self['pore.outlets'] = False
+#            self['pore.outlets'][pores[outlet_num]] = True
+#            pores_out.append(self['pore.outlets'])
+#        # print('outlets are', pores_out)
+#        self['pore.outlets']= False
+#        return pores_out   
+    def setup_rp_algs(self):
+        network=self.project.network
+        Stokes_alg_wp = StokesFlow(network=network, phase=self.settings['wp'])
+        Stokes_alg_wp.set_value_BC(pores=(self.settings['flow_inlet']),values=1)
+        Stokes_alg_wp.set_value_BC(pores=(self.settings['flow_outlet']),values=0)
+        Stokes_alg_wp.run()
+        val=Stokes_alg_wp.calc_effective_permeability(inlets=self.settings['flow_inlet'], outlets=self.settings['flow_outlet'])
+        self.settings['perm_wp']=val
+        self.project.purge_object(obj=Stokes_alg_wp)
+        Stokes_alg_nwp = StokesFlow(network=network, phase=self.settings['nwp'])
+        Stokes_alg_nwp.set_value_BC(pores=self.settings['flow_inlet'],values=1)
+        Stokes_alg_nwp.set_value_BC(pores=self.settings['flow_outlet'],values=0)
+        Stokes_alg_nwp.run()
+        val=Stokes_alg_nwp.calc_effective_permeability(inlets=self.settings['flow_inlet'], outlets=self.settings['flow_outlet'])
+        self.settings['perm_nwp']=val
+        self.project.purge_object(obj=Stokes_alg_nwp)
+        
+   
+        
+
+    def IP_occ(self, Snwp):
+        net = self.project.network
+        P12 = net['throat.conns']
+            # Fetch void volume for pores and throats
+        Vp = net[self.settings['pore_volume']]
+        Vt = net[self.settings['throat_volume']]
+            # Fetch the order of filling
+        Np = self.settings['pore_invasion_seq']
+        Nt = self.settings['throat_invasion_seq']
+        # Create Nt-long mask of which pores were filled when throat was filled
+        Pinv = (Np[P12].T == Nt).T
+        # If a pore and throat filled together, find combined volume
+        Vinv = sp.vstack(((Pinv*Vp[P12]).T, Vt)).T
+        Vinv = sp.sum(Vinv, axis=1)
+        # Convert to cumulative volume filled as each throat is invaded
+        x = sp.argsort(Nt)  # Find order throats were invaded
+        Vinv_cum = np.cumsum(Vinv[x])
+        # Normalized cumulative volume filled into saturation
+        S = Vinv_cum/(Vp.sum() + Vt.sum())
+        # Find throat invasion step where Snwp was reached
+        try:
+            N = sp.where(S < Snwp)[0][-1]
+        except:
+            N = -np.inf
+        data = {'pore_occupancy': Np <= N, 'throat_occupancy': Nt <= N}
+        return data
+    
+    
+    def run(self,Snw=None):
+        network=self.project.network
+        Snwparr=[]
+        if Snw==None:
+            Snw=np.linspace(0,1,num=20) 
+        for S in Snw:
+            res1=self.IP_occ(S)
+            occ_ts=res1['throat.occupancy']
+            if np.any(occ_ts):
+                    #print(Snw)
+                    Snwparr.append(S)
+                    self.settings['nwp']['pore.occupancy'] = res1['pore_occupancy']
+                    self.settings['wp']['pore.occupancy'] = 1-res1['pore_occupancy']
+                    self.settings['nwp']['throat.occupancy'] = res1['throat_occupancy']
+                    self.settings['wp']['throat.occupancy'] = 1-res1['throat_occupancy']
+                    #phase regenerate models
+                    mode=self.settings['mode']
+                    self.settings['wp'].add_model(model=models.physics.multiphase.conduit_conductance,
+                        propname='throat.conduit_hydraulic_conductance',
+                        throat_conductance='throat.hydraulic_conductance',
+                        mode=mode)
+                    self.settings['nwp'].add_model(model=models.physics.multiphase.conduit_conductance,
+                        propname='throat.conduit_hydraulic_conductance',
+                        throat_conductance='throat.hydraulic_conductance',
+                        mode=mode)
+                    Stokes_alg_mp_wp = StokesFlow(network=network,phase=self.settings['wp'])
+                    Stokes_alg_mp_wp.setup(conductance='throat.conduit_hydraulic_conductance')
+                    Stokes_alg_mp_wp.set_value_BC(pores=self.settings['flow_inlet'],values=1)
+                    Stokes_alg_mp_wp.set_value_BC(pores=self.settings['flow_outlet'],values=0)
+                    Stokes_alg_mp_nwp = StokesFlow(network=network,phase=self.settings['nwp'])
+                    Stokes_alg_mp_nwp.set_value_BC(pores=self.settings['flow_inlet'],values=1)
+                    Stokes_alg_mp_nwp.set_value_BC(pores=self.settings['flow_outlet'],values=0)
+                    Stokes_alg_mp_nwp.setup(conductance='throat.conduit_hydraulic_conductance')
+                    Stokes_alg_mp_wp.run()
+                    Stokes_alg_mp_nwp.run()
+                    Keff_mp_wp = Stokes_alg_mp_wp.calc_effective_permeability(inlets=self.settings['flow_inlet'], outlets=self.settings['flow_outlet'])
+                    Keff_mp_nwp = Stokes_alg_mp_nwp.calc_effective_permeability(inlets=self.settings['flow_inlet'], outlets=self.settings['flow_outlet'])
+                    self.settings['relperm_wp'].append(Keff_mp_wp/self.settings['perm_wp'])
+                    self.settings['relperm_nwp'].append(Keff_mp_nwp/self.settings['perm_nwp'])
+                    self.project.purge_object(obj=Stokes_alg_mp_wp)
+                    self.project.purge_object(obj=Stokes_alg_mp_nwp)
+                    #print(Keff_mp_wp)
+                    #print(Keff_mp_nwp)
+            self.settings['sat']=Snwparr
+            print('relperm_wp',self.settings['relperm_wp'])
+            print('relperm_nwp',self.settings['relperm_nwp'])
+            print('sat',self.settings['sat'])
+            
+            
+                
+    
+
---
+++ tests/fixtures/directionalrelpermc.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  9 14:59:50 2019
+
+@author: work
+"""
+# sample code for relperm
+import openpnm as op
+import matplotlib.pyplot as plt
+import numpy as np
+pn = op.network.Cubic(shape=[20, 20, 20], spacing=0.00006)
+geom = op.geometry.StickAndBall(network=pn, pores=pn['pore.all'],
+                                throats=pn['throat.all'])
+oil = op.phases.GenericPhase(network=pn, name='oil')
+water = op.phases.GenericPhase(network=pn, name='water')
+oil['pore.viscosity']=0.547
+oil['throat.surface_tension'] = 0.072
+oil['pore.surface_tension']=0.072
+oil['pore.contact_angle']=110
+water['throat.contact_angle'] = 70
+water['pore.contact_angle'] = 70
+water['throat.surface_tension'] = 0.0483
+water['pore.surface_tension'] = 0.0483
+water['pore.viscosity']=0.4554
+mod = op.models.physics.hydraulic_conductance.hagen_poiseuille
+oil.add_model(propname='throat.hydraulic_conductance',
+              model=mod)
+oil.add_model(propname='throat.entry_pressure',
+              model=op.models.physics.capillary_pressure.washburn)
+water.add_model(propname='throat.hydraulic_conductance',
+                model=mod)
+water.add_model(propname='throat.entry_pressure',
+                model=op.models.physics.capillary_pressure.washburn)
+
+
+Finlets_init = {'x':pn.pores('left'), 
+                'y':pn.pores('front'),
+                'z': pn.pores('top')}
+Finlets=dict()
+for key in Finlets_init.keys():
+    Finlets.update({key:([Finlets_init[key][x] for x in range(0, len(Finlets_init[key]), 2)])})
+
+Foutlets_init = {'x':pn.pores('right'), 
+                   'y':pn.pores('back'),
+                   'z':pn.pores('bottom')}
+#Foutlets=dict()
+#for key in Foutlets_init.keys():
+#    Foutlets.update({key:([Foutlets_init[key][x] for x in range(0, len(Foutlets_init[key]), 2)])})
+ip = op.algorithms.InvasionPercolation(network=pn, phase=oil)
+ip.set_inlets(pores=Finlets['x'])
+#ip.set_outlets(pores=Foutlets['x'])
+ip.run()
+
+rp = op.algorithms.DirectionalRelativePermeability(network=pn)
+rp.setup(invading_phase=oil, defending_phase=water, pore_invasion_sequence=ip['pore.invasion_sequence'],
+         throat_invasion_sequence=ip['throat.invasion_sequence'])
+rp.set_inlets(pores=Finlets_init['x'])
+rp.set_outlets(pores=Foutlets_init['x'])
+rp.run()
+rp.plot_relperm_curve()
+rp.get_relperm_data
+
+
+#named_tuple_of_data = relcalc.get_krel_data()
+#fig = relcalc.plot_krel_curves()
+#named_tuple_of_fitted_exponents = relcalc.fit_krel_curves()
