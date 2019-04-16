13d14a33b5baab6bc028e563a40fd93d77a2f0ff
Nil2M <misaghian.aut@gmail.com>
2019-04-16 18:42:19 -0400

    Add 9 tensor option for the user

--- openpnm/algorithms/RelativePermeability.py
+++ openpnm/algorithms/RelativePermeability.py
@@ -3,6 +3,9 @@ from openpnm.utils import logging
 from openpnm import models
 import numpy as np
 import openpnm
+import matplotlib.pyplot as plt
+from collections import namedtuple
+
 # import matplotlib.pyplot as plt
 logger = logging.getLogger(__name__)
 
@@ -34,26 +37,48 @@ class RelativePermeability(GenericAlgorithm):
         super().__init__(**kwargs)
         self.settings.update(default_settings)
         self.settings.update(settings)
-
-    def setup(self, inv_inlets={'x': 'left', 'y': 'front', 'z': 'top'},
-                    flow_inlets={'x': 'left', 'y': 'front', 'z': 'top'},
-                    flow_outlets={'x': 'right', 'y': 'back', 'z': 'bottom'},
-                    ):
-        self.settings['BP_1']=flow_inlets
-        self.settings['BP_2']=flow_outlets
-        self.settings['inv_inlets'] = inv_inlets
-        self.settings['flow_inlets'] = flow_inlets
-        self.settings['flow_outlets'] = flow_outlets
+        
+        
+        
+        
+    def setup(self, input_vect=['xx','xy','xz','yx','yy','yz','zx','zy','zz']):
+        # sort input_vect
+        input_vect.sort()
+        self.settings['input_vect']=input_vect
+        inlet_dict={'x': 'left', 'y': 'front', 'z': 'top'}
+        outlet_dict={'x': 'right', 'y': 'back', 'z': 'bottom'}
+        for i in range(len(input_vect)):
+            inv=input_vect[i][0]
+            flow=input_vect[i][1]
+            self.settings['BP_1'].update({flow:(inlet_dict[flow])})
+            self.settings['BP_2'].update({flow:(outlet_dict[flow])})
+            self.settings['inv_inlets'].update({inv:(inlet_dict[inv])})
+            self.settings['flow_inlets'].update({flow:(inlet_dict[flow])})
+            self.settings['flow_outlets'].update({flow:(outlet_dict[flow])})
         #self._setup_ip_algs()
-        #self.run()
-               
+        self.setup_ip_algs()
+        
+        
+        
+         
+    def plot_rel_perm(self,plot_vect=self.settings['input_vect'],fig=None):
+            if fig is None:
+                fig = plt.figure()
+            plt.ylabel('Relative Permeability')
+            plt.xlabel('Snwp')
+            plt.grid(True)
+            for dim in plot_vect:
+                plots=plt.plot(self.settings['sat'][dim],self.settings['relperm_wp'][dim])
+                plt.legend(plots, ['Kr_wp'+dim])
+                plots=plt.plot(self.settings['sat'][dim],self.settings['relperm_nwp'][dim])
+                plt.legend(plots, ['Kr_nwp'+dim])
+        return fig
 #    def run(self):
 #        self._setup_ip_algs()
 #        return {'ky': self.settings['relperm_wp']['y'], 'Saty': self.settings['sat']['y']}
     #def _setup_ip_algs(self):
     def setup_ip_algs(self):
         network = self.project.network
-        print('printing now',self.settings['inv_inlets'])
         oil = openpnm.phases.GenericPhase(network=network, name='oil')
         water = openpnm.phases.GenericPhase(network=network, name='water')
         oil['pore.viscosity']=0.547
@@ -77,23 +102,21 @@ class RelativePermeability(GenericAlgorithm):
         self.settings['nwp']= oil
         self.settings['wp']= water
         # define inlet/outlets
-        Iinlets_init = {'x':network.pores(self.settings['inv_inlets']['x']), 
-                   'y':network.pores(self.settings['inv_inlets']['y']),
-                   'z':network.pores(self.settings['inv_inlets']['z'])}
+        Iinlets_init=dict()
+        for dim in self.settings['inv_inlets']:
+            Iinlets_init.update({dim:network.pores(self.settings['inv_inlets'][dim])}) 
         Iinlets=dict()
         for key in Iinlets_init.keys():
             Iinlets.update({key:([Iinlets_init[key][x] for x in range(0, len(Iinlets_init[key]), 2)])})
-            
-        Foutlets_init = {'x':network.pores(self.settings['flow_outlets']['x']), 
-                   'y':network.pores(self.settings['flow_outlets']['y']),
-                   'z':network.pores(self.settings['flow_outlets']['z'])}
+        Foutlets_init=dict()   
+        for dim in self.settings['flow_outlets']:
+            Foutlets_init.update({dim:network.pores(self.settings['flow_outlets'][dim])})
         Foutlets=dict()
         for key in Foutlets_init.keys():
             Foutlets.update({key:([Foutlets_init[key][x] for x in range(0, len(Foutlets_init[key]), 2)])})
-            
-        Finlets_init = {'x':network.pores(self.settings['flow_inlets']['x']), 
-                   'y':network.pores(self.settings['flow_inlets']['y']),
-                   'z': network.pores(self.settings['flow_inlets']['z'])}
+        Finlets_init=dict()
+        for dim in self.settings['flow_inlets']:
+            Finlets_init.update({dim:network.pores(self.settings['flow_inlets'][dim])})
         Finlets=dict()
         for key in Finlets_init.keys():
             Finlets.update({key:([Finlets_init[key][x] for x in range(0, len(Finlets_init[key]), 2)])})
@@ -103,11 +126,10 @@ class RelativePermeability(GenericAlgorithm):
                   entry_pressure='throat.entry_pressure',
                   pore_volume='pore.volume',
                   throat_volume='throat.volume')
-        Sarr=np.linspace(0,1,num=20)
-
-        for dim in self.settings['inv_inlets']:
-            relperm_wp=[]
-            relperm_nwp=[]
+        
+        K_dir=set(self.settings['flow_inlets'].keys())
+        for dim in K_dir:
+            #check for duplicate ones
             Stokes_alg_wp = StokesFlow(network=network, phase=self.settings['wp'])
             Stokes_alg_wp.set_value_BC(pores=network.pores(self.settings['BP_1'][dim]),values=1)
             Stokes_alg_wp.set_value_BC(pores=network.pores(self.settings['BP_2'][dim]),values=0)
@@ -122,11 +144,15 @@ class RelativePermeability(GenericAlgorithm):
             val=Stokes_alg_nwp.calc_effective_permeability(inlets=Finlets_init[dim], outlets=Foutlets_init[dim])
             self.settings['perm_nwp'].update({dim:val})
             self.project.purge_object(obj=Stokes_alg_nwp)
-            #print(self.settings['perm_wp'])
-            #print(self.settings['perm_wp'])
-            inv.set_inlets(pores=Iinlets[dim])
+        Sarr=np.linspace(0,1,num=10)
+        for i in range(len(self.settings['input_vect'])):
+            invasion=self.settings['input_vect'][i][0]
+            flow=self.settings['input_vect'][i][1]
+            relperm_wp=[]
+            relperm_nwp=[]
+            inv.set_inlets(pores=Iinlets[invasion])
             inv.run()
-            Snwparr =  []
+            Snwparr = []
             for Snw in Sarr:
                 res1=inv.results(Snwp=Snw)
                 #print(res1['pore.occupancy'])
@@ -150,30 +176,34 @@ class RelativePermeability(GenericAlgorithm):
                         mode=mode)
                     Stokes_alg_mp_wp = StokesFlow(network=network,phase=self.settings['wp'])
                     Stokes_alg_mp_wp.setup(conductance='throat.conduit_hydraulic_conductance')
-                    Stokes_alg_mp_wp.set_value_BC(pores=network.pores(self.settings['BP_1'][dim]),values=1)
-                    Stokes_alg_mp_wp.set_value_BC(pores=network.pores(self.settings['BP_2'][dim]),values=0)
+                    Stokes_alg_mp_wp.set_value_BC(pores=network.pores(self.settings['BP_1'][flow]),values=1)
+                    Stokes_alg_mp_wp.set_value_BC(pores=network.pores(self.settings['BP_2'][flow]),values=0)
                     Stokes_alg_mp_nwp = StokesFlow(network=network,phase=self.settings['nwp'])
-                    Stokes_alg_mp_nwp.set_value_BC(pores=network.pores(self.settings['BP_1'][dim]),values=1)
-                    Stokes_alg_mp_nwp.set_value_BC(pores=network.pores(self.settings['BP_2'][dim]),values=0)
+                    Stokes_alg_mp_nwp.set_value_BC(pores=network.pores(self.settings['BP_1'][flow]),values=1)
+                    Stokes_alg_mp_nwp.set_value_BC(pores=network.pores(self.settings['BP_2'][flow]),values=0)
                     Stokes_alg_mp_nwp.setup(conductance='throat.conduit_hydraulic_conductance')
                     Stokes_alg_mp_wp.run()
                     Stokes_alg_mp_nwp.run()
-                    Keff_mp_wp = Stokes_alg_mp_wp.calc_effective_permeability(inlets=Finlets_init[dim], outlets=Foutlets_init[dim])
-                    Keff_mp_nwp = Stokes_alg_mp_nwp.calc_effective_permeability(inlets=Finlets_init[dim], outlets=Foutlets_init[dim])
-                    relperm_wp.append(Keff_mp_wp/self.settings['perm_wp'][dim])
-                    relperm_nwp.append(Keff_mp_nwp/self.settings['perm_nwp'][dim])
+                    Keff_mp_wp = Stokes_alg_mp_wp.calc_effective_permeability(inlets=Finlets_init[flow], outlets=Foutlets_init[flow])
+                    Keff_mp_nwp = Stokes_alg_mp_nwp.calc_effective_permeability(inlets=Finlets_init[flow], outlets=Foutlets_init[flow])
+                    relperm_wp.append(Keff_mp_wp/self.settings['perm_wp'][flow])
+                    relperm_nwp.append(Keff_mp_nwp/self.settings['perm_nwp'][flow])
                     self.project.purge_object(obj=Stokes_alg_mp_wp)
                     self.project.purge_object(obj=Stokes_alg_mp_nwp)
                     #print(Keff_mp_wp)
                     #print(Keff_mp_nwp)
-                self.settings['relperm_wp'].update({dim:relperm_wp})
-                self.settings['relperm_nwp'].update({dim:relperm_nwp})
-                self.settings['sat'].update({dim:Snwparr})
-            print('relperm_wp',self.settings['relperm_wp'][dim])
-            print('relperm_nwp',self.settings['relperm_nwp'][dim])
-            #print('sat',self.settings['sat'][dim])
-            
-            
+                self.settings['relperm_wp'].update({self.settings['input_vect'][i]:relperm_wp})
+                self.settings['relperm_nwp'].update({self.settings['input_vect'][i]:relperm_nwp})
+                self.settings['sat'].update({self.settings['input_vect'][i]:Snwparr})
                 
+                
+
+            
+            
+        
+        
+        
+#    def export_rel_perm_data():
+        
     
 
