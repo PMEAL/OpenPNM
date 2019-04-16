9671b39f3da0dc2b2e5c78275bda62e2ea680f9e
jgostick <jeff@gostick.ca>
2019-04-16 15:07:20 -0400

    Trying to undo the damage

--- openpnm/models/physics/hydraulic_conductance.py
+++ openpnm/models/physics/hydraulic_conductance.py
@@ -7,7 +7,6 @@ r"""
 """
 
 import scipy as _sp
-from .misc import generic_conductance
 import numpy as np
 
 
@@ -340,3 +339,185 @@ def classic_hagen_poiseuille(target,
     gt[~(gt > 0)] = _sp.inf  # Set 0 conductance pores (boundaries) to inf
     value = (1/gt + 1/gp1 + 1/gp2)**(-1)
     return value
+
+
+def generic_conductance(target, transport_type, pore_area, throat_area,
+                        pore_diffusivity, throat_diffusivity,
+                        conduit_lengths, conduit_shape_factors, **kwargs):
+    r"""
+    Calculate the generic conductance (could be mass, thermal, electrical,
+    ionic, or hydraylic) of conduits in the network, where a conduit is
+    ( 1/2 pore - full throat - 1/2 pore ).
+
+    Parameters
+    ----------
+    target : OpenPNM Object
+        The object which this model is associated with. This controls the
+        length of the calculated array, and also provides access to other
+        necessary properties.
+
+    transport_type : string
+        Dictionary key of the transport type
+
+    pore_area : string
+        Dictionary key of the pore area values
+
+    throat_area : string
+        Dictionary key of the throat area values
+
+    pore_diffusivity : string
+        Dictionary key of the pore diffusivity values
+
+    throat_diffusivity : string
+        Dictionary key of the throat diffusivity values
+
+    conduit_lengths : string
+        Dictionary key of the conduit length values
+
+    conduit_shape_factors : string
+        Dictionary key of the conduit DIFFUSION shape factor values
+
+    Returns
+    -------
+    g : ndarray
+        Array containing conductance values for conduits in the geometry
+        attached to the given physics object.
+
+    Notes
+    -----
+    (1) This function requires that all the necessary phase properties already
+    be calculated.
+
+    (2) This function calculates the specified property for the *entire*
+    network then extracts the values for the appropriate throats at the end.
+
+    (3) This function assumes cylindrical throats with constant cross-section
+    area. Corrections for different shapes and variable cross-section area can
+    be imposed by passing the proper shape factor.
+
+    (4) shape_factor depends on the physics of the problem, i.e. diffusion-like
+    processes and fluid flow need different shape factors.
+
+    """
+    network = target.project.network
+    throats = network.map_throats(throats=target.Ts, origin=target)
+    phase = target.project.find_phase(target)
+    cn = network['throat.conns'][throats]
+    # Getting equivalent areas
+    A1 = network[pore_area][cn[:, 0]]
+    At = network[throat_area][throats]
+    A2 = network[pore_area][cn[:, 1]]
+    # Getting conduit lengths
+    L1 = network[conduit_lengths + '.pore1'][throats]
+    Lt = network[conduit_lengths + '.throat'][throats]
+    L2 = network[conduit_lengths + '.pore2'][throats]
+    # Preallocating g
+    g1, g2, gt = _sp.zeros((3, len(Lt)))
+    # Setting g to inf when Li = 0 (ex. boundary pores)
+    # INFO: This is needed since area could also be zero, which confuses NumPy
+    m1, m2, mt = [Li != 0 for Li in [L1, L2, Lt]]
+    g1[~m1] = g2[~m2] = gt[~mt] = _sp.inf
+    # Getting shape factors
+    try:
+        SF1 = phase[conduit_shape_factors+'.pore1'][throats]
+        SFt = phase[conduit_shape_factors+'.throat'][throats]
+        SF2 = phase[conduit_shape_factors+'.pore2'][throats]
+    except KeyError:
+        SF1 = SF2 = SFt = 1.0
+    # Interpolate pore phase property values to throats
+    try:
+        Dt = phase[throat_diffusivity][throats]
+    except KeyError:
+        Dt = phase.interpolate_data(propname=pore_diffusivity)[throats]
+    try:
+        D1 = phase[pore_diffusivity][cn[:, 0]]
+        D2 = phase[pore_diffusivity][cn[:, 1]]
+    except KeyError:
+        D1 = phase.interpolate_data(propname=throat_diffusivity)[cn[:, 0]]
+        D2 = phase.interpolate_data(propname=throat_diffusivity)[cn[:, 1]]
+    # Find g for half of pore 1, throat, and half of pore 2
+    pi = _sp.pi
+    if transport_type == 'flow':
+        g1[m1] = A1[m1]**2 / (8*pi*D1*L1)[m1]
+        g2[m2] = A2[m2]**2 / (8*pi*D2*L2)[m2]
+        gt[mt] = At[mt]**2 / (8*pi*Dt*Lt)[mt]
+    elif transport_type == 'flow_power_law':
+        for k, v in kwargs.items():
+            if k == 'pore_consistency':
+                pore_consistency = v
+            elif k == 'throat_consistency':
+                throat_consistency = v
+            elif k == 'pore_flow_index':
+                pore_flow_index = v
+            elif k == 'throat_flow_index':
+                throat_flow_index = v
+            elif k == 'pore_pressure':
+                pore_pressure = v
+
+        # Check if pressure field exists
+        try:
+            phase[pore_pressure]
+        except KeyError:
+            phase[pore_pressure] = 0
+        P = phase[pore_pressure]
+
+        # Interpolate pore phase property values to throats
+        try:
+            Ct = phase[throat_consistency][throats]
+        except KeyError:
+            Ct = phase.interpolate_data(propname=pore_consistency)[throats]
+        try:
+            nt = phase[throat_flow_index][throats]
+        except KeyError:
+            nt = phase.interpolate_data(propname=pore_flow_index)[throats]
+        # Interpolate throat phase property values to pores
+        try:
+            C1 = phase[pore_consistency][cn[:, 0]]
+            C2 = phase[pore_consistency][cn[:, 1]]
+        except KeyError:
+            C1 = phase.interpolate_data(propname=throat_consistency)[cn[:, 0]]
+            C2 = phase.interpolate_data(propname=throat_consistency)[cn[:, 1]]
+        try:
+            n1 = phase[pore_flow_index][cn[:, 0]]
+            n2 = phase[pore_flow_index][cn[:, 1]]
+        except KeyError:
+            n1 = phase.interpolate_data(propname=throat_flow_index)[cn[:, 0]]
+            n2 = phase.interpolate_data(propname=throat_flow_index)[cn[:, 1]]
+        # Interpolate pore pressure values to throats
+        Pt = phase.interpolate_data(propname=pore_pressure)[throats]
+
+        # Pressure differences dP
+        dP1 = _sp.absolute(P[cn[:, 0]]-Pt)
+        dP2 = _sp.absolute(P[cn[:, 1]]-Pt)
+        dPt = _sp.absolute(_sp.diff(P[cn], axis=1).squeeze())
+
+        # Apparent viscosities
+        mu1 = (dP1**(1-1/n1)[m1] * C1**(1/n1)[m1] / ((4*n1/(3*n1+1)) *
+               (2*L1/((A1/pi)**0.5))**(1-1/n1))[m1])
+
+        mu2 = (dP2**(1-1/n2)[m2] * C2**(1/n2)[m2] / ((4*n2/(3*n2+1)) *
+               (2*L2/((A2/pi)**0.5))**(1-1/n2))[m2])
+
+        mut = (dPt**(1-1/nt)[mt] * Ct**(1/nt)[mt] / ((4*nt/(3*nt+1)) *
+               (2*Lt/((At/pi)**0.5))**(1-1/nt))[mt])
+
+        # Bound the apparent viscosity
+        vis_min = 1e-08
+        vis_max = 1e+04
+        mu1[mu1 < vis_min] = vis_min
+        mu1[mu1 > vis_max] = vis_max
+        mu2[mu2 < vis_min] = vis_min
+        mu2[mu2 > vis_max] = vis_max
+        mut[mut < vis_min] = vis_min
+        mut[mut > vis_max] = vis_max
+
+        phase['throat.viscosity_eff'] = mut
+
+        g1[m1] = A1[m1]**2 / ((8*pi*L1)[m1]*mu1)
+        g2[m2] = A2[m2]**2 / ((8*pi*L2)[m2]*mu2)
+        gt[mt] = At[mt]**2 / ((8*pi*Lt)[mt]*mut)
+    else:
+        raise Exception('Unknown keyword for "transport_type", can only be' +
+                        ' "flow" or "flow_powar_law"')
+    # Apply shape factors and calculate the final conductance
+    return (1/gt/SFt + 1/g1/SF1 + 1/g2/SF2)**(-1)
