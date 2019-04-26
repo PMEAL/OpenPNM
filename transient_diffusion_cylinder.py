2a25bed5db1683764ad17f9d9b3c78f7cc47d7f1
Unknown <mehrez.agnaou@ensam.eu>
2019-04-26 11:42:20 -0400

    Add a transient simulation example

--- transient_diffusion_cylinder.py
+++ transient_diffusion_cylinder.py
@@ -3,12 +3,23 @@ import numpy as np
 
 # work space and project
 ws = op.Workspace()
-ws.settings["loglevel"] = 30
 proj = ws.new_project()
 
 # network
+h = 1e-5  # height in m
+r = 5e-5  # radius
+R = int(round(r/h, 1))  # ratio
+nh = 5  # number of pores along the height
+nr = int(R*nh)  # number of pores along 1 radius
+s = h/(nh-1)  # spacing between pores
+cylinder = op.topotools.template_cylinder_annulus(height=nh, outer_radius=nr)
 np.random.seed(7)
-net = op.network.Cubic(shape=[29, 13, 1], spacing=1e-5, project=proj)
+net = op.network.CubicTemplate(cylinder, spacing=s, project=proj)
+
+Max = net['pore.coords'][:, 2].max()
+prs = np.where(net['pore.coords'][:, 2] >= Max)[0]
+net['pore.inlet'] = net['pore.all'] * False
+net['pore.inlet'][prs] = True
 
 # geometry
 geo = op.geometry.StickAndBall(network=net, pores=net.Ps, throats=net.Ts)
@@ -19,26 +30,19 @@ phase = op.phases.Water(network=net)
 # physics
 phys = op.physics.GenericPhysics(network=net, phase=phase, geometry=geo)
 phase['pore.diffusivity'] = 2e-09
-
+phase['throat.diffusivity'] = 2e-09
 
 mod = op.models.physics.diffusive_conductance.ordinary_diffusion
 phys.add_model(propname='throat.diffusive_conductance',
                model=mod, regen_mode='normal')
 
 # algorithms: Fickian diffusion
+c_inlet = 1000  # inlet concentration mol/m3
 fd = op.algorithms.TransientFickianDiffusion(network=net, phase=phase)
-fd.set_value_BC(pores=net.pores('front'), values=0.5)
-fd.set_value_BC(pores=net.pores('back'), values=0.2)
-fd.set_IC(0.2)
-fd.setup(t_scheme='cranknicolson', t_final=100, t_output=5, t_step=1,
-         t_tolerance=1e-12)
+fd.set_value_BC(pores=net.pores('inlet'), values=c_inlet)
+fd.setup(t_final=1, t_output=0.05, t_step=0.05, t_tolerance=1e-12)
 fd.run()
 phase.update(fd.results())
-
-sfd = op.algorithms.FickianDiffusion(network=net, phase=phase)
-sfd.set_value_BC(pores=net.pores('front'), values=0.5)
-sfd.set_value_BC(pores=net.pores('back'), values=0.2)
-sfd.run()
 
 # output results to a vtk file
 phase.update(fd.results())
