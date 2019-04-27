d42f2282f5540d3951423d577143d1b07e8fc73c
jgostick <jeff@gostick.ca>
2019-04-27 11:18:08 -0400

    Increased test coverage

--- tests/unit/physics/GenericPhysicsTest.py
+++ tests/unit/physics/GenericPhysicsTest.py
@@ -48,6 +48,34 @@ class GenericPhysicsTest:
         with pytest.raises(Exception):
             phys.project.find_geometry(phys)
 
+    def test_set_phase_afer_instantiation(self):
+        phys = op.physics.GenericPhysics(network=self.net)
+        phase = op.phases.GenericPhase(network=self.net)
+        assert 'pore.' + phys.name not in phase.keys()
+        phys.set_phase(phase=phase, mode='add')
+        assert 'pore.' + phys.name in phase.keys()
+        phys.set_phase(phase=phase, mode='remove')
+        assert 'pore.' + phys.name not in phase.keys()
+
+    def test_set_geom_after_instantiation(self):
+        phase = op.phases.GenericPhase(network=self.net)
+        phys = op.physics.GenericPhysics(network=self.net, phase=phase)
+        assert phase['pore.'+phys.name].sum() == 0
+        phys.set_geometry(geometry=self.geo)
+        assert phase['pore.'+phys.name].sum() == phase.Np
+
+    def test_set_phase_and_geometry_from_different_project(self):
+        net2 = op.network.Cubic(shape=[5, 5, 5])
+        geo2 = op.geometry.GenericGeometry(network=net2)
+        phase2 = op.phases.GenericPhase(network=net2)
+        phase = op.phases.GenericPhase(network=self.net)
+        phys = op.physics.GenericPhysics(network=self.net)
+        with pytest.raises(Exception):
+            phys.set_phase(phase=phase2)
+        phys.set_phase(phase=phase)
+        with pytest.raises(Exception):
+            phys.set_geometry(geometry=geo2)
+
 
 if __name__ == '__main__':
 
