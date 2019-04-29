8cdadf7086ce3dddeb00897f18fde067d96da5f5
Bryan W. Weber <bryan.w.weber@gmail.com>
2019-04-29 14:33:52 -0400

    Add license_file metadata and other metadata
    Also ignore the temp directories from builds.

--- setup.py
+++ setup.py
@@ -63,5 +63,10 @@ setup(
     author='OpenPNM Team',
     author_email='jgostick@uwaterloo.ca',
     download_url='https://github.com/pmeal/OpenPNM/',
-    url='http://openpnm.org'
+    url='http://openpnm.org',
+    project_urls={
+        'Documentation': 'https://openpnm.readthedocs.io/en/master/',
+        'Source': 'https://github.com/PMEAL/OpenPNM',
+        'Tracker': 'https://github.com/PMEAL/OpenPNM/issues',
+    },
 )
