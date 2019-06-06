dea642a3049036298e6b143e9730cdeaaa5977f5
Unknown <mehrez.agnaou@ensam.eu>
2019-06-06 14:28:36 -0400

    pep8

--- openpnm/models/physics/thermal_conductance.py
+++ openpnm/models/physics/thermal_conductance.py
@@ -95,15 +95,15 @@ def series_resistors(target,
         Dt = phase[throat_thermal_conductivity][throats]
     except KeyError:
         Dt = phase.interpolate_data(
-                propname=pore_thermal_conductivity)[throats]
+            propname=pore_thermal_conductivity)[throats]
     try:
         D1 = phase[pore_thermal_conductivity][cn[:, 0]]
         D2 = phase[pore_thermal_conductivity][cn[:, 1]]
     except KeyError:
         D1 = phase.interpolate_data(
-                propname=throat_thermal_conductivity)[cn[:, 0]]
+            propname=throat_thermal_conductivity)[cn[:, 0]]
         D2 = phase.interpolate_data(
-                propname=throat_thermal_conductivity)[cn[:, 1]]
+            propname=throat_thermal_conductivity)[cn[:, 1]]
     # Find g for half of pore 1, throat, and half of pore 2
     g1[m1] = (D1*A1)[m1] / L1[m1]
     g2[m2] = (D2*A2)[m2] / L2[m2]
