09752ed81f750973e39e3cc7c574063d9d653522
Amin Sadeghi <amin.sadeghi@live.com>
2019-09-17 14:05:42 -0400

    PEP8 fix

--- example_continuity_BC.py
+++ example_continuity_BC.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-## Example 1
+# Example 1
 net = op.network.Cubic(shape=[10, 1, 1])
 phase = op.phases.Air(network=net)
 phase["throat.diffusive_conductance"] = 1.0
@@ -13,16 +13,17 @@ fd.set_value_BC(pores=0, values=1.0)
 fd.set_value_BC(pores=9, values=0.0)
 fd.set_continuity_BC(ps1=4, ps2=5, K12=2.0)
 fd.run()
+plt.figure()
 plt.plot(fd["pore.concentration"], "ko:")
 plt.xlabel("x (m)")
 plt.ylabel("c (mol/m3)")
 
-## Example 2
+# Example 2
 net = op.network.Cubic(shape=[10, 10, 1])
 xyz = net["pore.coords"]
 air = op.phases.Air(network=net)
 air["throat.diffusive_conductance"] = 1.0
-Ps = xyz[:,1] < 5
+Ps = xyz[:, 1] < 5
 net.set_label("water", pores=Ps)
 net.set_label("air", pores=~Ps)
 Ts = net.find_neighbor_throats(Ps)
@@ -31,13 +32,14 @@ air["throat.diffusive_conductance"][Ts] = 1.0
 fd = op.algorithms.FickianDiffusion(network=net, phase=air)
 fd.set_value_BC(pores=net.pores(["front", "air"], mode="and"), values=1.0)
 fd.set_value_BC(pores=net.pores(["back", "air"], mode="and"), values=0.0)
-water_int = (xyz[:,1] < 5) & (xyz[:,1] > 4)
-air_int = (xyz[:,1] > 5) & (xyz[:,1] < 6)
+water_int = (xyz[:, 1] < 5) & (xyz[:, 1] > 4)
+air_int = (xyz[:, 1] > 5) & (xyz[:, 1] < 6)
 fd.set_continuity_BC(ps2=air_int, ps1=water_int, K12=0.5)
 fd.run()
 
 air.update(fd.results())
 c = fd["pore.concentration"]
+plt.figure()
 plt.imshow(np.rot90(c.reshape(10, 10)))
 plt.colorbar()
 op.io.XDMF.save(net, phases=air, filename="network")
