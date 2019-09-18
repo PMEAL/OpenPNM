78d7e0cb3cdca40f2b00531e3902c33b17d0912f
Amin Sadeghi <amin.sadeghi@live.com>
2019-09-18 16:24:13 -0400

    Fixed typo in example_Knudsen.py

--- example_Knudsen.py
+++ example_Knudsen.py
@@ -9,6 +9,8 @@ net = op.network.Cubic(shape=[10, 10, 10], spacing=spacing)
 geom = op.geometry.StickAndBall(network=net)
 air = op.phases.Air(network=net)
 phys = op.physics.Standard(network=net, geometry=geom, phase=air)
+odiff = op.models.physics.diffusive_conductance.ordinary_diffusion
+phys.add_model(propname="throat.diffusive_conductance", model=odiff)
 fd = op.algorithms.FickianDiffusion(network=net, phase=air)
 fd.set_value_BC(pores=net.pores("left"), values=1.0)
 fd.set_value_BC(pores=net.pores("right"), values=0.0)
@@ -19,7 +21,7 @@ Mdot = fd.rate(pores=net.pores("left")).squeeze()
 Deff0 =  Mdot * L / A
 
 # Get Deff w/ including Knudsen effect
-mdiff = op.models.physics.diffusive_conductance.mixed_diffusivity
+mdiff = op.models.physics.diffusive_conductance.mixed_diffusion
 phys.add_model(propname="throat.diffusive_conductance", model=mdiff)
 spacings = np.linspace(1e-9, 1e-4, 20)
 spacings = np.logspace(-9, -3, 25)
