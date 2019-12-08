4d7451b5c805809184aa9792e9cbf8ff0e1cee80
jgostick <jgostick@uwaterloo.ca>
2019-12-08 00:16:38 -0500

    Using pytest-notebook module for testing on extractions folder.  If successful, will convert all other notebooks tests too

--- tests/integration/examples/ExtractionsTest.py
+++ tests/integration/examples/ExtractionsTest.py
@@ -1,63 +1,20 @@
 import os
-import subprocess
-import logging
 import openpnm as op
+from pytest_notebook.nb_regression import NBRegressionFixture
+
+fixture = NBRegressionFixture(exec_timeout=100)
+fixture.diff_color_words = False
 
 rootdir = os.path.split(os.path.split(op.__file__)[0])[0]
-examples_dir = os.path.join(rootdir, 'examples')
+examples_dir = os.path.join(rootdir, r'examples')
 test_dir = os.path.join(examples_dir, 'extractions')
 
+skip = ['Working With Extracted Networks.ipynb']
 
-class ExtractionsTest():
-
-    def setup_class(self):
-        pass
-
-    def _run_shell_command(self, command_line_args):
-        try:
-            proc = subprocess.run(command_line_args, timeout=600)
-        except (OSError, subprocess.CalledProcessError) as exception:
-            logging.info('Exception occured: ' + str(exception))
-            logging.info('Subprocess failed')
-            return False
+for item in os.listdir(test_dir):
+    if item.endswith('ipynb'):
+        if item in skip:
+            pass
         else:
-            # no exception was raised
-            logging.info('Subprocess finished')
-        return proc.returncode == 0
-
-    def _notebook_run(self, path):
-        """Execute a notebook via nbconvert and collect output.
-           :returns (parsed nb object, execution errors)
-        """
-        dirname, __ = os.path.split(path)
-        args = ["jupyter", "nbconvert", "--to", "notebook", "--execute",
-                "--ExecutePreprocessor.timeout=360",
-                "--output", "temp_output.ipynb", path]
-        rc = self._run_shell_command(args)
-        print(path, rc)
-        print('-'*30)
-        if rc:
-            os.remove(os.path.join(dirname, "temp_output.ipynb"))
-        return rc
-
-    def test_Imported_Geometry(self):
-        fname = 'Managing Geometrical Properties of Imported Networks.ipynb'
-        nbook = os.path.join(test_dir, fname)
-        rc = self._notebook_run(nbook)
-        assert rc
-
-    def test_Working_With_Extracted_Networks(self):
-        fname = 'Working With Extracted Networks.ipynb'
-        nbook = os.path.join(test_dir, fname)
-        rc = self._notebook_run(nbook)
-        assert rc
-
-
-if __name__ == '__main__':
-    t = ExtractionsTest()
-    self = t
-    t.setup_class()
-    for item in t.__dir__():
-        if item.startswith('test'):
-            print('running test: '+item)
-            t.__getattribute__(item)()
+            nbook = os.path.join(test_dir, item)
+            fixture.check(str(nbook))
