eece726b6e9edf01f01b11117c8789d5d7f5eedd
jgostick <jgostick@uwaterloo.ca>
2019-12-08 19:24:29 -0500

    Now it runs properly with pytest script.

--- tests/integration/examples/ExtractionsTest.py
+++ tests/integration/examples/ExtractionsTest.py
@@ -6,17 +6,19 @@ fixture = NBRegressionFixture(exec_timeout=100)
 fixture.diff_color_words = False
 
 rootdir = os.path.split(os.path.split(op.__file__)[0])[0]
-examples_dir = os.path.join(rootdir, r'examples')
+examples_dir = os.path.join(rootdir, 'examples')
 test_dir = os.path.join(examples_dir, 'extractions')
 
 skip = ["Working With Extracted Networks.ipynb",
         "Managing Geometrical Properties of Imported Networks.ipynb"]
 skip = []
 
-for item in os.listdir(test_dir):
-    if item.endswith('ipynb'):
-        if item in skip:
-            pass
-        else:
-            nbook = os.path.join(test_dir, item)
-            fixture.check(str(nbook))
+
+def test_all_notebooks_in_folder(test_dir=test_dir, skip=skip):
+    for item in os.listdir(test_dir):
+        if item.endswith('ipynb'):
+            if item in skip:
+                pass
+            else:
+                nbook = os.path.join(test_dir, item)
+                fixture.check(str(nbook))
