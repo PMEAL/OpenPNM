5e05ad9f35cf4b3b7eeebe14e46d3effa42b8cbb
jgostick <jgostick@uwaterloo.ca>
2019-12-09 01:04:43 -0500

    Not this again :-(

--- tests/integration/examples/ExtractionsTest.py
+++ tests/integration/examples/ExtractionsTest.py
@@ -12,25 +12,27 @@ rootdir = os.path.split(os.path.split(op.__file__)[0])[0]
 examples_dir = os.path.join(rootdir, 'examples')
 test_dir = os.path.join(examples_dir, 'extractions')
 skip = []
-# nbooks = []
-# for item in os.listdir(test_dir):
-#     if item.endswith('ipynb'):
-#         if item in skip:
-#             pass
-#         else:
-#             nbooks.append(os.path.join(test_dir, item))
-#
-#
-# @pytest.mark.parametrize("notebook", nbooks)
-# def test_notebook(notebook):
-#     fixture.check(str(notebook))
 
 
-def test_all_notebooks_in_folder(test_dir=test_dir, skip=skip):
-    for item in os.listdir(test_dir):
-        if item.endswith('ipynb'):
-            if item in skip:
-                pass
-            else:
-                nbook = os.path.join(test_dir, item)
-                fixture.check(str(nbook))
+nbooks = []
+for item in os.listdir(test_dir):
+    if item.endswith('ipynb'):
+        if item in skip:
+            pass
+        else:
+            nbooks.append(os.path.join(test_dir, item))
+
+
+@pytest.mark.parametrize("notebook", nbooks)
+def test_notebook(notebook):
+    fixture.check(str(notebook))
+
+
+# def test_all_notebooks_in_folder(test_dir=test_dir, skip=skip):
+#     for item in os.listdir(test_dir):
+#         if item.endswith('ipynb'):
+#             if item in skip:
+#                 pass
+#             else:
+#                 nbook = os.path.join(test_dir, item)
+#                 fixture.check(str(nbook))
