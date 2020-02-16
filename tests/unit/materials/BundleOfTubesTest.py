c55c441c4785b3dbeffff1df0c70f440cf3dc36e
jgostick <jgostick@uwaterloo.ca>
2020-02-15 22:43:26 -0500

    Fixed BundleOfTubes tests, now only expecting 2 objects in project instead of 3

--- tests/unit/materials/BundleOfTubesTest.py
+++ tests/unit/materials/BundleOfTubesTest.py
@@ -10,7 +10,7 @@ class BundleOfTubesTest:
         pass
 
     def test_instantiate_with_defaults(self):
-        net, geo, phase = BundleOfTubes(shape=[30, 30])
+        net, geo = BundleOfTubes(shape=[30, 30])
         assert net.Np == 1800
         assert net.Nt == (net.Np/2)
 
@@ -35,25 +35,25 @@ class BundleOfTubesTest:
             proj = BundleOfTubes(shape=30, spacing=[0.001, 0.001], length=0.01)
 
     def test_instantiate_with_settings(self):
-        net, geo, phase = BundleOfTubes(shape=30, spacing=0.001, length=0.01,
-                                        psd_params={'distribution': 'normal',
-                                                    'loc': 0.02,
-                                                    'scale': 2},
-                                        settings={'adjust_psd': 'clip'})
+        net, geo = BundleOfTubes(shape=30, spacing=0.001, length=0.01,
+                                 psd_params={'distribution': 'normal',
+                                             'loc': 0.02,
+                                             'scale': 2},
+                                 settings={'adjust_psd': 'clip'})
         assert geo['throat.size_distribution'].max() > geo['throat.diameter'].max()
 
-        net, geo, phase = BundleOfTubes(shape=30, spacing=0.001, length=0.01,
-                                        psd_params={'distribution': 'norm',
-                                                    'loc': 0.02,
-                                                    'scale': 0.001},
-                                        settings={'adjust_psd': 'clip'})
+        net, geo = BundleOfTubes(shape=30, spacing=0.001, length=0.01,
+                                 psd_params={'distribution': 'norm',
+                                             'loc': 0.02,
+                                             'scale': 0.001},
+                                 settings={'adjust_psd': 'clip'})
         assert geo['throat.size_distribution'].max() > geo['throat.diameter'].max()
 
-        net, geo, phase = BundleOfTubes(shape=30, spacing=1.0, length=0.01,
-                                        psd_params={'distribution': 'normal',
-                                                    'loc': 0.02,
-                                                    'scale': 0.001},
-                                        settings={'adjust_psd': None})
+        net, geo = BundleOfTubes(shape=30, spacing=1.0, length=0.01,
+                                 psd_params={'distribution': 'normal',
+                                             'loc': 0.02,
+                                             'scale': 0.001},
+                                 settings={'adjust_psd': None})
         assert sp.all(geo['throat.size_distribution'] == geo['throat.diameter'])
 
 
