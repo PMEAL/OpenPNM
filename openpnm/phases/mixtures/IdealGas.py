d6866d1d303a60f33775d142e6e10850bead49f4
jgostick <jgostick@uwaterloo.ca>
2020-03-19 10:50:55 -0400

    Adding IdealGas class to mixtures submodule

---
+++ openpnm/phases/mixtures/IdealGas.py
@@ -0,0 +1,33 @@
+# from collections import ChainMap  # Might use eventually
+import numpy as np
+from openpnm.phases.mixtures import GenericMixture, species
+import openpnm.models as mods
+from openpnm.utils import logging, HealthDict, PrintableList
+logger = logging.getLogger(__name__)
+
+
+class IdealGas(GenericMixture):
+    r"""
+    Creates Mixture object that represents a ideal gas system
+    consisting of a given list of OpenPNM Phase objects as components.
+
+    Parameters
+    ----------
+    components : list of OpenPNM Phase objects
+        A list of all components that constitute this mixture
+
+    """
+
+    def __init__(self, components=[], settings={}, **kwargs):
+        self.settings.update({'components': [],
+                              })
+        super().__init__(settings={'prefix': 'mix'}, **kwargs)
+        self.settings.update(settings)
+
+        self.add_model(propname='pore.molar_mass',
+                       model=mods.phases.mixtures.mole_weighted_average,
+                       prop='pore.molecular_weight',
+                       regen_mode='deferred')
+
+        self.add_model(propname='pore.molar_density',
+                       model=mods.phases.molar_density.ideal_gas)
--- openpnm/phases/mixtures/__init__.py
+++ openpnm/phases/mixtures/__init__.py
@@ -1,6 +1,7 @@
 from .GenericMixture import GenericMixture
 from .GenericSpecies import GenericSpecies
 from . import species
+from .IdealGas import IdealGas
 from .SalineWater import SalineWater
 from .DryAir import DryAir
 from .HumidAir import HumidAir
