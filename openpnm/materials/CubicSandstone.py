c45cf425927394748730ffbf658238bc334bae1a
jgostick <jgostick@uwaterloo.ca>
2020-03-29 00:03:15 -0400

    Adding data to class defn

--- openpnm/materials/CubicSandstone.py
+++ openpnm/materials/CubicSandstone.py
@@ -7,87 +7,233 @@ logger = logging.getLogger(__name__)
 
 
 class SandstoneParams():
-    r"""
-    The following parameters are taken from the paper by Ioannidis and Chatzis,
-    Chemical Engineering Science, 1992.
-    """
-    berea_108 = {'alpha_t': 1.904e-6,
+    whetstone = {'sandstone': 'whetstone',
+                 'alpha_t': 0.178e-6,
+                 'alpha_b': 0.221e-6,
+                 'beta_t': 0.536,
+                 'beta_b': 1.18e-6,
+                 'bmin_b': 7.1e-6,
+                 'bmax_b': 47.5e-6,
+                 'bmin_t': 0.0,
+                 'bmax_t': 47.5e-6,
+                 'lattice_constant': 37.0e-6}
+    bandera = {'sandstone': 'bandera',
+               'alpha_t': 0.329e-6,
+               'alpha_b': 5.097e-6,
+               'beta_t': 0.536,
+               'beta_b': 1.18,
+               'bmin_b': 14.1e-6,
+               'bmax_b': 45.8e-6,
+               'bmin_t': 0.0,
+               'bmax_t': 45.8e-6,
+               'lattice_constant': 73.3e-6}
+    torpedo = {'sandstone': 'torpedo',
+               'alpha_t': 0.755e-6,
+               'alpha_b': 2.194e-6,
+               'beta_t': 0.536,
+               'beta_b': 1.18,
+               'bmin_b': 10.6e-6,
+               'bmax_b': 45.8e-6,
+               'bmin_t': 0.0,
+               'bmax_t': 45.8e-6,
+               'lattice_constant': 54.6e-6}
+    clear_creek = {'sandstone': 'clear_creek',
+                   'alpha_t': 0.780e-6,
+                   'alpha_b': 3.865e-6,
+                   'beta_t': 0.536,
+                   'beta_b': 1.18,
+                   'bmin_b': 21.1e-6,
+                   'bmax_b': 42.3e-6,
+                   'bmin_t': 0.0,
+                   'bmax_t': 42.3e-6,
+                   'lattice_constant': 92.3e-6}
+    cottage_grove = {'sandstone': 'cottage_grove',
+                     'alpha_t': 0.780e-6,
+                     'alpha_b': 3.166e-6,
+                     'beta_t': 0.536,
+                     'beta_b': 1.18,
+                     'bmin_b': 14.1e-6,
+                     'bmax_b': 49.3e-6,
+                     'bmin_t': 0.0,
+                     'bmax_t': 49.3e-6,
+                     'lattice_constant': 68.6e-6}
+    big_clifty = {'sandstone': 'big_clifty',
+                  'alpha_t': 1.137e-6,
+                  'alpha_b': 1.870e-6,
+                  'beta_t': 0.536,
+                  'beta_b': 1.18,
+                  'bmin_b': 14.1e-6,
+                  'bmax_b': 42.3e-6,
+                  'bmin_t': 0.0,
+                  'bmax_t': 42.3e-6,
+                  'lattice_constant': 71.1e-6}
+    st_meinrad = {'sandstone': 'st_meinrad',
+                  'alpha_t': 1.269e-6,
+                  'alpha_b': 3.903e-6,
+                  'beta_t': 0.536,
+                  'beta_b': 1.18,
+                  'bmin_b': 14.1e-6,
+                  'bmax_b': 49.3e-6,
+                  'bmin_t': 0.0,
+                  'bmax_t': 49.3e-6,
+                  'lattice_constant': 69.6e-6}
+    noxie_129 = {'sandstone': 'noxie_129',
+                 'alpha_t': 0.718e-6,
+                 'alpha_b': 2.935e-6,
+                 'beta_t': 0.536,
+                 'beta_b': 1.18,
+                 'bmin_b': 12.3e-6,
+                 'bmax_b': 49.3e-6,
+                 'bmin_t': 0.0,
+                 'bmax_t': 49.3e-6,
+                 'lattice_constant': 73.3e-6}
+    noxie_47 = {'sandstone': 'noxie_47',
+                'alpha_t': 1.842e-6,
+                'alpha_b': 4.600e-6,
+                'beta_t': 0.536,
+                'beta_b': 1.18,
+                'bmin_b': 19.4e-6,
+                'bmax_b': 52.8e-6,
+                'bmin_t': 0.0,
+                'bmax_t': 52.8e-6,
+                'lattice_constant': 87.0e-6}
+    bartlesville = {'sandstone': 'bartlesville',
+                    'alpha_t': 2.252e-6,
+                    'alpha_b': 4.251e-6,
+                    'beta_t': 0.536,
+                    'beta_b': 1.18,
+                    'bmin_b': 19.0e-6,
+                    'bmax_b': 44.1e-6,
+                    'bmin_t': 0.0,
+                    'bmax_t': 44.1e-6,
+                    'lattice_constant': 72.9e-6}
+    berea_BE1 = {'sandstone': 'berea_BE1',
+                 'alpha_t': 1.697e-6,
+                 'alpha_b': 8.333e-6,
+                 'beta_t': 0.536,
+                 'beta_b': 1.18,
+                 'bmin_b': 14.1e-6,
+                 'bmax_b': 42.3e-6,
+                 'bmin_t': 0.0,
+                 'bmax_t': 42.3e-6,
+                 'lattice_constant': 73.9e-6}
+    berea_108 = {'sandstone': 'berea_108',
+                 'alpha_t': 1.904e-6,
                  'alpha_b': 6.081e-6,
                  'beta_t': 0.536,
                  'beta_b': 1.18,
-                 'lattice_constant': 125.6e-6,
-                 'b_min': 24.6e-6,
-                 'b_max': 70.4e-6,
-                 'b_vo': 34.7e-6,
-                 'pore_shape': 1.18,  # shape is beta_b
-                 'pore_scale': 6.08e-6,  # scale is alpha_b
-                 'pore_loc': 24.6e-6,  # loc is b_min
-                 'throat_shape': 0.536,  # shape is beta_t
-                 'throat_scale': 1.904e-6,  # scale is alpha_t
-                 'throat_loc': 0.7e-6}  # loc is ??
-    # f = (beta_b/alpha_b)*((x - b_min)/alpha_b)**(beta_b-1)
-    # n1 = exp(-((x-b_min)/alpha_b)**(beta_b))
-    # n2 = exp(-((b_max-b_min)/alpha_b)**(beta_b))
-    # d = 1 - exp(-((b_max-b_min)/alpha_b)**(beta_b))
-    # F = (n1 - n2)/d
-    boise = {'alpha_t': 13.050e-6,
-             'alpha_b': 8.333e-6,
+                 'bmin_b': 24.6e-6,
+                 'bmax_b': 70.4e-6,
+                 'bmin_t': 0.0,
+                 'bmax_t': 70.4e-6,
+                 'lattice_constant': 125.6e-6}
+    boise = {'sandstone': 'boise',
+             'alpha_t': 13.050e-6,
+             'alpha_b': 18.333e-6,
              'beta_t': 0.536,
              'beta_b': 1.18,
-             'lattice_constant': 171.2e-6,
-             'b_min': 38.7e-6,
-             'b_max': 73.9e-6,
-             'b_vo': 42.3e-6,
-             'pore_shape': 1.75,
-             'pore_scale': 10e-6,
-             'pore_loc': 40.81e-6,
-             'throat_shape': 0.8,
-             'throat_scale': 7.5e-6,
-             'throat_loc': 1.1e-6}
+             'bmin_b': 38.7e-6,
+             'bmax_b': 73.9e-6,
+             'bmin_t': 0.0,
+             'bmax_t': 73.9e-6,
+             'lattice_constant': 171.2e-6}
 
 
 class CubicSandstoneSettings(GenericSettings):
     r"""
     The following parameters are used to generate the network and geometry
 
+    The pore and throat sizes are chosen from the Weibull distribution which
+    is defined as follows:
+
+    .. math::
+
+        cdf = 1 - exp\bigg[-\bigg(\frac{b_{i} -
+              b_{i,min}}{\alpha_{i}}\bigg)^{\beta_{i}}\bigg]
+
+    Values of $\alpha_i$, $beta_i$, and $b_{i,min}$ are unique to each type
+    of standstone.  $i=b$ indicates pore body and $i=t$ is throat.
+
     Parameters
     ----------
     sandstone : str (default = 'berea')
         The type of sandstone to model
-    pore_shape : float
-        The shape factor used in the Weibull distribution for pore sizes
-    pore_scale : float
-        The scale factor used in the Weibull distribution for pore sizes
-    pore_loc : float
-        The location used in the Weibull distribution for pore sizes
-    throat_shape : float
-        The shape factor used in the Weibull distribution for throat sizes
-    throat_scale : float
-        The scale factor used in the Weibull distribution for throat sizes
-    throat_loc : float
-        The location used in the Weibull distribution for throat sizes
     lattice_constant : float
         The lattice spacing used when creating the network
+    beta_b and beta_t : float
+        Controls the skewness of the Weibull distribution, with smaller
+        numbers creating a distribution skewed to the left. In the
+        ``scipy.stats.weibull_min`` function this is referred to as ``c``,
+        and it is referred to as :math:`\kappa` on the Wikipedia entry
+        for the Weibull distribution.  These values are held constant by
+        Ioannidis and Chatzis at ``beta_b=1.18`` and ``beta_t=0.536``.
+    alpha_b and alpha_t : float
+        Controls the scale of the Weibull distribution, with smaller numbers
+        creating a distribution shifted to small values.  This is the
+        ``scale`` used by the ``scipy.stats.weibull_min`` function
+        and is more generally referred to as :math:`\lambda`, for instance
+        on the Wikipedia entry for the Weibull distribution.
+    bmin_b, bmin_t : float
+        Controls the smallest value returned by the Weibull distributions.
+    bmax_b, bmax_t : float
+        Controls the largest value returned by the Weibull distributions.
     """
-    sandstone = 'berea'
-    pore_shape = 1.18
-    pore_scale = 6.08e-6
-    pore_loc = 24.6e-6
-    throat_shape = 0.536
-    throat_scale = 1.904e-6
-    throat_loc = 0.7e-6
+    sandstone = 'berea_108'
     lattice_constant = 0.0001712
+    beta_b = 1.18  # The beta values are set to constants for ALL materials
+    beta_t = 0.536
+    alpha_t = 1.904e-6
+    alpha_b = 6.08e-6
+    bmin_b = 24.6e-6  # min and max pore sizes are clearly given
+    bmax_b = 70.4e-6
+    bmin_t = 0  # Not sure where this comes from
+    bmax_t = 70.4e-6  # Not sure about this, setting to same as pores
 
 
 class CubicSandstone(Project):
     r"""
-    A network representing sandstone on a Cubic lattice
+    A network representing sandstone using a Cubic lattice.
 
-    Sandstone is one of the standard materials used on geoscience studies due
-    to it's importance in oil reservoir engineering as well as having well
-    defined pore structure.  This class creates a Cubic network with the
-    appropriate lattice spacing and connectivity, then adds a Geometry object
-    with the necessary pore-scale models and prescribed parameters.
+    This ``material`` is based on the 1993 paper of Ioannidis and Chatzis
+    in Chemical Engineering Science entitled "Network modelling of pore
+    structure and transport properties of porous media".  The pore and throat
+    size parameters are taken from their Table 1 and 5,and are compiled in
+    the table below for reference.  These values are looked-up based on the
+    ``sandstone`` parameter specified.  They also used a common shape factor
+    for all materials, :math:`\beta_b=1.18` and :math:`\beta_t=0.536`
+
+    Note that all the values listed below are in *um*, while the
+    actual values should be given in *m*.
+
+    +---------------+---------+---------+-------+--------+--------+
+    | Sandstone     | alpha_t | alpha_b | Lc    | bmin_b | bmax_b |
+    +===============+=========+=========+=======+========+========+
+    | boise         | 13.050  | 8.333   | 171.2 | 38.7   | 73.9   |
+    +---------------+---------+---------+-------+--------+--------+
+    | bartlesville  |  2.252  | 4.251   |  72.9 | 19.0   | 44.1   |
+    +---------------+---------+---------+-------+--------+--------+
+    | berea_108     |  1.904  | 6.081   | 125.6 | 24.6   | 70.4   |
+    +---------------+---------+---------+-------+--------+--------+
+    | berea_BE1     |  1.697  | 8.333   |  73.9 | 14.1   | 42.3   |
+    +---------------+---------+---------+-------+--------+--------+
+    | noxie_47      |  1.842  | 4.600   |  87.0 | 19.4   | 52.8   |
+    +---------------+---------+---------+-------+--------+--------+
+    | noxie_129     |  0.718  | 2.935   |  73.3 | 12.3   | 49.3   |
+    +---------------+---------+---------+-------+--------+--------+
+    | st_meinrad    |  1.269  | 3.903   |  69.6 | 14.1   | 49.3   |
+    +---------------+---------+---------+-------+--------+--------+
+    | big_clifty    |  1.137  | 1.870   |  71.1 | 14.1   | 42.3   |
+    +---------------+---------+---------+-------+--------+--------+
+    | cottage_grove |  0.780  | 3.166   |  68.6 | 14.1   | 49.3   |
+    +---------------+---------+---------+-------+--------+--------+
+    | clear_creek   |  0.780  | 3.865   |  92.3 | 21.1   | 42.3   |
+    +---------------+---------+---------+-------+--------+--------+
+    | torpedo       |  0.755  | 2.194   |  54.6 | 10.6   | 45.8   |
+    +---------------+---------+---------+-------+--------+--------+
+    | bandera       |  0.329  | 5.097   |  73.3 | 14.1   | 45.8   |
+    +---------------+---------+---------+-------+--------+--------+
+    | whetstone     |  0.178  | 0.221   |  37.0 |  7.1   | 47.5   |
+    +---------------+---------+---------+-------+--------+--------+
 
     Parameters
     ----------
@@ -95,7 +241,7 @@ class CubicSandstone(Project):
         The number of pores along each direction of the domain.  All other
         aspects of this model are prescribed by the code.
     sandstone : str
-        Options are 'berea' (default), 'boise', etc.
+        Options are listed in the table above.  The default is 'berea_108'
     settings : dict
         If ``sandstone`` is given as ``None``, then the user can specify their
         own parameters for the pore and throat size distribution via these
@@ -103,8 +249,7 @@ class CubicSandstone(Project):
 
     Notes
     -----
-    The source code for this Material is relatively straight-forward, so is a
-    good example starting point for creating custom materials.
+    The source code...
 
     References
     ----------
@@ -115,7 +260,7 @@ class CubicSandstone(Project):
 
     """
 
-    def __init__(self, shape=[10, 10, 10], sandstone='berea', settings={},
+    def __init__(self, shape=[10, 10, 10], sandstone='berea_108', settings={},
                  **kwargs):
         super().__init__(**kwargs)
         self.settings._update_settings_and_docs(CubicSandstoneSettings())
@@ -130,21 +275,17 @@ class CubicSandstone(Project):
         geom.add_model(propname='throat.seed',
                        model=mods.misc.neighbor_lookups.from_neighbor_pores,
                        pore_prop='pore.seed')
-
-        # Pore throat and pore body characteristic dimensions follow
-        # respective Weibull distribution, taking location parameters for
-        # Berea 108 sample from table 5.
         geom.add_model(propname='pore.size_z',
                        model=mods.geometry.pore_size.weibull,
-                       shape=self.settings['pore_shape'],
-                       loc=self.settings['pore_loc'],
-                       scale=self.settings['pore_scale'],
+                       shape=self.settings['beta_b'],
+                       loc=self.settings['bmin_b'],
+                       scale=self.settings['alpha_b'],
                        seeds='pore.seed')
         geom.add_model(propname='throat.size',
                        model=mods.geometry.throat_size.weibull,
-                       shape=self.settings['throat_shape'],
-                       loc=self.settings['throat_loc'],
-                       scale=self.settings['throat_scale'],
+                       shape=self.settings['beta_t'],
+                       loc=self.settings['bmin_t'],
+                       scale=self.settings['alpha_t'],
                        seeds='throat.seed')
 
         # All pores in this model are of square x-section
