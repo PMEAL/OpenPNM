e480f10197fc94108d1538b9ff28200fad71efa1
Amin Sadeghi <amin.sadeghi@live.com>
2020-03-31 09:25:43 -0400

    Change default loglevel from 40 to 30 (warnings are now showed by default)

--- openpnm/utils/Workspace.py
+++ openpnm/utils/Workspace.py
@@ -65,7 +65,7 @@ class Workspace(dict):
         if Workspace.__instance__ is None:
             Workspace.__instance__ = dict.__new__(cls)
             cls.settings = SettingsDict()
-            cls.settings['loglevel'] = 40
+            cls.settings['loglevel'] = 30
         return Workspace.__instance__
 
     def __init__(self):
