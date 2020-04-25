1c1eb6e85624c70f7bc1514f0c403199d04b1391
Shoaib <msbeg@uwaterloo.ca>
2020-04-25 16:03:23 -0400

    Updating database
    Changes in pore and throat seed

---
+++ openpnm/materials/BereaCubic.py
@@ -0,0 +1,136 @@
+import scipy as sp
+from openpnm.utils import logging, Project
+from openpnm.network import Cubic
+from openpnm.geometry import GenericGeometry
+import openpnm.models as mods
+logger = logging.getLogger(__name__)
+
+
+class BereaCubic(Project):
+    r"""
+    A traditional Berea Sandstone on a Cubic lattice
+
+    Berea Sandstone is one of the standard materials used on geoscience
+    studies due to it's importance in oil reservoir engineering as well as
+    having well defined pore structure.  This class creates a Cubic Network
+    with the appropriate lattice spacing and connectivity, then adds a Geometry
+    object with the necessary pore-scale models and prescribed parameters.
+
+    Parameters
+    ----------
+    shape : array_like
+        The number of pores along each direction of the domain.  All other
+        aspects of this model are prescribed by the code.
+
+    name : string, optional
+        The name to give the Project
+
+    Notes
+    -----
+    The source code for this Material is relatively straight-forward, so is a
+    good example starting point for creating custom materials.
+
+    References
+    ----------
+    [1] ???
+
+    Examples
+    --------
+
+    """
+
+    def __init__(self, shape=[10, 10, 10], Lc=0.000126, family='berea',
+                 **kwargs):
+        super().__init__(**kwargs)
+        settings = {'berea': {'shapepore': 1.18, 'shapethroat': 0.536,
+                              'scalepore': 6.08e-6, 'scalethroat': 1.904,
+                              'locpore': 24.6e-6, 'locthroat': 0.7e-6}}
+        pn = Cubic(shape=shape, spacing=Lc, connectivity=6, project=self)
+        geom = GenericGeometry(network=pn, pores=pn.Ps, throats=pn.Ts)
+        geom['pore.seed'] = sp.rand(pn.Np)
+        geom.add_model(propname='throat.seed',
+                       model=mods.misc.neighbor_lookups.from_neighbor_pores,
+                       pore_prop='pore.seed')
+
+        # Pore throat and pore body characteristic dimensions follow
+        # respective Weibull distribution, taking location parameters for
+        # Berea 108 sample from table 5.
+        geom.add_model(propname='pore.size_z',
+                       model=mods.geometry.pore_size.weibull,
+                       shape=settings[family['shapepore']],
+                       loc=settings[family['locpore']],
+                       scale=settings[family['scalepore']],
+                       seeds='pore.seed')
+        geom.add_model(propname='throat.size',
+                       model=mods.geometry.throat_size.weibull,
+                       shape=settings[family['shapethroat']],
+                       loc=settings[family['locthroat']],
+                       scale=settings[family['scalethroat']],
+                       seeds='throat.seed')
+
+        # All pores in this model are of square x-section
+        # All throats are of slit shape x-section
+        geom['pore.size_x'] = sp.copy(geom['pore.size_z'])
+        geom['pore.size_y'] = geom['pore.size_z']*1.5
+
+        # Fetch copies of conns and coords for subsequent size calcs
+        conns = pn['throat.conns']
+        coords = pn['pore.coords']
+        # Create Nt by 2 array of pore coords
+        temp = coords[conns]
+        temp = sp.absolute(temp[:, 0] - temp[:, 1])
+        # Find orientation of each throat and create a label
+        pn['throat.dir_x'] = temp[:, 0] > 0
+        pn['throat.dir_y'] = temp[:, 1] > 0
+        pn['throat.dir_z'] = temp[:, 2] > 0
+
+        # Find width and length of each throat based on it's orientation
+        # Start by initializing arrays with 0's
+        geom['throat.size_x'] = 0.0
+        geom['throat.size_y'] = 0.0
+        geom['throat.size_z'] = 0.0
+        geom['throat.length'] = 0.0
+        geom['throat.width'] = 0.0
+        geom['throat.height'] = 0.0
+
+        # Start with x-directional throats
+        Ts = pn.throats('dir_x')
+        geom['throat.size_z'][Ts] = geom['throat.size'][Ts]
+        geom['throat.size_y'][Ts] = geom['throat.size'][Ts]*6
+        geom['throat.size_x'][Ts] = Lc - geom['pore.size_x'][conns[Ts, 0]] \
+            - geom['pore.size_x'][conns[Ts, 1]]/2
+        geom['throat.length'][Ts] = geom['throat.size_x'][Ts]
+        geom['throat.width'][Ts] = geom['throat.size_y'][Ts]
+        geom['throat.height'][Ts] = geom['throat.size_z'][Ts]
+
+        # Start with y-directional throats
+        Ts = pn.throats('dir_y')
+        geom['throat.size_z'][Ts] = geom['throat.size'][Ts]
+        geom['throat.size_x'][Ts] = geom['throat.size'][Ts]*6
+        geom['throat.size_y'][Ts] = Lc - geom['pore.size_y'][conns[Ts, 0]]/2 \
+            - geom['pore.size_y'][conns[Ts, 1]]/2
+        geom['throat.length'][Ts] = geom['throat.size_y'][Ts]
+        geom['throat.width'][Ts] = geom['throat.size_x'][Ts]
+        geom['throat.height'][Ts] = geom['throat.size_z'][Ts]
+
+        # Start with z-directional throats
+        Ts = pn.throats('dir_z')
+        geom['throat.size_x'][Ts] = geom['throat.size'][Ts]
+        geom['throat.size_y'][Ts] = geom['throat.size'][Ts]*6
+        geom['throat.size_z'][Ts] = Lc - geom['pore.size_z'][conns[Ts, 0]]/2 \
+            - geom['pore.size_z'][conns[Ts, 1]]/2
+        geom['throat.length'][Ts] = geom['throat.size_z'][Ts]
+        geom['throat.width'][Ts] = geom['throat.size_y'][Ts]
+        geom['throat.height'][Ts] = geom['throat.size_x'][Ts]
+
+        geom.add_model(propname='throat.area',
+                       model=mods.misc.basic_math.product,
+                       prop1='throat.height', prop2='throat.width')
+        geom.add_model(propname='throat.volume',
+                       model=mods.misc.basic_math.product,
+                       prop1='throat.area',
+                       prop2='throat.length')
+        geom.add_model(propname='pore.volume',
+                       model=mods.misc.basic_math.product,
+                       prop1='pore.size_x', prop2='pore.size_y',
+                       prop3='pore.size_z')
--- openpnm/materials/CubicSandstone.py
+++ openpnm/materials/CubicSandstone.py
@@ -1,4 +1,5 @@
 import scipy as sp
+import openpnm as op
 from openpnm.utils import logging, Project, GenericSettings
 from openpnm.network import Cubic
 from openpnm.geometry import GenericGeometry
@@ -11,11 +12,11 @@ class SandstoneParams():
                  'alpha_t': 0.178e-6,
                  'alpha_b': 0.221e-6,
                  'beta_t': 0.536,
-                 'beta_b': 1.18e-6,
+                 'beta_b': 1.18,
                  'bmin_b': 7.1e-6,
                  'bmax_b': 47.5e-6,
-                 'bmin_t': 0.074e-6,
-                 'bmax_t': 45.0e-6,
+                 'bmin_t': 0.55e-6,
+                 'bmax_t': 15.0e-6,
                  'lattice_constant': 37.0e-6}
     bandera = {'sandstone': 'bandera',
                'alpha_t': 0.329e-6,
@@ -24,8 +25,8 @@ class SandstoneParams():
                'beta_b': 1.18,
                'bmin_b': 14.1e-6,
                'bmax_b': 45.8e-6,
-               'bmin_t': 0.137e-6,
-               'bmax_t': 45.0e-6,
+               'bmin_t': 1.84e-6,
+               'bmax_t': 32.0e-6,
                'lattice_constant': 73.3e-6}
     torpedo = {'sandstone': 'torpedo',
                'alpha_t': 0.755e-6,
@@ -34,8 +35,8 @@ class SandstoneParams():
                'beta_b': 1.18,
                'bmin_b': 10.6e-6,
                'bmax_b': 45.8e-6,
-               'bmin_t': 0.310e-6,
-               'bmax_t': 45.0e-6,
+               'bmin_t': 2.6e-6,
+               'bmax_t': 10.0e-6,
                'lattice_constant': 54.6e-6}
     clear_creek = {'sandstone': 'clear_creek',
                    'alpha_t': 0.780e-6,
@@ -44,18 +45,18 @@ class SandstoneParams():
                    'beta_b': 1.18,
                    'bmin_b': 21.1e-6,
                    'bmax_b': 42.3e-6,
-                   'bmin_t': 0.321e-6,
-                   'bmax_t': 45.0e-6,
+                   'bmin_t': 3.321e-6,
+                   'bmax_t': 40.0e-6,
                    'lattice_constant': 92.3e-6}
     cottage_grove = {'sandstone': 'cottage_grove',
-                     'alpha_t': 0.780e-6,
+                     'alpha_t': 0.786e-6,
                      'alpha_b': 3.166e-6,
                      'beta_t': 0.536,
                      'beta_b': 1.18,
                      'bmin_b': 14.1e-6,
                      'bmax_b': 49.3e-6,
-                     'bmin_t': 0.323e-6,
-                     'bmax_t': 45.0e-6,
+                     'bmin_t': 3.0e-6,
+                     'bmax_t': 15.0e-6,
                      'lattice_constant': 68.6e-6}
     big_clifty = {'sandstone': 'big_clifty',
                   'alpha_t': 1.137e-6,
@@ -64,8 +65,8 @@ class SandstoneParams():
                   'beta_b': 1.18,
                   'bmin_b': 14.1e-6,
                   'bmax_b': 42.3e-6,
-                  'bmin_t': 0.456e-6,
-                  'bmax_t': 45.0e-6,
+                  'bmin_t': 2.97e-6,
+                  'bmax_t': 11.0e-6,
                   'lattice_constant': 71.1e-6}
     st_meinrad = {'sandstone': 'st_meinrad',
                   'alpha_t': 1.269e-6,
@@ -74,8 +75,8 @@ class SandstoneParams():
                   'beta_b': 1.18,
                   'bmin_b': 14.1e-6,
                   'bmax_b': 49.3e-6,
-                  'bmin_t': 0.503e-6,
-                  'bmax_t': 45.0e-6,
+                  'bmin_t': 2.95e-6,
+                  'bmax_t': 10.0e-6,
                   'lattice_constant': 69.6e-6}
     noxie_129 = {'sandstone': 'noxie_129',
                  'alpha_t': 0.718e-6,
@@ -84,9 +85,9 @@ class SandstoneParams():
                  'beta_b': 1.18,
                  'bmin_b': 12.3e-6,
                  'bmax_b': 49.3e-6,
-                 'bmin_t': 0.296e-6,
-                 'bmax_t': 45.0e-6,
-                 'lattice_constant': 73.3e-6}
+                 'bmin_t': 2.7e-6,
+                 'bmax_t': 15.0e-6,
+                 'lattice_constant': 57.3e-6}
     noxie_47 = {'sandstone': 'noxie_47',
                 'alpha_t': 1.842e-6,
                 'alpha_b': 4.600e-6,
@@ -94,8 +95,8 @@ class SandstoneParams():
                 'beta_b': 1.18,
                 'bmin_b': 19.4e-6,
                 'bmax_b': 52.8e-6,
-                'bmin_t': 0.684e-6,
-                'bmax_t': 45.0e-6,
+                'bmin_t': 3.9e-6,
+                'bmax_t': 20.0e-6,
                 'lattice_constant': 87.0e-6}
     bartlesville = {'sandstone': 'bartlesville',
                     'alpha_t': 2.252e-6,
@@ -104,18 +105,18 @@ class SandstoneParams():
                     'beta_b': 1.18,
                     'bmin_b': 19.0e-6,
                     'bmax_b': 44.1e-6,
-                    'bmin_t': 0.791e-6,
-                    'bmax_t': 45.0e-6,
+                    'bmin_t': 2.69e-6,
+                    'bmax_t': 18.5e-6,
                     'lattice_constant': 72.9e-6}
     berea_BE1 = {'sandstone': 'berea_BE1',
                  'alpha_t': 1.697e-6,
-                 'alpha_b': 8.333e-6,
+                 'alpha_b': 1.803e-6,
                  'beta_t': 0.536,
                  'beta_b': 1.18,
                  'bmin_b': 14.1e-6,
                  'bmax_b': 42.3e-6,
-                 'bmin_t': 0.643e-6,
-                 'bmax_t': 45.0e-6,
+                 'bmin_t': 2.8e-6,
+                 'bmax_t': 11.0e-6,
                  'lattice_constant': 73.9e-6}
     berea_108 = {'sandstone': 'berea_108',
                  'alpha_t': 1.904e-6,
@@ -124,20 +125,19 @@ class SandstoneParams():
                  'beta_b': 1.18,
                  'bmin_b': 24.6e-6,
                  'bmax_b': 70.4e-6,
-                 'bmin_t': 0.702e-6,
-                 'bmax_t': 45.0e-6,
+                 'bmin_t': 5.9e-6,
+                 'bmax_t': 26.0e-6,
                  'lattice_constant': 125.6e-6}
     boise = {'sandstone': 'boise',
-             'alpha_t': 13.050e-6,
+             'alpha_t': 11.050e-6,
              'alpha_b': 8.333e-6,
              'beta_t': 0.536,
              'beta_b': 1.18,
              'bmin_b': 38.7e-6,
-             'bmax_b': 73.9e-6,
-             'bmin_t': 1.52e-6,
-             'bmax_t': 45.0e-6,
+             'bmax_b': 78.9e-6,
+             'bmin_t': 7.66e-6,
+             'bmax_t': 34.0e-6,
              'lattice_constant': 171.2e-6}
-
 
 class CubicSandstoneSettings(GenericSettings):
     r"""
@@ -186,8 +186,8 @@ class CubicSandstoneSettings(GenericSettings):
     alpha_b = 6.08e-6
     bmin_b = 24.6e-6  # min and max pore sizes are clearly given
     bmax_b = 70.4e-6
-    bmin_t = 0  # Not sure where this comes from
-    bmax_t = 70.4e-6  # Not sure about this, setting to same as pores
+    bmin_t = 2.30e-6  # Not sure where this comes from
+    bmax_t = 26.4e-6  # Not sure about this, setting to same as pores
 
 
 class CubicSandstone(Project):
@@ -271,34 +271,54 @@ class CubicSandstone(Project):
             self.settings.update(standstone_settings)
         pn = Cubic(shape=shape, spacing=self.settings['lattice_constant'],
                    connectivity=6, project=self)
+# Fetch weibull_min from scipy.stats
+        import numpy as np
+        import scipy.stats as spst
+        wb = spst.weibull_min
+# The CDF method tells you the 'probabiity' of finding a value of size 'x'
+# or smaller.  By putting the maximum size as 'x', then we're finding the
+# maximum seed value that we want to put into the distribution.
+        smax_b = wb.cdf(x=self.settings['bmax_b'], c=self.settings['beta_b'],
+                        loc=self.settings['bmin_b'],
+                        scale=self.settings['alpha_b'])
+        smax_t = wb.cdf(x=self.settings['bmax_t'], c=self.settings['beta_t'],
+                        loc=self.settings['bmin_t'],
+                        scale=self.settings['alpha_t'])
+# Start with random numbers between 0 and 1
         geom = GenericGeometry(network=pn, pores=pn.Ps, throats=pn.Ts)
-        geom['pore.seed'] = sp.rand(pn.Np)
+        geom['pore.seed'] = np.random.rand(pn.Np)
         geom.add_model(propname='throat.seed',
-                       model=mods.misc.neighbor_lookups.from_neighbor_pores,
+                     model=op.models.misc.neighbor_lookups.from_neighbor_pores,
                        pore_prop='pore.seed', mode='min')
-        geom.add_model(propname='pore.size_z',
+# Now scale the values by the max found above
+        geom['pore.seed'] *= smax_b
+        geom['throat.seed'] *= smax_t
+#        geom['pore.seed'] = sp.rand(pn.Np) * 0.5 + 0.2
+#        geom.add_model(propname='throat.seed',
+#                       model=mods.misc.neighbor_lookups.from_neighbor_pores,
+#                       pore_prop='pore.seed', mode='min')
+        geom.add_model(propname='pore.radius',
                        model=mods.geometry.pore_size.weibull,
                        shape=self.settings['beta_b'],
                        loc=self.settings['bmin_b'],
                        scale=self.settings['alpha_b'],
                        seeds='pore.seed')
-        geom.add_model(propname='throat.size',
+        geom.add_model(propname='throat.radius',
                        model=mods.geometry.throat_size.weibull,
                        shape=self.settings['beta_t'],
                        loc=self.settings['bmin_t'],
                        scale=self.settings['alpha_t'],
                        seeds='throat.seed')
-        # geom.add_model(propname='throat.size_from_neighbor',
-        #                model=mods.misc.neighbor_lookups.from_neighbor_pores,
-        #                pore_prop='pore.size_z')
-        # geom.add_model(propname='throat.size',
-        #                model=mods.misc.scaled,
-        #                prop='throat.size_from_neighbor', factor=0.5)
-
+        # geom['throat.radius'] = geom['throat.radius']/
+        # geom['throat.radius'].max()*45e-6
+        # geom['pore.radius'] = geom['pore.radius']/
+        # geom['pore.radius'].max()*self.settings['bmax_b']
         # All pores in this model are of square x-section
         # All throats are of slit shape x-section
-        geom['pore.size_x'] = sp.copy(geom['pore.size_z'])
-        geom['pore.size_y'] = geom['pore.size_z']*1.5
+        geom['pore.size_z'] = geom['pore.radius']*2
+        geom['pore.size_x'] = geom['pore.radius']*2
+        geom['pore.size_y'] = geom['pore.radius']*3
+        geom['throat.size'] = geom['throat.radius']*2
 
         # Fetch copies of conns and coords for subsequent size calcs
         conns = pn['throat.conns']
@@ -325,7 +345,7 @@ class CubicSandstone(Project):
         Ts = pn.throats('dir_x')
         geom['throat.size_z'][Ts] = geom['throat.size'][Ts]
         geom['throat.size_y'][Ts] = geom['throat.size'][Ts]*6
-        geom['throat.size_x'][Ts] = Lc - geom['pore.size_x'][conns[Ts, 0]] \
+        geom['throat.size_x'][Ts] = Lc - geom['pore.size_x'][conns[Ts, 0]]/2\
             - geom['pore.size_x'][conns[Ts, 1]]/2
         geom['throat.length'][Ts] = geom['throat.size_x'][Ts]
         geom['throat.width'][Ts] = geom['throat.size_y'][Ts]
@@ -335,7 +355,7 @@ class CubicSandstone(Project):
         Ts = pn.throats('dir_y')
         geom['throat.size_z'][Ts] = geom['throat.size'][Ts]
         geom['throat.size_x'][Ts] = geom['throat.size'][Ts]*6
-        geom['throat.size_y'][Ts] = Lc - geom['pore.size_y'][conns[Ts, 0]]/2 \
+        geom['throat.size_y'][Ts] = Lc - geom['pore.size_y'][conns[Ts, 0]]/2\
             - geom['pore.size_y'][conns[Ts, 1]]/2
         geom['throat.length'][Ts] = geom['throat.size_y'][Ts]
         geom['throat.width'][Ts] = geom['throat.size_x'][Ts]
@@ -345,7 +365,7 @@ class CubicSandstone(Project):
         Ts = pn.throats('dir_z')
         geom['throat.size_x'][Ts] = geom['throat.size'][Ts]
         geom['throat.size_y'][Ts] = geom['throat.size'][Ts]*6
-        geom['throat.size_z'][Ts] = Lc - geom['pore.size_z'][conns[Ts, 0]]/2 \
+        geom['throat.size_z'][Ts] = Lc - geom['pore.size_z'][conns[Ts, 0]]/2\
             - geom['pore.size_z'][conns[Ts, 1]]/2
         geom['throat.length'][Ts] = geom['throat.size_z'][Ts]
         geom['throat.width'][Ts] = geom['throat.size_y'][Ts]
@@ -356,9 +376,14 @@ class CubicSandstone(Project):
                        prop1='throat.height', prop2='throat.width')
         geom.add_model(propname='throat.volume',
                        model=mods.misc.basic_math.product,
-                       prop1='throat.area',
-                       prop2='throat.length')
+                       prop1='throat.area', prop2='throat.length')
         geom.add_model(propname='pore.volume',
                        model=mods.misc.basic_math.product,
                        prop1='pore.size_x', prop2='pore.size_y',
                        prop3='pore.size_z')
+        import numpy as np
+        conns = pn['throat.conns']
+        P1, P2 = pn['pore.size_z'][conns.T]
+        T = pn['throat.dir_z']
+        print(np.all(P1 > T))
+        print(np.all(P2 > T))
