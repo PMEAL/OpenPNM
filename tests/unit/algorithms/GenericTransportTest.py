0b9e3d89ae50ced72f83ce481e3b79f3a6109b4a
Mike McKague <mgmckagu@uwaterloo.ca>
2020-09-30 16:45:06 -0400

    correct test for error

--- tests/unit/algorithms/GenericTransportTest.py
+++ tests/unit/algorithms/GenericTransportTest.py
@@ -279,6 +279,8 @@ class GenericTransportTest:
     def test_total_rate(self):
         alg = op.algorithms.GenericTransport(network=self.net,
                                              phase=self.phase)
+        h = self.net.check_network_health()
+        op.topotools.trim(self.net, pores=h['trim_pores'])
         alg.settings['conductance'] = 'throat.diffusive_conductance'
         alg.settings['quantity'] = 'pore.mole_fraction'
         alg.set_rate_BC(pores=[0, 1, 2, 3], total_rate=1)
