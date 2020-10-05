fac10077bd6f0edf03a7bb4f35c8930f862e2c0b
jgostick <jgostick@gmail.com>
2020-10-05 16:19:08 -0400

    Playing with module import make ci happy [ci min]

--- tests/unit/core/ExtrasTest.py
+++ tests/unit/core/ExtrasTest.py
@@ -100,16 +100,25 @@ class ExtrasTest:
         ws.settings['loglevel'] = 50
         classes = [c for c in dir(op.physics) if not c.startswith('__')]
         mod = importlib.import_module('openpnm.physics')
+        print(mod)
         for c in classes:
             clss = getattr(mod, c)
             net = op.network.Cubic(shape=[2, 2, 2])
             # Eventually we want to create physics objects without a phase
             # but for now this is necessary.
             phase = op.phases.GenericPhase(network=net)
-            clss(project=net.project, phase=phase)
+            try:
+                clss(project=net.project, phase=phase)
+            except TypeError:
+                clss = getattr(clss, c)
+                clss(project=net.project, phase=phase)
             net = op.network.Cubic(shape=[2, 2, 2])
             phase = op.phases.GenericPhase(network=net)
-            clss(network=net, phase=phase)
+            try:
+                clss(network=net, phase=phase)
+            except TypeError:
+                clss = getattr(clss, c)
+                clss(network=net, phase=phase)
 
     def test_init_algorithm_with_either_network_or_project(self):
         ws.clear()
