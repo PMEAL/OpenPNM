5faf84f12c21277d1b6320331d46bddf7972c0ad
Amin Sadeghi <amin.sadeghi@live.com>
2020-10-09 11:47:43 -0400

    ci skip

--- .github/workflows/utils.sh
+++ .github/workflows/utils.sh
@@ -1,24 +1,11 @@
-function get_nth_recent_tag {
-    git fetch --all --tags
-    tags=($(git for-each-ref --sort=-creatordate --format '%(refname:strip=2)' refs/tags --count=$1))
-    echo "${tags[$(($1-1))]}"
-}
-
-
 function get_most_recent_tag {
-    git fetch --all --tags
-    echo "$(git tag | sort -V | tail -1)"
-}
-
-
-function test_func {
-    echo 09231
+    echo "$(get_nth_recent_tag 1)"
 }
 
 
 function get_version {
     version_loc=$1
-
+#-- Python code starts here --#
 PYTHON_CODE=$(cat <<EOF
 import sys
 sys.path.append("$version_loc")
@@ -26,6 +13,13 @@ from __version__ import __version__
 print(__version__)
 EOF
 )
-
+#--- Python code ends here ---#
     python -c "$PYTHON_CODE"
+}
+
+
+function get_nth_recent_tag {
+    git fetch --all --tags
+    tags=($(git for-each-ref --sort=-creatordate --format '%(refname:strip=2)' refs/tags --count=$1))
+    echo "${tags[$(($1-1))]}"
 }
