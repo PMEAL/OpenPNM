16bf49a78cb86e2dd8d801e605aec4da44f11cbe
Amin Sadeghi <amin.sadeghi@live.com>
2020-10-09 08:45:59 -0400

    MAINT: add comment on the logic behind allowing project.name = project.name

--- openpnm/utils/Workspace.py
+++ openpnm/utils/Workspace.py
@@ -53,8 +53,9 @@ class Workspace(dict):
         if name is None:
             name = self._gen_name()
         if name in self.keys():
-            if id(self.__instance__[name]) != id(project):
-                raise Exception("A project named " + name + " already exists")
+            # To ensures renaming a Project to its current name is allowed
+            if self.__instance__[name] is not project:
+                raise Exception(f"A project named {name} already exists")
         if project in self.values():
             self.pop(project.name, None)
         if not isinstance(project, openpnm.utils.Project):
