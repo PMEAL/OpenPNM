5093809ac7524add1e66723380d1cf3969848c66
Mehrez Agnaou <magnaoulab@gmail.com>
2020-10-17 10:29:03 -0400

    add salome export example script

---
+++ scripts/example_salome_export.py
@@ -0,0 +1,19 @@
+import openpnm as op
+import numpy as np
+
+# work space and project
+ws = op.Workspace()
+ws.settings["loglevel"] = 30
+proj = ws.new_project()
+
+# network
+np.random.seed(7)
+net = op.network.Cubic(shape=[4, 3, 3], spacing=1e-4, project=proj)
+
+# geometry
+geo = op.geometry.StickAndBall(network=net, pores=net.Ps, throats=net.Ts)
+
+# phase
+phase = op.phases.Water(network=net)
+
+proj.export_data(phases=[phase], filename='OUT', filetype='Salome')
