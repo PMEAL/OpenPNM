0b8afcf23f08598e245eb12031dbd44fa30251d3
Mehrez Agnaou <magnaoulab@gmail.com>
2020-10-17 10:36:33 -0400

    example description

--- scripts/example_salome_export.py
+++ scripts/example_salome_export.py
@@ -1,6 +1,14 @@
 import openpnm as op
 import numpy as np
 
+
+"""
+In the example script a generic network is created then exported as a Salome
+Python script. The script should be executed from Salome with "load script".
+The geometry is then built. The geometry generation on Salome may take some
+time depending on the number of pores.
+"""
+
 # work space and project
 ws = op.Workspace()
 ws.settings["loglevel"] = 30
