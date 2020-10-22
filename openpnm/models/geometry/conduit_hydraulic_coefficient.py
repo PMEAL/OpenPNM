19afcc96936bb6c9e5ab4e2235f10217520044ac
Ni2M <n2misagh@uwaterloo.ca>
2020-10-22 16:08:43 -0400

    [ci skip] added conical_frustum_and_stick to the hydraulic _shape_coeffs methods

--- openpnm/models/geometry/conduit_hydraulic_coefficient.py
+++ openpnm/models/geometry/conduit_hydraulic_coefficient.py
@@ -2,6 +2,7 @@ import numpy as _np
 from numpy import pi
 from numpy import arctanh as _atanh
 
+
 def spheres_and_cylinders(target,
                          pore_diameter='pore.diameter',
                          throat_diameter='throat.diameter',
@@ -170,7 +171,6 @@ def spheres_and_cylinders_2D(target,
     return vals
 
 
-
 def cylinders_in_series(target,
                         pore_diameter='pore.diameter',
                         throat_diameter='throat.diameter',
@@ -194,6 +194,100 @@ def pyramids_and_cuboids(target,
     network = target.network
     R1, R2 = (target[pore_diameter][network.conns]/2).T
     Rt = target[throat_diameter]/2
+
+
+def conical_frustum_and_stick(target,
+                              pore_diameter='pore.diameter',
+                              throat_diameter='throat.diameter',
+                              pore_area='pore.area',
+                              throat_area='throat.area',
+                              conduit_lengths='throat.conduit_lengths',
+                              return_elements=False):
+    r"""
+    Compute hydraulic shape coefficient for conduits of spheres and cylinders
+    assuming pores and throats are truncated pyramids (frustums) and constant
+    cross-section cylinders (sticks).
+
+    Parameters
+    ----------
+    target : OpenPNM Object
+        The object which this model is associated with. This controls the
+        length of the calculated array, and also provides access to other
+        necessary properties.
+
+    pore_diameter : string
+        Dictionary key of the pore diameter values
+
+    throat_diameter : string
+        Dictionary key of the throat diameter values
+
+    pore_area : string
+        Dictionary key of the pore area values
+
+    throat_area : string
+        Dictionary key of the throat area values
+
+    conduit_lengths : string
+        Dictionary key of the conduit lengths' values
+
+    return_elements : Bool
+        A boolean variable to whether return the values elementwise for
+        pores and throat or return the final value for the conduit.
+
+    Returns
+    -------
+    Either an array of diffusive_shape_coefficient or a dictionary containing the
+    diffusive_shape_coefficient, which can be accessed via the dict keys 'pore1',
+    'pore2', and 'throat'.
+
+    """
+    network = target.project.network
+    throats = network.map_throats(throats=target.Ts, origin=target)
+    cn = network['throat.conns'][throats]
+    # Get pore diameter
+    D1 = network[pore_diameter][cn[:, 0]]
+    D2 = network[pore_diameter][cn[:, 1]]
+    Dt = network[throat_diameter][throats]
+    # Get pore/throat baseline areas (the one used in generic conductance)
+    A1 = network[pore_area][cn[:, 0]]
+    A2 = network[pore_area][cn[:, 1]]
+    At = network[throat_area][throats]
+    # Get conduit lengths
+    L1 = network[conduit_lengths + '.pore1'][throats]
+    L2 = network[conduit_lengths + '.pore2'][throats]
+    Lt = network[conduit_lengths + '.throat'][throats]
+    # Preallocating g
+    g1, g2, gt = _np.zeros((3, len(Lt)))
+    # Setting g to inf when Li = 0 (ex. boundary pores)
+    # INFO: This is needed since area could also be zero, which confuses NumPy
+    m1, m2, mt = [Li != 0 for Li in [L1, L2, Lt]]
+    g1[~m1] = g2[~m2] = gt[~mt] = _np.inf
+    # Preallocating F, SF
+    # F is INTEGRAL(1/A^2) dx , x : 0 --> L
+    F1, F2, Ft = _np.zeros((3, len(Lt)))
+    SF1, SF2, SFt = _np.ones((3, len(Lt)))
+    # Setting SF to 1 when Li = 0 (ex. boundary pores)
+    # INFO: This is needed since area could also be zero, which confuses NumPy
+    SF1[~m1] = SF2[~m2] = SFt[~mt] = 1
+    # Calculate integral of 1/A^2
+    F1[m1] = 16/3 * (L1*(D1**2 + D1*Dt + Dt**2) / (D1**3 * Dt**3 * pi**2))[m1]
+    F2[m2] = 16/3 * (L2*(D2**2 + D2*Dt + Dt**2) / (D2**3 * Dt**3 * pi**2))[m2]
+    Ft[mt] = (Lt/At**2)[mt]
+    # Calculate conduit shape factors
+    SF1[m1] = (L1 / (A1**2 * F1))[m1]
+    SF2[m2] = (L2 / (A2**2 * F2))[m2]
+    SFt[mt] = (Lt / (At**2 * Ft))[mt]
+    # Calculate the g values
+    g1[m1] = A1[m1] ** 2 / (8 * _np.pi * L1)[m1]
+    g2[m2] = A2[m2] ** 2 / (8 * _np.pi * L2)[m2]
+    gt[mt] = At[mt] ** 2 / (8 * _np.pi * Lt)[mt]
+    # Apply shape factors and calculate the final conductance
+    g1, g2, gt = g1*SF1, g2*SF2, gt*SFt
+    if return_elements:
+        vals = {'pore1': g1, 'throat': gt, 'pore2': g2}
+    else:
+        vals = (1/gt + 1/g1 + 1/g2)**(-1)
+    return vals
 
 
 def cones_and_cylinders(target,
