b7bc83e1172f26b36f22d54e30c2eb04a6645170
Ni2M <n2misagh@uwaterloo.ca>
2020-10-22 15:55:41 -0400

    [ci skip] add spheres_and_cylinders_2D to hydraulic coeff

--- openpnm/models/geometry/conduit_hydraulic_coefficient.py
+++ openpnm/models/geometry/conduit_hydraulic_coefficient.py
@@ -2,19 +2,6 @@ import numpy as _np
 from numpy import pi
 from numpy import arctanh as _atanh
 
-
-def cylinders_in_series(target,
-                        pore_diameter='pore.diameter',
-                        throat_diameter='throat.diameter',
-                        n_cylinders=5,
-                        throat_length=None,
-                        return_elements=False):
-    r"""
-
-    """
-    network = target.network
-
-
 def spheres_and_cylinders(target,
                          pore_diameter='pore.diameter',
                          throat_diameter='throat.diameter',
@@ -108,6 +95,92 @@ def spheres_and_cylinders(target,
     else:
         vals = (1/gt + 1/g1 + 1/g2)**(-1)
     return vals
+
+
+def spheres_and_cylinders_2D(target,
+                             pore_diameter='pore.diameter',
+                             throat_diameter='throat.diameter',
+                             conduit_lengths=None,
+                             throat_length=None,
+                             return_elements=False):
+    r"""
+    Compute hydraulic shape coefficient for conduits of spheres and cylinders
+
+    Parameter
+    ---------
+    target: OpenPNM object
+
+    Notes
+    -----
+    The hydraulic shape coefficient is the geometrical part of the pre-factor
+    in Stoke's flow:
+
+    .. math::
+
+
+    Thus :math:`S_{hydraulic}` represents the combined effect of the area and
+    length of the *conduit*, which consists of a throat and 1/2 of the pore
+    on each end.
+
+    """
+    network = target.project.network
+    throats = network.map_throats(throats=target.Ts, origin=target)
+    cn = network['throat.conns'][throats]
+    # Get pore diameter
+    D1 = network[pore_diameter][cn[:, 0]]
+    D2 = network[pore_diameter][cn[:, 1]]
+    Dt = network[throat_diameter][throats]
+    # Get conduit lengths
+    L1 = network[conduit_lengths + '.pore1'][throats]
+    L2 = network[conduit_lengths + '.pore2'][throats]
+    Lt = network[conduit_lengths + '.throat'][throats]
+    # Get pore/throat baseline areas (the one used in generic conductance)
+    A1 = D1
+    A2 = D2
+    At = network[throat_diameter][throats]
+    # Preallocating F, SF
+    # F is INTEGRAL(1/A^2) dx , x : 0 --> L
+    F1, F2, Ft = _np.zeros((3, len(Lt)))
+    SF1, SF2, SFt = _np.ones((3, len(Lt)))
+    # Setting SF to 1 when Li = 0 (ex. boundary pores)
+    # INFO: This is needed since area could also be zero, which confuses NumPy
+    m1, m2, mt = [Li != 0 for Li in [L1, L2, Lt]]
+    SF1[~m1] = SF2[~m2] = SFt[~mt] = 1
+    F1[m1] = (_atanh(2*L1/D1) / (2*D1))[m1]
+    F2[m2] = (_atanh(2*L2/D2) / (2*D2))[m2]
+    Ft[mt] = (Lt / At**2)[mt]
+    # Calculate conduit shape factors
+    SF1[m1] = (L1 / (A1**2 * F1))[m1]
+    SF2[m2] = (L2 / (A2**2 * F2))[m2]
+    SFt[mt] = (Lt / (At**2 * Ft))[mt]
+    # Find g for half of pore 1, throat, and half of pore 2
+    g1 = D1 ** 3 / (12 * L1)
+    g2 = D2 ** 3 / (12 * L2)
+    gt = Dt ** 3 / (12 * Lt)
+    # Apply shape factors to individual g
+    g1, g2, gt = g1*SF1, g2*SF2, gt*SFt
+    # Ensure infinite conductance for elements with zero length
+    g1[L1 == 0] = _np.inf
+    g2[L2 == 0] = _np.inf
+    gt[Lt == 0] = _np.inf
+    if return_elements:
+        vals = {'pore1': g1, 'throat': gt, 'pore2': g2}
+    else:
+        vals = (1/gt + 1/g1 + 1/g2)**(-1)
+    return vals
+
+
+
+def cylinders_in_series(target,
+                        pore_diameter='pore.diameter',
+                        throat_diameter='throat.diameter',
+                        n_cylinders=5,
+                        throat_length=None,
+                        return_elements=False):
+    r"""
+
+    """
+    network = target.network
 
 
 def pyramids_and_cuboids(target,
