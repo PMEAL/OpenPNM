a4b39934bdb90361e092a304505ff144dfe05716
Ni2M <n2misagh@uwaterloo.ca>
2020-10-22 12:52:56 -0400

    [ci skip] added conical_frustum_and_stick to the diff_shape_coefficient methods. As for this method we can not assumme a simplified shape, the conduit length is given as an argument

--- openpnm/models/geometry/conduit_diffusive_coefficient.py
+++ openpnm/models/geometry/conduit_diffusive_coefficient.py
@@ -122,6 +122,97 @@ def pyramids_and_cuboids(target,
     Rt = target[throat_diameter]/2
 
 
+def conical_frustum_and_stick(target,
+                              pore_diameter='pore.diameter',
+                              throat_diameter='throat.diameter',
+                              pore_area='pore.area',
+                              throat_area='throat.area',
+                              conduit_lengths='throat.conduit_lengths',
+                              return_elements=False):
+    r"""
+    Compute diffusive shape coefficient for conduits of spheres and cylinders
+    assuming pores and throats are truncated pyramids (frustums) and constant
+    cross-section cylinders (sticks).
+
+    Parameters
+    ----------
+    target : OpenPNM Object
+        The object which this model is associated with. This controls the
+        length of the calculated array, and also provides access to other
+        necessary properties.
+
+    pore_diameter : string
+        Dictionary key of the pore diameter values
+
+    throat_diameter : string
+        Dictionary key of the throat diameter values
+
+    pore_area : string
+        Dictionary key of the pore area values
+
+    throat_area : string
+        Dictionary key of the throat area values
+
+    conduit_lengths : string
+        Dictionary key of the conduit lengths' values
+
+    return_elements : Bool
+        A boolean variable to whether return the values elementwise for
+        pores and throat or return the final value for the conduit.
+
+    Returns
+    -------
+    Either an array of diffusive_shape_coefficient or a dictionary containing the
+    diffusive_shape_coefficient, which can be accessed via the dict keys 'pore1',
+    'pore2', and 'throat'.
+
+    """
+    network = target.project.network
+    throats = network.map_throats(throats=target.Ts, origin=target)
+    cn = network['throat.conns'][throats]
+    # Get pore diameter
+    D1 = network[pore_diameter][cn[:, 0]]
+    D2 = network[pore_diameter][cn[:, 1]]
+    Dt = network[throat_diameter][throats]
+    # Get pore/throat baseline areas (the one used in generic conductance)
+    A1 = network[pore_area][cn[:, 0]]
+    A2 = network[pore_area][cn[:, 1]]
+    At = network[throat_area][throats]
+    # Get conduit lengths
+    L1 = network[conduit_lengths + '.pore1'][throats]
+    L2 = network[conduit_lengths + '.pore2'][throats]
+    Lt = network[conduit_lengths + '.throat'][throats]
+    # Find g for half of pore 1, the throat, and half of pore 2
+    g1, g2, gt = A1/L1, A2/L2, At/Lt
+    # Preallocating F, SF
+    # F is INTEGRAL(1/A) dx , x : 0 --> L
+    F1, F2, Ft = _np.zeros((3, len(Lt)))
+    SF1, SF2, SFt = _np.ones((3, len(Lt)))
+    # Setting SF to 1 when Li = 0 (ex. boundary pores)
+    # INFO: This is needed since area could also be zero, which confuses NumPy
+    m1, m2, mt = [Li != 0 for Li in [L1, L2, Lt]]
+    SF1[~m1] = SF2[~m2] = SFt[~mt] = 1
+    # Handle the rest (non-zero-length conduits)
+    F1[m1] = (4*L1/(D1*Dt*pi))[m1]
+    F2[m2] = (4*L2/(D2*Dt*pi))[m2]
+    Ft[mt] = (Lt/At)[mt]
+    # Calculate conduit shape factors
+    SF1[m1] = (L1 / (A1*F1))[m1]
+    SF2[m2] = (L2 / (A2*F2))[m2]
+    SFt[mt] = (Lt / (At*Ft))[mt]
+    # Apply shape factors to individual g
+    g1, g2, gt = g1*SF1, g2*SF2, gt*SFt
+    # Ensure infinite conductance for elements with zero length
+    g1[L1 == 0] = _np.inf
+    g2[L2 == 0] = _np.inf
+    gt[Lt == 0] = _np.inf
+    if return_elements:
+        vals = {'pore1': g1, 'throat': gt, 'pore2': g2}
+    else:
+        vals = (1/gt + 1/g1 + 1/g2)**(-1)
+    return vals
+
+
 def cones_and_cylinders(target,
                         pore_diameter='pore.diameter',
                         throat_diameter='throat.diameter',
