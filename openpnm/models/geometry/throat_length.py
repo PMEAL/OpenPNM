a3952440f3c54baebc98ec6097c9be8af1eae206
Amin Sadeghi <amin.sadeghi@live.com>
2020-11-01 21:38:20 -0500

    Style changes

--- openpnm/models/geometry/throat_length.py
+++ openpnm/models/geometry/throat_length.py
@@ -7,19 +7,20 @@ r"""
 """
 from numpy.linalg import norm as _norm
 from openpnm.utils import logging as _logging
+import openpnm.geometry.GenericGeometry as _GenericGeometry
 _logger = _logging.getLogger(__name__)
 
 
-def ctc(target):
+def ctc(target: _GenericGeometry):
     r"""
-    Calculate throat length assuming point-like pores, i.e. center-to-center
+    Calculates throat length assuming point-like pores, i.e. center-to-center
     distance between pores. Also, this model assumes that pores and throat
     centroids are colinear.
 
     Parameters
     ----------
-    target : OpenPNM Object
-        The object which this model is associated with. This controls the
+    target : GenericGeometry
+        Geometry object which this model is associated with. This controls the
         length of the calculated array, and also provides access to other
         necessary properties.
 
@@ -38,22 +39,23 @@ def ctc(target):
     return value
 
 
-def piecewise(target, throat_endpoints='throat.endpoints',
-              throat_centroid='throat.centroid'):
+def piecewise(
+    target: _GenericGeometry,
+    throat_endpoints='throat.endpoints',
+    throat_centroid='throat.centroid'
+):
     r"""
-    Calculate throat length from end points and optionally a centroid
+    Calculates throat length from end points and optionally a centroid
 
     Parameters
     ----------
-    target : OpenPNM Object
-        The object which this model is associated with. This controls the
+    target : GenericGeometry
+        Geometry object which this model is associated with. This controls the
         length of the calculated array, and also provides access to other
         necessary properties.
-
-    throat_endpoints : string
+    throat_endpoints : str
         Dictionary key of the throat endpoint values.
-
-    throat_centroid : string
+    throat_centroid : str
         Dictionary key of the throat centroid values, optional.
 
     Returns
@@ -65,7 +67,6 @@ def piecewise(target, throat_endpoints='throat.endpoints',
     -----
     (1) By default, the model assumes that the centroids of pores and the
     connecting throat in each conduit are colinear.
-
     (2) If `throat_centroid` is passed, the model accounts for the extra
     length. This could be useful for Voronoi or extracted networks.
 
@@ -86,24 +87,27 @@ def piecewise(target, throat_endpoints='throat.endpoints',
     return Lt
 
 
-def conduit_lengths(target, throat_endpoints='throat.endpoints',
-                    throat_length='throat.length',
-                    throat_centroid='throat.centroid'):
+def conduit_lengths(
+    target: _GenericGeometry,
+    throat_endpoints='throat.endpoints',
+    throat_length='throat.length',
+    throat_centroid='throat.centroid'
+):
     r"""
-    Calculate conduit lengths. A conduit is defined as half pore + throat
+    Calculates conduit lengths. A conduit is defined as half pore + throat
     + half pore.
 
     Parameters
     ----------
-    target : OpenPNM Object
-        The object which this model is associated with. This controls the
+    target : GenericGeometry
+        Geometry object which this model is associated with. This controls the
         length of the calculated array, and also provides access to other
         necessary properties.
 
-    throat_endpoints : string
+    throat_endpoints : str
         Dictionary key of the throat endpoint values.
 
-    throat_diameter : string
+    throat_diameter : str
         Dictionary key of the throat length values.
 
     throat_length : string (optional)
@@ -134,23 +138,25 @@ def conduit_lengths(target, throat_endpoints='throat.endpoints',
     # Calculate conduit lengths for pore 1 and pore 2
     L1 = _norm(C1 - EP1, axis=1)
     L2 = _norm(C2 - EP2, axis=1)
+
     return {'pore1': L1, 'throat': Lt, 'pore2': L2}
 
 
-def classic(target, pore_diameter='pore.diameter'):
+def classic(target: _GenericGeometry, pore_diameter='pore.diameter'):
     r"""
-    Find throat length as the pore-to-pore center distance, less the radii of
+    Finds throat length as the pore-to-pore center distance, less the radii of
     each pore.
 
     Parameters
     ----------
-    target : OpenPNM object
-        The object which this model is associated with. This controls the
+    target : GenericGeometry
+        Geometry object which this model is associated with. This controls the
         length of the calculated array, and also provides access to other
         necessary properties.
 
-    pore_diameter : string
+    pore_diameter : str
         Dictionary key of the pore diameter values
+
     """
     network = target.project.network
     throats = network.map_throats(throats=target.Ts, origin=target)
