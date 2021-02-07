e016c29ace7707d07d2c1072c43d5ee289383b2d
jgostick <jgostick@gmail.com>
2021-02-07 00:23:30 -0500

    script to call the statoil export format

---
+++ pnflow_script.py
@@ -0,0 +1,14 @@
+import openpnm as op
+ws = op.Workspace()
+ws.settings['loglevel'] = 30
+
+pn = op.network.Cubic(shape=[10, 10, 10], spacing=1e-4)
+geo = op.geometry.StickAndBall(network=pn, pores=pn.Ps, throats=pn.Ts)
+
+op.io.Statoil.add_reservoir_pore(network=pn, pores=pn.pores('left'))
+op.io.Statoil.add_reservoir_pore(network=pn, pores=pn.pores('right'))
+pn.add_model(propname='throat.total_length',
+             model=op.models.geometry.throat_length.ctc)
+pn['throat.shape_factor'] = 1.0
+pn['pore.shape_factor'] = 1.0
+op.io.Statoil.export_data(filename=r"C:\Users\jeff\Dropbox\Flash Sync\Code\Git\pnflow_projects\cubic\tester", network=pn, shape=[1e-3, 1e-3, 1e-3])
