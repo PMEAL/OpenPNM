d92c27ad8db3571089a6b84b72a7b1226b9c0693
Amin Sadeghi <amin.sadeghi@live.com>
2021-04-24 18:56:16 -0400

    Only move x0 to GPU if it's not None

--- openpnm/algorithms/GenericTransport.py
+++ openpnm/algorithms/GenericTransport.py
@@ -705,6 +705,7 @@ class GenericTransport(GenericAlgorithm):
                 r"""
                 Wrapper method for CuPy sparse linear solvers.
                 """
+                x0 = cupy.array(x0) if x0 is not None else x0
                 b = cupy.array(b)
                 A = cupy.sparse.csr_matrix(A)
                 direct = ["spsolve", "lsqr", "lschol"]
