2b2709cecf8ee2fde06f7983be2312712571c5d5
jgostick <jgostick@gmail.com>
2021-07-15 22:20:24 -0400

    Removing class methods from DelVorDual init

--- openpnm/network/DelaunayVoronoiDual.py
+++ openpnm/network/DelaunayVoronoiDual.py
@@ -148,7 +148,6 @@ class DelaunayVoronoiDual(GenericNetwork):
             pass
         else:
             self._trim_external_pores(shape=shape)
-            self._label_faces()
 
     def _trim_external_pores(self, shape):
         r'''
