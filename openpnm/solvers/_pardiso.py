46f8113920c51553af089bc4be13251494024396
Amin Sadeghi <amin.sadeghi@live.com>
2021-09-30 22:12:23 -0400

    Add PARDISO linear solver to solvers module

--- openpnm/solvers/_pardiso.py
+++ openpnm/solvers/_pardiso.py
@@ -1,12 +1,12 @@
-from scipy.sparse import csr_matrix, csc_matrix
-from scipy.sparse.linalg import spsolve
+from pypardiso import spsolve
 from openpnm.solvers import DirectSolver
+from scipy.sparse import csr_matrix, csc_matrix
 
 
-class ScipySpsolve(DirectSolver):
+class PardisoSpsolve(DirectSolver):
 
     def solve(self, A, b, **kwargs):
         if not isinstance(A, (csr_matrix, csc_matrix)):
             A = A.tocsr()
         # TODO: solver.solve should return (x, info) not just x
-        return spsolve(A, b)
+        return (spsolve(A, b), 0)
