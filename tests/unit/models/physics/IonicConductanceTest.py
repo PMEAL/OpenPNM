eb6e4bcab1e67bee51b5a161617413248c190fa7
Ni2M <n2misagh@uwaterloo.ca>
2021-10-12 16:27:45 -0400

    updated ionic_cond test now it only accepts size factor, as we removed shape_factor_based methods [ci skip]

--- tests/unit/models/physics/IonicConductanceTest.py
+++ tests/unit/models/physics/IonicConductanceTest.py
@@ -1,7 +1,9 @@
+import numpy as np
 import openpnm as op
 from openpnm.phases import mixtures
 from numpy.testing import assert_allclose
 import openpnm.models.geometry.conduit_lengths as _conduit_lengths
+
 
 class IonicConductanceTest:
     def setup_class(self):
@@ -13,8 +15,8 @@ class IonicConductanceTest:
         self.geo['throat.diameter'] = 0.56
         self.geo['pore.area'] = 1
         self.geo['throat.area'] = 1
-        self.geo['pore.volume']= 1
-        self.geo['throat.volume']=1
+        self.geo['pore.volume'] = 1
+        self.geo['throat.volume'] = 1
         L1, Lt, L2 = _conduit_lengths.spheres_and_cylinders(self.geo).T
         self.geo['throat.conduit_lengths.pore1'] = L1
         self.geo['throat.conduit_lengths.throat'] = Lt
@@ -24,39 +26,21 @@ class IonicConductanceTest:
         self.phys = op.physics.GenericPhysics(network=self.net,
                                               phase=self.phase,
                                               geometry=self.geo)
-        
 
-    def test_ionic_conductance_poisson(self):
-        # old poisson model, shape factor
-        mpo = op.models.physics.poisson_shape_factors.ball_and_stick
-        self.phys.add_model(propname="throat.poisson_shape_factors", model=mpo)
-        mod1 = op.models.physics.ionic_conductance.poisson
-        self.phys.add_model(propname='throat.ionic_conductance_from_mod',
-                            model=mod1)
-        self.phys.regenerate_models()
-        # new poisson model, size factor
-        mpo2 = op.models.geometry.diffusive_size_factors.spheres_and_cylinders
+    def test_generic_ionic_poisson_laplace(self):
+        mpo = op.models.geometry.diffusive_size_factors.spheres_and_cylinders
         self.geo.add_model(propname="throat.diffusive_size_factors",
-                           model=mpo2)
-        mod2 = op.models.physics.ionic_conductance.generic_ionic_poisson_laplace
+                           model=mpo)
+        mod = op.models.physics.ionic_conductance.generic_ionic_poisson_laplace
         self.phys.add_model(propname='throat.ionic_conductance_generic',
-                            model=mod2)
+                            model=mod)
         self.phys.regenerate_models()
-        actual = self.phys['throat.ionic_conductance_from_mod']
-        desired = self.phys['throat.ionic_conductance_generic']
-        assert_allclose(actual, desired, rtol=1e-5)
-        
-    def test_ionic_conductance_poisson_2D(self):
+        actual = np.mean(self.phys['throat.ionic_conductance_generic'])
+        assert_allclose(actual, desired=4.231022e-10, rtol=1e-5)
+
+    def test_generic_ionic_poisson_laplace_2D(self):
         self.geo['pore.area'] = self.geo['pore.diameter']
         self.geo['throat.area'] = self.geo['throat.diameter']
-        # old poisson model, shape factor
-        mpo = op.models.physics.poisson_shape_factors.ball_and_stick_2d
-        self.phys.add_model(propname="throat.poisson_shape_factors", model=mpo)
-        mod1 = op.models.physics.ionic_conductance.poisson
-        self.phys.add_model(propname='throat.ionic_conductance_from_mod',
-                            model=mod1)
-        self.phys.regenerate_models()
-        # new poisson model, size factor
         mpo2 = op.models.geometry.diffusive_size_factors.circles_and_rectangles
         self.geo.add_model(propname="throat.diffusive_size_factors",
                            model=mpo2)
@@ -64,16 +48,14 @@ class IonicConductanceTest:
         self.phys.add_model(propname='throat.ionic_conductance_generic',
                             model=mod2)
         self.phys.regenerate_models()
-        actual = self.phys['throat.ionic_conductance_from_mod']
-        desired = self.phys['throat.ionic_conductance_generic']
-        assert_allclose(actual, desired, rtol=1e-5)
+        actual = np.mean(self.phys['throat.ionic_conductance_generic'])
+        assert_allclose(actual, desired=6.257659e-10, rtol=1e-5)
 
     def test_ionic_conductance_electroneutrality(self):
         # old electroneutrality model, shape factor
         sw = mixtures.SalineWater(network=self.net)
         Na = sw.components['Na_' + sw.name]
         Cl = sw.components['Cl_' + sw.name]
-        H2O = sw.components['H2O_' + sw.name]
         # physics
         self.phys = op.physics.GenericPhysics(network=self.net,
                                               phase=sw, geometry=self.geo)
@@ -90,12 +72,6 @@ class IonicConductanceTest:
                                              13.9993082, 13.9993082, 13.9993082, 20., 20.,
                                              20., 20., 20., 20., 20.,
                                              20., 20.]
-        mpo = op.models.physics.poisson_shape_factors.ball_and_stick
-        self.phys.add_model(propname="throat.poisson_shape_factors", model=mpo)
-        current = op.models.physics.ionic_conductance.electroneutrality
-        self.phys.add_model(propname='throat.ionic_conductance',
-                            ions=[Na.name, Cl.name],
-                            model=current, regen_mode='normal')
         mpo2 = op.models.geometry.diffusive_size_factors.spheres_and_cylinders
         self.geo.add_model(propname="throat.diffusive_size_factors",
                            model=mpo2)
@@ -104,9 +80,8 @@ class IonicConductanceTest:
                             ions=[Na.name, Cl.name],
                             model=current2, regen_mode='normal')
         self.phys.regenerate_models()
-        actual = self.phys['throat.ionic_conductance']
-        desired = self.phys['throat.ionic_conductance_generic']
-        assert_allclose(actual, desired, rtol=1e-5)
+        actual = np.mean(self.phys['throat.ionic_conductance_generic'])
+        assert_allclose(actual, desired=0.11189, rtol=1e-5)
 
 
 if __name__ == '__main__':
