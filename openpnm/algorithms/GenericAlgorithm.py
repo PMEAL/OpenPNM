7faa672a157d9a8925560e13b82456cd6f6e4224
jgostick <jgostick@gmail.com>
2021-10-12 12:40:34 -0400

    updating physics and algorithm to utilize base.__init__ better

--- openpnm/algorithms/GenericAlgorithm.py
+++ openpnm/algorithms/GenericAlgorithm.py
@@ -44,16 +44,9 @@ class GenericAlgorithm(Base, LegacyMixin, LabelMixin):
     def __init__(self, network=None, project=None, settings={}, **kwargs):
         self.settings.setdefault('prefix', 'alg')
         self.settings.update(settings)
-        if network is not None:
-            project = network.project
-        super().__init__(project=project, **kwargs)
 
-        # Deal with network or project arguments
-        if network is not None:
-            if project is not None:
-                assert network is project.network
-            else:
-                project = network.project
+        super().__init__(project=project, network=network, **kwargs)
+        project = self.network.project
         if project:
             self['pore.all'] = np.ones((project.network.Np, ), dtype=bool)
             self['throat.all'] = np.ones((project.network.Nt, ), dtype=bool)
