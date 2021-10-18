df34ae704ac9b94cda48d9a7157c84536ab6ddb5
jgostick <jgostick@gmail.com>
2021-10-18 00:31:08 -0400

    added a few models

--- openpnm/models/topology/topology.py
+++ openpnm/models/topology/topology.py
@@ -1,7 +1,13 @@
+from numpy.linalg import norm as _norm
+import numpy as _np
+
+
 r"""
 Pore-scale models related to topology of the network.
 """
-__all__ = ["coordination_number"]
+
+__all__ = ["coordination_number",
+           "pore_to_pore_distance"]
 
 
 def coordination_number(target):
@@ -11,3 +17,47 @@ def coordination_number(target):
     N = network.num_neighbors(pores=network.Ps, flatten=False)
     vals = N[network.pores(target.name)]
     return vals
+
+
+def pore_to_pore_distance(target):
+    r"""
+    """
+    network = target.project.network
+    throats = network.map_throats(throats=target.Ts, origin=target)
+    cn = network['throat.conns'][throats]
+    C1 = network['pore.coords'][cn[:, 0]]
+    C2 = network['pore.coords'][cn[:, 1]]
+    values = _norm(C1 - C2, axis=1)
+    return values
+
+def nearest_neighbor_distance(target):
+    r"""
+    """
+    network = target.project.network
+    throats = network.map_throats(throats=target.Ts, origin=target)
+    cn = network['throat.conns'][throats]
+    C1 = network['pore.coords'][cn[:, 0]]
+    C2 = network['pore.coords'][cn[:, 1]]
+    values = _norm(C1 - C2, axis=1)
+
+    data = values
+    im = network.create_incidence_matrix()
+    values = _np.ones((network.Np, ))*_np.inf
+    _np.minimum.at(values, im.row, data[im.col])
+    return _np.array(values)
+
+def furthest_neighbor_distance(target):
+    r"""
+    """
+    network = target.project.network
+    throats = network.map_throats(throats=target.Ts, origin=target)
+    cn = network['throat.conns'][throats]
+    C1 = network['pore.coords'][cn[:, 0]]
+    C2 = network['pore.coords'][cn[:, 1]]
+    values = _norm(C1 - C2, axis=1)
+
+    data = values
+    im = network.create_incidence_matrix()
+    values = _np.ones((network.Np, ))*_np.inf
+    _np.maximum.at(values, im.row, data[im.col])
+    return _np.array(values)
