1ac06bd516f15c98581bb657489e5576a2d83605
Ni2M <n2misagh@uwaterloo.ca>
2021-10-25 09:37:57 -0400

    removed boundary from init (this will remove some errors)

--- openpnm/geometry/__init__.py
+++ openpnm/geometry/__init__.py
@@ -91,8 +91,6 @@ that change can be cascaded to all dependent properties ('pore.diameter').
 from .GenericGeometry import GenericGeometry
 from .StickAndBall import StickAndBall
 from .StickAndBall2D import StickAndBall2D
-from .Boundary import Boundary
-from .Boundary2D import Boundary2D
 from .Imported import Imported
 from .SpheresAndCylinders import SpheresAndCylinders
 from .CirclesAndRectangles import CirclesAndRectangles
