6469fd3bca8edd5ecc3131b2ee4617300251ae85
Amin Sadeghi <amin.sadeghi@live.com>
2021-10-27 17:09:18 -0400

    Fix electrical conductance

--- openpnm/models/physics/electrical_conductance.py
+++ openpnm/models/physics/electrical_conductance.py
@@ -1,9 +1,9 @@
 r"""
 Pore-scale models for calculating the electrical conductance of conduits.
 """
-from openpnm.models.physics.utils import generic_transport_conductance
+from openpnm.models.physics.utils import _poisson_conductance
 
-__all__ = ["generic_electrical"]
+__all__ = ["generic_electrical", "series_resistors"]
 
 
 def generic_electrical(target,
@@ -16,17 +16,14 @@ def generic_electrical(target,
 
     Parameterss
     ----------
-    target : OpenPNM Object
+    target : GenericPhysics
         The object which this model is associated with. This controls the
         length of the calculated array, and also provides access to other
         necessary properties.
-
-    pore_conductivity : string
+    pore_conductivity : str
         Dictionary key of the pore electrical conductivity values
-
-    throat_conductivity : string
+    throat_conductivity : str
         Dictionary key of the throat electrical conductivity values
-
     size_factors: str
         Dictionary key of the conduit diffusive size factors' values.
 
@@ -42,7 +39,46 @@ def generic_electrical(target,
     be calculated.
 
     """
-    return generic_transport_conductance(target=target,
-                                         pore_conductivity=pore_conductivity,
-                                         throat_conductivity=throat_conductivity,
-                                         size_factors=size_factors)
+    return _poisson_conductance(target=target,
+                                pore_conductivity=pore_conductivity,
+                                throat_conductivity=throat_conductivity,
+                                size_factors=size_factors)
+
+
+def series_resistors(target,
+                     pore_conductivity='pore.electrical_conductivity',
+                     throat_conductivity='throat.electrical_conductivity',
+                     size_factors='throat.diffusive_size_factors'):
+    r"""
+    Calculate the electrical conductance of conduits in network, where a
+    conduit is ( 1/2 pore - full throat - 1/2 pore ). See the notes section.
+
+    Parameterss
+    ----------
+    target : GenericPhysics
+        The object which this model is associated with. This controls the
+        length of the calculated array, and also provides access to other
+        necessary properties.
+    pore_conductivity : str
+        Dictionary key of the pore electrical conductivity values
+    throat_conductivity : str
+        Dictionary key of the throat electrical conductivity values
+    size_factors: str
+        Dictionary key of the conduit diffusive size factors' values.
+
+    Returns
+    -------
+    g : ndarray
+        Array containing electrical conductance values for conduits in the
+        geometry attached to the given physics object.
+
+    Notes
+    -----
+    This function requires that all the necessary phase properties already
+    be calculated.
+
+    """
+    return _poisson_conductance(target=target,
+                                pore_conductivity=pore_conductivity,
+                                throat_conductivity=throat_conductivity,
+                                size_factors=size_factors)
--- tests/unit/models/physics/ElectricalConductanceTest.py
+++ tests/unit/models/physics/ElectricalConductanceTest.py
@@ -1,53 +1,39 @@
 import numpy as np
 import openpnm as op
 from numpy.testing import assert_allclose
-import openpnm.models.geometry.conduit_lengths as _conduit_lengths
 
 
 class ElectricalConductanceTest:
+
     def setup_class(self):
         self.net = op.network.Cubic(shape=[4, 4, 4])
         self.geo = op.geometry.GenericGeometry(network=self.net,
                                                pores=self.net.Ps,
                                                throats=self.net.Ts)
-        self.geo['pore.diameter'] = 1
-        self.geo['throat.diameter'] = 1
+        self.geo['pore.diameter'] = 1.0
+        self.geo['throat.diameter'] = 0.5
+        self.geo['throat.diffusive_size_factors'] = {
+            "pore1": 0.123, "throat": 0.981, "pore2": 0.551
+        }
         self.phase = op.phases.GenericPhase(network=self.net)
-        self.phase['pore.electrical_conductivity'] = 1
+        self.phase['pore.electrical_conductivity'] = 1.0
         self.phys = op.physics.GenericPhysics(network=self.net,
                                               phase=self.phase,
                                               geometry=self.geo)
 
-    def test_electrical_conductance_with_zero_length_throats(self):
-        self.geo['throat.conduit_lengths.pore1'] = 0.25
-        self.geo['throat.conduit_lengths.throat'] = 0.0
-        self.geo['throat.conduit_lengths.pore2'] = 0.15
-        mpo = op.models.geometry.diffusive_size_factors.spheres_and_cylinders
-        self.geo.add_model(propname='throat.diffusive_size_factors_zl',
-                           model=mpo)
-        mod = op.models.physics.electrical_conductance.generic_electrical
-        self.phys.add_model(propname='throat.electrical_conductance', model=mod,
-                            size_factors='throat.diffusive_size_factors_zl')
-        self.phys.regenerate_models()
-        actual = self.phys['throat.electrical_conductance'].mean()
-        assert_allclose(actual, desired=0.785398, rtol=1e-5)
-
     def test_generic_electrical(self):
-        self.geo['pore.diameter'] = 1.12
-        self.geo['throat.diameter'] = 0.56
-        L1, Lt, L2 = _conduit_lengths.spheres_and_cylinders(self.geo).T
-        self.geo['throat.conduit_lengths.pore1'] = L1
-        self.geo['throat.conduit_lengths.throat'] = Lt
-        self.geo['throat.conduit_lengths.pore2'] = L2
-        mpo2 = op.models.geometry.diffusive_size_factors.spheres_and_cylinders
-        self.geo.add_model(propname='throat.diffusive_size_factors',
-                           model=mpo2)
-        mod2 = op.models.physics.electrical_conductance.generic_electrical
-        self.phys.add_model(propname='throat.electrical_conductance_generic',
-                            model=mod2)
+        mod = op.models.physics.electrical_conductance.generic_electrical
+        self.phys.add_model(propname='throat.electrical_conductance', model=mod)
         self.phys.regenerate_models()
-        actual = np.mean(self.phys['throat.electrical_conductance_generic'])
-        assert_allclose(actual, desired=0.61263, rtol=1e-5)
+        actual = np.mean(self.phys['throat.electrical_conductance'])
+        assert_allclose(actual, desired=0.091205, rtol=1e-5)
+
+    def test_series_resistors(self):
+        mod = op.models.physics.electrical_conductance.series_resistors
+        self.phys.add_model(propname='throat.electrical_conductance', model=mod)
+        self.phys.regenerate_models()
+        actual = np.mean(self.phys['throat.electrical_conductance'])
+        assert_allclose(actual, desired=0.091205, rtol=1e-5)
 
 
 if __name__ == '__main__':
