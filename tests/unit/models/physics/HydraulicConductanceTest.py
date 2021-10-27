cd337660f2bf4c0de27c8b7fbf5c18223a3c6700
Amin Sadeghi <amin.sadeghi@live.com>
2021-10-27 18:04:14 -0400

    Fix hydraulic conductance

--- tests/unit/models/physics/HydraulicConductanceTest.py
+++ tests/unit/models/physics/HydraulicConductanceTest.py
@@ -1,10 +1,11 @@
 import openpnm as op
 import numpy as _np
 from numpy.testing import assert_allclose
-import openpnm.models.geometry.conduit_lengths as _conduit_lengths
+from openpnm.utils import remove_prop_deep
 
 
 class HydraulicConductanceTest:
+
     def setup_class(self):
         self.net = op.network.Cubic(shape=[5, 5, 5], spacing=1.0)
         self.geo = op.geometry.GenericGeometry(network=self.net,
@@ -19,92 +20,46 @@ class HydraulicConductanceTest:
         self.phys = op.physics.GenericPhysics(network=self.net,
                                               phase=self.phase,
                                               geometry=self.geo)
+        self.size_factors_dict = {"pore1": 0.123, "throat": 0.981, "pore2": 0.551}
 
     def teardown_class(self):
         mgr = op.Workspace()
         mgr.clear()
 
-    def test_generic_hydraulic(self):
-        # Pass size factors as dict
-        self.geo['throat.hydraulic_size_factors'] = {
-            "pore1": 0.123, "throat": 0.981, "pore2": 0.551
-        }
+    def test_generic_hydraulic_size_factors_as_dict(self):
+        self.geo['throat.hydraulic_size_factors'] = self.size_factors_dict
         mod = op.models.physics.hydraulic_conductance.generic_hydraulic
         self.phys.add_model(propname='throat.g_hydraulic_conductance', model=mod)
         self.phys.regenerate_models()
         actual = self.phys['throat.g_hydraulic_conductance'].mean()
         assert_allclose(actual, desired=9120.483231751232)
-        # Pass size factors as an array
-        for elem in ["pore1", "throat", "pore2"]:
-            del self.geo[f"throat.hydraulic_size_factors.{elem}"]
+        remove_prop_deep(self.geo, "throat.hydraulic_size_factors")
+
+    def test_generic_hydraulic_size_factors_as_array(self):
         self.geo['throat.hydraulic_size_factors'] = 0.896
         self.phys.regenerate_models("throat.g_hydraulic_conductance")
         actual = self.phys['throat.g_hydraulic_conductance'].mean()
         assert_allclose(actual, desired=89600.0)
+        remove_prop_deep(self.geo, "throat.hydraulic_size_factors")
 
     def test_hagen_poiseuille(self):
-        del self.geo['throat.hydraulic_size_factors']
-        self.geo['throat.hydraulic_size_factors'] = {
-            "pore1": 0.123, "throat": 0.981, "pore2": 0.551
-        }
+        self.geo['throat.hydraulic_size_factors'] = self.size_factors_dict
         mod = op.models.physics.hydraulic_conductance.hagen_poiseuille
         self.phys.add_model(propname='throat.hydraulic_conductance', model=mod)
         actual = self.phys['throat.hydraulic_conductance'].mean()
         assert_allclose(actual, desired=9120.483231751232)
 
-    def test_hagen_poiseuille_2d(self):
-        mod = op.models.geometry.hydraulic_size_factors.circles_and_rectangles
-        self.geo.add_model(propname='throat.hydraulic_size_factor_2d', model=mod)
-        mod = op.models.physics.hydraulic_conductance.hagen_poiseuille
-        self.phys.add_model(propname='throat.HP_hydraulic_conductance', model=mod,
-                            size_factors='throat.hydraulic_size_factor_2d')
-        actual = self.phys['throat.HP_hydraulic_conductance'].mean()
-        assert_allclose(actual, desired=2972.1064015105035)
-
-    def test_hagen_poiseuille_zero_length_throat(self):
-        self.geo['throat.hydraulic_size_factors_zl'] = {
-            "pore1": 0.123, "throat": _np.inf, "pore2": 0.551
-        }
-        mod = op.models.physics.hydraulic_conductance.hagen_poiseuille
-        self.phys.add_model(propname='throat.hydraulic_conductance',
-                            model=mod,
-                            size_factors='throat.hydraulic_size_factor_zl')
-        actual = self.phys['throat.hydraulic_conductance'].mean()
-        assert_allclose(actual, desired=9120.4832317)
-
     def test_valvatne_blunt(self):
-        L1, Lt, L2 = _conduit_lengths.spheres_and_cylinders(self.geo).T
-        self.geo['throat.conduit_lengths.pore1'] = L1
-        self.geo['throat.conduit_lengths.throat'] = Lt
-        self.geo['throat.conduit_lengths.pore2'] = L2
-        self.phase = op.phases.GenericPhase(network=self.net)
-        self.phase['pore.viscosity'] = 1e-5
-        self.phys = op.physics.GenericPhysics(network=self.net,
-                                              phase=self.phase,
-                                              geometry=self.geo)
+        self.geo['throat.conduit_lengths.pore1'] = 0.15
+        self.geo['throat.conduit_lengths.throat'] = 0.2
+        self.geo['throat.conduit_lengths.pore2'] = 0.26
+        self.geo['pore.shape_factor'] = _np.ones(self.geo.Np) * 0.15
+        self.geo['throat.shape_factor'] = _np.ones(self.geo.Nt) * 0.15
         mod = op.models.physics.hydraulic_conductance.valvatne_blunt
-        sf = _np.sqrt(3) / 36.0
-        self.geo['pore.shape_factor'] = _np.ones(self.geo.Np) * sf
-        self.geo['throat.shape_factor'] = _np.ones(self.geo.Nt) * sf
         self.phys.add_model(propname='throat.valvatne_conductance', model=mod)
         actual = self.phys['throat.valvatne_conductance'].mean()
-        desired = 2059.13571716
+        desired = 6198.347107
         assert_allclose(actual, desired=desired)
-
-    def test_classic_hagen_poiseuille(self):
-        mod = op.models.geometry.hydraulic_size_factors.ncylinders_in_series
-        self.geo.add_model(propname='throat.hydraulic_size_factors', model=mod)
-        self.air = op.phases.Air(network=self.net)
-        self.phys = op.physics.GenericPhysics(network=self.net,
-                                              phase=self.air,
-                                              geometry=self.geo)
-        mod = op.models.physics.hydraulic_conductance.classic_hagen_poiseuille
-        self.phys.add_model(propname='throat.conductance',
-                            model=mod)
-        print(self.phys['throat.conductance'][0])
-        assert _np.allclose(a=self.phys['throat.conductance'][0],
-                            b=2.1118499573886901e-07)
-
 
 
 if __name__ == '__main__':
