1ee37c37432fbbd6bd0382ca09972979a56d7dec
Mike McKague <m.mckague19@gmail.com>
2021-10-28 13:12:38 -0400

    Calling circle and rectangle size factors in StickAndBall2D now

--- openpnm/geometry/StickAndBall2D.py
+++ openpnm/geometry/StickAndBall2D.py
@@ -231,6 +231,6 @@ class StickAndBall2D(GenericGeometry):
                        throat_endpoints='throat.endpoints',
                        throat_length='throat.length')
         self.add_model(propname='throat.hydraulic_size_factors',
-                       model=mods.geometry.hydraulic_size_factors.cones_and_cylinders)
+                       model=mods.geometry.hydraulic_size_factors.circles_and_rectangles)
         self.add_model(propname='throat.diffusive_size_factors',
-                       model=mods.geometry.diffusive_size_factors.cones_and_cylinders)
+                       model=mods.geometry.diffusive_size_factors.circles_and_rectangles)
