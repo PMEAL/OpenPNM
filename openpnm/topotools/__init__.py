4a2e0f17eaf6deb92c033f978f8b122c91ac3f64
Amin Sadeghi <amin.sadeghi@live.com>
2021-11-09 16:34:05 -0500

    Modify docstrings of topotools' init file

--- openpnm/topotools/__init__.py
+++ openpnm/topotools/__init__.py
@@ -1,76 +1,110 @@
 r"""
-
-**openpnm.topotools**
-
-----
+====================================
+TopoTools (:mod:`openpnm.topotools`)
+====================================
 
 This module contains a selection of functions that deal specifically with
 network topology.
 
+.. currentmodule:: openpnm.topotools
+
+Topological manipulation
+------------------------
+
+.. autosummary::
+   :template: mybase.rst
+   :toctree: generated/
+   :nosignatures:
+
+   add_boundary_pores
+   clone_pores
+   connect_pores
+   dimensionality
+   extend
+   filter_pores_by_z
+   find_surface_pores
+   find_pore_to_pore_distance
+   from_cylindrical
+   from_spherical
+   generate_base_points
+   get_shape
+   get_spacing
+   iscoplanar
+   isoutside
+   is_fully_connected
+   label_faces
+   merge_networks
+   merge_pores
+   reduce_coordination
+   reflect_base_points
+   rotate_coords
+   shear_coords
+   stitch
+   stitch_pores
+   subdivide
+   template_cylinder_annulus
+   template_sphere_shell
+   to_cylindrical
+   to_spherical
+   trim
+   trim_occluded_throats
+
+Percolation tools
+-----------------
+
+.. autosummary::
+   :template: mybase.rst
+   :toctree: generated/
+   :nosignatures:
+
+   ispercolating
+   remove_isolated_clusters
+   site_percolation
+   bond_percolation
+   find_clusters
+   find_path
+
+Graph tools
+-----------
+
+.. autosummary::
+   :template: mybase.rst
+   :toctree: generated/
+   :nosignatures:
+
+   drop_sites
+   find_neighbor_sites
+   find_neighbor_bonds
+   find_connected_sites
+   find_connecting_bonds
+   find_complement
+   istriu
+   istril
+   istriangular
+   issymmetric
+   tri_to_am
+   vor_to_am
+   conns_to_am
+
+Plot tools
+----------
+
+.. autosummary::
+   :template: mybase.rst
+   :toctree: generated/
+   :nosignatures:
+
+   plot_tutorial
+   plot_connections
+   plot_coordinates
+   plot_networkx
+   plot_network_jupyter
+   generate_voxel_image
 
 """
 
-from .topotools import add_boundary_pores
-from .topotools import clone_pores
-from .topotools import connect_pores
-from .topotools import dimensionality
-from .topotools import extend
-from .topotools import filter_pores_by_z
-from .topotools import find_surface_pores
-from .topotools import find_pore_to_pore_distance
-from .topotools import from_cyl
-from .topotools import from_sph
-from .topotools import generate_base_points
-from .topotools import get_shape
-from .topotools import get_spacing
-from .topotools import iscoplanar
-from .topotools import isoutside
-from .topotools import is_fully_connected
-from .topotools import label_faces
-from .topotools import merge_networks
-from .topotools import merge_pores
-from .topotools import reduce_coordination
-from .topotools import reflect_base_points
-from .topotools import rotate_coords
-from .topotools import shear_coords
-from .topotools import stitch
-from .topotools import stitch_pores
-from .topotools import subdivide
-from .topotools import template_cylinder_annulus
-from .topotools import template_sphere_shell
-from .topotools import to_cyl
-from .topotools import to_sph
-from .topotools import trim
-from .topotools import trim_occluded_throats
-
-from .perctools import ispercolating
-from .perctools import remove_isolated_clusters
-from .perctools import site_percolation
-from .perctools import bond_percolation
-from .perctools import find_clusters
-from .perctools import find_path
-
-from .graphtools import drop_sites
-from .graphtools import find_neighbor_sites
-from .graphtools import find_neighbor_bonds
-from .graphtools import find_connected_sites
-from .graphtools import find_connecting_bonds
-from .graphtools import find_complement
-from .graphtools import istriu
-from .graphtools import istril
-from .graphtools import istriangular
-from .graphtools import issymmetric
-from .graphtools import _am_to_im
-from .graphtools import _im_to_am
-from .graphtools import tri_to_am
-from .graphtools import vor_to_am
-from .graphtools import conns_to_am
-
-from .plottools import plot_tutorial
-from .plottools import plot_connections
-from .plottools import plot_coordinates
-from .plottools import plot_networkx
-from .plottools import plot_network_jupyter
-from .plottools import generate_voxel_image
-
+from .topotools import *
+from .perctools import *
+from .graphtools import *
+from .plottools import *
 from . import generators
