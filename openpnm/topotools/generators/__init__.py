5fe4dab51c8b7c725b49bd6352fbf531003ead4e
Amin Sadeghi <amin.sadeghi@live.com>
2021-11-09 16:33:39 -0500

    Add docstrings to generators' init file

--- openpnm/topotools/generators/__init__.py
+++ openpnm/topotools/generators/__init__.py
@@ -1,3 +1,29 @@
+r"""
+================================================
+Generators (:mod:`openpnm.topotools.generators`)
+================================================
+
+This module contains a selection of functions that deal specifically with
+generating sufficient information that can be turned into an openpnm network.
+
+.. currentmodule:: openpnm.topotools.generators
+
+.. autosummary::
+   :template: mybase.rst
+   :toctree: generated/
+   :nosignatures:
+
+   cubic
+   delaunay
+   gabriel
+   voronoi
+   voronoi_delaunay_dual
+   cubic_template
+   fcc
+   bcc
+
+"""
+
 from .cubic import cubic
 from .delaunay import delaunay
 from .gabriel import gabriel
