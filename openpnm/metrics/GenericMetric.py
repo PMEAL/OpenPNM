de2877eccfa584b01dbe13ac43a0f4b9d35260b4
Ni2M <n2misagh@uwaterloo.ca>
2021-11-12 15:20:37 -0500

    restore the genericMetrics from dev

--- openpnm/metrics/GenericMetric.py
+++ openpnm/metrics/GenericMetric.py
@@ -1,24 +1,15 @@
 from openpnm.core import Base
-from openpnm.utils import logging, SettingsAttr
+from openpnm.utils import logging
 import numpy as np
 logger = logging.getLogger(__name__)
 
 
-class GenericMetricSettings:
-    r"""
-
-    """
-    prefix = 'metric'
-
-
 class GenericMetric(Base):
     r"""
-
     """
 
-    def __init__(self, network=None, project=None, settings={}, **kwargs):
-        self.settings = SettingsAttr(GenericMetricSettings, settings)
-        super().__init__(project=project, **kwargs)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
         project = self.project
         self['pore.all'] = np.ones((project.network.Np, ), dtype=bool)
         self['throat.all'] = np.ones((project.network.Nt, ), dtype=bool)
