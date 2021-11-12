317016e4e83c1261ad8e8c332f38443be31db8b0
Ni2M <n2misagh@uwaterloo.ca>
2021-11-12 08:09:49 -0500

    changes one setting to see where it breaks

--- openpnm/metrics/GenericMetric.py
+++ openpnm/metrics/GenericMetric.py
@@ -1,10 +1,14 @@
 from openpnm.core import Base
-from openpnm.utils import logging, Docorator, prettify_logger_message
+from openpnm.utils import logging, SettingsAttr
 import numpy as np
-from openpnm.topotools import iscoplanar, is_fully_connected, dimensionality
-from scipy.spatial import ConvexHull
-from scipy.spatial import cKDTree
 logger = logging.getLogger(__name__)
+
+
+class GenericMetricSettings:
+    r"""
+
+    """
+    prefix = 'metric'
 
 
 class GenericMetric(Base):
@@ -13,7 +17,7 @@ class GenericMetric(Base):
     """
 
     def __init__(self, network=None, project=None, settings={}, **kwargs):
-        self.settings['prefix'] = 'metric'
+        self.settings = SettingsAttr(GenericMetricSettings, settings)
         super().__init__(project=project, **kwargs)
         project = self.project
         self['pore.all'] = np.ones((project.network.Np, ), dtype=bool)
