bcec3eaa6209a1ddcc47b83fac95f13fb9c03b3e
jgostick <jgostick@gmail.com>
2021-11-13 22:33:13 -0500

    rearranged the topology models to network, transferred some from topotools

---
+++ openpnm/models/network/__init__.py
@@ -0,0 +1,13 @@
+r"""
+
+**openpnm.models.network**
+
+----
+
+This submodule contains models for calculating topological properties of
+networks
+
+"""
+
+from .topology import *
+from .health import *
--- openpnm/models/network/health.py
+++ openpnm/models/network/health.py
@@ -1,62 +1,14 @@
-from numpy.linalg import norm as _norm
-import numpy as _np
+import numpy as np
 
 
-r"""
-Pore-scale models related to topology of the network.
-"""
-
-
-def coordination_number(target):
-    r"""
-    Find the number of neighbors for each pore
-    """
-    network = target.network
-    N = network.num_neighbors(pores=network.Ps, flatten=False)
-    return N
-
-
-def pore_to_pore_distance(target):
-    r"""
-    Find the center to center distance between each pair of pores
-    """
-    network = target.project.network
-    cn = network['throat.conns']
-    C1 = network['pore.coords'][cn[:, 0]]
-    C2 = network['pore.coords'][cn[:, 1]]
-    values = _norm(C1 - C2, axis=1)
-    return values
-
-
-def distance_to_nearest_neighbor(target):
-    r"""
-    Find the distance between each pore and its closest topological neighbor
-    """
-    network = target.project.network
-    cn = network['throat.conns']
-    C1 = network['pore.coords'][cn[:, 0]]
-    C2 = network['pore.coords'][cn[:, 1]]
-    D = _norm(C1 - C2, axis=1)
-    im = network.create_incidence_matrix()
-    values = _np.ones((network.Np, ))*_np.inf
-    _np.minimum.at(values, im.row, D[im.col])
-    return _np.array(values)
-
-
-def distance_to_furthest_neighbor(target):
-    r"""
-    Find the distance between each pore and its furthest topological neighbor
-    """
-    network = target.project.network
-    throats = network.map_throats(throats=target.Ts, origin=target)
-    cn = network['throat.conns'][throats]
-    C1 = network['pore.coords'][cn[:, 0]]
-    C2 = network['pore.coords'][cn[:, 1]]
-    D = _norm(C1 - C2, axis=1)
-    im = network.create_incidence_matrix()
-    values = _np.zeros((network.Np, ))
-    _np.maximum.at(values, im.row, D[im.col])
-    return _np.array(values)
+__all__ = [
+    'cluster_number',
+    'cluster_size',
+    'isolated_pores',
+    'reversed_throats',
+    'looped_throats',
+    'headless_throats',
+    ]
 
 
 def cluster_number(target):
@@ -96,7 +48,7 @@ def cluster_size(target, cluster=None):
         N, cluster_num = csg.connected_components(am, directed=False)
     else:
         cluster_num = net[cluster]
-    Cs, ind, N = _np.unique(cluster_num, return_inverse=True, return_counts=True)
+    Cs, ind, N = np.unique(cluster_num, return_inverse=True, return_counts=True)
     values = N[ind]
     return values
 
@@ -106,8 +58,8 @@ def isolated_pores(target):
     find which pores, if any, are not connected to a throat
     """
     net = target.network
-    values = _np.ones(net.Np, dtype=bool)
-    hits = _np.unique(net.conns)
+    values = np.ones(net.Np, dtype=bool)
+    hits = np.unique(net.conns)
     values[hits] = False
     return values
 
@@ -135,7 +87,7 @@ def headless_throats(target):
     Find any throats that point to a non-existent pore
     """
     net = target.network
-    hits = _np.any(net.conns > (net.Np -1), axis=1)
+    hits = np.any(net.conns > (net.Np -1), axis=1)
     return hits
 
 
@@ -146,23 +98,9 @@ def duplicate_throats(target):
     net = target.network
     conns = net.conns
     iconns = conns[:, 0] + 1j*conns[:, 1]
-    hits, inds = _np.unique(iconns, return_inverse=True)
-    values = _np.ones(net.Nt, dtype=bool)
+    hits, inds = np.unique(iconns, return_inverse=True)
+    values = np.ones(net.Nt, dtype=bool)
     values[inds] = False
-    return values
-
-
-def distance_to_nearest_pore(target):
-    r"""
-    Find distance to and index of nearest pore even if not topologically
-    connected
-    """
-    import scipy.spatial as sptl
-    net = target.network
-    coords = net.coords
-    tree = sptl.KDTree(coords)
-    ds, ids = tree.query(coords, k=2)
-    values = ds[:, 1]
     return values
 
 
@@ -189,9 +127,9 @@ def count_coincident_pores(target, thresh=1e-6):
     coords = net.coords
     tree = sptl.KDTree(coords)
     hits = tree.query_pairs(r=thresh)
-    arr = _np.array(list(hits)).flatten()
-    v, n = _np.unique(arr, return_counts=True)
-    values = _np.zeros(net.Np, dtype=int)
+    arr = np.array(list(hits)).flatten()
+    v, n = np.unique(arr, return_counts=True)
+    values = np.zeros(net.Np, dtype=int)
     values[v.astype(int)] = n
     return values
 
@@ -215,7 +153,6 @@ def find_coincident_pores(target, thresh=1e-6):
         within a distance of ``thresh``.
 
     """
-
     # This needs to be a bit complicated because it cannot be assumed
     # the coincident pores are topologically connected
     import scipy.spatial as sptl
