186e0ac66c56a6f54d5b46a6c43fbf046d0cf8c4
jgostick <jgostick@uwaterloo.ca>
2021-11-17 10:57:55 -0500

    removing from init files

--- openpnm/__init__.py
+++ openpnm/__init__.py
@@ -1,54 +1,9 @@
 r"""
 
-::
-
-     o-o                o--o  o   o o   o
-    o   o               |   | |\  | |\ /|
-    |   | o-o  o-o o-o  o--o  | \ | | o |
-    o   o |  | |-' |  | |     |  \| |   |
-     o-o  o-o  o-o o  o o     o   o o   o
-          |
-          o
-
 **OpenPNM**
 
 OpenPNM is a package for performing pore network simulations of transport in
 porous materials.
-
-It consists of the following submodules:
-
-+----------------+------------------------------------------------------------+
-| Submodule      | Contents and Description                                   |
-+================+============================================================+
-| ``core``       | Houses the ``Base`` & ``Subdomain`` classes, & model       |
-|                | related mixins                                             |
-+----------------+------------------------------------------------------------+
-| ``network``    | ``GenericNetwork`` class plus various network generators   |
-+----------------+------------------------------------------------------------+
-| ``geometry``   | ``GenericGeometry`` class plus some subclasses containing a|
-|                | predefined set of pore-scale models                        |
-+----------------+------------------------------------------------------------+
-| ``phases``     | ``GenericPhase`` class plus some subclasses containing     |
-|                | predefined models for common fluids like Water             |
-+----------------+------------------------------------------------------------+
-| ``physics``    | ``GenericPhysics`` class plus some subclasses containing a |
-|                | predefined set of pore-scale models                        |
-+----------------+------------------------------------------------------------+
-| ``algorithms`` | Algorithms for simulating transport and percolation        |
-+----------------+------------------------------------------------------------+
-| ``materials``  | A collection of predefined projects consisting of a network|
-|                | with suitable topology and a geometry with necessary models|
-+----------------+------------------------------------------------------------+
-| ``topotools``  | Tools for querying and manipulating network topology       |
-+----------------+------------------------------------------------------------+
-| ``io``         | Import from and export to various common data formats      |
-+----------------+------------------------------------------------------------+
-| ``utils``      | Helper utilites & classes, including ``Workspace`` and     |
-|                |  ``Project``                                               |
-+----------------+------------------------------------------------------------+
-| ``models``     | Library of pore-scale models for calculating geometric,    |
-|                | thermodynamic, and physical properties                     |
-+----------------+------------------------------------------------------------+
 
 """
 
