0b7c53fa2eb3951198db1553a5b672c605783971
Hamed Fathiannasab <hamed.fathian@gmail.com>
2021-11-25 14:51:26 -0500

    Update error when unknown filetype is passed to Project.export_data

--- openpnm/utils/Project.py
+++ openpnm/utils/Project.py
@@ -563,7 +563,9 @@ class Project(list):
                     filetype = temp[filetype]
             else:
                 raise Exception('File type not given')
-
+        temp = {"hdf": "hdf5", "xmf": "xdmf", "vtp": "vtk", "pkl": "pickle"}
+        if not filetype in temp.keys():
+            raise Exception('File type is not supported')
         # Fetch correct io class, using case insensitive look-up
         def igetattr(obj, attr):
             for a in dir(obj):
