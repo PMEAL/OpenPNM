0fc3b67f776901452dca4f5b5798418e3e01f736
jgostick <jgostick@gmail.com>
2021-12-07 22:12:03 -0500

    removing unused Settings classes left over from dev

--- openpnm/utils/_settings.py
+++ openpnm/utils/_settings.py
@@ -2,13 +2,16 @@ import logging
 from copy import deepcopy
 from traits.api import HasTraits, Trait
 from openpnm.utils import PrintableDict
-from auto_all import start_all, end_all
 
 
 logger = logging.getLogger(__name__)
+__all__ = [
+    'TypedMixin',
+    'TypedSet',
+    'TypedList',
+    'SettingsAttr',
+    ]
 
-
-start_all()
 
 class TypedMixin:
     """Brief explanation of 'TypedMixin'"""
@@ -152,158 +155,3 @@ class SettingsAttr:
 
     def __repr__(self):
         return self.__str__()
-
-
-class Settings(dict):
-    """Brief explanation of 'Settings'"""
-
-    __instance__ = None
-
-    def __new__(cls, *args, **kwargs):
-        if Settings.__instance__ is None:
-            Settings.__instance__ = super().__new__(cls)
-        return Settings.__instance__
-
-    def get_settings(self, cls):
-        c = cls()
-        self[c.__class__.__name__] = cls
-        return cls
-
-    def set_settings(self):
-        s = SettingsAttr()
-        for k, v in self.items():
-            s._update(v, docs=True)
-        return s
-
-
-class SettingsData(HasTraits):
-    """Brief explanation of 'SettingsData'"""
-
-    def __str__(self):
-        d = PrintableDict()
-        d._key = 'Settings'
-        d._value = 'Values'
-        for item in self.visible_traits():
-            d[item] = getattr(self, item)
-        return d.__str__()
-
-    def __repr__(self):
-        return self.__str__()
-
-
-class SettingsAttr2:
-
-    def __init__(self, settings):
-        r"""
-        Brief explanation of 'SettingsAttr2'
-
-        Parameters
-        ----------
-        settings : key-value collection
-            Ideally should be an `SettingsData`` object which is a subclass
-            of a the ``HasTraits`` class from the ``traits`` package. The
-            docstring for this class is adopted from the received argument only
-            **if** a ``HasTraits`` class is received. This is meant to work
-            with ``docrep`` so the inherited docstring on the ``SettingsData``
-            object will be shown by this wrapper. Optionally ``settings`` can
-            be a ``dict`` or any ``dataclasss``-type object with data stored
-            as attributes.
-
-        Notes
-        -----
-        This class is a wrapper around the ``HasTraits`` class from the
-        ``traits`` package. The main point is the provide a clean namespace
-        on the object so that only the specified attributes show up. It also
-        allows for combining multiple objects into one using the ``_update``
-        method.
-
-        """
-        # Add blank HasTraits object to self._settings
-        if isinstance(settings, SettingsData):
-            super().__setattr__('_settings', settings)
-        else:
-            super().__setattr__('_settings', SettingsData())
-            self._update(settings)
-        self._settings.__doc__ = settings.__doc__
-
-    def __setattr__(self, attr, val):
-        if attr in self._settings.visible_traits():
-            setattr(self._settings, attr, val)
-        else:
-            val = Trait(val, val.__class__)
-            self._settings.add_trait(attr, val)
-
-    def __getattr__(self, attr):
-        return getattr(self._settings, attr)
-
-    def __delattr__(self, attr):
-        self._settings.__delattr__(attr)
-
-    def __getitem__(self, key):
-        return getattr(self._settings, key)
-
-    def __setitem__(self, key, value):
-        setattr(self._settings, key, value)
-
-    def __dir__(self):
-        temp = self._settings.visible_traits()
-        temp = [i for i in temp if not i.startswith('_')]
-        return temp
-
-    def __contains__(self, attr):
-        return attr in self._settings.visible_traits()
-
-    def _update(self, settings, docs=False):
-        r"""
-        Merges new key-value collections onto the existing object.
-
-        This method allows several collections to be combined. It is hidden
-        to avoid polluting the name space, which is reserved for actual data
-        attributes.
-
-        Parameters
-        ----------
-        settings : key-value collection
-            Can be either a `SettingsData`` object which is a subclass
-            of a the ``HasTraits`` class from the ``traits`` package, or a
-            ``dict`` or ``dataclasss`` object.
-
-        Notes
-        -----
-        In the case of ``dict`` or ``dataclass``-like objects, the data-types
-        for the attributes are inferred from the values recieved. In all cases,
-        the docs of the ``settings`` are ignored; this can only be set during
-        init.
-
-        """
-        if hasattr(settings, 'items'):  # Dictionary
-            for k, v in settings.items():
-                setattr(self, k, v)
-        elif hasattr(settings, 'visible_traits'):
-            for k in settings.visible_traits():
-                v = getattr(settings, k)
-                self._settings.add_trait(k, Trait(v, v.__class__))
-        else:
-            attrs = [i for i in dir(settings) if not i.startswith('_')]
-            for k in attrs:
-                v = getattr(settings, k)
-                setattr(self, k, v)
-        if docs:
-            self._settings.__doc__ = settings.__doc__
-
-    def __str__(self):
-        return self._settings.__str__()
-
-    def __repr__(self):
-        return self.__str__()
-
-    @property
-    def __doc__(self):
-        return self._settings.__doc__
-
-    @property
-    def _attrs(self):
-        return self.__dir__()
-
-
-end_all()
