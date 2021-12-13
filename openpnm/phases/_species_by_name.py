325d96b555b86e1fea76516a4e86fef3c16f612a
jgostick <jgostick@gmail.com>
2021-12-12 22:49:10 -0500

    removed added LJ data since chemicals now has it

--- openpnm/phases/_species_by_name.py
+++ openpnm/phases/_species_by_name.py
@@ -17,10 +17,10 @@ class SpeciesByName(GenericSpecies):
         The network to which this phase object will be attached.
     species : str
         The name of the species to generate.  This is used to lookup tabulated
-        constants in the ``chemicals`` package, which attempts to find a match. For
-        instance, 'water', 'Water', and 'H2O' all work.
+        constants in the ``chemicals`` package, which attempts to find a match.
+        For instance, 'water', 'Water', and 'H2O' all work.
     name : str, optional
-        The name of the phase.  This is useful to keep track of the objects
+        The name of the phase. This is useful to keep track of the objects
         throughout the simulation.  The name must be unique to the project.
         If no name is given, one is generated.
 
@@ -32,7 +32,7 @@ class SpeciesByName(GenericSpecies):
         self.settings['CAS'] = CAS
         a = chem.identifiers.search_chemical(CAS)
         self.settings['common_name'] = a.common_name
-        self['param.molecular_weight'] = a.MW
+        self['param.molecular_weight'] = a.MW/1000  # Convert to kg/mol
         self['param.critical_temperature'] = chem.critical.Tc(CAS)
         self['param.critical_pressure'] = chem.critical.Pc(CAS)
         self['param.critical_volume'] = chem.critical.Vc(CAS)
@@ -41,18 +41,15 @@ class SpeciesByName(GenericSpecies):
         self['param.melting_temperature'] = chem.Tm(CAS)
         self['param.acentric_factor'] = chem.acentric.omega(CAS)
         self['param.dipole_moment'] = chem.dipole.dipole_moment(CAS)
-        if CAS in extra_LJ.keys():
-            s, e_k = extra_LJ[CAS]
-        else:
-            e_k = chem.lennard_jones.Stockmayer(CAS)
-            s = chem.lennard_jones.molecular_diameter(CAS)
-        if e_k is not None:
-            self['param.lennard_jones_epsilon'] = e_k*k
-        if s is not None:
-            self['param.lennard_jones_sigma'] = s
+        self['param.lennard_jones_epsilon'] = k*chem.lennard_jones.Stockmayer(CAS)
+        self['param.lennard_jones_sigma'] = chem.lennard_jones.molecular_diameter(CAS)
 
 
 class GasByName(SpeciesByName):
+    r"""
+    Creates a phase object based on given chemical name, including some
+    additional properties for a gas
+    """
     def __init__(self, species, **kwargs):
         super().__init__(species=species, **kwargs)
         self.add_model(propname='pore.heat_capacity',
@@ -64,6 +61,10 @@ class GasByName(SpeciesByName):
 
 
 class LiquidByName(SpeciesByName):
+    r"""
+        Creates a phase object based on given chemical name, including some
+        additional properties for a liquid
+    """
     def __init__(self, species, **kwargs):
         super().__init__(species=species, **kwargs)
         self.add_model(propname='pore.heat_capacity',
@@ -76,7 +77,3 @@ class LiquidByName(SpeciesByName):
                        model=mods.density.liquid_density)
         self.add_model(propname='pore.vapor_pressure',
                        model=mods.vapor_pressure.vapor_pressure)
-
-
-extra_LJ = {}
-extra_LJ['7727-37-9'] = (3.788, 71.4)
