488d914d458452c170718b8f6effbbb8f06f9418
Amin Sadeghi <amin.sadeghi@live.com>
2021-12-15 13:41:58 -0500

    Add unit test for Salome IO class

---
+++ tests/unit/io/SalomeTest.py
@@ -0,0 +1,40 @@
+import os
+import py
+import numpy as np
+import openpnm as op
+from openpnm.models.misc import from_neighbor_pores
+
+
+class SalomeTest:
+
+    def setup_class(self):
+        self.net = op.network.Cubic(shape=[2, 2, 2])
+        self.net["pore.diameter"] = 0.5 + np.random.rand(self.net.Np) * 0.5
+        Dt = from_neighbor_pores(target=self.net, prop="pore.diameter", mode="min") * 0.5
+        self.net["throat.diameter"] = Dt
+        self.net["throat.length"] = 1.0
+
+    def teardown_class(self):
+        os.remove(f"{self.net.name}.py")
+        os.remove("salome_custom.py")
+
+    def test_export_data_salome(self):
+        op.io.Salome.export_data(network=self.net)
+        assert os.path.isfile(f"{self.net.name}.py")
+        op.io.Salome.export_data(network=self.net, filename="salome_custom")
+        assert os.path.isfile("salome_custom.py")
+
+
+if __name__ == '__main__':
+    # All the tests in this file can be run with 'playing' this file
+    t = SalomeTest()
+    self = t  # For interacting with the tests at the command line
+    t.setup_class()
+    for item in t.__dir__():
+        if item.startswith('test'):
+            print(f'Running test: {item}')
+            try:
+                t.__getattribute__(item)()
+            except TypeError:
+                t.__getattribute__(item)(tmpdir=py.path.local())
+    t.teardown_class()
