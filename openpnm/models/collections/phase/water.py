3309f8ded927852fb1b59442471080f199f7d2cd
jgostick <jgostick@gmail.com>
2021-12-15 00:39:51 -0500

    starting on phases

--- openpnm/models/collections/phase/water.py
+++ openpnm/models/collections/phase/water.py
@@ -1,6 +1,41 @@
 import openpnm.models as mods
 
 water = {
+    'pore.molecular_weight': {
+        'model': mods.misc.constant,
+        'value': 0.01802,
+        'regen_mode': 'deferred',
+        },
+    'pore.critical_pressure': {
+        'model': mods.misc.constant,
+        'value': 2.2064E7,
+        'regen_mode': 'deferred',
+        },
+    'pore.critical_temperature': {
+        'model': mods.misc.constant,
+        'value': 647.1,
+        'regen_mode': 'deferred',
+        },
+    'pore.critical_volume': {
+        'model': mods.misc.constant,
+        'value': 0.003106,
+        'regen_mode': 'deferred',
+        },
+    'pore.contact_angle': {
+        'model': mods.misc.constant,
+        'value': 110.0,
+        'regen_mode': 'deferred',
+        },
+    'pore.electrical_conductivity': {
+        'model': mods.misc.constant,
+        'value': 1e-15,
+        'regen_mode': 'deferred',
+        },
+    'pore.diffusivity': {
+        'model': mods.misc.constant,
+        'value':  1e-9,
+        'regen_mode': 'deferred',
+        },
     'pore.density': {
         'model': mods.phases.density.water,
         'regen_mode': 'deferred',
--- openpnm/phases/_air.py
+++ openpnm/phases/_air.py
@@ -1,5 +1,5 @@
+from openpnm.models.collections.phase import air
 from openpnm.phases import GenericPhase
-import openpnm.models as mods
 from openpnm.utils import Docorator
 
 
@@ -29,28 +29,5 @@ class Air(GenericPhase):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-
-        self['pore.molecular_weight'] = 0.0291
-        self['pore.critical_pressure'] = 3.786E6
-        self['pore.critical_temperature'] = 132.5
-        self['pore.critical_volume'] = 0.002917
-        self['pore.contact_angle'] = 180.0
-        self['pore.surface_tension'] = 0.072
-        self.add_model(propname='pore.molar_density',
-                       model=mods.phases.molar_density.ideal_gas)
-        self.add_model(propname='pore.diffusivity',
-                       model=mods.phases.diffusivity.fuller,
-                       MA=0.032, MB=0.028,
-                       vA=16.6, vB=17.9)
-        self.add_model(propname='pore.thermal_conductivity',
-                       model=mods.misc.polynomial,
-                       prop='pore.temperature',
-                       a=[0.00422791, 0.0000789606, -1.56383E-08])
-        self.add_model(propname='pore.electrical_conductivity',
-                       model=mods.misc.constant,
-                       value=1e-15)
-        self.add_model(propname='pore.viscosity',
-                       model=mods.misc.polynomial,
-                       prop='pore.temperature',
-                       a=[0.00000182082, 6.51815E-08, -3.48553E-11,
-                          1.11409E-14])
+        self.models.update(air)
+        self.regenerate_models()
