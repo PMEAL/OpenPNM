82b3660e3e70de81e16967cfc4b3ae2aba564fa6
Ni2M <n2misagh@uwaterloo.ca>
2021-12-20 10:26:47 -0500

    changed dims str in xdmf file

--- openpnm/io/_xdmf.py
+++ openpnm/io/_xdmf.py
@@ -127,7 +127,8 @@ class XDMF(GenericIO):
                     ):
                         attr_type = 'Scalar'
                         shape = D[item].shape
-                        dims = (''.join([str(i) + ' ' for i in list(shape)[::-1]]))
+                        #dims = (''.join([str(i) + ' ' for i in list(shape)[::-1]]))
+                        dims = (' '.join([str(i) for i in shape]))
                         if '@' in item:
                             item = item.split('@')[0]+'@t'
                             hdf_loc = fname_hdf + ":" + item
