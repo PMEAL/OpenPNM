056b236be60f642b57a5a2a9d020148c5b1939d0
Mike McKague <m.mckague19@gmail.com>
2021-12-22 14:47:25 -0500

    Fixed style in unit test

--- tests/unit/algorithms/TransientMultiPhysicsTest.py
+++ tests/unit/algorithms/TransientMultiPhysicsTest.py
@@ -9,7 +9,7 @@ class TransientMultiPhysicsTest:
 
     def setup_class(self):
         np.random.seed(10)
-        spacing=1e-1 
+        spacing = 1e-1
         # 2d network
         self.net = op.network.Cubic(shape=[10, 10, 1], spacing=spacing)
         # geometry
@@ -40,17 +40,17 @@ class TransientMultiPhysicsTest:
                            throat_diameter="throat.diameter")
         # phase and physics
         self.air = op.phase.Air(network=self.net)
-        self.phys = op.physics.GenericPhysics(network=self.net, 
-                                              phase=self.air, 
+        self.phys = op.physics.GenericPhysics(network=self.net,
+                                              phase=self.air,
                                               geometry=self.geo)
         # add diffusive conductance model
         self.air['pore.temperature'] = 300
         self.air.add_model(propname='pore.diffusivity',
-                           model=op.models.misc.linear, 
+                           model=op.models.misc.linear,
                            m=1.860793056e-06,
                            b=-0.0005375624384,
                            prop='pore.temperature')
-        self.phys.add_model(propname='throat.diffusive_conductance', 
+        self.phys.add_model(propname='throat.diffusive_conductance',
                             model=pm.diffusive_conductance.generic_diffusive)
         # add thermal conductance model
         self.air.remove_model(propname='pore.thermal_conductivity')
@@ -59,24 +59,24 @@ class TransientMultiPhysicsTest:
                            value=0.0262,
                            regen_mode='constant')
         self.phys.add_model(propname='throat.thermal_conductance',
-                            model=pm.thermal_conductance.generic_thermal) 
+                            model=pm.thermal_conductance.generic_thermal)
         # settings
         self.tfd_settings = {
             "conductance": "throat.diffusive_conductance",
             "quantity": "pore.concentration",
             "cache_A": False,
-            "cache_b": False  
+            "cache_b": False
         }
         self.tfc_settings = {
             "conductance": "throat.thermal_conductance",
             "quantity": "pore.temperature",
             "pore_volume": "pore.heat_capacity",
             "cache_A": False,
-            "cache_b": False  
+            "cache_b": False
         }
         self.pardiso = op.solvers.PardisoSpsolve()
         self.rk45 = op.integrators.ScipyRK45(verbose=True)
-        
+
         # First algorithm, transient fourier conduction
         self.tfc = op.algorithms.TransientReactiveTransport(network=self.net,
                                                             phase=self.air)
@@ -97,30 +97,31 @@ class TransientMultiPhysicsTest:
         # add variable props to algs
         self.tfd.set_variable_props('pore.temperature')
         self.tfc.set_variable_props('pore.concentration')
-        
+
         # multiphysics algorithm
         algs = [self.tfc, self.tfd]
-        self.tmp = op.algorithms.TransientMultiPhysics(algs, network=self.net)
-        
+        self.tmp = op.contrib.TransientMultiPhysics(algs, network=self.net)
+
     def test_run_algs(self):
         t_initial = 0
         t_final = 200
         t_step = 20
         n_steps = int((t_final - t_initial)/t_step) + 1
         t = np.linspace(t_initial, t_final, n_steps)
-        y0 = np.hstack((self.T0, self.c0)) # ICs must include boundary condition
+        y0 = np.hstack((self.T0, self.c0))  # ICs include boundary condition
         tspan = [0, t_final]
         self.tmp.run(y0, tspan, saveat=t)
-    
+
     def test_concentration(self):
         C = self.tmp.soln[self.net.Np:2*self.net.Np, :]
         C_avg = C[self.net.pores("left", mode='nor')].mean(axis=0)
         actual = C_avg
-        desired = [50.00000000, 50.46652183, 51.00059257, 51.56467322, 
-                   52.13159943, 52.68356036, 53.21031621, 53.70729529, 
+        desired = [50.00000000, 50.46652183, 51.00059257, 51.56467322,
+                   52.13159943, 52.68356036, 53.21031621, 53.70729529,
                    54.17372048, 54.61117656, 55.02246821]
         nt.assert_allclose(actual, desired, rtol=1e-5)
-        
+
+
 if __name__ == '__main__':
 
     t = TransientMultiPhysicsTest()
