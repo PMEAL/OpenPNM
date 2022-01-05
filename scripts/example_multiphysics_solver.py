3db1a6047e4158885a1438a8e678d9843b54c0a3
Amin Sadeghi <amin.sadeghi@live.com>
2022-01-05 15:18:01 -0500

    Update multiphysics example script

--- scripts/example_multiphysics_solver.py
+++ scripts/example_multiphysics_solver.py
@@ -1,27 +1,10 @@
-import scipy as sp
+# %% Package imports
 import numpy as np
 import openpnm as op
 import pandas as pd
 import matplotlib.pyplot as plt
 import openpnm.models.geometry.diffusive_size_factors as gd
-
 np.random.seed(10)
-
-# %% Test scipy's solve_ivp
-# FOR EXAMPLE: solve dy / dt = y^2 + y
-
-
-def fun(t, y):
-
-    return y
-
-
-sol = sp.integrate.solve_ivp(fun, t_span=(0, 10), y0=np.array([5]))
-
-# plot
-t = sol.t
-y = np.reshape(sol.y, newshape=len(sol.t))
-plt.plot(t, y)
 
 # %% Set up for the solvers
 Nx = 100
@@ -31,30 +14,17 @@ net = op.network.Cubic(shape=shape, spacing=spacing)
 # geo = op.geometry.CirclesAndRectangles(network=net, pores=net.Ps, throats=net.Ts)
 # 2d square geometry
 geo = op.geometry.GenericGeometry(network=net, pores=net.Ps, throats=net.Ts)
-geo.add_model(propname='pore.diameter',
-              model=op.models.misc.constant,
-              value=spacing)
-geo.add_model(propname='throat.length',
-              model=op.models.misc.constant,
-              value=1e-15)
-geo.add_model(propname='throat.diameter',
-              model=op.models.misc.constant,
-              value=spacing)
+geo.add_model(propname='pore.diameter', model=op.models.misc.constant, value=spacing)
+geo.add_model(propname='throat.length', model=op.models.misc.constant, value=1e-15)
+geo.add_model(propname='throat.diameter', model=op.models.misc.constant, value=spacing)
 Ndim = op.topotools.dimensionality(network=net).sum()
-geo.add_model(propname='pore.cross_sectional_area',
-              model=op.models.misc.constant,
-              value=spacing**(Ndim-1))
-geo.add_model(propname='throat.cross_sectional_area',
-              model=op.models.misc.constant,
-              value=spacing**(Ndim-1))
-geo.add_model(propname='pore.volume',
-              model=op.models.misc.constant,
-              value=spacing**Ndim)
+Ap = At = spacing**(Ndim-1)
+Vp = spacing**Ndim
+geo["pore.cross_sectional_area"] = Ap
+geo["throat.cross_sectional_area"] = At
+geo["pore.volume"] = Vp
 mod = gd.squares_and_rectangles if Ndim == 2 else gd.cubes_and_cuboids
-geo.add_model(propname='throat.diffusive_size_factors',
-              model=mod,
-              pore_diameter="pore.diameter",
-              throat_diameter="throat.diameter")
+geo.add_model(propname='throat.diffusive_size_factors', model=mod)
 
 # Diff = op.metrics.EffectiveDiffusivity(network=net)
 # # Diff.settings._update({
@@ -66,50 +36,37 @@ geo.add_model(propname='throat.diffusive_size_factors',
 air = op.phase.Air(network=net)
 phys = op.physics.GenericPhysics(network=net, phase=air, geometry=geo)
 
-# make diffusivity a function of temperature - ALREADY IS!!
+# Make diffusivity a function of temperature
 air['pore.temperature'] = 300
+
 air.add_model(propname='pore.diffusivity',
               model=op.models.misc.linear,
               m=1.860793056e-06,
               b=-0.0005375624384,
               prop='pore.temperature')
-'''
-air.add_model(propname='pore.diffusivity',
-              model=op.models.misc.constant,
-              value=2.067547840000001e-04)
-'''
 phys.add_model(propname='throat.diffusive_conductance',
                model=op.models.physics.diffusive_conductance.generic_diffusive)
 
 air.remove_model(propname='pore.thermal_conductivity')
-air.add_model(propname='pore.thermal_conductivity',
-              model=op.models.misc.constant,
-              value=0.0262,
-              regen_mode='constant')
-
+air["thermal_conductivity"] = 0.0262
 phys.add_model(propname='throat.thermal_conductance',
                model=op.models.physics.thermal_conductance.generic_thermal)
 
 tfd_settings = {
     "conductance": "throat.diffusive_conductance",
     "quantity": "pore.concentration",
-    "cache_A": False,
-    "cache_b": False
 }
-
 tfc_settings = {
     "conductance": "throat.thermal_conductance",
     "quantity": "pore.temperature",
     "pore_volume": "pore.heat_capacity",
-    "cache_A": False,
-    "cache_b": False
 }
 
 pardiso = op.solvers.PardisoSpsolve()
 rk45 = op.integrators.ScipyRK45(verbose=True)
 
 # %% Define Algorithms
-# First algorithm, transient fourier conduction
+# First algorithm, transient Fourier conduction
 tfc = op.algorithms.TransientReactiveTransport(network=net, phase=air)
 geo['pore.heat_capacity'] = geo['pore.volume'] * 1.0035 * 1000 * 1.225
 tfc.settings._update(tfc_settings)
@@ -117,7 +74,7 @@ tfc.set_value_BC(net.pores("left"), 400)
 T0 = np.ones(tfc.Np) * 300
 T0[net.pores('left')] = 400
 
-# Second algorithm, transient fickian diffusion
+# Second algorithm, transient Fickian diffusion
 tfd = op.algorithms.TransientReactiveTransport(network=net, phase=air)
 # tfd.settings['variable_props'] = 'pore.temperature'
 tfd.settings._update(tfd_settings)
@@ -126,19 +83,19 @@ tfd.set_value_BC(net.pores("left"), 100)
 c0 = np.ones(tfd.Np)*50
 c0[net.pores('left')] = 100
 
-# time
+# Integrator parameters
 t_initial = 0
 t_final = 50
 t_step = 5
 n_steps = int((t_final - t_initial)/t_step) + 1
 t = np.linspace(t_initial, t_final, n_steps)
 
-# add variable props to algs
+# Add variable props to algs
 tfd.set_variable_props('pore.temperature')
 tfc.set_variable_props('pore.concentration')
 
 # %% For Loop Solution
-# solve multiphysics system assuming temperature change is small over t_step
+# Solve multiphysics system assuming temperature change is small over t_step
 not_BC_pores = net.pores("left", mode='nor')
 t_prev = 0
 C_1_avg = [c0[not_BC_pores].mean()]
@@ -150,27 +107,25 @@ for ti in t:
     t_prev = ti
     tout = ti
     # Solve for temperature first... add while loop
-    # temperature dependent thermal conductivity
+    # Temperature dependent thermal conductivity
     sol_1 = tfc.run(x0=T0, tspan=tspan, integrator=rk45, saveat=tout)
     air.regenerate_models()  # update diffusivuty
     phys.regenerate_models()  # update diffusive conductance
     sol_2 = tfd.run(x0=c0, tspan=tspan, integrator=rk45, saveat=tout)
-    # update initial coniditions
+    # Update initial coniditions
     T0 = sol_1[:, 1]
     c0 = sol_2[:, 1]
     C_1_avg.append(c0[not_BC_pores].mean())
 
-# note: dual coupling, time step needs to be small, hamed benchamrk solution
+# Note: dual coupling, time step needs to be small, hamed benchamrk solution
 T_1 = sol_1[:, -1]
 C_1 = sol_2[:, -1]
 
 # %% Build RHS manually
-
-
 def _build_rhs(algs):
+    """Time-derivative of the variables being solved (T and c): dydt = RHS"""
 
     def ode_func(t, y):
-
         # initialize rhs
         rhs = []
         for i, alg in enumerate(algs):
@@ -189,13 +144,11 @@ def _build_rhs(algs):
             V = geo[alg.settings['pore_volume']]
             rhs_alg = np.hstack(-A.dot(x) + b)/V
             rhs = np.hstack((rhs, rhs_alg))
-
         return rhs
 
     return ode_func
 
-
-# call solve_ivp and pass function that builds rhs as dydt
+# Call solve_ivp and pass function that builds rhs as dydt
 algs = [tfc, tfd]
 rhs = _build_rhs(algs)
 T0 = np.ones(tfc.Np) * 300
@@ -206,14 +159,6 @@ c0[net.pores('left')] = 100
 y0 = np.hstack((T0, c0))  # ICs must include boundary condition
 tspan = [0, t_final]
 rtol = 1e-5
-'''
-sol = sp.integrate.solve_ivp(fun=rhs,
-                             t_span=tspan,
-                             y0=y0,
-                             method="RK45",
-                             t_eval=t,
-                             rtol=rtol)
-'''
 tmp = op.contrib.TransientMultiPhysics(algorithms=[tfc, tfd], network=net)
 sol = tmp.run(y0, tspan, saveat=t)
 
@@ -227,9 +172,7 @@ C_2_avg = sol.T.transpose()[net.Np:, 0:][not_BC_pores].mean(axis=0)
 
 # %% Pure Fickian Diffusion and Pure Fourier Conduction
 air.remove_model(propname='pore.diffusivity')
-air.add_model(propname='pore.diffusivity',
-              model=op.models.misc.constant,
-              value=2.067547840000001e-04)
+air["pore.diffusivity"] = 2.067547840000001e-04
 phys.regenerate_models()
 tspan = [t_initial, t_final]
 sol_tfd = tfd.run(x0=c0, tspan=tspan, integrator=rk45, saveat=t)
@@ -247,7 +190,7 @@ C_3_avg = np.asarray(df['Coupled'].iloc[0:, 1])
 C_pure_comsol = np.asarray(df['Diffusion'].iloc[0:, 1])
 T_pure_comsol = np.asarray(df['Conduction'].iloc[0:, 1])
 
-# plot average concentrations comparing for loop, build rhs, and comsol
+# Plot average concentrations comparing for loop, build rhs, and comsol
 fig1, ax1 = plt.subplots()
 # ax1.plot(t, C_1_avg, time, C_3_avg)
 # ax1.legend(('For Loop', 'COMSOL'))
