9fdd79a7f531ac883838274923bed6b68ce51b50
jgostick <jgostick@uwaterloo.ca>
2022-04-28 13:50:41 -0400

    adding rxn to script

--- example.py
+++ example.py
@@ -1,7 +1,8 @@
 import numpy as np
 import openpnm as op
+import matplotlib.pyplot as plt
 
-pn = op.network.Cubic(shape=[4, 4, 1])
+pn = op.network.Cubic(shape=[25, 25, 1])
 Ps = pn.pores('left')
 Ts = pn.find_neighbor_throats(Ps, asmask=True)
 pn['throat.left'] = Ts
@@ -10,20 +11,20 @@ pn.add_model_collection(op.models.collections.geometry.circles_and_rectangles,
 pn.regenerate_models()
 
 air = op.phase.Air(network=pn)
+air.add_model_collection(op.models.collections.physics.standard, domain='all')
 air.regenerate_models()
-air.add_model(propname='throat.diffusive_conductance',
-              model=op.models.physics.diffusive_conductance.generic_diffusive)
 
-fd = op.algorithms.FickianDiffusion(network=pn, phase=air)
-fd.set_value_BC(pores=pn.pores('left'), values=1)
-fd.set_value_BC(pores=pn.pores('right'), values=0)
-fd.run()
+air['pore.reaction_sites'] = False
+air['pore.reaction_sites'][[310, 212, 113]] = True
 
+air.add_model(propname='pore.reaction',
+              model=op.models.physics.generic_source_term.power_law,
+              X='pore.concentration',
+              domain='reaction_sites',
+              A1=-1, A2=2, A3=0, regen_mode='deferred')
 
-pn['pore.temperature.air'] = 55
-pn['pore.viscosity.air'] = 33
-pn['pore.temperature.water'] = 55
-pn['pore.viscosity.water'] = 33
-
-a = op.core.Domain()
-a.update(pn['*air'])
+rxn = op.algorithms.FickianDiffusion(network=pn, phase=air)
+rxn.set_value_BC(pores=pn.pores('left'), values=1)
+rxn.set_source(pores=air.pores('reaction_sites'), propname='pore.reaction')
+rxn.run()
+plt.pcolormesh(rxn.x.reshape([25, 25]))
