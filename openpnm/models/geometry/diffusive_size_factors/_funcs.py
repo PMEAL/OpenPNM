f3effc4597c87fa43e8f81915f5515dc237699b7
jgostick <jgostick@gmail.com>
2022-05-01 00:03:07 -0400

    diffusive size factor tests work

--- openpnm/models/geometry/diffusive_size_factors/_funcs.py
+++ openpnm/models/geometry/diffusive_size_factors/_funcs.py
@@ -58,8 +58,7 @@ def spheres_and_cylinders(
     on each end.
 
     """
-    D1, Dt, D2 = target.get_conduit_data(poreprop=pore_diameter,
-                                         throatprop=throat_diameter).T
+    D1, Dt, D2 = target.get_conduit_data(pore_diameter.split('.', 1)[1]).T
     L1, Lt, L2 = _conduit_lengths.spheres_and_cylinders(
         target,
         pore_diameter=pore_diameter,
@@ -182,8 +181,7 @@ def trapezoids_and_rectangles(
     symmetry.
 
     """
-    D1, Dt, D2 = target.get_conduit_data(poreprop=pore_diameter,
-                                         throatprop=throat_diameter).T
+    D1, Dt, D2 = target.get_conduit_data(pore_diameter.split('.', 1)[-1]).T
     L1, Lt, L2 = _conduit_lengths.trapezoids_and_rectangles(
         target,
         pore_diameter=pore_diameter,
@@ -229,8 +227,7 @@ def pyramids_and_cuboids(
     symmetry.
 
     """
-    D1, Dt, D2 = target.get_conduit_data(poreprop=pore_diameter,
-                                         throatprop=throat_diameter).T
+    D1, Dt, D2 = target.get_conduit_data(pore_diameter.split('.')[-1]).T
     L1, Lt, L2 = _conduit_lengths.pyramids_and_cuboids(
         target,
         pore_diameter=pore_diameter,
@@ -277,8 +274,7 @@ def cubes_and_cuboids(
     symmetry.
 
     """
-    D1, Dt, D2 = target.network.get_conduit_data(poreprop=pore_diameter,
-                                                 throatprop=throat_diameter).T
+    D1, Dt, D2 = target.get_conduit_data(pore_diameter.split('.')[-1]).T
     L1, Lt, L2 = _conduit_lengths.cubes_and_cuboids(
         target,
         pore_diameter=pore_diameter,
@@ -325,8 +321,7 @@ def squares_and_rectangles(
     symmetry.
 
     """
-    D1, Dt, D2 = target.network.get_conduit_data(poreprop=pore_diameter,
-                                                 throatprop=throat_diameter).T
+    D1, Dt, D2 = target.get_conduit_data(pore_diameter.split('.')[-1]).T
     L1, Lt, L2 = _conduit_lengths.squares_and_rectangles(
         target,
         pore_diameter=pore_diameter,
@@ -457,8 +452,7 @@ def ncylinders_in_series(
     %(models.geometry.diffusive_size_factor.notes)s
 
     """
-    D1, Dt, D2 = target.get_conduit_data(poreprop=pore_diameter,
-                                         throatprop=throat_diameter).T
+    D1, Dt, D2 = target.get_conduit_data(pore_diameter.split('.')[-1]).T
     # Ensure throats are never bigger than connected pores
     Dt = _np.minimum(Dt, 0.99 * _np.minimum(D1, D2))
     L1, Lt, L2 = _conduit_lengths.spheres_and_cylinders(
