b92bb095e2798c34757179acbe5728b019ba4e4b
jgostick <jgostick@gmail.com>
2022-05-02 22:21:46 -0400

    a bit miraculously, all geometry models pass with mininal alteration

--- tests/unit/models/geometry/ThroatCentroidTest.py
+++ tests/unit/models/geometry/ThroatCentroidTest.py
@@ -7,20 +7,17 @@ class ThroatCentroidTest:
 
     def setup_class(self):
         self.net = op.network.Cubic(shape=[5, 1, 1], spacing=1)
-        self.geo = op.geometry.GenericGeometry(network=self.net,
-                                               pores=self.net.pores(),
-                                               throats=self.net.throats())
 
     def test_voronoi(self):
         pass
 
     def test_pore_coords(self):
-        self.geo.add_model(propname='throat.centroid',
+        self.net.add_model(propname='throat.centroid',
                            model=gm.throat_centroid.pore_coords)
         a = np.arange(1, 5, 1)
-        assert np.allclose(a, self.geo['throat.centroid'][:, 0])
-        assert np.all(self.geo['throat.centroid'][:, 1] == 0.5)
-        assert np.all(self.geo['throat.centroid'][:, 2] == 0.5)
+        assert np.allclose(a, self.net['throat.centroid'][:, 0])
+        assert np.all(self.net['throat.centroid'][:, 1] == 0.5)
+        assert np.all(self.net['throat.centroid'][:, 2] == 0.5)
 
 
 if __name__ == '__main__':
--- tests/unit/models/geometry/ThroatCrossSectionalAreaTest.py
+++ tests/unit/models/geometry/ThroatCrossSectionalAreaTest.py
@@ -7,39 +7,32 @@ from numpy.testing import assert_approx_equal
 class ThroatCrossSectionalAreaTest:
     def setup_class(self):
         self.net = op.network.Cubic(shape=[5, 5, 5], spacing=1.0)
-        self.geo = op.geometry.GenericGeometry(network=self.net,
-                                               pores=self.net.Ps,
-                                               throats=self.net.Ts)
-        self.air = op.phase.Air(network=self.net)
-        self.phys = op.physics.GenericPhysics(network=self.net,
-                                              phase=self.air,
-                                              geometry=self.geo)
-        self.geo['pore.diameter'] = 0.2
-        self.geo['throat.length'] = 1
-        self.geo['throat.diameter'] = 0.1
+        self.net['pore.diameter'] = 0.2
+        self.net['throat.length'] = 1
+        self.net['throat.diameter'] = 0.1
 
     def test_sphere(self):
-        self.geo.add_model(propname='throat.cross_sectional_area',
+        self.net.add_model(propname='throat.cross_sectional_area',
                            model=mods.cylinder,
                            regen_mode='normal')
         a = np.array([0.007853981])
-        b = np.unique(self.geo['throat.cross_sectional_area'])
+        b = np.unique(self.net['throat.cross_sectional_area'])
         assert_approx_equal(a, b)
 
     def test_cube(self):
-        self.geo.add_model(propname='throat.cross_sectional_area',
-                           model=mods.cuboid,
-                           regen_mode='normal')
+        self.net.add_model(propname='throat.cross_sectional_area',
+                              model=mods.cuboid,
+                              regen_mode='normal')
         a = np.array([0.01])
-        b = np.unique(self.geo['throat.cross_sectional_area'])
+        b = np.unique(self.net['throat.cross_sectional_area'])
         assert_approx_equal(a, b)
 
     def test_rectangle(self):
-        self.geo.add_model(propname='throat.cross_sectional_area',
+        self.net.add_model(propname='throat.cross_sectional_area',
                            model=mods.rectangle,
                            regen_mode='normal')
         a = np.array([0.1])
-        b = np.unique(self.geo['throat.cross_sectional_area'])
+        b = np.unique(self.net['throat.cross_sectional_area'])
         assert_approx_equal(a, b)
 
 
