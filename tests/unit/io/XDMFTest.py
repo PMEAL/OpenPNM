58bdeefaf475c9a2addf9d3b24d925690d0abb69
jgostick <jgostick@gmail.com>
2022-05-06 22:47:37 -0400

    xdmf works but not for transient!

--- tests/unit/io/XDMFTest.py
+++ tests/unit/io/XDMFTest.py
@@ -10,18 +10,8 @@ class XDMFTest:
         ws = op.Workspace()
         ws.settings['local_data'] = True
         self.net = op.network.Cubic(shape=[2, 2, 2])
-        Ps = [0, 1, 2, 3]
-        Ts = self.net.find_neighbor_throats(pores=Ps)
-        self.geo_1 = op.geometry.GenericGeometry(network=self.net,
-                                                 pores=Ps, throats=Ts)
-        self.geo_1['pore.boo'] = 1
-        self.geo_1['throat.boo'] = 1
-        Ps = [4, 5, 6, 7]
-        Ts = self.net.find_neighbor_throats(pores=Ps, mode='xnor')
-        self.geo_2 = op.geometry.GenericGeometry(network=self.net,
-                                                 pores=Ps, throats=Ts)
-        self.geo_2['pore.boo'] = 1
-        self.geo_2['throat.boo'] = 1
+        self.net['pore.boo'] = 1
+        self.net['throat.boo'] = 1
 
         self.phase_1 = op.phase.GenericPhase(network=self.net)
         self.phase_1['pore.bar'] = 2
@@ -29,30 +19,6 @@ class XDMFTest:
         self.phase_2 = op.phase.GenericPhase(network=self.net)
         self.phase_2['pore.bar'] = 2
         self.phase_2['throat.bar'] = 2
-
-        self.phys_1 = op.physics.GenericPhysics(network=self.net,
-                                                phase=self.phase_1,
-                                                geometry=self.geo_1)
-        self.phys_1['pore.baz'] = 11
-        self.phys_1['throat.baz'] = 11
-
-        self.phys_2 = op.physics.GenericPhysics(network=self.net,
-                                                phase=self.phase_1,
-                                                geometry=self.geo_2)
-        self.phys_2['pore.baz'] = 12
-        self.phys_2['throat.baz'] = 12
-
-        self.phys_3 = op.physics.GenericPhysics(network=self.net,
-                                                phase=self.phase_2,
-                                                geometry=self.geo_1)
-        self.phys_3['pore.baz'] = 21
-        self.phys_3['throat.baz'] = 21
-
-        self.phys_4 = op.physics.GenericPhysics(network=self.net,
-                                                phase=self.phase_2,
-                                                geometry=self.geo_2)
-        self.phys_4['pore.baz'] = 22
-        self.phys_4['throat.baz'] = 22
 
         self.net['pore.object'] = np.ones(self.net.Np, dtype=object)
 
