36fecd468d36ebffbf9cf712f6a80b6e0327f6ad
jgostick <jgostick@gmail.com>
2022-05-11 19:22:28 -0400

    adding wrappers to network module

---
+++ openpnm/network/_imported.py
@@ -0,0 +1,39 @@
+
+
+__all__ = [
+    'from_porespy',
+    'from_marock',
+    'from_networkx',
+    'from_pergeos',
+    'from_jsongraph',
+]
+
+
+def from_porespy(file):
+    from openpnm.io import network_from_porespy
+    net = network_from_porespy(file)
+    return net
+
+
+def from_marock(*args, **kwargs):
+    from openpnm.io import network_from_marock
+    net = network_from_marock(*args, **kwargs)
+    return net
+
+
+def from_networkx(*args, **kwargs):
+    from openpnm.io import network_from_networkx
+    net = network_from_networkx(*args, **kwargs)
+    return net
+
+
+def from_pergeos(*args, **kwargs):
+    from openpnm.io import network_from_pergeos
+    net = network_from_pergeos(*args, **kwargs)
+    return net
+
+
+def from_jsongraph(*args, **kwargs):
+    from openpnm.io import network_from_jsongraph
+    net = network_from_jsongraph(*args, **kwargs)
+    return net
