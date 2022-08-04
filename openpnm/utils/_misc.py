53d5b48d81dc1160d0f3ca9518a70989d74c0286
jgostick <jgostick@gmail.com>
2022-08-04 13:16:52 -0400

    removed tic, toc

--- openpnm/utils/_misc.py
+++ openpnm/utils/_misc.py
@@ -19,8 +19,6 @@ __all__ = [
     'PrintableDict',
     'HealthDict',
     'NestedDict',
-    'tic',
-    'toc',
     'unique_list',
     'flat_list',
     'sanitize_dict',
@@ -231,119 +229,8 @@ class HealthDict(PrintableDict):
 
     health = property(fget=_get_health)
 
-
-"""
-BSD 3-Clause License
-
-- Copyright (c) 2008-Present, IPython Development Team
-- Copyright (c) 2001-2007, Fernando Perez <fernando.perez@colorado.edu>
-- Copyright (c) 2001, Janko Hauser <jhauser@zscout.de>
-- Copyright (c) 2001, Nathaniel Gray <n8gray@caltech.edu>
-
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-* Redistributions of source code must retain the above copyright notice, this
-  list of conditions and the following disclaimer.
-
-* Redistributions in binary form must reproduce the above copyright notice,
-  this list of conditions and the following disclaimer in the documentation
-  and/or other materials provided with the distribution.
-
-* Neither the name of the copyright holder nor the names of its
-  contributors may be used to endorse or promote products derived from
-  this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-"""
-
-
-def _format_time(timespan, precision=3):
-    """Formats the timespan in a human readable form"""
-
-    if timespan >= 60.0:
-        # we have more than a minute, format that in a human readable form
-        # Idea from http://snipplr.com/view/5713/
-        parts = [("d", 60*60*24), ("h", 60*60), ("min", 60), ("s", 1)]
-        time = []
-        leftover = timespan
-        for suffix, length in parts:
-            value = int(leftover / length)
-            if value > 0:
-                leftover = leftover % length
-                time.append(u'%s%s' % (str(value), suffix))
-            if leftover < 1:
-                break
-        return " ".join(time)
-
-    # Unfortunately the unicode 'micro' symbol can cause problems in
-    # certain terminals.
-    # See bug: https://bugs.launchpad.net/ipython/+bug/348466
-    # Try to prevent crashes by being more secure than it needs to
-    # E.g. eclipse is able to print a µ, but has no sys.stdout.encoding set.
-    units = [u"s", u"ms", u'us', "ns"]  # the save value
-    if hasattr(sys.stdout, 'encoding') and sys.stdout.encoding:
-        try:
-            u'\xb5'.encode(sys.stdout.encoding)
-            units = [u"s", u"ms", u'\xb5s', "ns"]
-        except:
-            pass
-    scaling = [1, 1e3, 1e6, 1e9]
-
-    if timespan > 0.0:
-        order = min(-int(math.floor(math.log10(timespan)) // 3), 3)
-    else:
-        order = 3
-    return u"%.*g %s" % (precision, timespan * scaling[order], units[order])
-
-
-def tic():
-    r"""
-    Homemade version of matlab tic and toc function, tic starts or resets
-    the clock, toc reports the time since the last call of tic.
-
-    See Also
-    --------
-    toc
-
-    """
-    global _startTime_for_tictoc
-    _startTime_for_tictoc = time.time()
-
-
-def toc(quiet=False):
-    r"""
-    Homemade version of matlab tic and toc function, tic starts or resets
-    the clock, toc reports the time since the last call of tic.
-
-    Parameters
-    ----------
-    quiet : bool, default is False
-        If False then a message is output to the console. If
-        True the message is not displayed and the elapsed time is returned.
-
-    See Also
-    --------
-    tic
-
-    """
-    if "_startTime_for_tictoc" not in globals():
-        raise Exception("Start time not set, call tic first")
-    t = time.time() - _startTime_for_tictoc
-    if quiet is False:
-        print(f"Elapsed time: {_format_time(t)}")
-    return t
+    def __bool__(self):
+        return self.health
 
 
 def unique_list(input_list):
