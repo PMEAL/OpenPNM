7c972a1f4223aae2e6c8acfe66687a8626348e98
Amin Sadeghi <amin.sadeghi@live.com>
2022-08-22 11:23:54 -0400

    Add warning if a model can't be regenerated

--- openpnm/utils/_misc.py
+++ openpnm/utils/_misc.py
@@ -1,3 +1,5 @@
+from asyncio.log import logger
+import os
 import sys
 import math
 import time
@@ -668,3 +670,13 @@ def nbr_to_str(nbr, t_precision=12):
             * (round(nbr, t_precision) != int(nbr)))
     nbr_str = (str(int(round(nbr, t_precision)*10**n)) + ('e-'+str(n))*(n != 0))
     return nbr_str
+
+
+def prettify_logger_message(msg):
+    r"""Prettifies logger messages by breaking them up into multi lines"""
+    from textwrap import wrap
+    logger_prefix_length = 13
+    column_width = os.get_terminal_size().columns - logger_prefix_length
+    indent = "\n" + " " * logger_prefix_length
+    temp = wrap(msg, width=column_width)
+    return indent.join(temp)
