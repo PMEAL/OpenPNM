702a8a4d533d0d858f9b64f4bc34c1954435759a
jgostick <jgostick@gmail.com>
2022-10-06 20:50:10 -0400

    adding tests to voronoi_delaunay_dual class

--- tests/unit/network/VoronoiTest.py
+++ tests/unit/network/VoronoiTest.py
@@ -25,6 +25,22 @@ class VoronoiTest:
     def test_voronoi_delaunay_dual(self):
         dual = op.network.DelaunayVoronoiDual(points=100, shape=[1, 1, 1])
 
+    def test_find_throat_facets(self):
+        np.random.seed(0)
+        dual = op.network.DelaunayVoronoiDual(points=10, shape=[1, 1, 1])
+        f = dual.find_throat_facets(throats=[1, 5])
+        assert np.all(f[0] == [48, 49, 50, 55, 57])
+        assert np.all(f[1] == [48, 33, 30, 49])
+
+    def test_find_pore_hulls(self):
+        np.random.seed(0)
+        dual = op.network.DelaunayVoronoiDual(points=10, shape=[1, 1, 1])
+        f = dual.find_pore_hulls(pores=[0, 5])
+        assert np.all(f[0] == [12, 14, 15, 19, 20, 21, 30,
+                               33, 35, 48, 49, 50, 55, 57])
+        assert np.all(f[1] == [36, 37, 38, 39, 40, 41, 42,
+                               43, 51, 58, 60, 61])
+
 
 if __name__ == '__main__':
 
