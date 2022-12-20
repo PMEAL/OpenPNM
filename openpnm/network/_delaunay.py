c3dc2c0e5462e094f6471a6674e6982ce3ac3597
jgostick <jgostick@gmail.com>
2022-12-20 13:57:24 -0500

    Updating Delauny to use skgraphs delaunay

--- openpnm/network/_delaunay.py
+++ openpnm/network/_delaunay.py
@@ -1,7 +1,7 @@
 import numpy as np
 from openpnm.network import Network
 from openpnm.utils import Docorator
-from openpnm._skgraph.generators import voronoi_delaunay_dual, tools
+from openpnm._skgraph.generators import voronoi_delaunay_dual, delaunay, tools
 from openpnm._skgraph.tools import isoutside
 from openpnm._skgraph.operations import trim_nodes
 
@@ -52,16 +52,17 @@ class Delaunay(Network):
     def __init__(self, shape, points, reflect=True, trim=True, **kwargs):
         super().__init__(**kwargs)
         points = tools.parse_points(shape=shape, points=points, reflect=reflect)
-        net, vor, tri = voronoi_delaunay_dual(points=points,
-                                              shape=shape,
-                                              trim=trim,
-                                              node_prefix='pore',
-                                              edge_prefix='throat')
-        net = trim_nodes(network=net, inds=np.where(net['pore.voronoi'])[0])
+        net, tri = delaunay(points=points,
+                            shape=shape,
+                            node_prefix='pore',
+                            edge_prefix='throat')
         self.update(net)
 
 
 if __name__ == "__main__":
     import openpnm as op
     dn = Delaunay(shape=[2, 0], points=1000, reflect=True, trim=True)
+    op.visualization.plot_connections(dn)
+    pts = np.random.rand(500, 2)
+    dn = Delaunay(points=pts, shape=[1, 1, 0])
     op.visualization.plot_connections(dn)
