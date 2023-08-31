1dcf965e5f749c5931fc130d19548addc56e4c4c
jgostick <jgostick@users.noreply.github.com>
2023-08-31 08:48:32 +0000

    deploy: 8009641e3994303e579f6e52b6a1250c11bc8a34

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.1.2.dev2',
+    VERSION: '3.2.0.dev4',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
