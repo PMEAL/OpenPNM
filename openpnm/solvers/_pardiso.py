b4091cf09a17ba507db132dfb7f4f8aab87dbb97
Amin Sadeghi <amin.sadeghi@live.com>
2023-09-30 20:07:33 -0400

    Fix pep8

--- openpnm/solvers/_pardiso.py
+++ openpnm/solvers/_pardiso.py
@@ -7,11 +7,11 @@ __all__ = ['PardisoSpsolve']
 
 class PardisoSpsolve(DirectSolver):
     """Solves a linear system using ``pypardiso.spsolve``."""
-   
+
     def solve(self, A, b, **kwargs):
         """Solves the given linear system of equations Ax=b."""
         from pypardiso import spsolve
-        
+
         if not isinstance(A, (csr_matrix, csc_matrix)):
             A = A.tocsr()
         return (spsolve(A, b), 0)
