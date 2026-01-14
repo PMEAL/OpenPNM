ef7f3fa0c351d9c8a96ec8bafa20aab1d2548776
jgostick <jgostick@gmail.com>
2026-01-14 09:46:55 -0500

    Updating example script to improve graph

--- example.py
+++ example.py
@@ -73,4 +73,19 @@ rxn2.run()
 
 # Plot concentration profile
 fig, ax = plt.subplots()
-ax.pcolormesh(rxn.x.reshape([25, 25]))
+ax = op.visualization.plot_coordinates(
+    network=pn,
+    ax=ax,
+    color_by=rxn2['pore.concentration'],
+    size_by=pn['pore.diameter'],
+    s=75,
+    ec='k',
+    linewidth=0.5,
+)
+ax = op.visualization.plot_connections(
+    network=pn,
+    ax=ax,
+    zorder=0,
+    c='grey',
+    linewidth=0.5,
+)
